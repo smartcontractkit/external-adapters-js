d4fee28bc8124408edfe15d39b2c4bce23c33a14
boxhock <contact@jonashals.me>
2020-05-12 11:09:40 +0200

    Add WS support for RPC_URL

--- eth/readReferenceContract.js
+++ eth/readReferenceContract.js
@@ -1,13 +1,28 @@
 const ethers = require('ethers')
+const web3 = require('web3')
 const rpcUrl = process.env.RPC_URL
 
+const getProvider = (callback) => {
+  if (rpcUrl.startsWith('ws')) {
+    const web3prov = new web3.providers.WebsocketProvider(rpcUrl)
+    callback(new ethers.providers.Web3Provider(web3prov), () => {
+      web3prov.disconnect(1000, '')
+    })
+  } else {
+    callback(new ethers.providers.JsonRpcProvider(rpcUrl), () => {})
+  }
+}
+
 const getContractPrice = async (contractAddress) => {
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-  const ABI = [
-    'function latestAnswer() external view returns (int256)'
-  ]
-  const contract = new ethers.Contract(contractAddress, ABI, provider)
-  return await contract.latestAnswer()
+  return new Promise((resolve, reject) => {
+    getProvider((provider, done) => {
+      const ABI = [
+        'function latestAnswer() external view returns (int256)'
+      ]
+      const contract = new ethers.Contract(contractAddress, ABI, provider)
+      contract.latestAnswer().then(resolve).catch(reject).finally(done)
+    })
+  })
 }
 
 exports.getContractPrice = getContractPrice
