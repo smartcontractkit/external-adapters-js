f6407ea8a5f33dc218c9ba8631414286be70bd84
John Barker <dev@j16r.net>
2020-05-14 17:48:26 -0600

    Implement consul connection by hand, set host

--- bootstrap/lib/server.js
+++ bootstrap/lib/server.js
@@ -1,16 +1,29 @@
-const pjson = require(path.join(process.cwd(), 'package.json'))
-const withConsul = require('with-consul')
+const consul = require('consul')({host: 'consul.mgnt.devnet.tools'})
 const express = require('express')
 const bodyParser = require('body-parser')
-const port = process.env.EA_PORT || 8080
 
-const app = withConsul(express(), {
-  name: `${pjson.name}`,
-  tags: ['external-adapter', 'fargate', `version-${pjson.version}`]
-})
+const pjson = require(path.join(process.cwd(), 'package.json'))
+
+const port = process.env.EA_PORT || 8080
+const app = express()
 
 function init (createRequest) {
+  const consulOptions = {
+    name: `${pjson.name}`,
+    tags: ['external-adapter', 'fargate', `version-${pjson.version}`],
+    port: port,
+  }
+
   return () => {
+    consul.agent.service.register(consulOptions, err => {
+      if (err) {
+        console.error('Could not register service', err);
+        throw err
+      } else {
+        console.log(`Registered External Adapter '${consulOptions.name}' ${pjson.version} with Consul`);
+      }
+    })
+
     app.use(bodyParser.json())
 
     app.post('/', (req, res) => {
@@ -24,6 +37,11 @@ function init (createRequest) {
     app.listen(port, () => console.log(`Listening on port ${port}!`))
 
     process.on('SIGINT', () => {
+      consul.agent.service.deregister(consulOptions, err => {
+        if (err) {
+          console.error('Failed to deregister with consul on shutdown', err);
+        }
+      })
       process.exit()
     })
   }
