351d9deeee348cfe6e7985d8652de75c212fb901
Thomas Hodges <me@thodges.com>
2020-05-14 14:26:24 -0500

    Add example, helper server, and Action to test

--- helpers/server.js
+++ helpers/server.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 18081
 
 app.get('/price', (req, res) => {
-  res.status(200).json({
+  const response = {
+    Response: 'Success',
     price: 77777.77
-  })
+  }
+  if (!(req.query.base === 'ETH' && req.query.quote === 'USD')) {
+    response.Response = 'Error'
+  }
+  res.status(200).json(response)
 })
 
 app.listen(port, () => {})
