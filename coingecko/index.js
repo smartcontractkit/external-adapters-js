514b956d8e3672b294aa75b7a405d0f8814dbe5b
Leo Vigna <leo@vulcan.link>
2020-08-05 15:51:55 +0200

    Fix linting

--- coingecko/index.js
+++ coingecko/index.js
@@ -3,11 +3,10 @@ const { createRequest } = require('./adapter')
 
 const { openwhiskWrap } = require('./openwhisk')
 
-
 module.exports = {
-    server: bootstrap.server.init(createRequest),
-    gcpservice: bootstrap.serverless.initGcpService(createRequest),
-    handler: bootstrap.serverless.initHandler(createRequest),
-    handlerv2: bootstrap.serverless.initHandlerV2(createRequest),
-    openwhisk: openwhiskWrap(createRequest)
+  server: bootstrap.server.init(createRequest),
+  gcpservice: bootstrap.serverless.initGcpService(createRequest),
+  handler: bootstrap.serverless.initHandler(createRequest),
+  handlerv2: bootstrap.serverless.initHandlerV2(createRequest),
+  openwhisk: openwhiskWrap(createRequest)
 }
--- coingecko/openwhisk.js
+++ coingecko/openwhisk.js
@@ -1,17 +1,17 @@
 const openwhiskCallback = (resolve) => (statusCode, data) => {
-    resolve({
-        statusCode,
-        body: JSON.stringify(data),
-        isBase64Encoded: false
-    })
+  resolve({
+    statusCode,
+    body: JSON.stringify(data),
+    isBase64Encoded: false
+  })
 }
 const openwhiskWrap = (createRequest) => async (params) => {
-    return new Promise((resolve) => {
-        createRequest(params, openwhiskCallback(resolve))
-    })
+  return new Promise((resolve) => {
+    createRequest(params, openwhiskCallback(resolve))
+  })
 }
 
 module.exports = {
-    openwhiskCallback,
-    openwhiskWrap
+  openwhiskCallback,
+  openwhiskWrap
 }
