6c8dd453c0d5f86edc0436ef96284dbf58768d48
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-08-27 11:51:31 +0200

    Test helper assert fix

--- external-adapter/test/helpers/index.js
+++ external-adapter/test/helpers/index.js
@@ -4,13 +4,13 @@ exports.assertError = (statusCode, data, expectedJobId) => {
   assert.equal(statusCode.actual, statusCode.expected)
   assert.equal(data.jobRunID, expectedJobId)
   assert.equal(data.status, 'errored')
-  assert.isNotEmpty(data.error)
+  assert.exists(data.error)
 }
 
 exports.assertSuccess = (statusCode, data, expectedJobId) => {
   assert.equal(statusCode.actual, statusCode.expected)
   assert.equal(data.jobRunID, expectedJobId)
-  assert.isEmpty(data.error)
+  assert.notExists(data.error)
   assert.isNotEmpty(data.data)
   assert.equal(data.result, data.data.result)
 }
