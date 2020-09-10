553f6e75a6c89fdc5f56f3d578608331b624d800
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-09-10 17:01:50 +0200

    Comment NPM publish action (does not work)

--- 2-step/adapter.js
+++ 2-step/adapter.js
@@ -39,30 +39,35 @@ const createRequest = (input, callback) => {
   }
 
   logger.info('Getting value from contract: ' + contract)
-  getContractPrice(contract).then(price => {
-    price = price / multiply
-    logger.info('Value: ' + price)
-    if (price <= 0) {
-      return callback(500, Requester.errored(jobRunID, 'on-chain value equal or less than 0'))
-    }
-
-    adapterCreateRequest(input, (statusCode, response) => {
-      if (response.status === 'errored') {
-        return callback(statusCode, response)
+  getContractPrice(contract)
+    .then((price) => {
+      price = price / multiply
+      logger.info('Value: ' + price)
+      if (price <= 0) {
+        return callback(
+          500,
+          Requester.errored(jobRunID, 'on-chain value equal or less than 0')
+        )
       }
 
-      const result = transform(response.result, price, operator, dividend)
-      logger.info('New result: ' + result)
+      adapterCreateRequest(input, (statusCode, response) => {
+        if (response.status === 'errored') {
+          return callback(statusCode, response)
+        }
 
-      response.data.result = result
-      response.result = result
-      return callback(statusCode, response)
+        const result = transform(response.result, price, operator, dividend)
+        logger.info('New result: ' + result)
+
+        response.data.result = result
+        response.result = result
+        return callback(statusCode, response)
+      })
     })
-  }).catch((error) => {
-    logger.error('Error reading contract')
-    logger.error(error.toString())
-    return callback(500, Requester.errored(jobRunID, error.toString()))
-  })
+    .catch((error) => {
+      logger.error('Error reading contract')
+      logger.error(error.toString())
+      return callback(500, Requester.errored(jobRunID, error.toString()))
+    })
 }
 
 exports.createRequest = createRequest
