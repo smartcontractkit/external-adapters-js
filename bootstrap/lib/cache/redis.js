f5d3fa8089c7a766c6c49a95d99c8f42bbd87298
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-10-01 19:44:11 +0200

    Work on middleware, Redis retry strategy and error handling

--- bootstrap/lib/cache/redis.js
+++ bootstrap/lib/cache/redis.js
@@ -1,3 +1,4 @@
+const { logger } = require('@chainlink/external-adapter')
 const { promisify } = require('util')
 const redis = require('redis')
 
@@ -20,11 +21,34 @@ const envOptions = () => ({
   maxAge: Number(env.CACHE_MAX_AGE) || DEFAULT_CACHE_MAX_AGE,
 })
 
-// TODO: reconnections and error handling
+const retryStrategy = (options) => {
+  logger.warn('Redis retry strategy activated.', options)
+  if (options.error && options.error.code === 'ECONNREFUSED') {
+    // End reconnecting on a specific error and flush all commands with
+    // a individual error
+    return new Error('The server refused the connection')
+  }
+  if (options.total_retry_time > 1000 * 60 * 60) {
+    // End reconnecting after a specific timeout and flush all commands
+    // with a individual error
+    return new Error('Retry time exhausted')
+  }
+  if (options.attempt > 10) {
+    // End reconnecting with built in error
+    return undefined
+  }
+  // reconnect after
+  return Math.min(options.attempt * 100, 3000)
+}
+
 class RedisCache {
   constructor(options) {
     this.options = options
-    const client = redis.createClient(options)
+    const client = redis.createClient({ ...options, retry_strategy: retryStrategy })
+    client.on('error', function (err) {
+      logger.error('Error connecting to redis', err)
+    })
+
     this._auth = promisify(client.auth).bind(client)
     this._get = promisify(client.get).bind(client)
     this._set = promisify(client.set).bind(client)
