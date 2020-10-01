4f8f8ced39ab1dd324f0ec983cba3afac4a113f6
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-10-01 19:44:11 +0200

    Work on middleware, Redis retry strategy and error handling

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -1,42 +1,56 @@
-const { logger } = require('@chainlink/external-adapter')
+const { Requester, logger } = require('@chainlink/external-adapter')
 const server = require('./lib/server')
 const gcp = require('./lib/gcp')
 const aws = require('./lib/aws')
 const { withCache, envOptions } = require('./lib/cache')
+const { toAsync } = require('./lib/util')
 
-const withStatusCode = (execute) => (data, callback) => {
-  // Make sure data has the same statusCode as the one sent in callback
-  const _callback = (statusCode, data) =>
-    data && typeof data === 'object' && data.statusCode
-      ? callback(statusCode, { ...data, statusCode })
-      : callback(statusCode, data)
-  return execute(data, _callback)
+// Make sure data has the same statusCode as the one we got as a result
+const withStatusCode = (execute) => async (data_) => {
+  const { statusCode, data } = await execute(data_)
+  if (data && typeof data === 'object' && data.statusCode) {
+    return {
+      statusCode,
+      data: {
+        ...data,
+        statusCode,
+      },
+    }
+  }
+  return { statusCode, data }
+}
+
+// Transform sync execute function to async
+const withAsync = (execute) => async (data) => toAsync(execute, data)
+
+let executeWithMiddleware
+const middleware = [withAsync, withCache, withStatusCode]
+
+// Init all middleware, and return a wrapped execute fn
+const withMiddleware = async (execute) => {
+  // Init middleware only once
+  if (executeWithMiddleware) return executeWithMiddleware
+  // Init and wrap middleware one by one
+  for (let i = 0; i < middleware.length; i++) {
+    execute = await middleware[i](execute)
+  }
+  return (executeWithMiddleware = execute)
+}
+
+// Execution helper async => sync
+const executeSync = (execute) => {
+  // Return sync function
+  return (data, callback) => {
+    return withMiddleware(execute)
+      .then((executeWithMiddleware) => executeWithMiddleware(data))
+      .then((result) => callback(result.statusCode, result.data))
+      .catch((error) => callback(500, Requester.errored(data.id, error)))
+  }
 }
 
 // Log cache default options once
 const cacheOptions = envOptions()
 if (cacheOptions.enabled) logger.info('Cache enabled: ', cacheOptions)
-
-const withMiddleware = async (execute) => withCache(withStatusCode(execute))
-
-// Execution helper async => sync
-const executeSync = (execute) => {
-  let _execute
-  // Return sync function
-  return (data, callback) => {
-    // Init middleware only once
-    const init = _execute
-      ? Promise.resolve(_execute)
-      : withMiddleware(execute).then((fn) => {
-          _execute = fn
-          return _execute
-        })
-
-    return init
-      .then((executeWithMiddleware) => executeWithMiddleware(data))
-      .then((result) => callback(result.statusCode, result.data))
-  }
-}
 
 module.exports = {
   server: { init: (execute) => server.initHandler(executeSync(execute)) },
