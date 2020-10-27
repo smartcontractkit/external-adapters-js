813194714797c96002d94c87d41c976d558a5702
Justin Kaseman <justinkaseman@pop-os.localdomain>
2020-10-27 09:31:49 -0700

    Simplify 'customError'

--- example/adapter.js
+++ example/adapter.js
@@ -1,9 +1,6 @@
 const { Requester, Validator } = require('@chainlink/external-adapter')
 
-const customError = (data) => {
-  if (data.Response === 'Error') return true
-  return false
-}
+const customError = (data) => (data.Response === 'Error')
 
 const customParams = {
   base: ['base', 'from', 'coin'],
