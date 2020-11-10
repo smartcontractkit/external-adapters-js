744aac70331cfdc1481d6b252a16bd90afe4f427
Jonas Hals <jonas@smartcontract.com>
2020-11-10 16:33:36 +0100

    Use latest Derbit value, regardless of date

--- iv-outlier-detection/src/derbit.ts
+++ iv-outlier-detection/src/derbit.ts
@@ -1,25 +1,23 @@
 import { Requester } from '@chainlink/external-adapter'
 import { ExternalFetch } from './adapter'
 
-export const fetchDerbit: ExternalFetch = async (
-  currency: string,
-  days: number,
-): Promise<number> => {
+export const fetchDerbit: ExternalFetch = async (currency: string): Promise<number> => {
   const url = 'https://www.deribit.com/api/v2/public/get_historical_volatility'
   const params = { currency }
   const config = { url, params }
 
   const response = await Requester.request(config)
   const result: number[][] = response.data['result']
+  const resultSorted = result.sort((a, b) => {
+    if (a.length < 1 || b.length < 1) return 1
+    if (a[0] < b[0]) return 1
+    if (a[0] > b[0]) return -1
+    return 0
+  })
 
-  const date = new Date()
-  date.setMinutes(0, 0, 0)
-  date.setDate(date.getDate() - days)
-
-  const resultForDate = result.find((elem) => elem[0] === date.getTime())
-  if (!resultForDate || resultForDate.length < 2) {
-    throw new Error('no derbit value for this date')
+  if (resultSorted.length < 1 || resultSorted[0].length < 2) {
+    throw new Error('no derbit value')
   }
 
-  return resultForDate[1]
+  return resultSorted[0][1]
 }
