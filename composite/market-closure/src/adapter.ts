e61aabe9eb72373c41426a270faed9bdcde637ab
Jonas Hals <jonas@smartcontract.com>
2020-11-11 13:39:35 +0100

    Use reference contract helper

--- composite/market-closure/src/adapter.ts
+++ composite/market-closure/src/adapter.ts
@@ -1,12 +1,13 @@
-import { AdapterRequest, Execute } from '@chainlink/types'
+import { AdapterRequest, AdapterResponse, Execute } from '@chainlink/types'
 import { Requester, Validator } from '@chainlink/external-adapter'
 import { CheckExecute, getCheck, getCheckImpl } from './checks'
-import { AdapterError } from '@chainlink/external-adapter'
 import { getImpl, getPriceDataProvider } from './price'
+import { getLatestAnswer } from '@chainlink/reference-data-reader'
 
 const customParams = {
   base: ['base', 'asset', 'from'],
-  multiply: false,
+  referenceContract: ['referenceContract', 'contract'],
+  multiply: true,
   schedule: false,
 }
 
@@ -16,31 +17,24 @@ export const execute: Execute = async (input) => {
   return await executeWithAdapters(input, adapterExecute, checkExecute)
 }
 
-const executeWithAdapters = async (
+export const executeWithAdapters = async (
   input: AdapterRequest,
   adapter: Execute,
   check: CheckExecute,
-) => {
+): Promise<AdapterResponse> => {
   const validator = new Validator(input, customParams)
   if (validator.error) throw validator.error
 
   const jobRunID = validator.validated.id
   const symbol = validator.validated.data.base.toUpperCase()
   const schedule = validator.validated.data.schedule || {}
-  const multiply = validator.validated.data.multiply || 100000000
+  const referenceContract = validator.validated.data.referenceContract
+  const multiply = validator.validated.data.multiply
 
   const halted = await check(symbol, schedule)
   if (halted) {
-    if (!input.meta || !input.meta.latestAnswer)
-      throw new AdapterError({
-        jobRunID,
-        statusCode: 400,
-        message: 'market is closed and no latestAnswer meta data',
-      })
-
-    const contractPrice = input.meta.latestAnswer
-    const price = contractPrice / multiply
-    return Requester.success(jobRunID, { data: { result: price } })
+    const result = await getLatestAnswer(referenceContract, multiply, input.meta)
+    return Requester.success(jobRunID, { data: { result }, result, status: 200 })
   }
 
   return await adapter(input)
