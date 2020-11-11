9b9839d8ba858b08e14f96b3f7f5c64b5f61fce6
Justin Kaseman <justinkaseman@live.com>
2020-11-11 14:36:50 -0800

    Add BTC.com adapter

---
+++ btc.com/src/adapter.ts
@@ -0,0 +1,39 @@
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { Execute } from '@chainlink/types'
+import { Config, getConfig, logConfig, DEFAULT_ENDPOINT } from './config'
+import { balance } from './endpoint'
+
+const inputParams = {
+  endpoint: false,
+}
+
+// Export function to integrate with Chainlink node
+export const execute: Execute = async (request, config: Config) => {
+  const validator = new Validator(request, inputParams)
+  if (validator.error) throw validator.error
+
+  logConfig(config)
+
+  const jobRunID = validator.validated.id
+  const endpoint = validator.validated.data.endpoint || DEFAULT_ENDPOINT
+
+  let result
+  switch (endpoint) {
+    case balance.Name: {
+      result = await balance.execute(config, request)
+      break
+    }
+    default: {
+      throw Requester.errored(jobRunID, `Endpoint ${endpoint} not supported.`, 400)
+    }
+  }
+
+  return Requester.success(jobRunID, {
+    data: { result },
+    result,
+    status: 200,
+  })
+}
+
+// Export function to integrate with Chainlink node
+export const executeWithDefaults: Execute = async (request) => execute(request, getConfig())
