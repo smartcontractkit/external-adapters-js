33fdae96f1d55d7a6ef22bf15f56741a0d8757c3
Justin Kaseman <justinkaseman@live.com>
2020-11-11 16:38:54 -0800

    Refactor Blockchair adapter for multiple endpoints

---
+++ blockchair/src/endpoint/index.ts
@@ -0,0 +1,5 @@
+export * as balance from './balance'
+export * as difficulty from './difficulty'
+
+export type CoinType = 'btc'
+export type ChainType = 'mainnet'
---
+++ blockchair/test/difficulty.test.ts
@@ -0,0 +1,72 @@
+import { assert } from 'chai'
+import { Requester, assertSuccess, assertError, AdapterError } from '@chainlink/external-adapter'
+import { AdapterRequest } from '@chainlink/types'
+import { execute } from '../src/adapter'
+
+describe('execute', () => {
+  const jobID = '1'
+
+  context('successful calls @integration', () => {
+    const requests = [
+      {
+        name: 'id not supplied',
+        testData: { data: { blockchain: 'BTC' } },
+      },
+      {
+        name: 'blockchain',
+        testData: { id: jobID, data: { blockchain: 'BTC' } },
+      },
+      {
+        name: 'coin',
+        testData: { id: jobID, data: { coin: 'BTC' } },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        const data = await execute(req.testData as AdapterRequest, {})
+        assertSuccess({ expected: 200, actual: data.statusCode }, data, jobID)
+        assert.isAbove(data.result, 0)
+        assert.isAbove(data.data.result, 0)
+      })
+    })
+  })
+
+  context('validation error', () => {
+    const requests = [
+      { name: 'empty body', testData: {} },
+      { name: 'empty data', testData: { data: {} } },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, new AdapterError(error))
+          assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+
+  context('error calls @integration', () => {
+    const requests = [
+      {
+        name: 'unknown blockchain',
+        testData: { id: jobID, data: { blockchain: 'not_real' } },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, new AdapterError(error))
+          assertError({ expected: 500, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+})
