8064967dc71823db05aaff3d257f3d6f66cdba1e
Justin Kaseman <justinkaseman@live.com>
2020-11-12 18:27:15 -0800

    Refactor Amberdata adapter

---
+++ amberdata/src/adapter.ts
@@ -0,0 +1,40 @@
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { Execute, ExecuteWithDefaults } from '@chainlink/types'
+import { Config, getConfig, logConfig, DEFAULT_ENDPOINT } from './config'
+import { price, balance } from './endpoint'
+
+const inputParams = {
+  endpoint: false,
+}
+
+// Export function to integrate with Chainlink node
+export const execute: Execute = async (request, config: Config) => {
+  const validator = new Validator(request, inputParams)
+  if (validator.error) throw validator.error
+
+  logConfig(config)
+
+  const jobRunID = validator.validated.id
+  const endpoint = validator.validated.data.endpoint || DEFAULT_ENDPOINT
+
+  let response
+  switch (endpoint) {
+    case price.Name: {
+      response = await price.execute(config, request)
+      break
+    }
+    case balance.Name: {
+      response = await balance.execute(config, request)
+      break
+    }
+    default: {
+      throw Requester.errored(jobRunID, `Endpoint ${endpoint} not supported.`, 400)
+    }
+  }
+
+  return Requester.success(jobRunID, response)
+}
+
+// Export function to integrate with Chainlink node
+export const executeWithDefaults: ExecuteWithDefaults = async (request) =>
+  execute(request, getConfig())
---
+++ amberdata/src/endpoint/balance.ts
@@ -0,0 +1,98 @@
+import objectPath from 'object-path'
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { AdapterRequest } from '@chainlink/types'
+import { Config, DEFAULT_DATA_PATH, getBaseURL } from '../config'
+import { CoinType, ChainType, isCoinType, isChainType } from '.'
+
+export const Name = 'balance'
+
+type Address = {
+  address: string
+  coin?: CoinType
+  chain?: ChainType
+  balance?: number
+  warning?: string
+}
+
+type RequestData = {
+  dataPath: string
+  confirmations: number
+}
+
+const blockchains: { [ticker: string]: string } = {
+  btc: 'bitcoin',
+  eth: 'ethereum',
+  bch: 'bitcoin-abc',
+  ltc: 'litecoin',
+  btsv: 'bitcoin-sv',
+  zec: 'zcash',
+}
+
+const WARNING_NO_OPERATION_COIN = 'No Operation: unsupported coin'
+const WARNING_NO_OPERATION_TESTNET = "No Operation: 'testnet' is only supported on 'eth'"
+const WARNING_NO_OPERATION_CHAIN =
+  "No Operation: invalid chain parameter, must be one of 'mainnet' or 'testnet'"
+const WARNING_NO_OPERATION_MISSING_ADDRESS = 'No Operation: address param is missing'
+
+const getBalanceURI = (address: string) => `/api/v2/addresses/${address}/account-balances/latest`
+
+const getBlockchainHeader = (chain: string, coin: string) => {
+  const network = blockchains[coin]
+  if (chain === 'testnet') {
+    if (network === 'ethereum') return 'ethereum-rinkeby'
+    throw WARNING_NO_OPERATION_TESTNET
+  }
+  return `${network}-mainnet`
+}
+
+const getBalances = async (config: Config, addresses: Address[]): Promise<Address[]> =>
+  Promise.all(
+    addresses.map(async (addr: Address) => {
+      if (!addr.address) return { ...addr, warning: WARNING_NO_OPERATION_MISSING_ADDRESS }
+
+      console.log(addr.coin)
+      if (!addr.coin) addr.coin = 'btc'
+      if (isCoinType(addr.coin) === false) return { ...addr, warning: WARNING_NO_OPERATION_COIN }
+
+      if (!addr.chain) addr.chain = 'mainnet'
+      if (isChainType(addr.chain) === false) return { ...addr, warning: WARNING_NO_OPERATION_CHAIN }
+
+      const reqConfig: any = {
+        ...config.api,
+        baseURL: getBaseURL(),
+        url: getBalanceURI(addr.address),
+      }
+
+      reqConfig.headers['x-amberdata-blockchain-id'] = getBlockchainHeader(addr.chain, addr.coin)
+
+      return {
+        ...addr,
+        balance: (await Requester.request(reqConfig)).data.payload.balance,
+      }
+    }),
+  )
+
+export const inputParams = {
+  dataPath: false,
+  confirmations: false,
+}
+
+// Export function to integrate with Chainlink node
+export const execute = async (config: Config, request: AdapterRequest) => {
+  const validator = new Validator(request, inputParams)
+  if (validator.error) throw validator.error
+  const jobRunID = validator.validated.id
+
+  const data: RequestData = validator.validated.data
+  const dataPath = data.dataPath || DEFAULT_DATA_PATH
+  const inputData = <Address[]>objectPath.get(request.data, dataPath)
+
+  // Check if input data is valid
+  if (!inputData || !Array.isArray(inputData) || inputData.length === 0)
+    throw Requester.errored(
+      jobRunID,
+      `Input, at '${dataPath}' path, must be a non-empty array.`,
+      400,
+    )
+  return await getBalances(config, inputData)
+}
