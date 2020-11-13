00b6abda8afd926b51975f8b66498c2e59b4eb1b
Justin Kaseman <justinkaseman@live.com>
2020-11-13 12:46:56 -0800

    Balance endpoint returns original response

--- amberdata/test/price.test.ts
+++ amberdata/test/price.test.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai'
-import { Requester, assertSuccess, assertError, AdapterError } from '@chainlink/external-adapter'
+import { Requester, assertSuccess, assertError } from '@chainlink/external-adapter'
 import { AdapterRequest } from '@chainlink/types'
 import { executeWithDefaults } from '../src/adapter'
 
@@ -32,7 +32,7 @@ describe('price endpoint', () => {
 
     requests.forEach((req) => {
       it(`${req.name}`, async () => {
-        const data = await executeWithDefaults(req.testData as AdapterRequest, {})
+        const data = await executeWithDefaults(req.testData as AdapterRequest)
         assertSuccess({ expected: 200, actual: data.statusCode }, data, jobID)
         assert.isAbove(data.result, 0)
         assert.isAbove(data.data.result, 0)
@@ -59,7 +59,7 @@ describe('price endpoint', () => {
         try {
           await executeWithDefaults(req.testData as AdapterRequest)
         } catch (error) {
-          const errorResp = Requester.errored(jobID, new AdapterError(error))
+          const errorResp = Requester.errored(jobID, error)
           assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
         }
       })
@@ -83,7 +83,7 @@ describe('price endpoint', () => {
         try {
           await executeWithDefaults(req.testData as AdapterRequest)
         } catch (error) {
-          const errorResp = Requester.errored(jobID, new AdapterError(error))
+          const errorResp = Requester.errored(jobID, error)
           assertError({ expected: 500, actual: errorResp.statusCode }, errorResp, jobID)
         }
       })
