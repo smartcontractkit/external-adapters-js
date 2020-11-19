b15757fd81fa98a64ed2f25242fe958f86d8d428
Justin Kaseman <justinkaseman@live.com>
2020-11-19 09:13:38 -0800

    Fix COVID-tracker test for tests now catching 400 errors

--- covid-tracker/test/adapter_test.js
+++ covid-tracker/test/adapter_test.js
@@ -45,6 +45,14 @@ describe('execute', () => {
         name: 'location not supplied',
         testData: { id: jobID, data: { field: 'deaths' } },
       },
+      {
+        name: 'unknown date format',
+        testData: { id: jobID, data: { field: 'deaths', date: 'not_real', location: 'USA' } },
+      },
+      {
+        name: 'unknown date format 2',
+        testData: { id: jobID, data: { field: 'deaths', date: '2020111', location: 'USA' } },
+      },
     ]
 
     requests.forEach((req) => {
@@ -59,14 +67,6 @@ describe('execute', () => {
 
   context('error calls @integration', () => {
     const requests = [
-      {
-        name: 'unknown date format',
-        testData: { id: jobID, data: { field: 'deaths', date: 'not_real', location: 'USA' } },
-      },
-      {
-        name: 'unknown date format 2',
-        testData: { id: jobID, data: { field: 'deaths', date: '2020111', location: 'USA' } },
-      },
       {
         name: 'date not found',
         testData: { id: jobID, data: { field: 'deaths', date: '17601010', location: 'USA' } },
