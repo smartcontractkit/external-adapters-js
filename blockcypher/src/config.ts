6dc6e78248a2773f14107f316a6607b82edf8fdc
Justin Kaseman <justinkaseman@live.com>
2020-11-19 18:37:09 -0800

    Add additional types & move config to 'external-adapter/requester'

--- blockcypher/src/config.ts
+++ blockcypher/src/config.ts
@@ -1,23 +1,8 @@
-import { logger } from '@chainlink/external-adapter'
-import { util } from '@chainlink/ea-bootstrap'
-export const ENV_API_KEY = 'API_KEY'
+import { Requester } from '@chainlink/external-adapter'
+import { Config } from '@chainlink/types'
 
 export const DEFAULT_DATA_PATH = 'result'
 export const DEFAULT_CONFIRMATIONS = 6
 export const DEFAULT_ENDPOINT = 'balance'
 
-export type Config = {
-  apiKey?: string
-}
-
-export const getConfig = (prefix = ''): Config => ({
-  apiKey: util.getEnv(ENV_API_KEY, prefix),
-})
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const cloneNoSecrets = (config: Config): Config => (({ apiKey, ...o }) => o)(config)
-
-export const logConfig = (config: Config): void => {
-  logger.debug('Adapter configuration:', { config: config && cloneNoSecrets(config) })
-  if (!config.apiKey) logger.warn('API will be rate limited without an API key.')
-}
+export const getConfig = (): Config => Requester.getDefaultConfig()
