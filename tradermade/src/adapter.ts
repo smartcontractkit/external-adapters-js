1f04fcc0bdc0433a54c6c9d727580bc739adc12e
Jonas Hals <jonas@smartcontract.com>
2020-11-25 11:05:06 +0100

    Add adapter for Tradermade

---
+++ tradermade/src/adapter.ts
@@ -0,0 +1,37 @@
+import { Execute } from '@chainlink/types'
+import { Requester, Validator } from '@chainlink/external-adapter'
+
+const customParams = {
+  symbol: ['base', 'from', 'symbol'],
+  to: false,
+}
+
+const commonKeys: Record<string, string> = {
+  FTSE: 'UK100',
+  N225: 'JPN225',
+}
+
+export const execute: Execute = async (input) => {
+  const validator = new Validator(input, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const url = 'https://marketdata.tradermade.com/api/v1/live'
+  const symbol = validator.validated.data.symbol.toUpperCase()
+  const to = (validator.validated.data.to || '').toUpperCase()
+  const currency = (commonKeys[symbol] || symbol) + to
+
+  const params = {
+    currency,
+    api_key: process.env.API_KEY,
+  }
+
+  const config = {
+    url,
+    params,
+  }
+
+  const response = await Requester.request(config)
+  response.data.result = Requester.validateResultNumber(response.data, ['quotes', 0, 'mid'])
+  return Requester.success(jobRunID, response)
+}
