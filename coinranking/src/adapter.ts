3578b3a5014df8f793e3828cb471e63c7f2f5c0b
Jonas Hals <jonas@smartcontract.com>
2020-12-02 11:31:51 +0100

    Add Coinranking adapter

---
+++ coinranking/src/adapter.ts
@@ -0,0 +1,61 @@
+import { Execute } from '@chainlink/types'
+import { Requester, Validator } from '@chainlink/external-adapter'
+
+const customParams = {
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+}
+
+const baseUrl = 'https://api.coinranking.com/v2'
+
+const headers = {
+  'x-access-token': process.env.API_KEY,
+}
+
+const referenceSymbolToUuid = async (symbol: string): Promise<string> => {
+  const url = baseUrl + '/reference-currencies'
+
+  const config = {
+    url,
+    headers,
+  }
+
+  const response = await Requester.request(config)
+  const currency = response.data.data.currencies.find(
+    (x: Record<string, unknown>) => (x['symbol'] as string).toLowerCase() === symbol.toLowerCase(),
+  )
+  if (typeof currency === 'undefined') {
+    throw 'coin not found'
+  }
+  return currency.uuid
+}
+
+export const execute: Execute = async (input) => {
+  const validator = new Validator(input, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const base = validator.validated.data.base.toUpperCase()
+  const quote = validator.validated.data.quote.toUpperCase()
+  const referenceCurrencyUuid = await referenceSymbolToUuid(quote)
+
+  const url = baseUrl + `/coins`
+
+  const params = {
+    referenceCurrencyUuid,
+    'symbols[]': base,
+  }
+
+  const config = {
+    url,
+    params,
+    headers,
+  }
+
+  const response = await Requester.request(config)
+  response.data = response.data.data.coins.find(
+    (x: Record<string, unknown>) => (x['symbol'] as string).toLowerCase() === base.toLowerCase(),
+  )
+  response.data.result = Requester.validateResultNumber(response.data, ['price'])
+  return Requester.success(jobRunID, response)
+}
