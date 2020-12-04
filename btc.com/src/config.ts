06b842122f4a1eda9ae459c21d9904afedfb8085
Justin Kaseman <justinkaseman@live.com>
2020-12-04 15:48:39 -0800

    Requested changes

--- btc.com/src/config.ts
+++ btc.com/src/config.ts
@@ -1,41 +1,6 @@
-import { logger } from '@chainlink/external-adapter'
-import { util } from '@chainlink/ea-bootstrap'
-
-export const ENV_API_TIMEOUT = 'API_TIMEOUT'
-
 export const ENDPOINT_MAIN = 'https://chain.api.btc.com'
 
 export const DEFAULT_DATA_PATH = 'addresses'
-export const DEFAULT_TIMEOUT = 30000
-export const DEFAULT_CONFIRMATIONS = 6
 export const DEFAULT_ENDPOINT = 'balance'
 
-export type Config = {
-  apiKey?: string
-  api: Record<string, unknown>
-}
-
 export const getBaseURL = (): string => ENDPOINT_MAIN
-
-export const getConfig = (prefix = ''): Config => ({
-  api: {
-    returnRejectedPromiseOnError: true,
-    withCredentials: false,
-    timeout: parseInt(util.getEnv(ENV_API_TIMEOUT, prefix) as string) || DEFAULT_TIMEOUT,
-    headers: {
-      common: {
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        Pragma: 'no-cache',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    },
-  },
-})
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const cloneNoSecrets = (config: Config): Config => (({ apiKey, ...o }) => o)(config)
-
-export const logConfig = (config: Config): void => {
-  logger.debug('Adapter configuration:', { config: config && cloneNoSecrets(config) })
-}
--- btc.com/src/endpoint/balance.ts
+++ btc.com/src/endpoint/balance.ts
@@ -1,17 +1,9 @@
 import objectPath from 'object-path'
-import { Requester, Validator } from '@chainlink/external-adapter'
-import { AdapterRequest } from '@chainlink/types'
-import { Config, DEFAULT_DATA_PATH, getBaseURL } from '../config'
-import { CoinType, ChainType } from '.'
+import { Requester, Validator, AdapterError } from '@chainlink/external-adapter'
+import { AdapterRequest, Address, Account, Config } from '@chainlink/types'
+import { DEFAULT_DATA_PATH, getBaseURL } from '../config'
 
 export const Name = 'balance'
-
-type Address = {
-  address: string
-  coin?: CoinType
-  chain?: ChainType
-  balance?: number
-}
 
 type RequestData = {
   dataPath: string
@@ -19,8 +11,9 @@ type RequestData = {
 }
 
 type ResponseWithResult = {
-  response?: any
-  result?: Address
+  response: any
+  result: Account
+  warning?: string
 }
 
 const WARNING_NO_OPERATION = 'No Operation: only btc mainnet is supported by BTC.com adapter'
@@ -28,15 +21,29 @@ const WARNING_NO_OPERATION_MISSING_ADDRESS = 'No Operation: address param is mis
 
 const getBalanceURI = (address: string) => `/v3/address/${address}`
 
-const toBalances = async (config: Config, addresses: Address[]): Promise<ResponseWithResult[]> =>
+const toBalances = async (
+  config: Config,
+  addresses: Address[],
+  jobRunID: string,
+): Promise<ResponseWithResult[]> =>
   Promise.all(
     addresses.map(async (addr: Address) => {
-      if (!addr.address) return { ...addr, warning: WARNING_NO_OPERATION_MISSING_ADDRESS }
+      const warnedResponse = {
+        result: { ...addr, balance: 0 },
+        response: null,
+      }
+
+      if (!addr.address)
+        throw new AdapterError({
+          jobRunID,
+          message: WARNING_NO_OPERATION_MISSING_ADDRESS,
+          statusCode: 400,
+        })
 
       if (!addr.coin) addr.coin = 'btc'
-      if (addr.coin !== 'btc') return { ...addr, warning: WARNING_NO_OPERATION }
+      if (addr.coin !== 'btc') return { ...warnedResponse, warning: WARNING_NO_OPERATION }
       if (!addr.chain) addr.chain = 'mainnet'
-      if (addr.chain !== 'mainnet') return { ...addr, warning: WARNING_NO_OPERATION }
+      if (addr.chain !== 'mainnet') return { ...warnedResponse, warning: WARNING_NO_OPERATION }
 
       const reqConfig = {
         ...config.api,
@@ -83,12 +90,12 @@ export const execute = async (config: Config, request: AdapterRequest): Promise<
 
   // Check if input data is valid
   if (!inputData || !Array.isArray(inputData) || inputData.length === 0)
-    throw Requester.errored(
+    throw new AdapterError({
       jobRunID,
-      `Input, at '${dataPath}' path, must be a non-empty array.`,
-      400,
-    )
+      message: `Input, at '${dataPath}' path, must be a non-empty array.`,
+      statusCode: 400,
+    })
 
-  const responses = await toBalances(config, inputData)
+  const responses = await toBalances(config, inputData, jobRunID)
   return reduceResponse(responses)
 }
