041fc99c8035b496bf378e2842b53514d280b4a4
ryan <ryanemmick4@gmail.com>
2020-12-14 18:18:54 -0500

    util function for splitting string and picking random

--- bootstrap/src/lib/util.ts
+++ bootstrap/src/lib/util.ts
@@ -22,6 +22,12 @@ export const toObjectWithNumbers = (obj: any) => {
   return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, toNumber(v)]))
 }
 
+// pick a random string after splitting with the delimiter ("a&b&c" "&" -> choice(["a","b","c"]))
+export const pickRandomFromString = (str: string, delimiter: string): string => {
+  const items = str.split(delimiter)
+  return items[Math.floor(Math.random() * items.length)]
+}
+
 // We generate an UUID per instance
 export const uuid = (): string => {
   if (!process.env.UUID) process.env.UUID = uuidv4()
