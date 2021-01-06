c414929968e45bb9c52b757c29d59346087a78e1
Jonas Hals <jonas@smartcontract.com>
2021-01-06 14:58:18 +0100

    Remove old one-off outlier detection adapters

--- iv-outlier-detection/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { expose, util } from '@chainlink/ea-bootstrap'
-import { execute } from './adapter'
-
-const NAME = 'IvOutlierDetection'
-
-export = { NAME, execute, ...expose(util.wrapExecute(execute)) }
