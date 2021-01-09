eeb2a2fc77b2c8b7352fadbd140ea8400eae2703
Justin Kaseman <justinkaseman@live.com>
2021-01-08 18:06:24 -0600

    Reduce BigNumber package switched to 'bignumber.js' to handle floats

--- reduce/src/adapter.ts
+++ reduce/src/adapter.ts
@@ -1,7 +1,7 @@
 import objectPath from 'object-path'
 import { Requester, Validator } from '@chainlink/external-adapter'
 import { Execute } from '@chainlink/types'
-import BN from 'bn.js'
+import BN from 'bignumber.js'
 
 const inputParams = {
   reducer: true,
@@ -50,22 +50,21 @@ export const execute: Execute = async (request) => {
   let result: BN
   switch (data.reducer) {
     case 'sum': {
-      result = inputData.reduce(
-        (acc, val) => acc.add(new BN(_get(val))),
-        new BN(data.initialValue) || new BN(0),
-      )
+      result = inputData.reduce((acc, val) => {
+        return acc.plus(new BN(_get(val)))
+      }, new BN(data.initialValue) || new BN(0))
       break
     }
     case 'product': {
       result = inputData.reduce(
-        (acc, val) => acc.mul(new BN(_get(val))),
+        (acc, val) => acc.multipliedBy(new BN(_get(val))),
         new BN(data.initialValue) || new BN(1),
       )
       break
     }
     case 'average': {
       result = inputData.reduce(
-        (acc, val, _, { length }) => acc.add(new BN(_get(val)).div(new BN(length))),
+        (acc, val, _, { length }) => acc.plus(new BN(_get(val)).div(new BN(length))),
         new BN(data.initialValue) || new BN(0),
       )
       break
@@ -75,7 +74,7 @@ export const execute: Execute = async (request) => {
       const mid = Math.ceil(inputData.length / 2)
       result =
         inputData.length % 2 === 0
-          ? new BN(sortedData[mid]).add(new BN(sortedData[mid - 1])).div(new BN(2))
+          ? new BN(sortedData[mid]).plus(new BN(sortedData[mid - 1])).div(new BN(2))
           : new BN(sortedData[mid - 1])
       break
     }
