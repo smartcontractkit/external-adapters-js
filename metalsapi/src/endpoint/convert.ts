cfd4d5ce8c1d7fe0125e694f9896d831dbcc35ff
Evangelos Barakos <e.mparakos@gmail.com>
2021-01-12 15:50:43 +0200

    metalsapi TS migration

--- metalsapi/src/endpoint/convert.ts
+++ metalsapi/src/endpoint/convert.ts
@@ -1,41 +1,33 @@
 import { Requester, Validator } from '@chainlink/external-adapter'
 import { AdapterRequest, Config } from '@chainlink/types'
-import { DEFAULT_INTERVAL, DEFAULT_LIMIT } from '../config'
 import { util } from '@chainlink/ea-bootstrap'
 
-export const Name = 'eod'
+export const Name = 'convert'
 
 const customError = (data: any) => data.Response === 'Error'
 
 const customParams = {
   base: ['base', 'from', 'coin'],
-  interval: false,
-  limit: false,
+  quote: ['quote', 'to', 'market'],
 }
 
 export const execute = async (config: Config, request: AdapterRequest) => {
   const validator = new Validator(request, customParams)
   if (validator.error) throw validator.error
 
-  const symbols = validator.validated.data.base.toUpperCase()
-  const interval = validator.validated.data.interval || DEFAULT_INTERVAL
-  const limit = validator.validated.data.limit || DEFAULT_LIMIT
-  const url = `eod`
+  const from = validator.validated.data.base.toUpperCase()
+  const to = validator.validated.data.quote.toUpperCase()
+  const url = `convert`
 
   const params = {
-    symbols,
-    interval,
-    limit,
     access_key: util.getRandomRequiredEnv('API_KEY'),
+    from,
+    to,
+    amount: 1,
   }
 
-  const reqConfig = {
-    ...config.api,
-    params,
-    baseURL: 'http://api.marketstack.com/v1/',
-    url,
-  }
+  const reqConfig = { ...config.api, params, baseURL: 'https://metals-api.com/api/', url }
 
   const response = await Requester.request(reqConfig, customError)
-  return [response.data, Requester.validateResultNumber(response.data, ['data', 0, 'close'])]
+  return response.data
 }
