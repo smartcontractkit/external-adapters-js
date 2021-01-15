d88758f27e3c3f216d9fd4e45287cfb3853c35f1
Justin Kaseman <justinkaseman@live.com>
2021-01-15 17:33:50 -0600

    Add note to binance-dex's differing use of API_ENDPOINT env var

--- binance-dex/src/config.ts
+++ binance-dex/src/config.ts
@@ -4,6 +4,7 @@ import { Config } from '@chainlink/types'
 export const DEFAULT_API_ENDPOINT = 'dex-asiapacific'
 export const DEFAULT_DATA_ENDPOINT = 'v1/ticker/24hr'
 
+// TODO: this usage of the process.env.API_ENDPOINT differs from most other adapters and should be changed
 const getBaseURL = (region: string) => `https://${region}.binance.org`
 
 export const makeConfig = (prefix = ''): Config => {
