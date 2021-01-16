f6a2ded06bb27330cdbee3f9c00f9f2383453f38
Justin Kaseman <justinkaseman@live.com>
2021-01-15 18:08:26 -0600

    Brave New Coin to TS

---
+++ bravenewcoin/src/endpoint/price.ts
@@ -0,0 +1,27 @@
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { ExecuteWithConfig, Config } from '@chainlink/types'
+const { authenticate, convert, getAssetId } = require('../../../helpers/bravenewcoin/helpers')
+
+export const NAME = 'price'
+
+const customParams = {
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, config) => {
+  const validator = new Validator(request, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const from = validator.validated.data.base
+  const to = validator.validated.data.quote
+
+  const token = await authenticate()
+  const baseAssetId = await getAssetId(from)
+  const quoteAssetId = to.toUpperCase() === 'USD' ? 'USD' : await getAssetId(to)
+
+  const response = await convert(token, baseAssetId, quoteAssetId)
+
+  return Requester.success(jobRunID, response)
+}
