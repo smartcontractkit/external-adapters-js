8c1e5c6249ca1ae66e6389f643f5e6942451e688
Jonas Hals <jonas@smartcontract.com>
2021-01-20 14:05:53 +0100

    Expect new token-allocation output format

--- composite/crypto-volatility-index/src/dominanceDataProvider.ts
+++ composite/crypto-volatility-index/src/dominanceDataProvider.ts
@@ -1,6 +1,5 @@
 import * as TokenAllocation from '@chainlink/token-allocation-adapter'
 import { Execute } from '@chainlink/types'
-import { Response } from '@chainlink/token-allocation-adapter/dist/types'
 
 export const getDominanceAdapter = (): Execute => {
   const config = TokenAllocation.makeConfig('DOMINANCE')
@@ -8,13 +7,18 @@ export const getDominanceAdapter = (): Execute => {
   return TokenAllocation.makeExecute(config)
 }
 
-export const dominanceByCurrency = (result: Response, quote: string): Record<string, number> => {
+export const dominanceByCurrency = (
+  response: Record<string, any>,
+  quote: string,
+): Record<string, number> => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { result, sources, ...allocations } = response
   return Object.fromEntries(
-    Object.keys(result.allocations).map((symbol) => {
-      const allocation = result.allocations[symbol]
-      const marketcap = allocation.quote[quote].marketcap
+    Object.keys(allocations).map((symbol) => {
+      const allocation = allocations[symbol]
+      const marketCap = allocation.quote[quote].marketCap
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return [symbol, marketcap! / result.result]
+      return [symbol, marketCap! / result]
     }),
   )
 }
