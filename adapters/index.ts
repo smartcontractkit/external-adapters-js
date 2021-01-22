0ece357b2589536eaa4b449bb539b342bff01f66
Evangelos Barakos <e.mparakos@gmail.com>
2021-01-22 10:08:53 +0200

    removed json-rpc from workspace. working with both kinds of imports

--- adapters/index.ts
+++ adapters/index.ts
@@ -1,7 +1,11 @@
-import { default as jsonrpc } from './json-rpc/src/index'
+import { default as jsonrpc } from './json-rpc/index'
 import { default as dnsquery } from '../dns-query/src/index'
 
 export const adapters = {
   jsonrpc,
   dnsquery,
 }
+// export const jsonrpc = {
+//   jsonrpc
+//   // dnsquery,
+// }
