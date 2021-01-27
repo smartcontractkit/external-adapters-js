bb209fb9c4729cdc509906bcf2f144b619306e72
Justin Kaseman <justinkaseman@live.com>
2021-01-27 16:29:10 -0600

    Blockcypher uses batch request

--- blockcypher/src/endpoint/balance.ts
+++ blockcypher/src/endpoint/balance.ts
@@ -1,6 +1,6 @@
 import bcypher from 'blockcypher'
 import { balance } from '@chainlink/ea-factories'
-import { Config } from '@chainlink/types'
+import { Config, Account } from '@chainlink/types'
 import { CoinType, ChainType, isCoinType, isChainType } from '.'
 
 export const Name = 'balance'
@@ -28,25 +28,36 @@ const getChainId = (coin: CoinType, chain: ChainType): string => {
   }
 }
 
-const getBalance: balance.GetBalance = async (account, config) => {
-  const chainId = getChainId(account.coin as CoinType, account.chain as ChainType)
-  const api = new bcypher(account.coin, chainId, config.apiKey)
+const getBalances: balance.GetBalances = async (accounts, config) => {
+  const addresses = accounts.map((a) => a.address)
+  const { coin, chain } = accounts[0]
+  const chainId = getChainId(coin as CoinType, chain as ChainType)
+  const api = new bcypher(coin, chainId, config.apiKey)
   const params = { confirmations: config.confirmations }
-  const _getAddrBal = (): Promise<AddressBalance> =>
+  const _getAddrBal = (): Promise<AddressBalance[]> =>
     new Promise((resolve, reject) => {
-      api.getAddrBal(account.address, params, (error: Error, body: AddressBalance) =>
+      api.getAddrBal(addresses.join(';'), params, (error: Error, body: AddressBalance[]) =>
         error ? reject(error) : resolve(body),
       )
     })
 
   const response = await _getAddrBal()
 
+  const responseLookup: { [key: string]: AddressBalance } = {}
+  response.forEach((a) => (responseLookup[a.address] = a))
+
+  const toResultWithBalance = (acc: Account) => ({
+    ...acc,
+    balance: String(responseLookup[acc.address].final_balance),
+  })
+  const resultWithBalance = accounts.map(toResultWithBalance)
+
   return {
     payload: response,
-    result: [{ ...account, balance: String(response.balance) }],
+    result: resultWithBalance,
   }
 }
 
 const isSupported: balance.IsSupported = (coin, chain) => isChainType(chain) && isCoinType(coin)
 
-export const makeExecute = (config: Config) => balance.make({ ...config, getBalance, isSupported })
+export const makeExecute = (config: Config) => balance.make({ ...config, getBalances, isSupported })
