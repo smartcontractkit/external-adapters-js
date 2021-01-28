5fac1a4b3f2872ffc88dd61d0dfb8e40ef428273
Evangelos Barakos <e.mparakos@gmail.com>
2021-01-28 13:05:56 +0200

    marketstack url + api_key through config

--- marketstack/src/config.ts
+++ marketstack/src/config.ts
@@ -4,5 +4,10 @@ import { Config } from '@chainlink/types'
 export const DEFAULT_INTERVAL = '1min'
 export const DEFAULT_LIMIT = 1
 export const DEFAULT_ENDPOINT = 'eod'
+export const DEFAULT_API_ENDPOINT = 'http://api.marketstack.com/v1/'
 
-export const makeConfig = (prefix?: string): Config => Requester.getDefaultConfig(prefix)
+export const makeConfig = (prefix = ''): Config => {
+  const config = Requester.getDefaultConfig(prefix)
+  config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
+  return config
+}
