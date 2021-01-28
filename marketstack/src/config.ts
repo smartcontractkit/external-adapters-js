c74626b50eeb4f9935ed122d4aa8fa4b4876c264
Evangelos Barakos <e.mparakos@gmail.com>
2021-01-28 13:45:27 +0200

    changes in kaiko's config

--- marketstack/src/config.ts
+++ marketstack/src/config.ts
@@ -4,7 +4,7 @@ import { Config } from '@chainlink/types'
 export const DEFAULT_INTERVAL = '1min'
 export const DEFAULT_LIMIT = 1
 export const DEFAULT_ENDPOINT = 'eod'
-export const DEFAULT_API_ENDPOINT = 'http://api.marketstack.com/v1/'
+export const DEFAULT_API_ENDPOINT = 'https://us.market-api.kaiko.io'
 
 export const makeConfig = (prefix = ''): Config => {
   const config = Requester.getDefaultConfig(prefix)
