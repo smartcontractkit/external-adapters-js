adea9e2e50761ddaa525621a12d0fa7e2044f577
Evangelos Barakos <e.mparakos@gmail.com>
2021-02-02 10:23:44 +0200

    fix kaiko random null results

--- kaiko/src/config.ts
+++ kaiko/src/config.ts
@@ -10,6 +10,5 @@ export const makeConfig = (prefix = ''): Config => {
   const config = Requester.getDefaultConfig(prefix)
   config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
   config.api.headers['X-Api-Key'] = config.apiKey
-  config.api.headers['User-Agent'] = 'Chainlink'
   return config
 }
