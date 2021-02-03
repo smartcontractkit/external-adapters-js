1d834e7fd3b490514b6d008e2627c8992da4bcb8
Justin Kaseman <justinkaseman@live.com>
2021-02-03 14:57:03 -0600

    Brings back dummy API_KEYs for tests

--- coinmarketcap/test/marketcap.test.ts
+++ coinmarketcap/test/marketcap.test.ts
@@ -7,7 +7,7 @@ import { makeExecute } from '../src/adapter'
 describe('execute', () => {
   const jobID = '1'
   const execute = makeExecute()
-  // process.env.API_KEY = process.env.API_KEY ?? 'test_api_key'
+  process.env.API_KEY = process.env.API_KEY ?? 'test_api_key'
 
   context('successful calls @integration', () => {
     const requests = [
