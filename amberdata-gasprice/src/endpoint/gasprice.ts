9cfa19b3d2e6ed80aa7247903e6babc4f919d871
Justin Kaseman <justinkaseman@live.com>
2021-02-04 12:41:54 -0600

    EA migration - batch #1 (#226)
    * 1forge to TS
    
    * Add Config type for recent change to generic types
    
    * Utilize ExecuteWithConfig in 1forge
    
    * Adds example README template & remove 'server' script
    
    * Alphavantage adapter to TS
    
    * Remove alphavantage-sdr folder
    
    * Amberdata-gasprice to TS
    
    * Anyblock-gasprice to TS
    
    * Binance-DEX to TS
    
    * Add note to binance-dex's differing use of API_ENDPOINT env var
    
    * Brave New Coin to TS
    
    * Bring Brave New Coin VWAP into Brave New Coin EA
    
    * Coinapi to TS
    
    * Coinbase to TS
    
    * Coingecko to TS
    
    * Move BNC helpers into BNC adapter
    
    * Increase alphavantage test timeout
    
    * Moves alphavantage APIkey param into endpoint - issue open for improvement
    
    * Clean up leftover bravenewcoin-vwap traces
    
    * Remove 1forge's conflicting and not useful endpoint param
    
    * Requested changes: 1forge balance -> price, alphavantage example output
    
    * Re-add server script & spread response data into endpoint responses
    
    * Uses verbose env var to determine response data
    
    * Gets API_KEY from makeConfig
    
    * Add test dummy API_KEY
    
    * Re-add server script to example

---
+++ amberdata-gasprice/src/endpoint/gasprice.ts
@@ -0,0 +1,41 @@
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { ExecuteWithConfig, Config } from '@chainlink/types'
+
+export const NAME = 'gasprice'
+
+const customError = (data: any) => {
+  return Object.keys(data.payload).length < 1
+}
+
+const customParams = {
+  speed: false,
+  endpoint: false,
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, config) => {
+  const validator = new Validator(request, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const speed = validator.validated.data.speed || 'average'
+  const endpoint = validator.validated.data.endpoint || 'ethereum-mainnet'
+  const url = '/api/v2/transactions/gas/predictions'
+
+  const options = {
+    ...config.api,
+    url,
+    headers: {
+      ...config.api.headers,
+      'x-amberdata-blockchain-id': endpoint,
+    },
+  }
+
+  const response = await Requester.request(options, customError)
+  const result = Requester.validateResultNumber(response.data, ['payload', speed, 'gasPrice'])
+
+  return Requester.success(jobRunID, {
+    data: config.verbose ? { ...response.data, result } : { result },
+    result,
+    status: 200,
+  })
+}
