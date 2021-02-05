caca6dd2a968d8b9d985cf39965dc216679be7e3
Justin Kaseman <justinkaseman@live.com>
2021-02-04 18:23:02 -0600

    Remove util.wrapExecute from all EAs

--- composite/bitcoin-json-rpc/src/index.ts
+++ composite/bitcoin-json-rpc/src/index.ts
@@ -1,6 +1,6 @@
 import { makeConfig } from './config'
 import { makeExecute } from './adapter'
-import { expose, util } from '@chainlink/ea-bootstrap'
+import { expose } from '@chainlink/ea-bootstrap'
 
 const NAME = 'BITCOIN-JSON-RPC'
-export = { NAME, makeExecute, makeConfig, ...expose(util.wrapExecute(makeExecute())) }
+export = { NAME, makeExecute, makeConfig, ...expose(makeExecute()) }
