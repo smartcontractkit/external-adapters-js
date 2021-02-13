c7fce454f7a1bd397411e4d83e2b321fe8365926
Alex Kwiatkowski <rupurt@users.noreply.github.com>
2021-02-12 16:06:38 -0800

    Add logging to defi pulse composite adapter
    - process.env.DATA_PROVIDER
    - adapter
    - rpcUrl
    - address
    - network
    - input

--- composite/defi-pulse/src/adapter.ts
+++ composite/defi-pulse/src/adapter.ts
@@ -1,4 +1,4 @@
-import { Validator } from '@chainlink/external-adapter'
+import { Validator, logger } from '@chainlink/external-adapter'
 import { AdapterResponse, AdapterRequest, Execute } from '@chainlink/types'
 import { getAllocations } from './index-allocations'
 import * as TokenAllocation from '@chainlink/token-allocation-adapter'
@@ -17,6 +17,13 @@ export const execute = async (input: AdapterRequest, config: Config): Promise<Ad
 
   const jobRunID = validator.validated.id
   const asset = validator.validated.data
+
+  logger.debug(`DATA_PROVIDER: ${process.env.DATA_PROVIDER}`)
+  logger.debug(`adapter: ${asset.adapter}`)
+  logger.debug(`address: ${asset.address}`)
+  logger.debug(`rpcUrl: ${config.rpcUrl}`)
+  logger.debug(`network: ${config.network}`)
+  logger.debug(`input: ${JSON.stringify(input)}`)
 
   const allocations = await getAllocations(
     asset.adapter,
