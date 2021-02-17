fb60fa92a1c042652286802c928e1e3e1d5fb472
Evangelos Barakos <e.mparakos@gmail.com>
2021-02-17 18:16:29 +0200

    removed helper files

--- ethwrite/deploy_contract.ts
+++
@@ -1,8 +0,0 @@
-import { deploy } from './src/contract_helpers'
-;(async function () {
-  if (process.env.PRIVATE_KEY && process.env.RPC_URL) {
-    deploy(process.env.PRIVATE_KEY, process.env.RPC_URL)
-  } else {
-    console.log('Please set PRIVATE_KEY and RPC_URL env vars')
-  }
-})()
