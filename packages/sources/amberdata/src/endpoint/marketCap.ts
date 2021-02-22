7ad47314fd11713ff322bdba2f161b44767cfe60
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-22 13:33:19 +0100

    amberdata market cap support

--- packages/sources/amberdata/src/endpoint/marketCap.ts
+++ packages/sources/amberdata/src/endpoint/marketCap.ts
@@ -1,25 +1,15 @@
 import { Requester, Validator } from '@chainlink/external-adapter'
 import { ExecuteWithConfig, Config } from '@chainlink/types'
 
-export const Name = 'token'
+export const Name = 'marketcap'
 
 const customError = (data: any) => {
   return Object.keys(data.payload).length === 0
 }
 
-export enum TokenEndpoints {
-  MarketCap = 'marketcap',
-  TotalSupply = 'totalsupply',
-}
-
-const paths: Record<TokenEndpoints, string> = {
-  [TokenEndpoints.MarketCap]: 'marketCapUSD',
-  [TokenEndpoints.TotalSupply]: 'totalSupply',
-}
-
 const customParams = {
   base: ['base', 'from', 'coin'],
-  endpoint: true,
+  quote: ['quote', 'to', 'market'],
 }
 
 export const execute: ExecuteWithConfig<Config> = async (input, config) => {
@@ -28,8 +18,7 @@ export const execute: ExecuteWithConfig<Config> = async (input, config) => {
   const jobRunID = validator.validated.id
 
   const coin = validator.validated.data.base
-  const endpoint: TokenEndpoints = validator.validated.data.endpoint
-  const path = paths[endpoint] || 'marketCapUSD'
+  const market = validator.validated.data.quote
   const url = `/api/v2/market/tokens/prices/${coin.toLowerCase()}/latest`
 
   const reqConfig = { ...config.api, url }
@@ -38,10 +27,10 @@ export const execute: ExecuteWithConfig<Config> = async (input, config) => {
   const coinData = response.data.payload.find(
     (asset: Record<string, any>) => asset.symbol.toUpperCase() === coin.toUpperCase(),
   )
-  const result = Requester.validateResultNumber(coinData, [path])
+  const result = Requester.validateResultNumber(coinData, [`marketCap${market.toUpperCase()}`])
 
   return Requester.success(jobRunID, {
-    data: config.verbose ? { ...response.data, result } : { result },
+    data: config.verbose ? { ...response, result } : { result },
     result,
     status: 200,
   })
