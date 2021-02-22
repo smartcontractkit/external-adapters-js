990f06ba9902ad223ae686e8c363097c78939022
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-22 18:31:09 +0100

    nomics multi support

--- packages/sources/coingecko/src/endpoint/price.ts
+++ packages/sources/coingecko/src/endpoint/price.ts
@@ -84,7 +84,7 @@ const getPayload = (symbols: string[], prices: Prices, quote: string) => {
     const key = symbol
     const val = {
       quote: {
-        [quote]: {
+        [quote.toUpperCase()]: {
           price: prices[symbol],
         },
       },
