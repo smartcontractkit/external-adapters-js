806615ec7c3b198380d0a7a6411e4c9a796a794c
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-22 10:09:48 +0100

    cmc batch support

--- packages/sources/coinmarketcap/src/endpoint/price.ts
+++ packages/sources/coinmarketcap/src/endpoint/price.ts
@@ -67,65 +67,88 @@ const priceParams = {
   path: false,
 }
 
+const getPayload = (prices: { [symbol: string]: number }, quote: string) => {
+  const payloadEntries = Object.entries(prices).map(([symbol, price]) => {
+    const val = {
+      quote: {
+        [quote]: { price },
+      },
+    }
+    return [symbol, val]
+  })
+
+  const payload: ResponsePayload = Object.fromEntries(payloadEntries)
+  return payload
+}
+
 export const execute: ExecuteWithConfig<Config> = async (request, config) => {
-  const url = 'cryptocurrency/quotes/latest'
   const validator = new Validator(request, priceParams)
   if (validator.error) throw validator.error
 
   const jobRunID = validator.validated.id
-
   const symbol = validator.overrideSymbol(AdapterName)
+  const assets = Array.isArray(symbol) ? symbol : [symbol]
   // CMC allows a coin name to be specified instead of a symbol
   const slug = validator.validated.data.slug
   // CMC allows a coin ID to be specified instead of a symbol
-  const cid = validator.validated.data.cid || ''
+  const cid = validator.validated.data.cid
   // Free CMCPro API only supports a single symbol to convert
   const convert = validator.validated.data.convert
   const path = validator.validated.data.path || Paths.Price
 
-  const params: Record<string, string> = { convert }
-  if (cid) {
-    params.id = cid
-  } else if (slug) {
-    params.slug = slug
-  } else {
-    const slugForSymbol = presetSlugs[symbol]
-    if (slugForSymbol) {
-      params.slug = slugForSymbol
-    } else {
-      params.symbol = symbol
+  const _getPriceData = async (params: any): Promise<any> => {
+    const url = 'cryptocurrency/quotes/latest'
+    const options = {
+      ...config.api,
+      url,
+      params,
     }
+    const response = await Requester.request(options)
+    return response.data && response.data.data
   }
 
-  const options = {
-    ...config.api,
-    url,
-    params,
-  }
-  const response = await Requester.request(options)
-
-  // CMC API currently uses ID as key in response, when querying with "slug" param
-  const _keyForSlug = (data: any, slug: string) => {
-    if (!data || !data.data) return
-    // First try to find slug key in response (in case the API starts returning keys correctly)
-    if (Object.keys(data.data).includes(slug)) return slug
-    // Fallback to ID
-    const _iEqual = (s1: string, s2: string) => s1.toUpperCase() === s2.toUpperCase()
-    const o: any = Object.values(data.data).find((o: any) => _iEqual(o.slug, slug))
-    return o && o.id
+  const _success = (prices: any, response: any) => {
+    const payload = getPayload(prices, convert)
+    const result = Object.values(prices).length === 1 && Object.values(prices)[0]
+    return Requester.success(jobRunID, {
+      data: config.verbose ? { ...response, result, payload } : { result, payload },
+      result,
+      status: 200,
+    })
   }
 
-  const key = params.id || _keyForSlug(response.data, params.slug || '') || params.symbol
-  const resultPaths: { [key: string]: string[] } = {
-    [Paths.Price]: ['data', key, 'quote', convert, 'price'],
-    [Paths.MarketCap]: ['data', key, 'quote', convert, 'market_cap'],
+  const params: Record<string, string> = { convert }
+  const _validate = (data: any) => Requester.validateResultNumber(data, ['quote', convert, 'price'])
+  if (cid) {
+    const response = await _getPriceData({ ...params, id: cid })
+    const prices = { [response[cid].symbol]: _validate(response[cid]) }
+    return _success(prices, response)
+  } else if (slug) {
+    const response = await _getPriceData({ ...params, slug })
+    const asset: any = Object.values(response).find(
+      (o: any) => o.slug.toLowerCase() === slug.toLowerCase(),
+    )
+    return _success({ [asset.symbol]: _validate(asset) }, response)
+  } else {
+    const slugs = assets.map((s) => presetSlugs[s]).filter(Boolean)
+    const symbols = assets.filter((s) => !presetSlugs[s])
+    let response: Record<string, any> = {}
+    // Queries for slugs and symbols cannot be together
+    if (slugs.length > 0) {
+      const slugPrices = await _getPriceData({ ...params, slug: slugs.join() })
+      response = { ...response, ...slugPrices }
+    }
+
+    if (symbols.length > 0) {
+      const symbolPrices = await _getPriceData({ ...params, symbol: symbols.join() })
+      response = { ...response, ...symbolPrices }
+    }
+
+    const indexMap = new Map()
+    Object.values(response).forEach((asset) => indexMap.set(asset.symbol.toUpperCase(), asset))
+    const prices = Object.fromEntries(
+      assets.map((symbol) => [symbol, _validate(indexMap.get(symbol.toUpperCase()))]),
+    )
+    return _success(prices, response)
   }
-
-  const result = Requester.validateResultNumber(response.data, resultPaths[path])
-
-  return Requester.success(jobRunID, {
-    data: config.verbose ? { ...response.data, result } : { result },
-    result,
-    status: 200,
-  })
 }
