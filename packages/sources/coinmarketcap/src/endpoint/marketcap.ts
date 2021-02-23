424c2a9b8ab5f0d4f6fe6e881df0bfd4397631bf
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-23 16:08:23 +0100

    symbol in payload uppercase

--- packages/sources/coinmarketcap/src/endpoint/marketcap.ts
+++ packages/sources/coinmarketcap/src/endpoint/marketcap.ts
@@ -32,7 +32,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, config) => {
         [quote]: { marketCap: _validateMarketCap(price) },
       },
     }
-    return [symbol, val]
+    return [symbol.toUpperCase(), val]
   })
 
   const payload: ResponsePayload = Object.fromEntries(payloadEntries)
