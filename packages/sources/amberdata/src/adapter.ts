498087b05f19f84cd12b2f96a0468d9629c139ee
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-23 13:58:15 +0100

    empty result removed. lowercase endpoint

--- packages/sources/amberdata/src/adapter.ts
+++ packages/sources/amberdata/src/adapter.ts
@@ -17,7 +17,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, config) => {
   const jobRunID = validator.validated.id
   const endpoint = validator.validated.data.endpoint || DEFAULT_ENDPOINT
 
-  switch (endpoint) {
+  switch (endpoint.toLowerCase()) {
     case price.Name: {
       return price.execute(request, config)
     }
