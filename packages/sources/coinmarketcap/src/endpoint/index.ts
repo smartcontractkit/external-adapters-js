ebeb5f240d9dd9ce7f0521d5f56d443499c514d6
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-02-23 11:29:07 +0100

    fix typo

--- packages/sources/coinmarketcap/src/endpoint/index.ts
+++ packages/sources/coinmarketcap/src/endpoint/index.ts
@@ -1,4 +1,4 @@
 export * as dominance from './dominance'
 export * as price from './price'
 export * as globalMarketCap from './globalMarketCap'
-export * as marketCap from './marketCap'
+export * as marketCap from './marketcap'
