cbbdb6662ef1d8bb346df0a65c97035dbc52d017
PanaW <wangpan@conflux-chain.org>
2021-02-23 12:00:04 +0800

    fix lint

--- conflux/test/adapter.test.ts
+++ conflux/test/adapter.test.ts
@@ -3,10 +3,12 @@ import { Requester } from '@chainlink/external-adapter'
 import { assertSuccess, assertError } from '@chainlink/adapter-test-helpers'
 import { AdapterRequest } from '@chainlink/types'
 import { makeExecute } from '../src/adapter'
+// import { Config } from '../src/config'
 
 const TestOracleAddress = '0x8688ebA9Bf38cBb1Fa27A8C3Fda11414D6057887'
 const FunctionSelector = '0x4ab0d190'
-const DataPrefixExample = '0x9b8c5ab8dc03bb19d6a5047c213537c5000d151e66e49458e054c68cb504904f0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000869795b26b0f5f00ba2b3cf3380634fb5f4806094357855e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000060346c9e'
+const DataPrefixExample =
+  '0x9b8c5ab8dc03bb19d6a5047c213537c5000d151e66e49458e054c68cb504904f0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000869795b26b0f5f00ba2b3cf3380634fb5f4806094357855e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000060346c9e'
 
 describe('execute', () => {
   const jobID = 'fd26a90e0aa84040bc6b4d6f5036a23a'
@@ -38,19 +40,19 @@ describe('execute', () => {
     })
   }) */
 
-   context('validation error', () => {
+  context('validation error', () => {
     const requests = [
       { name: 'empty body', testData: {} },
       { name: 'empty data', testData: { data: {} } },
       {
         name: 'value not supplied',
-        testData: { 
-          id: jobID, 
-          data: { 
+        testData: {
+          id: jobID,
+          data: {
             address: TestOracleAddress,
             dataPrefix: DataPrefixExample,
             functionSelector: FunctionSelector,
-          } 
+          },
         },
       },
     ]
@@ -72,13 +74,13 @@ describe('execute', () => {
       {
         name: 'invalid id',
         testData: {
-          id: jobID, 
+          id: jobID,
           data: {
             address: TestOracleAddress,
             dataPrefix: DataPrefixExample,
             functionSelector: FunctionSelector,
-            value: '168007'
-          }
+            value: '168007',
+          },
         },
       },
     ]
