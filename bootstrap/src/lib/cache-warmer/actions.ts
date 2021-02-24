eae4dd2ba8c500517e1a26c803e772351fe092b1
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-02-24 00:50:52 -0500

    Initial cache warmer implementation

---
+++ bootstrap/src/lib/cache-warmer/actions.ts
@@ -0,0 +1,48 @@
+import { AdapterRequest, Execute } from '@chainlink/types'
+import { createAction } from '@reduxjs/toolkit'
+
+interface WarmupRequestSubscribedPayload extends AdapterRequest {
+  executeFn: Execute
+}
+interface WarmupRequestUnsubscribedPayload {
+  key: string
+}
+
+export const warmupRequestSubscribed = createAction<WarmupRequestSubscribedPayload>(
+  'WARMUP_REQUEST_SUBSCRIBED',
+)
+export const warmupRequestUnsubscribed = createAction<WarmupRequestUnsubscribedPayload>(
+  'WARMUP_REQUEST_UNSUBSCRIBED',
+)
+
+interface WarmupRequestRequestedPayload {
+  /**
+   * State lookup key so that the warmup request handler can find the slice of data it needs
+   * to warmup the cold EA
+   */
+  key: string
+}
+interface WarmupRequestFulfilledPayload {
+  /**
+   * State lookup key
+   */
+  key: string
+}
+interface WarmupRequestFailedPayload {
+  /**
+   * State lookup key
+   */
+  key: string
+  error: Error
+}
+/**
+ * These set of events are emitted when our warmup handler requests the EA itself to warm up
+ * the cache for a particular key
+ */
+export const warmupRequestRequested = createAction<WarmupRequestRequestedPayload>(
+  'WARMUP_REQUEST_REQUESTED',
+)
+export const warmupRequestFulfilled = createAction<WarmupRequestFulfilledPayload>(
+  'WARMUP_REQUEST_FULFILLED',
+)
+export const warmupRequestFailed = createAction<WarmupRequestFailedPayload>('WARMUP_REQUEST_FAILED')
