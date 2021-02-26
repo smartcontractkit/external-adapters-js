e70c8b76ceed3979705f02e63319e50224d3008a
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-02-25 21:02:27 -0500

    Terseify naming

--- bootstrap/src/lib/cache-warmer/actions.ts
+++ bootstrap/src/lib/cache-warmer/actions.ts
@@ -1,40 +1,36 @@
 import { AdapterRequest, Execute } from '@chainlink/types'
 import { createAction } from '@reduxjs/toolkit'
 
-interface WarmupRequestSubscribedPayload extends AdapterRequest {
+interface WarmupSubscribedPayload extends AdapterRequest {
   executeFn: Execute
 }
-interface WarmupRequestUnsubscribedPayload {
+interface WarmupUnsubscribedPayload {
   key: string
 }
-interface WarmupRequestSubscriptionTimeoutResetPayload {
+interface WarmupSubscriptionTimeoutResetPayload {
   key: string
 }
 
-export const warmupRequestSubscribed = createAction<WarmupRequestSubscribedPayload>(
-  'WARMUP_REQUEST_SUBSCRIBED',
+export const warmupSubscribed = createAction<WarmupSubscribedPayload>('WARMUP_SUBSCRIBED')
+export const warmupSubscriptionTimeoutReset = createAction<WarmupSubscriptionTimeoutResetPayload>(
+  'WARMUP_SUBSCRIPTION_TIMEOUT_RESET',
 )
-export const warmupRequestSubscriptionTimeoutReset = createAction<WarmupRequestSubscriptionTimeoutResetPayload>(
-  'WARMUP_REQUEST_SUBSCRIPTION_TIMEOUT_RESET',
-)
-export const warmupRequestUnsubscribed = createAction<WarmupRequestUnsubscribedPayload>(
-  'WARMUP_REQUEST_UNSUBSCRIBED',
-)
+export const warmupUnsubscribed = createAction<WarmupUnsubscribedPayload>('WARMUP_UNSUBSCRIBED')
 
-interface WarmupRequestRequestedPayload {
+interface WarmupRequestedPayload {
   /**
-   * State lookup key so that the warmup request handler can find the slice of data it needs
+   * State lookup key so that the warmup handler can find the slice of data it needs
    * to warmup the cold EA
    */
   key: string
 }
-interface WarmupRequestFulfilledPayload {
+interface WarmupFulfilledPayload {
   /**
    * State lookup key
    */
   key: string
 }
-interface WarmupRequestFailedPayload {
+interface WarmupFailedPayload {
   /**
    * State lookup key
    */
@@ -45,10 +41,6 @@ interface WarmupRequestFailedPayload {
  * These set of events are emitted when our warmup handler requests the EA itself to warm up
  * the cache for a particular key
  */
-export const warmupRequestRequested = createAction<WarmupRequestRequestedPayload>(
-  'WARMUP_REQUEST_REQUESTED',
-)
-export const warmupRequestFulfilled = createAction<WarmupRequestFulfilledPayload>(
-  'WARMUP_REQUEST_FULFILLED',
-)
-export const warmupRequestFailed = createAction<WarmupRequestFailedPayload>('WARMUP_REQUEST_FAILED')
+export const warmupRequested = createAction<WarmupRequestedPayload>('WARMUP_REQUESTED')
+export const warmupFulfilled = createAction<WarmupFulfilledPayload>('WARMUP_FULFILLED')
+export const warmupFailed = createAction<WarmupFailedPayload>('WARMUP_FAILED')
