39ff2efc6a80f4fca1f8c23aaa3cead003dd769b
Jonas Hals <jonas@smartcontract.com>
2021-03-02 20:07:57 +0100

    Increase timespan for Kaiko requests

--- kaiko/src/config.ts
+++ kaiko/src/config.ts
@@ -3,7 +3,7 @@ import { Config } from '@chainlink/types'
 
 export const DEFAULT_INTERVAL = '1m'
 export const DEFAULT_SORT = 'desc'
-export const DEFAULT_MILLISECONDS = 1000000
+export const DEFAULT_MILLISECONDS = 1800000
 export const DEFAULT_API_ENDPOINT = 'https://us.market-api.kaiko.io/v2/data/trades.v1'
 
 export const makeConfig = (prefix = ''): Config => {
