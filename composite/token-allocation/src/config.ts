0b0d536b182cb17477abec859929c309c8c231e6
Rodrigo Ariza <15104916+RodrigoAD@users.noreply.github.com>
2021-03-03 14:09:20 +0100

    Token allocation data reliance (#325)
    * price adapters updated
    
    * endpoint selector
    
    * markecap on payload
    
    * removed old price adapters
    
    * removed default adapter urls
    
    * fixed tests
    
    synthx test fix
    
    fix tests
    
    * batching ignored
    
    * fixed synthx test

---
+++ composite/token-allocation/src/config.ts
@@ -0,0 +1,20 @@
+import { util } from '@chainlink/ea-bootstrap'
+import { getDefaultConfig } from '@chainlink/external-adapter'
+import { getDataProvider } from './dataProvider'
+import { Config } from './types'
+
+export const DEFAULT_TOKEN_DECIMALS = 18
+export const DEFAULT_TOKEN_BALANCE = 1
+
+export const makeConfig = (prefix = ''): Config => {
+  const dataProviderUrl = util.getRequiredEnv('DATA_PROVIDER_URL', prefix)
+  const defaultConfig = getDefaultConfig(prefix)
+  defaultConfig.api.baseURL = defaultConfig.api.baseURL || dataProviderUrl
+  defaultConfig.api.method = 'post'
+
+  return {
+    priceAdapter: getDataProvider(defaultConfig.api),
+    defaultMethod: util.getEnv('DEFAULT_METHOD', prefix) || 'price',
+    defaultQuote: util.getEnv('DEFAULT_QUOTE') || 'USD',
+  }
+}
