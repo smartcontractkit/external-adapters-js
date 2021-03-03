fdf1d89e363375e550da745439e46cdce934d11c
Rodrigo Ariza <15104916+RodrigoAD@users.noreply.github.com>
2021-03-03 14:09:20 +0100

    Token allocation data reliance (#325)
    * price adapters updated
    
    * endpoint selector
    
    * markecap on payload
    
    * removed old price adapters
    
    * removed default adapter urls
    
    * fixed tests
    
    synthx test fix
    
    fix tests
    
    * batching ignored
    
    * fixed synthx test

--- packages/composites/token-allocation/src/config.ts
+++ packages/composites/token-allocation/src/config.ts
@@ -1,44 +1,19 @@
 import { util } from '@chainlink/ea-bootstrap'
-import * as cryptocompare from './data-providers/cryptocompare'
-import * as nomics from './data-providers/nomics'
-import * as coinpaprika from './data-providers/coinpaprika'
-import * as coinmarketcap from './data-providers/coinmarketcap'
-import * as coingecko from './data-providers/coingecko'
-import * as coinapi from './data-providers/coinapi'
-import * as amberdata from './data-providers/amberdata'
-import * as kaiko from './data-providers/kaiko'
-import { Config, PriceAdapter } from './types'
-
-enum DataProvider {
-  Amberdata = 'amberdata',
-  Bravenewcoin = 'bravenewcoin',
-  Cryptocompare = 'cryptocompare',
-  Coinpaprika = 'coinpaprika',
-  Nomics = 'nomics',
-  Coinmarketcap = 'coinmarketcap',
-  Coingecko = 'coingecko',
-  Coinapi = 'coinapi',
-  Kaiko = 'kaiko',
-}
-
-const providers: Record<string, PriceAdapter> = {
-  [DataProvider.Amberdata]: amberdata,
-  [DataProvider.Cryptocompare]: cryptocompare,
-  [DataProvider.Coinpaprika]: coinpaprika,
-  [DataProvider.Nomics]: nomics,
-  [DataProvider.Coinmarketcap]: coinmarketcap,
-  [DataProvider.Coingecko]: coingecko,
-  [DataProvider.Coinapi]: coinapi,
-  [DataProvider.Kaiko]: kaiko,
-}
+import { getDefaultConfig } from '@chainlink/external-adapter'
+import { getDataProvider } from './dataProvider'
+import { Config } from './types'
 
 export const DEFAULT_TOKEN_DECIMALS = 18
 export const DEFAULT_TOKEN_BALANCE = 1
 
-export const makeConfig = (prefix = '', provider = ''): Config => {
-  const dataProvider = provider || util.getRequiredEnv('DATA_PROVIDER', prefix)
+export const makeConfig = (prefix = ''): Config => {
+  const dataProviderUrl = util.getRequiredEnv('DATA_PROVIDER_URL', prefix)
+  const defaultConfig = getDefaultConfig(prefix)
+  defaultConfig.api.baseURL = defaultConfig.api.baseURL || dataProviderUrl
+  defaultConfig.api.method = 'post'
+
   return {
-    priceAdapter: providers[dataProvider],
+    priceAdapter: getDataProvider(defaultConfig.api),
     defaultMethod: util.getEnv('DEFAULT_METHOD', prefix) || 'price',
     defaultQuote: util.getEnv('DEFAULT_QUOTE') || 'USD',
   }
