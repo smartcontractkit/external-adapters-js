4812fb837af875c698ab6764d7e06719c5fa6b54
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-03-07 18:35:20 +0100

    Polish Rate Limit implementation

--- bootstrap/src/lib/rate-limit/config.ts
+++ bootstrap/src/lib/rate-limit/config.ts
@@ -1,20 +1,22 @@
 import objectHash from 'object-hash'
+import { getHashOpts } from '../util'
 
 export interface Config {
-  hashOpts: Required<Parameters<typeof objectHash>>['1']
+  /**
+   * The time to live on a subscription, if no new requests come in that do not
+   * originate from the warm up engine itself
+   */
   totalCapacity: number
+
+  /**
+   * Hashing options for differentiating requests
+   */
+  hashOpts: Required<Parameters<typeof objectHash>>['1']
 }
 
-export function getConfig(): Config {
+export function get(): Config {
   return {
-    hashOpts: {
-      algorithm: 'sha1',
-      encoding: 'hex',
-      excludeKeys: (props: string) =>
-        ['jobId', 'id', 'maxAge', 'meta']
-          .concat((process.env.CACHE_KEY_IGNORED_PROPS || '').split(',').filter((k) => k))
-          .includes(props),
-    },
+    hashOpts: getHashOpts(),
     totalCapacity: parseInt(process.env.RATE_LIMIT_CAPACITY || ''),
   }
 }
