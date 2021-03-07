86746ad1d8363f2b7a72ae1898005fd2f08ff388
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-03-07 18:35:20 +0100

    Polish Rate Limit implementation

--- bootstrap/src/lib/rate-limit/store.ts
+++ bootstrap/src/lib/rate-limit/store.ts
@@ -1,24 +1,5 @@
 import { createStore, PreloadedState, Reducer } from 'redux'
 
-export interface Interval {
-  [key: string]: number
-}
-
-export enum IntervalNames {
-  SEC = 'SEC',
-  MINUTE = 'MINUTE',
-  HOUR = 'HOUR',
-  DAY = 'DAY',
-  ALL = 'ALL',
-}
-
-export const Intervals: Interval = {
-  [IntervalNames.SEC]: 1000,
-  [IntervalNames.MINUTE]: 1000 * 60,
-  [IntervalNames.HOUR]: 1000 * 60 * 60,
-  [IntervalNames.DAY]: 1000 * 60 * 60 * 24,
-}
-
 export function configureStore(rootReducer: Reducer, preloadedState: PreloadedState<any> = {}) {
   const store = createStore(rootReducer, preloadedState)
   return { store }
--- packages/core/bootstrap/src/lib/cache-warmer/index.ts
+++ packages/core/bootstrap/src/lib/cache-warmer/index.ts
@@ -1,8 +1,10 @@
 import { rootReducer } from './reducer'
 import { rootEpic } from './side-effects'
 import { configureStore } from './store'
+
 export * as actions from './actions'
-export { store }
 
 const { epicMiddleware, store } = configureStore(rootReducer)
 epicMiddleware.run(rootEpic)
+
+export { store }
