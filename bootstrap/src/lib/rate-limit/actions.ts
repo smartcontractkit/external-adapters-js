ff67798c03055a80df12a7f082ff9d6b5beef16c
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-03-08 11:05:59 +0100

    Polish Rate Limit implementation #2

--- bootstrap/src/lib/rate-limit/actions.ts
+++ bootstrap/src/lib/rate-limit/actions.ts
@@ -1,28 +1,16 @@
-import { AdapterRequest } from '@chainlink/types'
-import { createAction, nanoid } from '@reduxjs/toolkit'
-import { getParticipantId } from './util'
+import { createAction } from '@reduxjs/toolkit'
+import { ActionBase, toActionPayload } from '../store'
 
 const DEFAULT_COST = 1
 
-const toActionPayload = <A extends ActionBase>(data: any): A => ({
-  id: nanoid(),
-  createdAt: new Date().toISOString(),
-  ...data,
-})
-
-export interface ActionBase {
-  id: string
-  createdAt: string
-}
-
 export interface RequestObservedPayload extends ActionBase {
-  requestId: string
+  typeId: string
   cost: number
 }
 
 export const requestObserved = createAction(
   'RL/REQUEST_OBSERVED',
-  (input: AdapterRequest, cost = DEFAULT_COST) => ({
-    payload: toActionPayload({ requestId: getParticipantId(input), cost }),
+  (typeId: string, cost = DEFAULT_COST) => ({
+    payload: toActionPayload({ typeId, cost }),
   }),
 )
