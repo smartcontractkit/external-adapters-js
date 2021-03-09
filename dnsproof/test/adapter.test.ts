7d6664fa7816bf4086aeb948f5ca8501b8b03051
christianagnew <christian.agnew@outlook.com>
2021-03-09 14:47:45 -0500

    dnsproof adapter

--- dnsproof/test/adapter.test.ts
+++ dnsproof/test/adapter.test.ts
@@ -12,19 +12,35 @@ describe('execute', () => {
     const requests = [
       {
         name: 'id not supplied',
-        testData: { data: { base: 'ETH', quote: 'USD' } },
+        testData: {
+          data: {
+            domain: 'www5.infernos.io',
+            address: '0x4d3407ddfdeb3feb4e8a167484701aced7056826',
+          },
+        },
+        expectedResult: true,
       },
       {
-        name: 'base/quote',
-        testData: { id: jobID, data: { base: 'ETH', quote: 'USD' } },
+        name: 'domain owned',
+        testData: {
+          id: jobID,
+          data: {
+            domain: 'www5.infernos.io',
+            address: '0x4d3407ddfdeb3feb4e8a167484701aced7056826',
+          },
+        },
+        expectedResult: true,
       },
       {
-        name: 'from/to',
-        testData: { id: jobID, data: { from: 'ETH', to: 'USD' } },
-      },
-      {
-        name: 'coin/market',
-        testData: { id: jobID, data: { coin: 'ETH', market: 'USD' } },
+        name: 'domain not owned',
+        testData: {
+          id: jobID,
+          data: {
+            domain: 'www.test.com',
+            address: '0x4d3407ddfdeb3feb4e8a167484701aced7056826',
+          },
+        },
+        expectedResult: false,
       },
     ]
 
@@ -32,8 +48,8 @@ describe('execute', () => {
       it(`${req.name}`, async () => {
         const data = await execute(req.testData as AdapterRequest)
         assertSuccess({ expected: 200, actual: data.statusCode }, data, jobID)
-        assert.isAbove(data.result, 0)
-        assert.isAbove(data.data.result, 0)
+        assert.isTrue(data.result === req.expectedResult)
+        assert.isTrue(data.data.result === req.expectedResult)
       })
     })
   })
@@ -43,12 +59,12 @@ describe('execute', () => {
       { name: 'empty body', testData: {} },
       { name: 'empty data', testData: { data: {} } },
       {
-        name: 'base not supplied',
-        testData: { id: jobID, data: { quote: 'USD' } },
+        name: 'domain not supplied',
+        testData: { id: jobID, data: { address: '0x4d3407ddfdeb3feb4e8a167484701aced7056826' } },
       },
       {
-        name: 'quote not supplied',
-        testData: { id: jobID, data: { base: 'ETH' } },
+        name: 'address not supplied',
+        testData: { id: jobID, data: { domain: 'www5.infernos.io' } },
       },
     ]
 
@@ -59,30 +75,6 @@ describe('execute', () => {
         } catch (error) {
           const errorResp = Requester.errored(jobID, error)
           assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
-        }
-      })
-    })
-  })
-
-  context('error calls @integration', () => {
-    const requests = [
-      {
-        name: 'unknown base',
-        testData: { id: jobID, data: { base: 'not_real', quote: 'USD' } },
-      },
-      {
-        name: 'unknown quote',
-        testData: { id: jobID, data: { base: 'ETH', quote: 'not_real' } },
-      },
-    ]
-
-    requests.forEach((req) => {
-      it(`${req.name}`, async () => {
-        try {
-          await execute(req.testData as AdapterRequest)
-        } catch (error) {
-          const errorResp = Requester.errored(jobID, error)
-          assertError({ expected: 500, actual: errorResp.statusCode }, errorResp, jobID)
         }
       })
     })
