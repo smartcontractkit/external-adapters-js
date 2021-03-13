2c410d3c266d60529f75409274a815b62d309511
Justin Kaseman <justinkaseman@live.com>
2021-03-12 19:52:32 -0500

    Refactor Outlier Detection to use underlying data source
    (cherry picked from commit 248099e6ea5d53bbc857b90caffb43cb8d3ab845)

--- packages/composites/outlier-detection/src/source.ts
+++ packages/composites/outlier-detection/src/source.ts
@@ -1,43 +1,9 @@
-import { Execute } from '@chainlink/types'
-import * as GenesisVolatility from '@chainlink/genesis-volatility-adapter'
-import * as XBTO from '@chainlink/xbto-adapter'
-import * as dxFeed from '@chainlink/dxfeed-adapter'
-import * as Tradermade from '@chainlink/tradermade-adapter'
-import { util } from '@chainlink/ea-bootstrap'
+import { AdapterImplementation } from '@chainlink/types'
+// source adapters
+import GenesisVolatility from '@chainlink/genesis-volatility-adapter'
+import XBTO from '@chainlink/xbto-adapter'
+import dxFeed from '@chainlink/dxfeed-adapter'
 
-export enum SourceDataProvider {
-  XBTO = 'xbto',
-  GenesisVolatility = 'genesisvolatility',
-  dxFeed = 'dxfeed',
-  Tradermade = 'tradermade',
-}
+export const adapters: AdapterImplementation[] = [GenesisVolatility, XBTO, dxFeed]
 
-export const ENV_SOURCE_ADAPTERS = 'SOURCE_ADAPTERS'
-
-const isSourceDataProvider = (envVar?: string): envVar is SourceDataProvider =>
-  Object.values(SourceDataProvider).includes(envVar as any)
-
-export const getSourceDataProviders = (prefix = ''): SourceDataProvider[] => {
-  const sourceDataProviders = (util.getEnv(ENV_SOURCE_ADAPTERS, prefix) || '').split(
-    ',',
-  ) as string[]
-  return sourceDataProviders
-    .filter(isSourceDataProvider)
-    .map((provider) => provider as SourceDataProvider)
-}
-
-export const getSourceImpl = (type: SourceDataProvider): Execute => {
-  const prefix = type?.toUpperCase()
-  switch (type) {
-    case SourceDataProvider.XBTO:
-      return XBTO.makeExecute(XBTO.makeConfig(prefix))
-    case SourceDataProvider.GenesisVolatility:
-      return GenesisVolatility.makeExecute(GenesisVolatility.makeConfig(prefix))
-    case SourceDataProvider.dxFeed:
-      return dxFeed.makeExecute(dxFeed.makeConfig(prefix))
-    case SourceDataProvider.Tradermade:
-      return Tradermade.makeExecute(Tradermade.makeConfig(prefix))
-    default:
-      throw Error(`Unknown source data provider adapter type: ${type}`)
-  }
-}
+export type Source = typeof adapters[number]['NAME']
