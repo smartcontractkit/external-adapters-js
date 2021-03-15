016d668a85e6bff48d2ca6a07d4581553d50e461
christianagnew <christian.agnew@outlook.com>
2021-03-15 14:40:35 -0400

    watchsignals adapter

--- watchsignals/src/index.ts
+++ watchsignals/src/index.ts
@@ -2,6 +2,6 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute } from './adapter'
 import { makeConfig } from './config'
 
-const NAME = 'EXAMPLE' // This should be filled in with a name corresponding to the data provider using UPPERCASE and _underscores_.
+const NAME = 'WATCHSIGNALS'
 
 export = { NAME, makeExecute, makeConfig, ...expose(makeExecute()) }
