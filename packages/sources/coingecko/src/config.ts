c1e11648e07f63be4f239f9cfcd50d70ca559095
Justin Kaseman <justinkaseman@live.com>
2021-03-15 15:59:55 -0400

    Better handling of AxiosRequestConfig type, Requester.request now is a generic, & catches some errors via TS :sparkles:
    (cherry picked from commit 2b774fccf60ef775b2f99b245f6634f5e7e9a2a0)

--- packages/sources/coingecko/src/config.ts
+++ packages/sources/coingecko/src/config.ts
@@ -6,6 +6,6 @@ export const DEFAULT_API_ENDPOINT = 'https://api.coingecko.com/api/v3'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
-  config.api.baseURL = config.api.baseUrl || DEFAULT_API_ENDPOINT
+  config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
   return config
 }
