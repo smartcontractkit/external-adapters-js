44a2f27dba9c4f4347a202009f5f9cc548706f09
Justin Kaseman <justinkaseman@live.com>
2021-03-15 15:59:55 -0400

    Better handling of AxiosRequestConfig type, Requester.request now is a generic, & catches some errors via TS :sparkles:
    (cherry picked from commit 2b774fccf60ef775b2f99b245f6634f5e7e9a2a0)

--- packages/sources/cryptocompare/src/config.ts
+++ packages/sources/cryptocompare/src/config.ts
@@ -8,7 +8,7 @@ export const DEFAULT_API_ENDPOINT = 'https://min-api.cryptocompare.com'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix, true)
-  config.api.baseURL = config.api.baseUrl || DEFAULT_API_ENDPOINT
+  config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
   if (config.apiKey)
     config.api.headers = {
       ...config.api.headers,
