a1f5e3b00d256cd7f4a0ce4465e409b8cf739360
Justin Kaseman <justinkaseman@live.com>
2021-03-16 13:04:25 -0400

    Add unit tests & more TS tweaks
    (cherry picked from commit 1a5af3dbf125e9c124a681609ffa3219594d479e)

--- packages/sources/deribit/src/index.ts
+++ packages/sources/deribit/src/index.ts
@@ -1,6 +1,6 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeConfig, makeExecute } from './adapter'
 
-const NAME = 'Deribit'
+const NAME = 'DERIBIT'
 
 export = { NAME, makeExecute, makeConfig, ...expose(makeExecute()) }
