3117d0c6e2352042e264a86ea3906275b8f18cb3
Justin Kaseman <justinkaseman@live.com>
2021-03-16 16:03:06 -0400

    Merge changes

--- packages/core/bootstrap/src/lib/rateLimit/actions.ts
+++
@@ -1,9 +0,0 @@
-import { AdapterRequest } from '@chainlink/types'
-import { createAction } from '@reduxjs/toolkit'
-
-export interface ParticipantPayload {
-  data: AdapterRequest
-  cost: number
-}
-
-export const newRequest = createAction<ParticipantPayload>('NEW_REQUEST')
