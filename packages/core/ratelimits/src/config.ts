2fc64d0dee36decae1882883c7745132cf9a7d82
Ryan Emmick <ryanemmick4@gmail.com>
2021-03-16 16:21:51 -0400

    Data provider ratelimits (#352)
    * remove undefined limits from config file
    
    * prettify limits.json
    
    * add ratelimits package
    
    * add ratelimits to top-level package.json
    
    * Update ratelimits/src/index.ts
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>
    
    * Update ratelimits/package.json
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>
    
    * Update ratelimits/src/index.ts
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>
    
    * Update ratelimits/src/index.ts
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>
    
    * address PR comments, add defaults and config
    
    * rebase
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

---
+++ packages/core/ratelimits/src/config.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_SECOND_RATELIMIT = 2 // used when no tiers are defined for a provider
+export const DEFAULT_MINUTE_RATELIMIT = 60 // used when no tiers are defined for a provider
+export const BURST_UNDEFINED_QUOTA_MULTIPLE = 2 // used there is a quota limit but not burst limit defined
