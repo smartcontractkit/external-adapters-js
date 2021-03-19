0098b9ab20b2fff089df5a333e49b5d676d52940
Justin Kaseman <justinkaseman@live.com>
2021-03-19 18:25:13 -0400

    Exposes CMC adapter makeConfig for type checks

--- packages/sources/coinmarketcap/src/index.ts
+++ packages/sources/coinmarketcap/src/index.ts
@@ -1,6 +1,7 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute } from './adapter'
+import { makeConfig } from './config'
 
 const NAME = 'COINMARKETCAP'
 
-export = { NAME, makeExecute, ...expose(makeExecute()) }
+export = { NAME, makeExecute, makeConfig, ...expose(makeExecute()) }
