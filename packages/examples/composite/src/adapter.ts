69c107c6b3de985286323841ca132c0cd018651f
Justin Kaseman <justinkaseman@live.com>
2021-03-25 10:48:48 -0400

    Adds to the examaple composite adapter

--- packages/examples/composite/src/adapter.ts
+++ packages/examples/composite/src/adapter.ts
@@ -1,1 +1,19 @@
-//
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { AdapterResponse, Execute, AdapterRequest } from '@chainlink/types'
+import { makeConfig, Config } from './config'
+
+const customParams = {}
+
+export const execute = async (input: AdapterRequest, config: Config): Promise<AdapterResponse> => {
+  const validator = new Validator(input, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.jobRunID
+
+  const response = { data: {}, status: 200 }
+  return Requester.success(jobRunID, response)
+}
+
+export const makeExecute = (config?: Config): Execute => {
+  return async (request: AdapterRequest) => execute(request, config || makeConfig())
+}
