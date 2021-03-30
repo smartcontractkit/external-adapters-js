281106a17bdb3919a43d96a6244ce27a5cfa7917
Justin Kaseman <justinkaseman@live.com>
2021-03-29 21:41:05 -0400

    Turns 2-step into a composite adapter

---
+++ packages/composites/reference-transform/src/config.ts
@@ -0,0 +1,27 @@
+import { util, Requester } from '@chainlink/ea-bootstrap'
+import legos from '@chainlink/ea-legos'
+import { Config as DefaultConfig } from '@chainlink/types'
+
+export type Config = {
+  sources: { [name: string]: DefaultConfig }
+}
+
+export const ENV_DATA_PROVIDER_URL = 'DATA_PROVIDER_URL'
+
+export const makeConfig = (prefix = ''): Config => {
+  const sources: { [name: string]: DefaultConfig } = {}
+
+  for (const a of legos.sources) {
+    const url = util.getEnv(ENV_DATA_PROVIDER_URL, a.toUpperCase())
+    if (url) {
+      const defaultConfig = Requester.getDefaultConfig(prefix)
+      defaultConfig.api.baseURL = url
+      defaultConfig.api.method = 'post'
+      sources[a.toLowerCase()] = defaultConfig
+    }
+  }
+
+  return {
+    sources,
+  }
+}
