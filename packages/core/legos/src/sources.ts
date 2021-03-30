73c4a52e3ced4ca99c85316b146a143f85ee741a
Justin Kaseman <justinkaseman@live.com>
2021-03-29 21:40:52 -0400

    Add EA-legos package

---
+++ packages/core/legos/src/sources.ts
@@ -0,0 +1,161 @@
+import * as _1forge from '@chainlink/1forge-adapter'
+import * as agoric from '@chainlink/agoric-adapter'
+import * as alphachain from '@chainlink/alphachain-adapter'
+import * as alphavantage from '@chainlink/alphavantage-adapter'
+import * as amberdata from '@chainlink/amberdata-adapter'
+import * as amberdata_gasprice from '@chainlink/amberdata-gasprice-adapter'
+import * as anyblock_gasprice from '@chainlink/anyblock-gasprice-adapter'
+import * as anyblock_uniswap_vwap from '@chainlink/anyblock-uniswap-vwap-adapter'
+import * as binance_dex from '@chainlink/binance-dex-adapter'
+import * as bitex from '@chainlink/bitex-adapter'
+import * as bitso from '@chainlink/bitso-adapter'
+import * as blockchain_com from '@chainlink/blockchain.com-adapter'
+import * as blockchair from '@chainlink/blockchair-adapter'
+import * as blockcypher from '@chainlink/blockcypher-adapter'
+import * as bravenewcoin from '@chainlink/bravenewcoin-adapter'
+import * as btc_com from '@chainlink/btc.com-adapter'
+import * as cfbenchmarks from '@chainlink/cfbenchmarks-adapter'
+import * as coinapi from '@chainlink/coinapi-adapter'
+import * as coinbase from '@chainlink/coinbase-adapter'
+import * as coincodex from '@chainlink/coincodex-adapter'
+import * as coingecko from '@chainlink/coingecko-adapter'
+import * as coinlore from '@chainlink/coinlore-adapter'
+import * as coinmarketcap from '@chainlink/coinmarketcap-adapter'
+import * as coinpaprika from '@chainlink/coinpaprika-adapter'
+import * as coinranking from '@chainlink/coinranking-adapter'
+import * as conflux from '@chainlink/conflux-adapter'
+import * as covid_tracker from '@chainlink/covid-tracker-adapter'
+import * as cryptoapis from '@chainlink/cryptoapis-adapter'
+import * as cryptocompare from '@chainlink/cryptocompare-adapter'
+import * as cryptoid from '@chainlink/cryptoid-adapter'
+import * as cryptomkt from '@chainlink/cryptomkt-adapter'
+import * as currencylayer from '@chainlink/currencylayer-adapter'
+import * as deribit from '@chainlink/deribit-adapter'
+import * as dns_query from '@chainlink/dns-query-adapter'
+import * as dwolla from '@chainlink/dwolla-adapter'
+import * as dxfeed from '@chainlink/dxfeed-adapter'
+import * as dxfeed_secondary from '@chainlink/dxfeed-secondary-adapter'
+import * as dydx_stark from '@chainlink/dydx-stark-adapter'
+import * as etherchain from '@chainlink/etherchain-adapter'
+import * as ethgasstation from '@chainlink/ethgasstation-adapter'
+import * as ethwrite from '@chainlink/ethwrite-adapter'
+import * as fcsapi from '@chainlink/fcsapi-adapter'
+import * as finage from '@chainlink/finage-adapter'
+import * as finnhub from '@chainlink/finnhub-adapter'
+import * as fixer from '@chainlink/fixer-adapter'
+import * as fmpcloud from '@chainlink/fmpcloud-adapter'
+import * as genesis_adapter from '@chainlink/genesis-volatility-adapter'
+import * as geodb from '@chainlink/geodb-adapter'
+import * as harmony from '@chainlink/harmony-adapter'
+import * as iex_adapter from '@chainlink/iex-cloud-adapter'
+import * as json_adapter from '@chainlink/json-rpc-adapter'
+import * as kaiko from '@chainlink/kaiko-adapter'
+import * as lcx from '@chainlink/lcx-adapter'
+import * as linkpool from '@chainlink/linkpool-adapter'
+import * as lition from '@chainlink/lition-adapter'
+import * as marketstack from '@chainlink/marketstack-adapter'
+import * as messari from '@chainlink/messari-adapter'
+import * as metalsapi from '@chainlink/metalsapi-adapter'
+import * as nikkei from '@chainlink/nikkei-adapter'
+import * as nomics from '@chainlink/nomics-adapter'
+import * as oilpriceapi from '@chainlink/oilpriceapi-adapter'
+import * as onchain from '@chainlink/onchain-adapter'
+import * as openexchangerates from '@chainlink/openexchangerates-adapter'
+import * as orchid_adapter from '@chainlink/orchid-bandwidth-adapter'
+import * as paxos from '@chainlink/paxos-adapter'
+import * as poa_adapter from '@chainlink/poa-gasprice-adapter'
+import * as polygon from '@chainlink/polygon-adapter'
+import * as reduce from '@chainlink/reduce-adapter'
+import * as renvm_adapter_address from '@chainlink/renvm-address-set-adapter'
+import * as satoshitango from '@chainlink/satoshitango-adapter'
+import * as stasis from '@chainlink/stasis-adapter'
+import * as taapi from '@chainlink/taapi-adapter'
+import * as therundown from '@chainlink/therundown-adapter'
+import * as tiingo from '@chainlink/tiingo-adapter'
+import * as tradermade from '@chainlink/tradermade-adapter'
+import * as tradingeconomics from '@chainlink/tradingeconomics-adapter'
+import * as trueusd from '@chainlink/trueusd-adapter'
+import * as wbtc_adapter_address from '@chainlink/wbtc-address-set-adapter'
+import * as xbto from '@chainlink/xbto-adapter'
+
+export default {
+  _1forge,
+  agoric,
+  alphachain,
+  alphavantage,
+  amberdata,
+  amberdata_gasprice,
+  anyblock_gasprice,
+  anyblock_uniswap_vwap,
+  binance_dex,
+  bitex,
+  bitso,
+  blockchain_com,
+  blockchair,
+  blockcypher,
+  bravenewcoin,
+  btc_com,
+  cfbenchmarks,
+  coinapi,
+  coinbase,
+  coincodex,
+  coingecko,
+  coinlore,
+  coinmarketcap,
+  coinpaprika,
+  coinranking,
+  conflux,
+  covid_tracker,
+  cryptoapis,
+  cryptocompare,
+  cryptoid,
+  cryptomkt,
+  currencylayer,
+  deribit,
+  dns_query,
+  dwolla,
+  dxfeed,
+  dxfeed_secondary,
+  dydx_stark,
+  etherchain,
+  ethgasstation,
+  ethwrite,
+  fcsapi,
+  finage,
+  finnhub,
+  fixer,
+  fmpcloud,
+  genesis_adapter,
+  geodb,
+  harmony,
+  iex_adapter,
+  json_adapter,
+  kaiko,
+  lcx,
+  linkpool,
+  lition,
+  marketstack,
+  messari,
+  metalsapi,
+  nikkei,
+  nomics,
+  oilpriceapi,
+  onchain,
+  openexchangerates,
+  orchid_adapter,
+  paxos,
+  poa_adapter,
+  polygon,
+  reduce,
+  renvm_adapter_address,
+  satoshitango,
+  stasis,
+  taapi,
+  therundown,
+  tiingo,
+  tradermade,
+  tradingeconomics,
+  trueusd,
+  wbtc_adapter_address,
+  xbto,
+}
