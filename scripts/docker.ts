c35a61279c38410fe71b7ca899495fa88efbb635
Justin Kaseman <justinkaseman@live.com>
2021-03-31 11:21:29 -0400

    make scripts to ts scripts (#401)
    * make => ts scripts
    
    * Fix workflow targets docker command

---
+++ scripts/docker.ts
@@ -0,0 +1,25 @@
+const shell = require('shelljs')
+
+const log = { red: (text: string) => console.log('\x1b[31m%s\x1b[0m', text) }
+
+const ADAPTER_TYPES = ['composites', 'sources', 'core', 'examples', 'targets']
+
+;(() => {
+  const type: string = process.argv[2]
+  if (!type) return log.red('Missing first argument: type')
+  if (!ADAPTER_TYPES.includes(type))
+    return log.red(`Type must be one of: ${ADAPTER_TYPES.join(', ')}`)
+
+  const n: string = process.argv[3]
+  if (!n) return log.red('Missing second argument: name')
+
+  const repo: string = process.argv[4] || ''
+  const tag: string = process.argv[5]
+
+  const package = JSON.parse(shell.cat(`packages/${type}/${n}/package.json`)).name
+  shell.exec(
+    `docker build --build-arg type=${type} --build-arg name=${n} --build-arg package=${package} -f Dockerfile . -t ${repo}${n}-adapter ${
+      tag ? `-t ${repo}${package}${tag}` : ''
+    }`,
+  )
+})()
---
+++ scripts/new.ts
@@ -0,0 +1,33 @@
+const shell = require('shelljs')
+
+const log = { red: (text: string) => console.log('\x1b[31m%s\x1b[0m', text) }
+
+const ADAPTER_TYPES = ['composite', 'source']
+
+;(() => {
+  const type: string = process.argv[2]
+  if (!type) return log.red('Missing first argument: type')
+  if (!ADAPTER_TYPES.includes(type))
+    return log.red(`Type must be one of: ${ADAPTER_TYPES.join(', ')}`)
+
+  const n: string = process.argv[3]
+  if (!n) return log.red('Missing second argument: name')
+
+  shell.mkdir(`packages/${type}s/${n}`)
+  shell.cp('-R', `packages/examples/${type}/*`, `packages/${type}s/${n}`)
+  // cp -R will not copy hidden & special files, so we copy manualy
+  shell.cp(`packages/examples/${type}/.eslintrc.js`, `packages/${type}s/${n}`)
+  shell
+    .cat('.github/strategy/adapters.json')
+    .exec(`jq '.${type}.adapter += ["${n}"]'`)
+    .exec('tee .github/strategy/adapters.json')
+    .to('.github/strategy/adapters.json')
+  shell
+    .cat(`packages/${type}s/${n}/package.json`)
+    .exec(
+      `jq '.name = "@chainlink/${n}-adapter" | .description = "Chainlink ${n} adapter." | .keywords += ["${n}"]'`,
+    )
+    .exec(`tee packages/${type}s/${n}/package.json`)
+    .to(`packages/${type}s/${n}/package.json`)
+  shell.sed('-i', `s/Example/${n}/`, `packages/${type}s/${n}/README.md`)
+})()
