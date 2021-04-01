150cd28ae408e18ca8b05b9822779d89244bc6ba
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-01 19:27:56 -0400

    Support basic docker-compose generation

---
+++ packages/scripts/src/docker-build.ts
@@ -0,0 +1,67 @@
+import * as fs from 'fs'
+import * as s from 'shelljs'
+import * as yaml from 'yaml'
+
+interface WorkspacePackage {
+  location: string
+  name: string
+}
+const VALID_ADAPTER_TYPES = ['composites', 'sources', 'examples', 'targets']
+const scope = '@chainlink/'
+const r = s
+  .exec('yarn workspaces list --json')
+  .split('\n')
+  .filter(Boolean)
+  .map((v) => JSON.parse(v))
+  .map(({ location, name }: WorkspacePackage) => ({
+    location,
+    name,
+    descopedName: name.replace(scope, ''),
+    type: location.split('/')[1],
+  }))
+  .filter((v) => VALID_ADAPTER_TYPES.includes(v.type))
+
+interface Service {
+  image: string
+  build: {
+    context: string
+    dockerfile: string
+    args: Record<string, string>
+  }
+}
+
+interface Dockerfile {
+  version: string
+  services: Record<string, Service>
+}
+
+function makeDockerComposeFile(packages: typeof r): Dockerfile {
+  return {
+    version: '3.9',
+    services: packages.reduce<Record<string, Service>>((prev, next) => {
+      prev[next.descopedName] = {
+        image: `${next.descopedName}${getTag()}`,
+        build: {
+          context: '.',
+          dockerfile: './Dockerfile',
+          args: {
+            location: next.location,
+            package: next.name,
+          },
+        },
+      }
+
+      return prev
+    }, {}),
+  }
+}
+
+function getTag() {
+  const tag = process.env.TAG || 'latest'
+  return `:${tag}`
+}
+
+fs.writeFileSync(
+  'docker-compose.generated.yaml',
+  yaml.stringify(makeDockerComposeFile(r), { merge: true }),
+)
