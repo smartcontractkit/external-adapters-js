20408e33e9fb80bf98a7764f236d4272e3815f45
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-01 16:52:39 -0400

    chore/jest migration (#408)
    * Run codemod
    
    * Run BDD codemod
    
    * Swap out chai + mocha deps for jest
    
    * Fix invalid assertions
    
    * Split up unit and integration tests
    
    * Edit root test commands
    
    * Run lint:fix
    
    * Fixup ethwrite test
    
    * Use self-hosted prod runners for CI
    
    * Manually install yarn
    
    * Ghetto parallelisation
    
    * Clean up package scripts
    
    * Remove jest as dep from subpackages
    
    * Fix CI script

---
+++ packages/sources/bravenewcoin/test/unit/vwap.test.ts
@@ -0,0 +1,28 @@
+import { Requester } from '@chainlink/ea-bootstrap'
+import { assertError } from '@chainlink/ea-test-helpers'
+import { AdapterRequest } from '@chainlink/types'
+import { makeExecute } from '../../src/adapter'
+
+describe('price endpoint', () => {
+  const jobID = '1'
+  const execute = makeExecute()
+
+  describe('validation error', () => {
+    const requests = [
+      { name: 'empty body', testData: { endpoint: 'vwap' } },
+      { name: 'empty data', testData: { data: { endpoint: 'vwap' } } },
+      { name: 'base not supplied', testData: { id: jobID, data: { endpoint: 'vwap' } } },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, error)
+          assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+})
