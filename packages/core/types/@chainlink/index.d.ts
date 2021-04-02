5a16c0021a25c4cc9c6ed3fd5e314f465f1650e6
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-02 00:57:02 -0400

    Fix linting errors

--- packages/core/types/@chainlink/index.d.ts
+++ packages/core/types/@chainlink/index.d.ts
@@ -66,7 +66,7 @@ declare module '@chainlink/types' {
   export type Config = {
     apiKey?: string
     network?: string
-    returnRejectedPromiseOnError?: Boolean
+    returnRejectedPromiseOnError?: boolean
     verbose?: boolean
     api: RequestConfig
   }
