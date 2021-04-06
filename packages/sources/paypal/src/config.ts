be16d16480d212c40c13592afd1ea0165d80b8bd
aalu1418 <aalu1418@gmail.com>
2021-04-06 16:33:43 -0400

    new paypal adapter with sendPayout endpoint, untested

---
+++ packages/sources/paypal/src/config.ts
@@ -0,0 +1,34 @@
+import { Requester, util, AdapterError } from '@chainlink/ea-bootstrap'
+import { Config } from '@chainlink/types'
+import * as paypal from '@paypal/payouts-sdk'
+
+export const DEFAULT_ENDPOINT = 'sendpayout'
+export const DEFAULT_MODE = 'sandbox'
+
+export const makeConfig = (prefix = ''): Config => {
+  // environment variable checks
+  const clientId: string = util.getRequiredEnv('CLIENT_ID', prefix)
+  const clientSecret: string = util.getRequiredEnv('CLIENT_SECRET', prefix)
+  const mode: string = util.getEnv('MODE', prefix) || DEFAULT_MODE
+
+  let environment
+  switch (mode.toLowerCase()) {
+    case 'sandbox':
+      environment = new paypal.core.SandboxEnvironment(clientId, clientSecret)
+      break
+    case 'live':
+      environment = new paypal.core.LiveEnvironment(clientId, clientSecret)
+      break
+    default: {
+      throw new AdapterError({
+        jobRunID: undefined,
+        message: `${mode} mode is not supported.`,
+        statusCode: 400,
+      })
+    }
+  }
+  const client = new paypal.core.PayPalHttpClient(environment)
+  const config = Requester.getDefaultConfig(prefix)
+  config.api.client = { ...client }
+  return config
+}
