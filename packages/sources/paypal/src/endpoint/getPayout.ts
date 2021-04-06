7af28d020626fefbd730f7e9a718fa07bda45467
aalu1418 <aalu1418@gmail.com>
2021-04-06 16:33:43 -0400

    new paypal adapter with sendPayout endpoint, untested

---
+++ packages/sources/paypal/src/endpoint/getPayout.ts
@@ -0,0 +1,35 @@
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { Config, ExecuteWithConfig } from '@chainlink/types'
+
+export const NAME = 'getpayout'
+
+const customError = (data: any) => data.Response === 'Error'
+
+const customParams = {
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+  field: false,
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, config) => {
+  const validator = new Validator(request, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const base = validator.validated.data.base
+  const quote = validator.validated.data.quote
+  const url = `price`
+
+  const params = {
+    base,
+    quote,
+    api_key: config.apiKey,
+  }
+
+  const options = { ...config.api, params, url }
+
+  const response = await Requester.request(options, customError)
+  response.data.result = Requester.validateResultNumber(response.data, ['price'])
+
+  return Requester.success(jobRunID, response, config.verbose)
+}
