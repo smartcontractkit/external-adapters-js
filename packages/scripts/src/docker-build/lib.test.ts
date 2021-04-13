cefbdf668f5f03eb4fd1d732c64fb4a2357e394e
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-13 18:04:40 -0400

    Add latest tag support

--- packages/scripts/src/docker-build/lib.test.ts
+++ packages/scripts/src/docker-build/lib.test.ts
@@ -2,17 +2,31 @@ import { generateFileJSON, ImageNameConfig } from './lib'
 describe('docker compose file generation', () => {
   const nullImageNameConfig: ImageNameConfig = {
     prefix: '',
-    tag: 'latest',
+    branch: '',
+    useLatest: false,
   }
   describe('image name generation', () => {
-    it('should generate base dockerfile images', () => {
+    it('should generate base dockerfile images with versions', () => {
       const output = generateFileJSON(nullImageNameConfig)
+      expect(output).toMatchSnapshot()
+    })
+    it('should generate base dockerfile images with latest', () => {
+      const output = generateFileJSON({ ...nullImageNameConfig, useLatest: true })
       expect(output).toMatchSnapshot()
     })
     it('should generate aws ecr dockerfile images', () => {
       const output = generateFileJSON({
         prefix: 'public.ecr.aws/chainlink-staging/adapters/',
-        tag: 'develop',
+        branch: 'develop',
+        useLatest: false,
+      })
+      expect(output).toMatchSnapshot()
+    })
+    it('should generate aws ecr dockerfile images with latest tag', () => {
+      const output = generateFileJSON({
+        prefix: 'public.ecr.aws/chainlink-staging/adapters/',
+        branch: 'develop',
+        useLatest: true,
       })
       expect(output).toMatchSnapshot()
     })
--- packages/scripts/src/gha/lib.test.ts
+++ packages/scripts/src/gha/lib.test.ts
@@ -6,7 +6,7 @@ describe('job matrix generation', () => {
   })
   it('should generate a job matrix suitable for pushing to ecr', () => {
     process.env.IMAGE_PREFIX = 'public.ecr.aws/chainlink-staging/adapters/'
-    process.env.TAG = 'EAEE'
+    process.env.BRANCH = 'EAEE'
     expect(getJobMatrix()).toMatchSnapshot()
   })
 })
