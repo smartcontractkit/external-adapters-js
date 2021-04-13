912e4caed65f74adda989466d4232ce7a03054f0
aalu1418 <aalu1418@gmail.com>
2021-04-13 14:46:31 -0400

    script modifications to add adapter to tsconfig and source adapters to legos

--- packages/scripts/src/new.ts
+++ packages/scripts/src/new.ts
@@ -18,14 +18,50 @@ function main() {
   const jq: string = shell.exec('command -v jq').toString()
   if (!jq) return log(red('jq is not installed'))
 
-  // copying files and adding to adapter lists
+  // copying files
   shell.mkdir(`packages/${type}s/${n}`)
   shell.cp('-R', `packages/examples/${type}/*`, `packages/${type}s/${n}`)
+
+  // add to yarn workflow
   shell
     .cat('.github/strategy/adapters.json')
     .exec(`jq '.${type}s.adapter += ["${n}"]'`)
     .exec('tee .github/strategy/adapters.json')
     .to('.github/strategy/adapters.json')
+
+  // add to packages/tsconfig.json
+  shell
+    .cat('packages/tsconfig.json')
+    .exec(`jq '.references += [{"path": "./${type}s/${n}"}]'`)
+    .exec('tee packages/tsconfig.json')
+    .to('packages/tsconfig.json')
+
+  // add to ea legos package for source adapters
+  if (type === 'source') {
+    const legos_path = 'packages/core/legos'
+
+    // update legos/tsconfig.json
+    shell
+      .cat(`${legos_path}/tsconfig.json`)
+      .exec(`jq '.references += [{"path": "../../${type}s/${n}"}]'`)
+      .exec(`tee ${legos_path}/tsconfig.json`)
+      .to(`${legos_path}/tsconfig.json`)
+
+    // update legos/package.json
+    shell
+      .cat(`${legos_path}/package.json`)
+      .exec(`jq '.dependencies += {"@chainlink/${n}-adapter": "*"}'`)
+      .exec(`tee ${legos_path}/package.json`)
+      .to(`${legos_path}/package.json`)
+
+    // updating loegs/src/sources.ts
+    let output = shell.cat(`${legos_path}/src/sources.ts`).split('\n')
+    const insert_index = output.indexOf('')
+    output.splice(insert_index, 0, `import * as ${n} from '@chainlink/${n}-adapter'`) // insert import statement
+    output.splice(-2, 0, `  ${n},`) // insert in the export default object
+  }
+
+  // editing adapter README
   shell
     .cat(`packages/${type}s/${n}/package.json`)
     .exec(
