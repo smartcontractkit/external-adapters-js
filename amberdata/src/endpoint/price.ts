08a8f45c0fb776bd1ef6082cb8991a3a4c18a24d
Brendon Van Essen <42727620+nzleet@users.noreply.github.com>
2021-04-16 01:03:19 +0800

    add cases to handle RGT and RARI

--- amberdata/src/endpoint/price.ts
+++ amberdata/src/endpoint/price.ts
@@ -40,7 +40,9 @@ export const execute: ExecuteWithConfig<Config> = async (input, config) => {
   if (
     includes.length > 0 &&
     ((includes[0].toLowerCase() === 'wbtc' && coin.toLowerCase() === 'digg') ||
-      (includes[0].toLowerCase() === 'weth' && coin.toLowerCase() === 'rai'))
+      (includes[0].toLowerCase() === 'weth' && coin.toLowerCase() === 'rai') || 
+        (includes[0].toLowerCase() === 'weth' && coin.toLowerCase() === 'rgt') ||
+          (includes[0].toLowerCase() === 'weth' && coin.toLowerCase() === 'rari'))
   ) {
     const fromAddress = addressMapping[coin.toUpperCase()]
     const toAddress = addressMapping[includes[0].toUpperCase()]
