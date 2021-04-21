5a684e5af2d80d9e52b71f183fe063d7e038ae55
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-21 19:37:10 -0400

    refactor: setup env generation for adapters based on tsconfig refs

--- packages/scripts/src/workspace.ts
+++ packages/scripts/src/workspace.ts
@@ -1,40 +1,64 @@
-import * as fs from 'fs'
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { readFileSync } from 'fs'
 import { join } from 'path'
 import * as s from 'shelljs'
+interface TsConfig {
+  references: { path: string }[]
+}
 
 export interface WorkspacePackage {
+  tsconf: TsConfig | undefined
   location: string
   name: string
   descopedName: string
   type: string
-  environment: string[]
+  environment: Record<string, string> | undefined
   version: string
 }
 
-export const VALID_ADAPTER_TYPES = ['composites', 'sources', 'examples', 'targets']
+/**
+ * Types of adapters that are publically consumed
+ */
+export const PUBLIC_ADAPTER_TYPES = ['composites', 'sources', 'examples', 'targets']
 const scope = '@chainlink/'
 
 export type WorkspacePackages = ReturnType<typeof getWorkspacePackages>
 export function getWorkspacePackages(additionalTypes: string[] = []): WorkspacePackage[] {
-  const adapterTypes = VALID_ADAPTER_TYPES.concat(additionalTypes)
+  const adapterTypes = PUBLIC_ADAPTER_TYPES.concat(additionalTypes)
   return s
     .exec('yarn workspaces list --json')
     .split('\n')
     .filter(Boolean)
     .map((v) => JSON.parse(v))
     .map(({ location, name }: WorkspacePackage) => {
-      const pkg: { version: string } = JSON.parse(
-        fs.readFileSync(join(location, 'package.json'), 'utf-8'),
-      )
+      const pkg: { version: string } = getJsonFile(join(location, 'package.json'))
+
+      let tsconf: TsConfig | undefined
+      try {
+        tsconf = getJsonFile(join(location, 'tsconfig.json'))
+      } catch {
+        console.warn(`${join(location, 'tsconfig.json')} does not exist`)
+      }
+
+      let environment: Record<string, string> | undefined
+      try {
+        environment = getJsonFile(join(location, 'env.json'))
+      } catch {
+        console.warn(`${join(location, 'env.json')} does not exist`)
+      }
 
       return {
+        environment,
+        tsconf,
         location,
         name,
         descopedName: name.replace(scope, ''),
         type: location.split('/')[1],
-        environment: [],
         version: pkg.version,
       }
     })
     .filter((v) => adapterTypes.includes(v.type))
+}
+function getJsonFile(path: string) {
+  return JSON.parse(readFileSync(path, 'utf-8'))
 }
