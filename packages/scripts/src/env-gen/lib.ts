2ab5a3b36d2aced8fc8d66b95e09c54f3191b794
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-22 00:16:36 -0400

    refactor: update env gen to create json schema env files
    BREAKING CHANGE: update env gen to create json schema env files

--- packages/scripts/src/env-gen/lib.ts
+++ packages/scripts/src/env-gen/lib.ts
@@ -1,17 +1,19 @@
 import { writeFileSync } from 'fs'
 import { join } from 'path'
+import { mkdir } from 'shelljs'
 import { getWorkspacePackages, PUBLIC_ADAPTER_TYPES, WorkspacePackage } from '../workspace'
 
 export function writeMergedEnvs(): void {
   const mergedEnvs = getMergedEnvs()
   mergedEnvs.forEach((x) => {
-    writeFileSync(join(x.location, 'env.generated.json'), JSON.stringify(x.mergedEnv, null, 1))
+    mkdir('-p', join(x.location, 'schemas'))
+    writeFileSync(join(x.location, 'schemas/env.json'), JSON.stringify(x.payload, null, 1))
   })
 }
 
 export function getMergedEnvs(): {
   location: string
-  mergedEnv: Record<string, string>
+  payload: any
 }[] {
   const packages = getWorkspacePackages(['core'])
   const refIgnore = [
@@ -39,20 +41,50 @@ export function getMergedEnvs(): {
       })
       .filter((p) => !refIgnore.includes(p.name))
 
-    const mergedEnv =
-      refPackages?.reduce((prev, next) => {
-        // sanity check
-        const prevKeys = Object.keys(prev)
-        const nextKeys = Object.keys(next)
-        prevKeys.forEach((k) => {
-          if (nextKeys.includes(k)) {
-            console.warn(`Env key collision found for: "${k}"`)
-          }
+    const mergedRefs = refPackages?.map((p) => {
+      return { $ref: makeId(p.descopedName) }
+    })
+
+    const env: Record<string, any> = p.environment ?? {}
+    const envKeys = Object.keys(env)
+    const requiredKeys = envKeys.filter((k) => env[k]?.required)
+    const newenv: Record<string, any> = {}
+    const allOf: any = []
+    for (const [k, v] of Object.entries(env)) {
+      newenv[k] = { ...v }
+      newenv[k].type = v.type || typeof v.default !== 'undefined' ? typeof v.default : 'string'
+      delete newenv[k]['required']
+      if (newenv[k].oneOf) {
+        allOf.push({
+          anyOf: newenv[k].oneOf.map((additional: any) => {
+            newenv[additional] = {
+              type: 'string',
+            }
+
+            return { required: [additional] }
+          }),
         })
+        delete newenv[k]
+      }
+      if (k.includes('URL')) {
+        newenv[k].format = 'uri'
+      }
+    }
 
-        return { ...prev, ...next.environment }
-      }, p.environment ?? {}) ?? {}
-
-    return { location: p.location, mergedEnv }
+    return {
+      location: p.location,
+      payload: {
+        $id: makeId(p.descopedName),
+        title: `${p.name} env var schema`,
+        required: requiredKeys,
+        type: 'object',
+        properties: newenv,
+        allOf: [...(mergedRefs || []), ...allOf],
+      },
+    }
   })
+}
+
+function makeId(path: string) {
+  return join('https://external-adapters.chainlinklabs.com/schemas', `${path}.json`)
 }
