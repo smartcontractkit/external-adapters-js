8d920a09ed6c1b686fa0094af880d7cc8a2577c1
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-04-26 16:09:48 +0200

    feat: coinapi handler declaration

--- packages/sources/coinapi/src/index.ts
+++ packages/sources/coinapi/src/index.ts
@@ -1,5 +1,5 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute } from './adapter'
-import { makeConfig, NAME } from './config'
+import { makeConfig, NAME, makeWSHandler } from './config'
 
-export = { NAME, makeExecute, makeConfig, ...expose(makeExecute()) }
+export = { NAME, makeExecute, makeConfig, ...expose(makeExecute(), makeWSHandler(makeConfig())) }
