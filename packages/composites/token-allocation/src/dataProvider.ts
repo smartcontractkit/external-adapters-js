72bb6ede11a6c0a521c378e33b2e03a9724405f3
Justin Kaseman <justinkaseman@live.com>
2021-04-26 18:46:18 -0600

    perf: bring back batch requests for price endpoint symbols of TA source adapters

--- packages/composites/token-allocation/src/dataProvider.ts
+++ packages/composites/token-allocation/src/dataProvider.ts
@@ -59,7 +59,6 @@ export const getPriceProvider = (
     })
     return Object.fromEntries(payloadEntries)
   }
-
   const results = await Promise.all(
     symbols.map(async (base) => {
       const data = {
