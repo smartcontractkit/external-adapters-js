29829156497cb9fa1114c05cf60af423c467dd05
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-04-27 21:15:30 -0400

    fix: address PR feedback

--- packages/scripts/src/schema-flatten/config.ts
+++ packages/scripts/src/schema-flatten/config.ts
@@ -1,36 +1,17 @@
+import { WorkspacePackage } from '../workspace'
+
 /**
  * Ignore any collisions based on the keys in the bootstrap package
  */
-export const collisionIgnoreMap = {
-  EXPERIMENTAL_METRICS_ENABLED: true,
-  METRICS_PORT: true,
-  METRICS_NAME: true,
-  EXPERIMENTAL_RATE_LIMIT_ENABLED: true,
-  RATE_LIMIT_CAPACITY: true,
-  RATE_LIMIT_API_PROVIDER: true,
-  RATE_LIMIT_API_TIER: true,
-  CACHE_ENABLED: true,
-  CACHE_TYPE: true,
-  CACHE_KEY_GROUP: true,
-  CACHE_KEY_IGNORED_PROPS: true,
-  EXPERIMENTAL_WARMUP_ENABLED: true,
-  WARMUP_UNHEALTHY_THRESHOLD: true,
-  WARMUP_SUBSCRIPTION_TTL: true,
-  CACHE_MAX_AGE: true,
-  CACHE_MAX_ITEMS: true,
-  CACHE_UPDATE_AGE_ON_GET: true,
-  CACHE_REDIS_HOST: true,
-  CACHE_REDIS_PORT: true,
-  CACHE_REDIS_PATH: true,
-  CACHE_REDIS_URL: true,
-  CACHE_REDIS_PASSWORD: true,
-  CACHE_REDIS_TIMEOUT: true,
-  REQUEST_COALESCING_ENABLED: true,
-  REQUEST_COALESCING_INTERVAL: true,
-  REQUEST_COALESCING_INTERVAL_MAX: true,
-  REQUEST_COALESCING_INTERVAL_COEFFICIENT: true,
-  REQUEST_COALESCING_ENTROPY_MAX: true,
-} as const
+export function getCollisionIgnoreMapFrom(pkg: WorkspacePackage): Record<string, true> {
+  return Object.keys(pkg?.environment?.properties ?? {}).reduce<Record<string, true>>(
+    (prev, next) => {
+      prev[next] = true
+      return prev
+    },
+    {},
+  )
+}
 
 /**
  * Always rename adapter specific things such as "API_KEY"
