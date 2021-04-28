d04ca0bdbe8ff81f235fae1f68298d24bf48da66
aalu1418 <aalu1418@gmail.com>
2021-04-28 16:38:11 -0400

    refactor(intrinio): code refactor for intrinio adapter

--- packages/sources/intrinio/src/config.ts
+++ packages/sources/intrinio/src/config.ts
@@ -1,36 +1,65 @@
-import { util } from '@chainlink/ea-bootstrap'
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { WSSubscriptionHandler, Config } from '@chainlink/types'
+import { customParams } from './adapter'
+var IntrinioRealtime = require('intrinio-realtime')
 
 /**
  * @swagger
  * securityDefinitions:
  *  environment-variables:
- *    API_PROVIDER:
- *      required: false
  *    API_KEY:
- *      required: true
- *    SYMBOLS:
  *      required: true
  */
 
 export const NAME = 'INTRINIO'
 
-export const PROVIDER_OPTIONS = ['iex', 'quodd', 'fxcm']
-
-export type Config = {
-  key: string
-  symbols: string
-  provider: string
-}
+export const DEFAULT_API_ENDPOINT = 'https://api-v2.intrinio.com/'
+// const DEFAULT_WS_API_ENDPOINT = ''
 
 export const makeConfig = (prefix?: string): Config => {
-  let provider = util.getEnv('API_PROVIDER', prefix) || ''
-  if (!PROVIDER_OPTIONS.includes(provider)) {
-    provider = PROVIDER_OPTIONS[0]
-  }
+  const config = Requester.getDefaultConfig(prefix, true)
+  config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
+  return config
+}
 
+export const makeWSHandler = (config: Config): WSSubscriptionHandler => {
+  // https://github.com/intrinio/intrinio-realtime-node-sdk
+  const ws = new IntrinioRealtime({
+    api_key: config.apiKey,
+    provider: 'iex',
+  })
   return {
-    key: util.getRequiredEnv('API_KEY', prefix),
-    symbols: util.getRequiredEnv('SYMBOLS', prefix),
-    provider: provider.toLowerCase(),
+    init: obj =>
+      new Promise(async (res, rej) => {
+        try {
+          await ws._refreshToken()
+          obj.connection.url = ws._makeSocketUrl()
+          res(0)
+        } catch (e) {
+          rej()
+        }
+      }),
+    connection: {
+      url: '',
+    },
+    subscribe: input => {
+      const validator = new Validator(input, customParams)
+      if (validator.error) {
+        return
+      }
+      const base = validator.overrideSymbol(NAME).toUpperCase()
+      return ws._makeJoinMessage(base)
+    },
+    unsubscribe: () => '',
+    subsFromMessage: message => ws._makeJoinMessage(message.payload.ticker),
+    isError: (message: any) => Number(message.TYPE) > 400 && Number(message.TYPE) < 900,
+    filter: message => {
+      console.log('filter', message)
+      return message.event == 'quote' && message.payload?.type == 'last'
+    },
+    parse: (wsResponse: any): number => {
+      console.log('parse', wsResponse)
+      return Number(wsResponse?.payload?.price)
+    },
   }
 }
