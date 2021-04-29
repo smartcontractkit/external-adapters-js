c6234f099c90d87325f08069b51515326e54c16b
Justin Kaseman <justinkaseman@live.com>
2021-04-29 11:33:56 -0600

    fix: Intrinio needs all handlers, not just server

--- intrinio/src/index.ts
+++ intrinio/src/index.ts
@@ -1,19 +1,10 @@
-import * as bootstrap from '@chainlink/ea-bootstrap'
+import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, startService } from './adapter'
-import { AdapterRequest, Execute, ExecuteSync } from '@chainlink/types'
-import { Requester } from '@chainlink/external-adapter'
-import { makeConfig } from './config'
+import { makeConfig, NAME } from './config'
 
-// Execution helper async => sync
-const executeSync = (execute: Execute): ExecuteSync => {
-  return (data: AdapterRequest, callback: any) => {
-    return execute(data)
-      .then((result) => callback(result.statusCode, result))
-      .catch((error) => callback(error.statusCode || 500, Requester.errored(data.id, error)))
-  }
-}
-
-export const server = (): void => {
+const makeExecuteWS = () => {
   startService(makeConfig())
-  bootstrap.server.initHandler(executeSync(makeExecute()))()
+  return makeExecute()
 }
+
+export = { NAME, makeExecute, makeConfig, ...expose(makeExecuteWS()) }
