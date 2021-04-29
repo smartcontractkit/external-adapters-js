c37bce36f9010b0dea7823cfdae3d1c6967caa89
RodrigoAD <15104916+RodrigoAD@users.noreply.github.com>
2021-04-29 11:46:21 +0200

    refactor: coinapi ws factory

--- packages/sources/coinapi/src/config.ts
+++ packages/sources/coinapi/src/config.ts
@@ -1,5 +1,5 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
-import { Config, WSSubscriptionHandler } from '@chainlink/types'
+import { Config, MakeWSHandler } from '@chainlink/types'
 import * as endpoint from './endpoint'
 
 /**
@@ -28,35 +28,35 @@ export const makeConfig = (prefix?: string): Config => {
   return config
 }
 
-export const makeWSHandler = (config: Config): WSSubscriptionHandler => {
-  // TODO: Fix. This subscribes to the channels related to any of the products. This will receive indefinite number of messages
-  const getSubscription = (products: string[]) =>  ({
-    type: 'hello',
-    apikey: config.apiKey,
-    heartbeat: false,
-    subscribe_data_type: ['exrate'],
-    subscribe_filter_asset_id: products
-  }) 
-  return {
-    connection: {
-      url: config.api.baseWsURL || DEFAULT_WS_API_ENDPOINT
-    },
-    subscribe: (input) => {
-      const validator = new Validator(input, endpoint.price.customParams)
-      if (validator.error) {
-        return 
+export const makeWSHandler = (config?: Config): MakeWSHandler => () => {
+    const defaultConfig = config || makeConfig()
+    const getSubscription = (products: string[]) =>  ({
+      type: 'hello',
+      apikey: defaultConfig.apiKey,
+      heartbeat: false,
+      subscribe_data_type: ['exrate'],
+      subscribe_filter_asset_id: products
+    })
+    return {
+      connection: {
+        url: defaultConfig.api.baseWsURL || DEFAULT_WS_API_ENDPOINT
+      },
+      subscribe: (input) => {
+        const validator = new Validator(input, endpoint.price.customParams)
+        if (validator.error) {
+          return 
+        }
+        const base = validator.overrideSymbol(NAME).toLowerCase()
+        const quote = validator.validated.data.quote.toLowerCase()
+        return getSubscription([base, quote])
+      },
+      unsubscribe: () => '',
+      subsFromMessage: (message) => getSubscription([message.asset_id_base, message.asset_id_quote]),
+      isError: () => false,
+      filter: (message) => message?.type === 'exrate',
+      parse: (wsResponse: any): number => {
+        const result = Requester.validateResultNumber(wsResponse, ['rate'])
+        return result
       }
-      const base = validator.overrideSymbol(NAME).toLowerCase()
-      const quote = validator.validated.data.quote.toLowerCase()
-      return getSubscription([base, quote])
-    },
-    unsubscribe: () => '',
-    subsFromMessage: (message) => getSubscription([message.asset_id_base, message.asset_id_quote]),
-    isError: () => false,
-    filter: (message) => message?.type === 'exrate',
-    parse: (wsResponse: any): number => {
-      const result = Requester.validateResultNumber(wsResponse, ['rate'])
-      return result
     }
   }
-}
