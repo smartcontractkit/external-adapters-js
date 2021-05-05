4373002650e25026d76efddffbb4293fd2ff5340
aalu1418 <aalu1418@gmail.com>
2021-05-05 12:59:10 -0400

    fix(tradingeconomics): fix missing overrides for websocket

--- packages/sources/tradingeconomics/src/adapter.ts
+++ packages/sources/tradingeconomics/src/adapter.ts
@@ -6,20 +6,12 @@ export const customParams = {
   base: ['base', 'from', 'asset'],
 }
 
-const commonSymbols: { [symbol: string]: string } = {
-  N225: 'NKY:IND',
-  FTSE: 'UKX:IND',
-}
-
 export const execute = async (input: AdapterRequest, config: Config) => {
   const validator = new Validator(input, customParams)
   if (validator.error) throw validator.error
 
   const jobRunID = validator.validated.id
-  let symbol = validator.validated.data.base.toUpperCase()
-  if (symbol in commonSymbols) {
-    symbol = commonSymbols[symbol]
-  }
+  const symbol = validator.overrideSymbol(NAME).toUpperCase()
 
   // Fall back to getting the data from HTTP endpoint
   const url = `/symbol/${symbol}`
