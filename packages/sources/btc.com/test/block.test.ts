b72f2e778d2d78b6125b7c9fed54933eb76c540f
Justin Kaseman <justinkaseman@live.com>
2021-05-07 16:20:16 -0600

    feat: Add block endpoint for BTC.com that supports height and difficulty

---
+++ packages/sources/btc.com/test/block.test.ts
@@ -0,0 +1,88 @@
+import { assert } from 'chai'
+import { Requester } from '@chainlink/external-adapter'
+import { assertSuccess, assertError } from '@chainlink/adapter-test-helpers'
+import { AdapterRequest } from '@chainlink/types'
+import { makeExecute } from '../src/adapter'
+
+describe('execute', () => {
+  const jobID = '1'
+  const execute = makeExecute()
+
+  context('successful calls @integration', () => {
+    const requests = [
+      {
+        name: 'empty data',
+        testData: { data: {} },
+      },
+      {
+        name: 'id not supplied (endpoint height)',
+        testData: {
+          data: {
+            endpoint: 'height',
+          },
+        },
+      },
+      {
+        name: 'endpoint difficulty',
+        testData: {
+          id: jobID,
+          data: {
+            endpoint: 'difficulty',
+          },
+        },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        const data = await execute(req.testData as AdapterRequest)
+        assertSuccess({ expected: 200, actual: data.statusCode }, data, jobID)
+        assert.isAbove(data.result, 0)
+        assert.isAbove(data.data.result, 0)
+      })
+    })
+  })
+
+  context('validation error', () => {
+    const requests = [
+      {
+        name: 'empty body',
+        testData: {},
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, error)
+          assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+
+  context('error calls @integration', () => {
+    const requests = [
+      {
+        name: 'unknown endpoint',
+        testData: {
+          id: jobID,
+          data: { endpoint: 'not_real' },
+        },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, error)
+          assertError({ expected: 400, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+})
