88c092a1473399d79fa074ba333f662b341a6911
Justin Kaseman <justinkaseman@live.com>
2021-05-08 09:15:09 -0400

    feat: add crypto endpoint to tiingo adapter (#510)

--- packages/sources/tiingo/src/endpoint/index.ts
+++ packages/sources/tiingo/src/endpoint/index.ts
@@ -1,2 +1,3 @@
 export * as eod from './eod'
 export * as iex from './iex'
+export * as top from './crypto/top'
---
+++ tiingo/src/endpoint/crypto/top.ts
@@ -0,0 +1,68 @@
+import { Requester, Validator } from '@chainlink/external-adapter'
+import { ExecuteWithConfig, Config } from '@chainlink/types'
+
+export const NAME = 'top'
+
+export interface ResponseSchema {
+  ticker: string
+  baseCurrency: string
+  quoteCurrency: string
+  topOfBookData: {
+    askSize: number
+    bidSize: number
+    lastSaleTimestamp: string
+    lastPrice: number
+    askPrice: number
+    quoteTimestamp: string
+    bidExchange: string
+    lastSizeNotional: number
+    lastExchange: string
+    askExchange: string
+    bidPrice: number
+    lastSize: number
+  }[]
+}
+
+const customParams = {
+  base: ['base', 'from', 'coin'],
+  quote: ['quote', 'to', 'market'],
+  field: false,
+}
+
+// When an invalid symbol is given the response body is empty
+const customError = (data: ResponseSchema[]) => !data.length
+
+export const execute: ExecuteWithConfig<Config> = async (request, config) => {
+  const validator = new Validator(request, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const base = validator.validated.data.base.toLowerCase()
+  const quote = validator.validated.data.quote.toLowerCase()
+  const field = validator.validated.data.field || 'lastPrice'
+
+  const url = '/tiingo/crypto/top'
+
+  const options = {
+    ...config.api,
+    params: {
+      token: config.apiKey,
+      tickers: base + quote,
+    },
+    url,
+  }
+
+  const response = await Requester.request(options, customError)
+  const result = Requester.validateResultNumber(response.data as ResponseSchema[], [
+    0,
+    'topOfBookData',
+    0,
+    field,
+  ])
+
+  return Requester.success(jobRunID, {
+    data: config.verbose ? { ...response.data, result } : { result },
+    result,
+    status: 200,
+  })
+}
