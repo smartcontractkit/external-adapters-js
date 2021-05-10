cb4e60e402e9ee122625e4b3e44da8021418ac3d
aalu1418 <aalu1418@gmail.com>
2021-05-10 15:00:23 -0400

    feat(logger): redact sensitive data at logger level

--- packages/core/bootstrap/src/lib/external-adapter/logger.ts
+++ packages/core/bootstrap/src/lib/external-adapter/logger.ts
@@ -1,23 +1,30 @@
-import { v4 as uuidv4 } from 'uuid'
 import { createLogger, format, transports } from 'winston'
+import { uuid, redact } from '../util'
 const { combine, timestamp, json, prettyPrint } = format
 
-// We generate an UUID per instance and add it to the logs
-const uuid = () => {
-  if (!process.env.UUID) process.env.UUID = uuidv4()
-  return process.env.UUID
-}
-
-const instanceId = format((info) => {
+const instanceId = format(info => {
   if (!info.instanceId) info.instanceId = uuid()
   return info
+})
+
+// https://github.com/winstonjs/winston/issues/1079#issuecomment-729050088
+const redactData = format(info => {
+  const result = redact(info)
+
+  const levelSym = Symbol.for('level')
+  const splatSym = Symbol.for('splat')
+
+  result[levelSym] = info[(levelSym as unknown) as string]
+  result[splatSym] = info[(splatSym as unknown) as string]
+
+  return result
 })
 
 export const logger = createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format:
     process.env.NODE_ENV === 'development'
-      ? combine(instanceId(), timestamp(), json(), prettyPrint())
-      : combine(instanceId(), timestamp(), json()),
+      ? combine(instanceId(), timestamp(), redactData(), json(), prettyPrint())
+      : combine(instanceId(), timestamp(), redactData(), json()),
   transports: [new transports.Console()],
 })
