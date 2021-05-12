92a4acc3f2f5094c9db6a3feb9c0fdcded8b89f9
Jonas Hals <jonas@smartcontract.com>
2021-05-12 22:58:53 +0200

    fix(augur): treat `0.0001` as a special case

--- packages/composites/augur/src/methods/createMarkets.ts
+++ packages/composites/augur/src/methods/createMarkets.ts
@@ -63,10 +63,10 @@ export const execute: ExecuteWithConfig<Config> = async (input, config) => {
     const [headToHeadMarket, spreadMarket, totalScoreMarket]: [ethers.BigNumber, ethers.BigNumber, ethers.BigNumber] = await contract.getEventMarkets(eventId)
 
     // only create spread and totalScore markets if lines exist; always create headToHead market
-    let homeSpread = event.lines?.[affiliateId].spread.point_spread_home
-    let totalScore = event.lines?.[affiliateId].total.total_over
-    const createSpread = !!homeSpread
-    const createTotalScore = !!totalScore
+    let homeSpread = transformSpecialNone(event.lines?.[affiliateId].spread.point_spread_home)
+    let totalScore = transformSpecialNone(event.lines?.[affiliateId].total.total_over)
+    const createSpread = homeSpread !== undefined
+    const createTotalScore = totalScore !== undefined
     homeSpread = homeSpread || 0
     totalScore = totalScore || 0
     const canCreate = headToHeadMarket.isZero() || (spreadMarket.isZero() && createSpread) || (totalScoreMarket.isZero() && createTotalScore)
@@ -82,6 +82,8 @@ export const execute: ExecuteWithConfig<Config> = async (input, config) => {
 
   return Requester.success(input.id, {})
 }
+
+const transformSpecialNone = (val?: number) => val === 0.0001 ? undefined : val
 
 export const packCreation = (
   eventId: string,
