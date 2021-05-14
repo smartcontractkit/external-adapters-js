2ec00bd9a9c228fc5188a5b5a7d31280708f817b
Justin Kaseman <justinkaseman@live.com>
2021-05-14 15:20:04 -0400

    feat(tiingo): add crypto WS handler (#532)
    * feat(tiingo): add crypto WS handler
    
    * refactor(tiingo): move defaultConfig inside return function

--- packages/sources/tiingo/src/config.ts
+++ packages/sources/tiingo/src/config.ts
@@ -1,8 +1,11 @@
 import { Requester } from '@chainlink/ea-bootstrap'
 import { Config } from '@chainlink/types'
 
+export const NAME = 'TIINGO'
+
 export const DEFAULT_ENDPOINT = 'crypto'
 export const DEFAULT_BASE_URL = 'https://api.tiingo.com'
+export const DEFAULT_WS_API_ENDPOINT = 'wss://api.tiingo.com/crypto'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix, true)
