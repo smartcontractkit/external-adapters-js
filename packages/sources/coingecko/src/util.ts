e2c8149a6e165e663afdf1bc4991b84fc018dca0
Justin Kaseman <justinkaseman@live.com>
2021-05-14 19:10:36 -0600

    chore: replay commit for ID overrides in coingecko and coinpaprika adapters

--- packages/sources/coingecko/src/util.ts
+++ packages/sources/coingecko/src/util.ts
@@ -2,6 +2,16 @@ import { makeExecute } from './adapter'
 import { executeSync } from '@chainlink/ea-bootstrap'
 import { CoinsResponse } from './endpoint/coins'
 
+const coingeckoBlacklist = [
+  'leocoin',
+  'farmatrust',
+  'freetip',
+  'compound-coin',
+  'uni-coin',
+  'unicorn-token',
+  'kyber-network-crystal', // TEMP blacklisted due to no volume
+]
+
 export function getCoinIds(id: string): Promise<CoinsResponse[]> {
   const execute = makeExecute()
   const executeWithMiddleware = executeSync(execute)
@@ -27,12 +37,21 @@ export const getSymbolsToIds = (
   symbols: string[],
   coinList: CoinsResponse[],
 ): Record<string, string> => {
-  const idToSymbol: Record<string, string> = {}
+  const idToSymbol: Record<string, string> = {
+    // Pre-set IDs here
+    'kyber-network': 'KNC',
+  }
+
   symbols.forEach((symbol) => {
-    const coin = coinList.find((d) => d.symbol.toLowerCase() === symbol.toLowerCase())
+    const coin = coinList.find(
+      (d) =>
+        d.symbol.toLowerCase() === symbol.toLowerCase() &&
+        !coingeckoBlacklist.includes(d.id.toLowerCase()),
+    )
     if (coin && coin.id) {
       idToSymbol[coin.id] = symbol
     }
   })
+
   return idToSymbol
 }
--- packages/sources/coinpaprika/src/util.ts
+++ packages/sources/coinpaprika/src/util.ts
@@ -23,11 +23,24 @@ export function getCoinIds(id: string): Promise<CoinsResponse[]> {
   })
 }
 
+/**
+ * A map containing IDs that are lower in rank,
+ * but should take precedence over higher ranked coins
+ *
+ * NOTE: this is a kludge that will be removed with overrides improvement
+ */
+const directIds: { [key: string]: string } = {
+  cream: 'cream-cream',
+}
+
 export const getSymbolToId = (symbol: string, coinList: CoinsResponse[]): string => {
+  if (directIds[symbol.toLowerCase()]) return directIds[symbol.toLowerCase()]
+
   const coin = coinList.find(
     ({ symbol: coinSymbol, rank }) =>
       coinSymbol.toLowerCase() === symbol.toLowerCase() && rank !== 0,
   )
   if (coin && coin.id) return coin.id.toLowerCase()
+
   throw new Error('Coin id not found')
 }
