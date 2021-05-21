a89edc3bfb7c3c04c223a3340ad5268b83cd54ac
Silas Lenihan <sjl58@duke.edu>
2021-05-21 15:34:49 -0400

    refactor(to refactor and update the old bob adapter): updated Bob adapter

--- packages/composites/bob/src/config.ts
+++ packages/composites/bob/src/config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { util } from '@chainlink/ea-bootstrap'
 
 export type Config = {
@@ -12,3 +13,12 @@ export const makeConfig = (): Config => {
         RPC_URL: util.getRequiredEnv('RPC_URL')
     }
 }
+=======
+import { Requester } from '@chainlink/ea-bootstrap'
+import { Config } from '@chainlink/types'
+
+export const DEFAULT_ENDPOINT = 'difficulty'
+
+// TODO: needs to setup config for underlying JSON-RPC adapter
+export const makeConfig = (prefix?: string): Config => Requester.getDefaultConfig(prefix)
+>>>>>>> refactor(to refactor and update the old bob adapter): updated Bob adapter
