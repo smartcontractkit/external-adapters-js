44bd958a45ec7c38d9748b77c178fb7c03b40b45
Silas Lenihan <sjl58@duke.edu>
2021-05-24 14:17:20 -0400

    refactor: refactored Bob external adapter and cleaned up code

--- packages/composites/bob/src/adapter.ts
+++ packages/composites/bob/src/adapter.ts
@@ -1,52 +1,32 @@
-import JSONRPC from '@chainlink/json-rpc-adapter'
-import { Config, ExecuteWithConfig, ExecuteFactory, AdapterRequest } from '@chainlink/types'
-import { Validator, Requester } from '@chainlink/ea-bootstrap'
-import { DEFAULT_ENDPOINT, makeConfig } from './config'
-
-const { ethers } = require('ethers')
+import { Execute, AdapterResponse, AdapterRequest } from '@chainlink/types'
+import { Validator, AdapterError } from '@chainlink/ea-bootstrap'
+import { DEFAULT_ENDPOINT } from './config'
+import { format } from './endpoint'
 
 const inputParams = {
-  url: false,
-  chainId: true,
-  blockNumber: true
-}
-
-const convertEndpoint: { [key: string]: string } = {
-  height: 'headers',
+  endpoint: false
 }
 
 // Export function to integrate with Chainlink node
-const execute: ExecuteWithConfig<Config> = async (request: AdapterRequest) => {
+export const execute = async (request: AdapterRequest): Promise<AdapterResponse> => {
   const validator = new Validator(request, inputParams)
   if (validator.error) throw validator.error
-  const url = process.env.RPC_URL || validator.validated.data.url || 'http://localhost:8545'
-  const provider = new ethers.providers.JsonRpcProvider(url)
   const jobRunID = validator.validated.id
-  
-  const chainId = validator.validated.data.chainId
-  const blockNumber = validator.validated.data.blockNumber
-
-  const data = {
-    id: jobRunID,
-    jsonrpc: '2.0',
-    method: 'eth_getBlockByHash'
+  const endpoint = validator.validated.data.endpoint || DEFAULT_ENDPOINT
+  switch (endpoint.toLowerCase()) {
+    case format.NAME: {
+      return format.execute(request)
+    }
+    default: {
+      throw new AdapterError({
+        jobRunID,
+        message: `Endpoint ${endpoint} not supported.`,
+        statusCode: 400,
+      })
+    }
   }
-
-  const block = await provider.getBlock(blockNumber)
-
-  let response = await JSONRPC.execute({
-    ...request,
-    data: { ...request.data, method: 'eth_getBlockByHash', params: [block.hash, false] },
-  })
-  const coder = new ethers.utils.AbiCoder()
-  response.data.result = coder.encode(
-    ['uint8', 'bytes32', 'bytes32'],
-    [chainId, response.data.result.hash, response.data.result.receiptsRoot]
-  )
-  response.data.result = response.data.result.slice(2)
-  return Requester.success(jobRunID, response)
 }
 
-export const makeExecute: ExecuteFactory<Config> = (config) => {
-  return async (request) => execute(request, config || makeConfig())
+export const makeExecute = (): Execute => {
+  return async (request: AdapterRequest) => execute(request)
 }
--- packages/composites/bob/src/config.ts
+++ packages/composites/bob/src/config.ts
@@ -1,7 +1,2 @@
-import { Requester } from '@chainlink/ea-bootstrap'
-import { Config } from '@chainlink/types'
-
-export const DEFAULT_ENDPOINT = 'difficulty'
-
-// TODO: needs to setup config for underlying JSON-RPC adapter
-export const makeConfig = (prefix?: string): Config => Requester.getDefaultConfig(prefix)
+export const DEFAULT_RPC_URL = 'http://localhost:8545'
+export const DEFAULT_ENDPOINT = 'format'
