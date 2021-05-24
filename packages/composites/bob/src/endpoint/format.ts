08041bf10f9387977202f928f02d0cb7b193c544
Silas Lenihan <sjl58@duke.edu>
2021-05-24 14:17:20 -0400

    refactor: refactored Bob external adapter and cleaned up code

---
+++ packages/composites/bob/src/endpoint/format.ts
@@ -0,0 +1,38 @@
+import JSONRPC from '@chainlink/json-rpc-adapter'
+import { Config, ExecuteWithConfig, AdapterRequest } from '@chainlink/types'
+import { Validator, Requester } from '@chainlink/ea-bootstrap'
+import { DEFAULT_RPC_URL } from '../config'
+import { ethers } from 'ethers'
+
+export const NAME = 'format'
+
+export const inputParams = {
+    url: false,
+    chainId: true,
+    blockNumber: true
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request: AdapterRequest) => {
+    const validator = new Validator(request, inputParams)
+    if (validator.error) throw validator.error
+    const url = process.env.RPC_URL || validator.validated.data.url || DEFAULT_RPC_URL
+    const provider = new ethers.providers.JsonRpcProvider(url)
+    const jobRunID = validator.validated.id
+
+    const chainId = validator.validated.data.chainId
+    const blockNumber = validator.validated.data.blockNumber
+
+    const block = await provider.getBlock(blockNumber)
+
+    const response = await JSONRPC.execute({
+      ...request,
+      data: { ...request.data, method: 'eth_getBlockByHash', params: [block.hash, false] },
+    })
+    const coder = new ethers.utils.AbiCoder()
+    response.data.result = coder.encode(
+      ['uint8', 'bytes32', 'bytes32'],
+      [chainId, response.data.result.hash, response.data.result.receiptsRoot]
+    )
+    response.data.result = response.data.result.slice(2)
+    return Requester.success(jobRunID, response)
+  }
