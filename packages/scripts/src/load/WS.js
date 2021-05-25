254fd01232c059ab1b023b85763bf37e783bd9bd
Justin Kaseman <justinkaseman@live.com>
2021-05-24 22:01:13 -0700

    chore(scripts): add k6 12 soak test

---
+++ packages/scripts/src/load/WS.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    name: 'BTC/USD',
+    id: '86f45dcc-90db-4c39-b385-53945c5a9a30',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "BTC",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'ETH/USD',
+    id: 'a16f830b-f51c-4c5f-8a00-772c8f0715e5',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "ETH",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'LINK/USD',
+    id: 'ef3d128d-6b9a-4606-bd97-21d276bc24db',
+    method: 'POST',
+    data: '{\n    "data": {\n        "from": "LINK",\n        "to": "USD"\n    }\n}',
+  },
+
+  {
+    name: 'DOT/USD',
+    id: 'e9d122be-d93b-49d2-8a68-d14124122510',
+    method: 'POST',
+    data: '{\n "data": {\n  "base": "DOT",\n  "quote": "USD"\n }\n}',
+  },
+
+  {
+    name: 'SNX/USD',
+    id: 'a8a5783f-fdee-4afd-8092-701ec38753e7',
+    method: 'POST',
+    data: '{\n "data": {\n  "base": "SNX",\n  "quote": "USD"\n }\n}',
+  },
+
+  {
+    name: 'AAVE/USD',
+    id: '4b2c326a-e5b1-4d98-97ea-c20e95130dce',
+    method: 'POST',
+    data: '{\n "data": {\n  "base": "AAVE",\n  "quote": "USD"\n }\n}',
+  },
+
+  {
+    name: 'CRV/USD',
+    id: 'f52e9b76-1183-4f72-9194-fe1e768236b9',
+    method: 'POST',
+    data: '{\n "data": {\n  "base": "CRV",\n  "quote": "USD"\n }\n}',
+  },
+
+  {
+    name: 'AVAX/USD',
+    id: 'c08f6752-9490-44cc-818a-03f1070ad144',
+    method: 'POST',
+    data: '{\n "data": {\n  "base": "AVAX",\n  "quote": "USD"\n }\n}',
+  },
+
+  {
+    name: 'UMA/USD',
+    id: 'de05d7ac-62d8-46ae-a4d3-711b6b24f3b6',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "UMA",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'DOGE/USD ',
+    id: 'd8d4695c-523b-443a-afcd-cfb03b41b957',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "DOGE",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: '1INCH/USD',
+    id: '4b9b791a-5dca-4129-b04e-6f47df1d505c',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "1INCH",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'SUSHI/USD',
+    id: 'f8bf7125-910d-4f88-914b-ba3ad11afb57',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "SUSHI",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'UNI/USD',
+    id: '7cded783-64f4-4227-95e5-e91af1e980a1',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "UNI",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'YFI/USD',
+    id: '4d813f3a-3777-4603-8a01-c35733a2bf5e',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "YFI",\n  "to": "USD"\n }\n}',
+  },
+
+  {
+    name: 'SOL/USD',
+    id: 'dd48f78c-98a7-4845-a8e0-ad947b0bae18',
+    method: 'POST',
+    data: '{\n "data": {\n  "from": "SOL",\n  "to": "USD"\n }\n}',
+  },
+]
---
+++ packages/scripts/src/load/config.js
@@ -0,0 +1,5 @@
+const GROUP_COUNT = 4
+
+const ADAPTERS = ['amberdata', 'cryptocompare', 'tiingo']
+
+export default { GROUP_COUNT, ADAPTERS }
