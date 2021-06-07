2249fdbc9501d1dfc7dba333c4b90707dd895c32
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-06-07 19:32:10 -0400

    fix epic's dispatching of actions

--- packages/core/bootstrap/src/lib/cache-warmer/util.ts
+++ packages/core/bootstrap/src/lib/cache-warmer/util.ts
@@ -1,9 +1,11 @@
-import hash from 'object-hash'
-import { WarmupSubscribedPayload } from './actions'
-import { get } from './config'
 import { omit } from 'lodash'
+import hash from 'object-hash'
+import { WarmupExecutePayload, WarmupSubscribedPayload } from './actions'
+import { get } from './config'
 
 const conf = get()
-export function getSubscriptionKey(request: WarmupSubscribedPayload): string {
+export function getSubscriptionKey(
+  request: WarmupSubscribedPayload | WarmupExecutePayload,
+): string {
   return hash(omit(request, ['parent', 'children']), conf.hashOpts)
 }
