3ec849cfe00b52a87623c1ce9cb56b82a3d4756b
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-06-07 16:37:31 -0400

    Allow rate adjustment independent of vus (#596)
    * Allow rate adjustment independent of vus
    
    * Reconfigure tests
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

--- packages/k6/src/config/index.ts
+++ packages/k6/src/config/index.ts
@@ -1,5 +1,18 @@
 export * from './http'
 export * from './ws'
 
-export const GROUP_COUNT = 4
-export const ADAPTERS = ['amberdata', 'cryptocompare', 'tiingo']
+export const GROUP_COUNT = 1
+
+export type AdapterNames = 'nomics' | 'cryptocompare' | 'tiingo'
+interface AdapterConfig {
+  name: AdapterNames
+  /**
+   * How many seconds to wait inbetween each call to this adapter
+   */
+  secondsPerCall: number
+}
+export const ADAPTERS: AdapterConfig[] = [
+  { name: 'nomics', secondsPerCall: 10 },
+  { name: 'cryptocompare', secondsPerCall: 1 },
+  { name: 'tiingo', secondsPerCall: 5 },
+]
