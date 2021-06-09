2649f843b610dd823b984560245b46a1e6627790
Jonas Hals <contact@jonashals.me>
2021-06-09 16:22:18 +0200

    Support Pro API keys on Coingecko (#603)

--- packages/sources/coingecko/src/config.ts
+++ packages/sources/coingecko/src/config.ts
@@ -5,9 +5,12 @@ export const NAME = 'COINGECKO'
 
 export const DEFAULT_ENDPOINT = 'price'
 export const DEFAULT_API_ENDPOINT = 'https://api.coingecko.com/api/v3'
+export const PRO_API_ENDPOINT = 'https://pro-api.coingecko.com/api/v3'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
-  config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
+  if (!config.api.baseURL) {
+    config.api.baseURL = config.apiKey !== undefined ? PRO_API_ENDPOINT : DEFAULT_API_ENDPOINT
+  }
   return config
 }
