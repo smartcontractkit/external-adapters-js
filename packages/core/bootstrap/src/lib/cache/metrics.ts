56d543e7846c912db3a67e9df35bad20a259a61f
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-06-15 18:04:18 -0400

    Reduce buckets for cache execution duration

--- packages/core/bootstrap/src/lib/cache/metrics.ts
+++ packages/core/bootstrap/src/lib/cache/metrics.ts
@@ -66,7 +66,7 @@ const cache_execution_duration_seconds = new client.Histogram({
   name: 'cache_execution_duration_seconds',
   help: 'A histogram bucket of the distribution of cache execution durations',
   labelNames: [...baseLabels, 'cache_hit'] as const,
-  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
+  buckets: [0.01, 0.1, 1, 10],
 })
 
 const cache_data_get_count = new client.Counter({
