4b86fe9293b4ce567996be8284ac3144c367c7d1
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-06-24 06:28:24 -0700

    export ResponseSchema interface from tiingo adapter

--- packages/sources/tiingo/src/index.ts
+++ packages/sources/tiingo/src/index.ts
@@ -1,6 +1,7 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler, endpointSelector } from './adapter'
 import { NAME, makeConfig } from './config'
+import { ResponseSchema } from './endpoint/crypto/top'
 
 const { server } = expose(makeExecute(), makeWSHandler())
-export { NAME, makeExecute, makeConfig, server }
+export { NAME, makeExecute, makeConfig, server, ResponseSchema }
