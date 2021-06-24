0ca84aff42229ae746ccbd65cbf47538a386c698
Justin Kaseman <justinkaseman@live.com>
2021-06-24 14:04:18 -0700

    fix(apy-finance): fix syntax

--- packages/composites/apy-finance/test-payload.js
+++ packages/composites/apy-finance/test-payload.js
@@ -1,16 +1,16 @@
 const environmentVariables = [
-  {envKey: 'AMBERDATA_DATA_PROVIDER_URL', defaultValue: 'amberdata'},
-  {envKey: 'COINAPI_DATA_PROVIDER_URL', defaultValue: 'coinapi'},
-  {envKey: 'COINGECKO_DATA_PROVIDER_URL', defaultValue: 'coingecko'},
-  {envKey: 'COINMARKETCAP_DATA_PROVIDER_URL', defaultValue: 'coinmarketcap'},
-  {envKey: 'COINPAPRIKA_DATA_PROVIDER_URL', defaultValue: 'coinpaprika'},
-  {envKey: 'CRYPTOCOMPARE_DATA_PROVIDER_URL', defaultValue: 'cryptocompare'},
-  {envKey: 'KAIKO_DATA_PROVIDER_URL', defaultValue: 'kaiko'},
-  {envKey: 'NOMICS_DATA_PROVIDER_URL', defaultValue: 'nomics'},
+  { envKey: 'AMBERDATA_DATA_PROVIDER_URL', defaultValue: 'amberdata' },
+  { envKey: 'COINAPI_DATA_PROVIDER_URL', defaultValue: 'coinapi' },
+  { envKey: 'COINGECKO_DATA_PROVIDER_URL', defaultValue: 'coingecko' },
+  { envKey: 'COINMARKETCAP_DATA_PROVIDER_URL', defaultValue: 'coinmarketcap' },
+  { envKey: 'COINPAPRIKA_DATA_PROVIDER_URL', defaultValue: 'coinpaprika' },
+  { envKey: 'CRYPTOCOMPARE_DATA_PROVIDER_URL', defaultValue: 'cryptocompare' },
+  { envKey: 'KAIKO_DATA_PROVIDER_URL', defaultValue: 'kaiko' },
+  { envKey: 'NOMICS_DATA_PROVIDER_URL', defaultValue: 'nomics' },
 ]
 
 function searchEnvironment() {
-  for (const {envKey, defaultValue} of environmentVariables) {
+  for (const { envKey, defaultValue } of environmentVariables) {
     const isSetEnvVar = process.env[envKey]
     if (isSetEnvVar) return defaultValue
   }
@@ -19,7 +19,7 @@ function searchEnvironment() {
 function generateTestPayload() {
   const payload = {
     request: {
-        source = searchEnvironment()
+      source: searchEnvironment(),
     },
   }
   return JSON.stringify(payload)
