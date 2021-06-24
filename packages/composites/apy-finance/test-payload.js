37cb7c94156b35993d267eb168f0ed585bc6fcea
Justin Kaseman <justinkaseman@live.com>
2021-06-24 14:01:42 -0700

    refactor(apy-finance): clean up from code review comments

--- packages/composites/apy-finance/test-payload.js
+++ packages/composites/apy-finance/test-payload.js
@@ -1,39 +1,27 @@
-const basePayload = {
-  request: {},
-}
-
-const testAndReplace = [
-  ['AMBERDATA_DATA_PROVIDER_URL', 'amberdata'],
-  ['COINAPI_DATA_PROVIDER_URL', 'coinapi'],
-  ['COINGECKO_DATA_PROVIDER_URL', 'coingecko'],
-  ['COINMARKETCAP_DATA_PROVIDER_URL', 'coinmarketcap'],
-  ['COINPAPRIKA_DATA_PROVIDER_URL', 'coinpaprika'],
-  ['CRYPTOCOMPARE_DATA_PROVIDER_URL', 'cryptocompare'],
-  ['KAIKO_DATA_PROVIDER_URL', 'kaiko'],
-  ['NOMICS_DATA_PROVIDER_URL', 'nomics'],
+const environmentVariables = [
+  {envKey: 'AMBERDATA_DATA_PROVIDER_URL', defaultValue: 'amberdata'},
+  {envKey: 'COINAPI_DATA_PROVIDER_URL', defaultValue: 'coinapi'},
+  {envKey: 'COINGECKO_DATA_PROVIDER_URL', defaultValue: 'coingecko'},
+  {envKey: 'COINMARKETCAP_DATA_PROVIDER_URL', defaultValue: 'coinmarketcap'},
+  {envKey: 'COINPAPRIKA_DATA_PROVIDER_URL', defaultValue: 'coinpaprika'},
+  {envKey: 'CRYPTOCOMPARE_DATA_PROVIDER_URL', defaultValue: 'cryptocompare'},
+  {envKey: 'KAIKO_DATA_PROVIDER_URL', defaultValue: 'kaiko'},
+  {envKey: 'NOMICS_DATA_PROVIDER_URL', defaultValue: 'nomics'},
 ]
 
-const getEnvName = (name, prefix = '') => {
-  const envName = prefix ? `${prefix}_${name}` : name
-  if (!isEnvNameValid(envName))
-    throw Error(`Invalid environment var name: ${envName}. Only '/^[_a-z0-9]+$/i' is supported.`)
-  return envName
-}
-
-const isEnvNameValid = (name) => /^[_a-z0-9]+$/i.test(name)
-
-const getEnv = (name, prefix = '') => process.env[getEnvName(name, prefix)]
-
 function searchEnvironment() {
-  for (const [environmentVariable, output] of testAndReplace) {
-    const isSetEnvVar = getEnv(environmentVariable)
-    if (isSetEnvVar) return output
+  for (const {envKey, defaultValue} of environmentVariables) {
+    const isSetEnvVar = process.env[envKey]
+    if (isSetEnvVar) return defaultValue
   }
 }
 
 function generateTestPayload() {
-  const payload = { ...basePayload }
-  payload.request.source = searchEnvironment()
+  const payload = {
+    request: {
+        source = searchEnvironment()
+    },
+  }
   return JSON.stringify(payload)
 }
 
