92da0c5c63f3a9cbf7cfd26b5c75719e841e1e5e
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-06-24 16:20:18 -0700

    export types from tiingo adapter

--- packages/sources/tiingo/src/index.ts
+++ packages/sources/tiingo/src/index.ts
@@ -1,7 +1,7 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler, endpointSelector } from './adapter'
 import { NAME, makeConfig } from './config'
-import { ResponseSchema } from './endpoint/crypto/top'
+import * as types from "./endpoint"
 
 const { server } = expose(makeExecute(), makeWSHandler())
-export { NAME, makeExecute, makeConfig, server, ResponseSchema }
+export { NAME, makeExecute, makeConfig, server, types }
