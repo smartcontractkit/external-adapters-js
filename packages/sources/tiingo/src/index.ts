8776b62ba028daf005c3f0f0188b158e0856597c
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-06-23 20:18:27 -0700

    update the way variables are exported from tiingo adapter

--- packages/sources/tiingo/src/index.ts
+++ packages/sources/tiingo/src/index.ts
@@ -2,9 +2,5 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler, endpointSelector } from './adapter'
 import { NAME, makeConfig } from './config'
 
-export = {
-  NAME,
-  makeExecute,
-  makeConfig,
-  ...expose(NAME, makeExecute(), makeWSHandler(), endpointSelector),
-}
+const { server } = expose(makeExecute(), makeWSHandler())
+export { NAME, makeExecute, makeConfig, server }
