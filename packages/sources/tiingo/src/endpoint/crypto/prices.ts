5cdb5f088e52a6fc5b82a79aca47891ffb56ac1a
Silas Lenihan <sjl58@duke.edu>
2021-07-16 13:47:42 -0400

    Added overrides support for tiingo

--- packages/sources/tiingo/src/endpoint/crypto/prices.ts
+++ packages/sources/tiingo/src/endpoint/crypto/prices.ts
@@ -1,5 +1,6 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/types'
+import { NAME as AdapterName } from '../../config'
 
 export const supportedEndpoints = ['prices', 'crypto']
 
