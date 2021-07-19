ab7607fa749873165d3c0f7bffb5a8c8e1a3977e
Justin Kaseman <justinkaseman@live.com>
2021-07-19 12:49:54 -0700

    revert(bootstrap): revert connection refused retry time

--- packages/core/bootstrap/src/lib/cache/redis.ts
+++ packages/core/bootstrap/src/lib/cache/redis.ts
@@ -47,7 +47,8 @@ const retryStrategy = (options: any) => {
   if (options.error && options.error.code === 'ECONNREFUSED') {
     // End reconnecting on a specific error and flush all commands with
     // a individual error
-    return 2500
+    logger.warn('Connection refused.', options)
+    return new Error('The server refused the connection')
   }
   if (options.total_retry_time > 1000 * 60 * 60) {
     // End reconnecting after a specific timeout and flush all commands
