5cb89bc66defb3b7828d84b9d31332a1d2620b4c
Justin Kaseman <justinkaseman@live.com>
2021-07-19 11:48:52 -0700

    revert(bootstrap): revert connection_timeout

--- packages/core/bootstrap/src/lib/cache/redis.ts
+++ packages/core/bootstrap/src/lib/cache/redis.ts
@@ -11,7 +11,7 @@ const DEFAULT_CACHE_REDIS_PORT = 6379 // Port of the Redis server
 const DEFAULT_CACHE_REDIS_PATH = undefined // The UNIX socket string of the Redis server
 const DEFAULT_CACHE_REDIS_URL = undefined // The URL of the Redis server
 const DEFAULT_CACHE_REDIS_PASSWORD = undefined // The password required for redis auth
-const DEFAULT_CACHE_REDIS_CONNECTION_TIMEOUT = 15000 // Timeout per long lived connection in ms
+// const DEFAULT_CACHE_REDIS_CONNECTION_TIMEOUT = 15000 // Timeout per long lived connection in ms
 const DEFAULT_CACHE_REDIS_REQUEST_TIMEOUT = 1000 // Timeout per request in ms
 const DEFAULT_CACHE_REDIS_INITIAL_DELAY = 30000
 // Options
@@ -30,8 +30,8 @@ export const defaultOptions = (): RedisOptions => ({
   password: env.CACHE_REDIS_PASSWORD || DEFAULT_CACHE_REDIS_PASSWORD,
   maxAge: Number(env.CACHE_MAX_AGE) || DEFAULT_CACHE_MAX_AGE,
   timeout: Number(env.CACHE_REDIS_TIMEOUT) || DEFAULT_CACHE_REDIS_REQUEST_TIMEOUT,
-  connect_timeout:
-    Number(env.CACHE_REDIS_CONNECTION_TIMEOUT) || DEFAULT_CACHE_REDIS_CONNECTION_TIMEOUT,
+  // connect_timeout:
+  //   Number(env.CACHE_REDIS_CONNECTION_TIMEOUT) || DEFAULT_CACHE_REDIS_CONNECTION_TIMEOUT,
   socket_initial_delay: Number(env.CACHE_REDIS_INITIAL_DELAY) || DEFAULT_CACHE_REDIS_INITIAL_DELAY,
 })
 
@@ -52,10 +52,11 @@ const retryStrategy = (options: any) => {
   if (options.total_retry_time > 1000 * 60 * 60) {
     // End reconnecting after a specific timeout and flush all commands
     // with a individual error
+    logger.warn('Redis retry strategy exhausted.', options)
     return new Error('Retry time exhausted')
   }
   if (options.attempt > 10) {
-    // End reconnecting with built in error
+    logger.warn(`Redis retry attempt #${options.attempt}`, options)
     return undefined
   }
   // reconnect after
