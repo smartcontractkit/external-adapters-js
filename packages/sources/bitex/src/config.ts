b9d221ae124855ad5c060085ccb0c920df8abaff
Justin Kaseman <justinkaseman@live.com>
2021-07-19 16:11:59 -0700

    fix(bitex): make API key optional

--- packages/sources/bitex/src/config.ts
+++ packages/sources/bitex/src/config.ts
@@ -9,7 +9,7 @@ export const DEFAULT_BASE_URL = 'https://bitex.la/api'
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
-  config.api.headers.authorization = config.apiKey
+  if (config.apiKey) config.api.headers.authorization = config.apiKey
   config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
 }
