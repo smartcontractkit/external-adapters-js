e30cf67e0a751b57e608b2c2ff4ce010f670730c
Silas Lenihan <sjl58@duke.edu>
2021-07-19 16:45:19 -0400

    Added support for stock endpoint in eodhistoricaldata

--- packages/sources/eodhistoricaldata/src/endpoint/stock.ts
+++ packages/sources/eodhistoricaldata/src/endpoint/stock.ts
@@ -1,7 +1,7 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/types'
 
-export const supportedEndpoints = ['price']
+export const supportedEndpoints = ['price', 'stock']
 
 const customError = (data: any) => data.Response === 'Error'
 
