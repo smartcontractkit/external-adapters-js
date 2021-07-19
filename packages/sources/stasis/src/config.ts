2b16a75303fffe3208ac4ac2ffd2270247a79f7a
Justin Kaseman <justinkaseman@live.com>
2021-07-19 15:51:29 -0700

    fix(stasis): remove requirement of API key

--- packages/sources/stasis/src/config.ts
+++ packages/sources/stasis/src/config.ts
@@ -6,7 +6,7 @@ export const NAME = 'STASIS'
 export const DEFAULT_BASE_URL = 'https://stasis.net'
 
 export const makeConfig = (prefix?: string): Config => {
-  const config = Requester.getDefaultConfig(prefix, true)
+  const config = Requester.getDefaultConfig(prefix)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
   return config
 }
