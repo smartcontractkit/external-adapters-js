cfb72f40fc64e320c724210aa7953e540f66a74b
Jonas Hals <jonas@smartcontract.com>
2021-07-20 18:00:00 +0200

    style(google-weather): cleanup

--- packages/composites/google-weather/src/adapter.ts
+++ packages/composites/google-weather/src/adapter.ts
@@ -116,11 +116,8 @@ class QueryBuilder {
   private from() {
     const diff = this.dateTo.getFullYear() - this.dateFrom.getFullYear()
     if (diff === 0) {
-      console.log('diff==0')
       return `SELECT \`stn\`, \`${this.column}\`, \`date\` FROM \`${this.dataset}.gsod${this.dateTo.getFullYear()}\``
     }
-
-    console.log('diff', diff)
 
     const years = new Array(diff + 1).fill(0)
       .map((_, i) => `SELECT \`stn\`, \`${this.column}\`, \`date\` FROM \`${this.dataset}.gsod${this.dateTo.getFullYear()-i}\``)
@@ -128,42 +125,11 @@ class QueryBuilder {
     return years.join('\nUNION ALL\n')
   }
 
-  // SELECT
-  //     *
-  //   FROM
-  //     `bigquery-public-data.noaa_gsod.stations`
-  //   WHERE
-  //     (ST_CONTAINS( ST_GEOGFROMGEOJSON('{ "type": "Polygon", "coordinates": [ [ [ 5.2947235107421875, 60.53398151134199 ], [ 5.2342987060546875, 60.49207143471124 ], [ 5.276870727539062, 60.40571488624096 ], [ 5.2397918701171875, 60.40503667234999 ], [ 5.167694091796875, 60.38027218998218 ], [ 5.167694091796875, 60.340541868001196 ], [ 5.2095794677734375, 60.32286840124156 ], [ 5.3249359130859375, 60.32150850738404 ], [ 5.427932739257812, 60.34801622338117 ], [ 5.4636383056640625, 60.366355109034046 ], [ 5.46295166015625, 60.42436022980665 ], [ 5.4437255859375, 60.471772897267975 ], [ 5.367507934570312, 60.51776468674299 ], [ 5.2947235107421875, 60.53398151134199 ] ] ] }'),
-  //       ST_GEOGPOINT(lon,
-  //         lat) )
-  //     AND PARSE_DATE("%Y%m%d",
-  //       `begin`) <= DATE(2020, 12, 01)
-  //     AND PARSE_DATE("%Y%m%d",
-  //       `end`) >= DATE(2021, 01, 30)
-  //     ) OR (
-  //         ST_EQUALS(
-  //             ST_GEOGPOINT(lon, lat),
-  //             (
-  //                 SELECT ST_GEOGPOINT(lon, lat)
-  //                 FROM `bigquery-public-data.noaa_gsod.stations`
-  //                 WHERE PARSE_DATE("%Y%m%d", `begin`) <= DATE(2020, 12, 01)
-  //                 AND PARSE_DATE("%Y%m%d", `end`) >= DATE(2021, 01, 30)
-  //                 ORDER BY ST_DISTANCE(
-  //                     ST_GEOGFROMGEOJSON('{ "type": "Point", "coordinates": [ 5.325965881347656, 60.3945225493865 ] }'),
-  //                     ST_GEOGPOINT(lon, lat)
-  //                 )
-  //                 LIMIT 1
-  //             )
-  //         )
-  //     )
-  // LIMIT 10;
-
   private geoJsonQuery(): string[] {
     // Note: this is only efficient for when you have one or more Polygons with an optional Point.
     // When only using a single Point, this will cause an extra, unnecessary, lookup query.
     // This is done in order to make it easier to also query stations within Polygon(s).
     return this.geoJson.features.map((ft, i) => {
-      // console.log(ft.type)
       switch (ft.geometry.type) {
         case "Polygon": {
           return `ST_CONTAINS(ST_GEOGFROMGEOJSON(@geoJson${i}), stations.geog)`
