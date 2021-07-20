9063ef892e4feb125c6fb92c37ec522fa9a20da4
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-07-20 09:47:57 -0700

    DXDao Adapter (#680)
    * Implement DxDao adapter
    
    * Calculate TVL using eth usd price feed
    
    * Update ReadMe and add test payload json file
    
    * Add tests for dx dao tvl adapter
    
    * Tidy up tvl endpoint
    
    * Fix test payload json file
    
    * Fix integration tests for dxdao adapter
    
    * Set proper dependency versions in dx dao adapter
    
    * Update DxDao adapter ReadME and env json files
    
    * Update ReadMe and remove shouldReturnInUSD variable
    
    * Add integration test case for when request is missing price feed address
    
    * Move reading json rpc url from required environment variable from config file to tvl endpoint
    
    * Refactor tests for dxdao adapter
    
    * Use number directly in BigNumber operations instead of creating new BigNumber instances
    
    * Add missing newlines in dxdao adapter
    
    * Move dxdao source adapter to composite adapter
    
    * Use tokan allocations to get USD price of WETH
    
    * Address PR comments
    
    * Prevent reassigning tiingo data provider url in dxdao e2e test
    
    * Use token allocation adapter to convert eth value to fiat currency value
    
    * Set weth contract address as environment variable
    
    * Update dxdao adapter test payload
    
    * Update dxdao adapter schema and read me
    
    * Update DxDao adapter tests
    
    * Remove wethContractAddress from example dxdao adapter input
    
    * Update packages/composites/dxdao/schemas/env.json
    
    Co-authored-by: Jonas Hals <contact@jonashals.me>
    
    * Update packages/composites/dxdao/README.md
    
    Co-authored-by: Jonas Hals <contact@jonashals.me>
    
    * Fix dxdao unit tests
    
    * Update packages/composites/dxdao/schemas/env.json
    
    Co-authored-by: Jonas Hals <contact@jonashals.me>
    
    * Directly use tvlInWei as big number when calculating dxdao tvl
    
    Co-authored-by: Jonas Hals <contact@jonashals.me>
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

---
+++ packages/composites/dxdao/test/integration/adapter.test.ts
@@ -0,0 +1,79 @@
+import { AdapterRequest, Execute } from '@chainlink/types'
+import * as dxdaoAdapter from "../../src/index"
+import { ethers, BigNumber } from 'ethers'
+
+const mockBigNum = BigNumber.from(2000)
+
+jest.mock("ethers", () => ({
+  ...(jest.requireActual("ethers")),
+  ethers: {
+    providers: {
+      JsonRpcProvider: function(_: string): ethers.provider.JsonRpcProvider { return {} } 
+    },
+    Contract: function() {
+      return {
+        balanceOf: (____: string) => {
+          return mockBigNum
+        }
+      }
+    }
+  }
+}))
+
+jest.mock("@chainlink/token-allocation-adapter", () => ({
+  ...(jest.requireActual("@chainlink/token-allocation-adapter")),
+  makeExecute: jest.fn().mockReturnValue(() => ({
+    "jobRunID": "1",
+    "data": {
+      "sources": [],
+      "payload": {
+        "WETH": {
+          "quote": {
+            "USD": {
+              "price": "2000"
+            }
+          }
+        }
+      },
+      "result": 2000
+    },
+    "result": 2000,
+    "statusCode": 200
+  }))
+}))
+
+let oldEnv: NodeJS.ProcessEnv
+
+beforeAll(() => {
+  oldEnv = JSON.parse(JSON.stringify(process.env))
+  process.env.RPC_URL = process.env.RPC_URL || "https://rpc.xdaichain.com/" 
+  process.env.WETH_CONTRACT_ADDRESS = process.env.WETH_CONTRACT_ADDRESS || "0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1"
+})
+
+afterAll(() => {
+  process.env = oldEnv
+})
+
+describe('execute', () => {
+  let execute: Execute 
+  const id = '1'
+
+  beforeAll(async () => {
+    execute = await dxdaoAdapter.makeExecute()
+  })
+
+  describe("with source and pair contract addresses correctly provided", () => {
+    const data: AdapterRequest = { 
+      id,
+      data: {
+        "pairContractAddress": "0x1bDe964eCd52429004CbC5812C07C28bEC9147e9",
+        "source": "tiingo"
+      } 
+    }
+
+    it("should return success", async () => {
+      const resp = await execute(data)
+      expect(resp).toMatchSnapshot()
+    })
+  })
+})
