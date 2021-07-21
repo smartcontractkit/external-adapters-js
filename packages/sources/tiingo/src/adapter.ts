cd247c6d275f62d5ca347c3d688ee0b005f55517
Justin Kaseman <justinkaseman@live.com>
2021-07-21 16:08:59 -0700

    fix(tiingo): add missing endpointSelector

--- packages/sources/tiingo/src/adapter.ts
+++ packages/sources/tiingo/src/adapter.ts
@@ -5,6 +5,7 @@ import {
   ExecuteFactory,
   ExecuteWithConfig,
   MakeWSHandler,
+  APIEndpoint,
 } from '@chainlink/types'
 import { DEFAULT_WS_API_ENDPOINT, makeConfig } from './config'
 import * as endpoints from './endpoint'
@@ -12,6 +13,9 @@ import * as endpoints from './endpoint'
 export const execute: ExecuteWithConfig<Config> = async (request, context, config) => {
   return Builder.buildSelector(request, context, config, endpoints)
 }
+
+export const endpointSelector = (request: AdapterRequest): APIEndpoint =>
+  Builder.selectEndpoint(request, makeConfig(), endpoints)
 
 export const makeExecute: ExecuteFactory<Config> = (config) => {
   return async (request, context) => execute(request, context, config || makeConfig())
