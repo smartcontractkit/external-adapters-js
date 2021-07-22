318d1bc098733fbfaf2f7e3fdfeaa13d18900ca1
Jonas Hals <jonas@smartcontract.com>
2021-07-22 20:41:49 +0200

    fix(dydx-rewards): change callback param order

--- packages/composites/dydx-rewards/src/contracts.ts
+++ packages/composites/dydx-rewards/src/contracts.ts
@@ -2,8 +2,8 @@ export const OracleRequester = [
   {
     inputs: [
       { internalType: 'bytes32', name: 'merkleRoot', type: 'bytes32' },
-      { internalType: 'bytes', name: 'ipfsCid', type: 'bytes' },
       { internalType: 'uint256', name: 'epoch', type: 'uint256' },
+      { internalType: 'bytes', name: 'ipfsCid', type: 'bytes' },
     ],
     name: 'writeOracleData',
     outputs: [],
