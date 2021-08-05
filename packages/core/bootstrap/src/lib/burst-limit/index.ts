7042a86f6ebc321c2a9bb39b67b1470ca0570fb5
Justin Kaseman <justinkaseman@live.com>
2021-08-05 11:18:04 -0700

    perf(bootstrap): flatten Burst Limit middleware redux state

--- packages/core/bootstrap/src/lib/burst-limit/index.ts
+++ packages/core/bootstrap/src/lib/burst-limit/index.ts
@@ -16,41 +16,41 @@ export * as reducer from './reducer'
 
 const BURST_CAP_BUFFER = 0.9
 
-export const withBurstLimit = (store?: Store<BurstLimitState>): Middleware => async (
-  execute,
-  context,
-) => async (input) => {
-  const config = rateLimitConfig.get(context)
-  if (!store || !config.enabled || !config.burstCapacity) return await execute(input, context)
+export const withBurstLimit =
+  (store?: Store<BurstLimitState>): Middleware =>
+  async (execute, context) =>
+  async (input) => {
+    const config = rateLimitConfig.get(context)
+    if (!store || !config.enabled || !config.burstCapacity) return await execute(input, context)
 
-  const state = store.getState()
-  const { requests }: { requests: RequestsState } = state
-  const observedRequestsOfParticipant = selectTotalNumberOfRequestsFor(
-    requests,
-    IntervalNames.MINUTE,
-  )
-
-  if (
-    input.id !== WARMUP_BATCH_REQUEST_ID && // Always allow Batch Warmer requests through
-    observedRequestsOfParticipant > config.burstCapacity * BURST_CAP_BUFFER
-    // TODO: determine BATCH_REQUEST_BUFFER dynamically based on (number of batch warmers * 3)
-  ) {
-    logger.error(
-      `Burst rate limit cap of ${
-        config.burstCapacity * BURST_CAP_BUFFER
-      } reached. ${observedRequestsOfParticipant} requests sent in the last minute.`,
+    const state = store.getState()
+    const { requests }: { requests: RequestsState } = state
+    const observedRequestsOfParticipant = selectTotalNumberOfRequestsFor(
+      requests,
+      IntervalNames.MINUTE,
     )
-    throw new AdapterError({
-      jobRunID: input.id,
-      message: 'New request backoff: Burst rate limit cap reached.',
-      statusCode: 429,
-    })
-  }
 
-  const requestObservedPayload: actions.RequestObservedPayload = {
-    input,
-  }
-  store.dispatch(actions.requestObserved(requestObservedPayload))
+    if (
+      input.id !== WARMUP_BATCH_REQUEST_ID && // Always allow Batch Warmer requests through
+      observedRequestsOfParticipant > config.burstCapacity * BURST_CAP_BUFFER
+      // TODO: determine BATCH_REQUEST_BUFFER dynamically based on (number of batch warmers * 3)
+    ) {
+      logger.error(
+        `Burst rate limit cap of ${
+          config.burstCapacity * BURST_CAP_BUFFER
+        } reached. ${observedRequestsOfParticipant} requests sent in the last minute.`,
+      )
+      throw new AdapterError({
+        jobRunID: input.id,
+        message: 'New request backoff: Burst rate limit cap reached.',
+        statusCode: 429,
+      })
+    }
 
-  return await execute(input, context)
-}
+    const requestObservedPayload: actions.RequestObservedPayload = {
+      input,
+    }
+    store.dispatch(actions.requestObserved(requestObservedPayload))
+
+    return await execute(input, context)
+  }
