8593eda43b38ebd5708dd4760cbd99b768730a2a
Thomas <thomas@smartcontract.com>
2021-08-06 13:14:43 -0500

    Google weather (#761)
    * feat(google-bigquery): google BigQuery adapter
    
    * feat: google weather project
    
    * refactor(google-weather): rename mode to method
    
    * feat(google-weather): make table name configurable
    
    * feat(google-weather): use new dataset, with new features
    
    * fix(google-weather): accept stringified geoJson and update schemas
    
    * style(google-weather): cleanup
    
    * docs(google-weather): clearify date format support
    
    * feat(google-weather): add unit conversion
    
    * docs(google-weather): include additional columns in readme
    
    * refactor(google-weather): update docs and cleanup
    
    * refactor(google-bigquery): cleanup
    
    * fix(google-weather): output dates in UTC instead of local TZ
    
    * build(google-weather): update dependencies
    
    * fix(google-weather): add context support
    
    Co-authored-by: Jonas Hals <jonas@smartcontract.com>

---
+++ packages/sources/google-bigquery/src/config.ts
@@ -0,0 +1,24 @@
+import { util } from '@chainlink/ea-bootstrap'
+import { RequestConfig } from '@chainlink/types'
+
+export const NAME = 'GOOGLE_BIGQUERY'
+
+export type Config = {
+  projectId?: string
+  keyFilename?: string
+  email?: string
+  autoRetry: boolean
+  maxRetries: number
+  location?: string
+
+  api: RequestConfig
+}
+
+export const makeConfig = (prefix?: string): Config => ({
+  projectId: util.getEnv('PROJECT_ID', prefix),
+  keyFilename: util.getEnv('KEY_FILENAME', prefix),
+  autoRetry: util.parseBool(util.getEnv('AUTO_RETRY', prefix) || true),
+  maxRetries: parseInt(util.getEnv('MAX_RETRIES', prefix) || '3'),
+  location: util.getEnv('LOCATION', prefix),
+  api: {}
+})
