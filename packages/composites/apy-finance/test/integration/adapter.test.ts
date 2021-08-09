147d491e7906a3867a7948e425f334774423a9d1
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-09 09:32:17 -0700

    Write integration tests for api finance adapter

---
+++ packages/composites/apy-finance/test/integration/adapter.test.ts
@@ -0,0 +1,99 @@
+import { AdapterRequest, Execute } from '@chainlink/types'
+import * as apyFinanceAdapter from "../../src/index"
+import { ethers } from 'ethers'
+
+const mockData = {
+    '1': {
+        symbol: 'WETH',
+        balance: 100,
+        decimals: 18
+    },
+    '2': {
+        symbol: 'LINK',
+        balance: 300,
+        decimals: 18
+    }
+}
+
+const mockRegistry = {
+    chainlinkRegistryAddress: () => "mock-address",
+    getAssetAllocationIds: () => ['1', '2'],
+    symbolOf: (id: string) => mockData[id].symbol,
+    balanceOf: (id: string) => mockData[id].balance,
+    decimalsOf: (id: string) => mockData[id].decimals 
+}
+
+jest.mock("ethers", () => ({
+  ...(jest.requireActual("ethers")),
+  ethers: {
+    providers: {
+      JsonRpcProvider: function(_: string): ethers.provider.JsonRpcProvider { return {} } 
+    },
+    Contract: function() {
+      return mockRegistry
+    }
+  }
+}))
+
+jest.mock("@chainlink/ea-bootstrap", () => ({
+  ...(jest.requireActual("@chainlink/ea-bootstrap")),
+  requester: jest.fn().mockReturnValue(() => ({
+    "jobRunID": "1",
+    "data": {
+      "sources": [],
+      "payload": {
+        "WETH": {
+          "quote": {
+            "USD": {
+              "price": "1800"
+            }
+          }
+        },
+        "LINK": {
+            "quote": {
+              "USD": {
+                "price": "2000"
+              }
+            }
+        }
+      },
+      "result": 2000
+    },
+    "result": 2000,
+    "statusCode": 200
+  }))
+}))
+
+let oldEnv: NodeJS.ProcessEnv
+
+beforeAll(() => {
+  oldEnv = JSON.parse(JSON.stringify(process.env))
+  process.env.REGISTRY_ADDRESS = process.env.REGISTRY_ADDRESS || "fake-registry-address" 
+})
+
+afterAll(() => {
+  process.env = oldEnv
+})
+
+describe('execute', () => {
+  let execute: Execute 
+  const id = '1'
+
+  beforeAll(async () => {
+    execute = await apyFinanceAdapter.makeExecute()
+  })
+
+  describe("with request properly formatted", () => {
+    const data: AdapterRequest = { 
+      id,
+      data: {
+        "source": "tiingo"
+      } 
+    }
+
+    it("should return success", async () => {
+      const resp = await execute(data)
+      expect(resp).toMatchSnapshot()
+    })
+  })
+})
--- packages/composites/token-allocation/test/integration/adapter.test.ts
+++ packages/composites/token-allocation/test/integration/adapter.test.ts
@@ -1,34 +1,151 @@
-import { Requester } from '@chainlink/ea-bootstrap'
-import { assertError } from '@chainlink/ea-test-helpers'
-import { AdapterRequest } from '@chainlink/types'
-import { makeExecute } from '../../src/adapter'
-import { makeConfig } from '../../src/config'
+import { AdapterRequest, Execute } from '@chainlink/types'
+import * as tokenAllocationAdapter from "../../src/index" 
+import { Requester } from "@chainlink/ea-bootstrap"
+
+const jobRunID = 1
+const sources = {
+    "amberdata": {
+        providerUrlEnvVar: `AMBERDATA_DATA_PROVIDER_URL`
+    }, 
+    "coinapi": {
+        providerUrlEnvVar: `COINAPI_DATA_PROVIDER_URL`
+    }, 
+    "coingecko": {
+        providerUrlEnvVar: `COINGECKO_DATA_PROVIDER_URL`,
+        isBatched: true 
+    }, 
+    "coinmarketcap": {
+        providerUrlEnvVar: `COINMARKETCAP_DATA_PROVIDER_URL`,
+        isBatched: true
+    }, 
+    "coinpaprika": {
+        providerUrlEnvVar: `COINPAPRIKA_DATA_PROVIDER_URL`
+    }, 
+    "cryptocompare": {
+        providerUrlEnvVar: `CRYPTOCOMPARE_DATA_PROVIDER_URL`,
+        isBatched: true
+    }, 
+    "kaiko": {
+        providerUrlEnvVar: `KAIKO_DATA_PROVIDER_URL`
+    }, 
+    "nomics": {
+        providerUrlEnvVar: `NOMICS_DATA_PROVIDER_URL`,
+        isBatched: true
+    }, 
+    "tiingo": {
+        providerUrlEnvVar: `TIINGO_DATA_PROVIDER_URL`
+    }
+}
+Requester.request = (request: any) => {
+    const source = sources[request.baseURL]
+    const { quote } = request.data.data 
+    if(source.isBatched || (request.baseURL === "coinapi" && request.data.data.quote === "USD")) {
+        return {
+            "jobRunID": "1",
+            "data": {
+                "data": {
+                    "results": [
+                        [
+                            {
+                                "data": {
+                                    "base": "WBTC",
+                                    "quote": quote
+                                }
+                            },
+                            200
+                        ],
+                        [
+                            {
+                                "data": {
+                                    "base": "DAI",
+                                    "quote": quote
+                                }
+                            },
+                            400
+                        ]
+                    ]
+                }
+            },
+            "statusCode": 200
+        }
+    }
+    return {
+        "jobRunID": "1",
+        "data": {
+          "result": 130.27
+        },
+        "result": 130.27,
+        "statusCode": 200
+    }
+}
+const getPriceRequest = (source: string, method: string = "price", quote: string = "EUR"): AdapterRequest => ({ 
+    id: jobRunID,
+    data: {
+      source,
+      "allocations": [
+        {
+          "symbol": "wBTC",
+          "balance": 100000000,
+          "decimals": 8
+        },
+        {
+          "symbol": "DAI",
+          "balance": "1000000000000000000"
+        }
+      ],
+      quote,
+      method 
+    }
+})
+
+let oldEnv: NodeJS.ProcessEnv
 
 describe('execute', () => {
-  const jobID = '1'
-  process.env.DATA_PROVIDER_URL = 'ignoreable'
-  const execute = makeExecute(makeConfig(''))
+  let execute: Execute 
+  const id = '1'
 
-  describe('error calls @integration', () => {
-    const requests = [
-      {
-        name: 'invalid units',
-        testData: {
-          id: jobID,
-          data: { allocations: [{ symbol: 'DAI', balance: '1000000000000000000', decimals: 18 }] },
-        },
-      },
-    ]
+  beforeAll(async () => {
+    execute = await tokenAllocationAdapter.makeExecute()
+    oldEnv = JSON.parse(JSON.stringify(process.env))
+    for (const source of Object.keys(sources)) {
+        const { providerUrlEnvVar } = sources[source]
+        process.env[providerUrlEnvVar] = source
+    }
+  })
 
-    requests.forEach((req) => {
-      it(`${req.name}`, async () => {
-        try {
-          await execute(req.testData as AdapterRequest)
-        } catch (error) {
-          const errorResp = Requester.errored(jobID, error)
-          assertError({ expected: 500, actual: errorResp.statusCode }, errorResp, jobID)
-        }
-      })
+  afterAll(() => {
+    process.env = oldEnv
+  })
+
+  describe("price method", () => {
+    for(const source of Object.keys(sources)) {
+        const request = getPriceRequest(source)
+        it(`should return the correct price for source ${source}`, async () => {
+            const resp = await execute(request)
+            expect(resp).toMatchSnapshot()
+        })
+    }
+
+    it('should return the correct price using a batched request when pulling data from coin API and the quote is USD', async () => {
+        const request = getPriceRequest("coinapi", "price", "USD")
+        const resp = await execute(request)
+        expect(resp).toMatchSnapshot()
+    })
+  })
+
+  describe("marketcap method", () => {
+    for(const source of Object.keys(sources)) {
+        const request = getPriceRequest(source, "marketcap")
+        it(`should return the correct price for source ${source}`, async () => {
+            const resp = await execute(request)
+            expect(resp).toMatchSnapshot()
+        })
+    }
+
+    it('should return the correct price using a batched request when pulling data from coin API and the quote is USD', async () => {
+        const request = getPriceRequest("coinapi", "marketcap", "USD")
+        const resp = await execute(request)
+        expect(resp).toMatchSnapshot()
     })
   })
 })
