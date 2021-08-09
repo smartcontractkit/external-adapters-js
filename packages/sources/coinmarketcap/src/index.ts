1dc80043d1803a4e7d00f425edf43c2d2504fe75
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-09 09:42:34 -0700

    Fix compilation issue in coinmarketcap index file

--- packages/sources/coinmarketcap/src/index.ts
+++ packages/sources/coinmarketcap/src/index.ts
@@ -2,6 +2,6 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, endpointSelector } from './adapter'
 import { makeConfig, NAME } from './config'
 
-const server = expose(makeExecute(), undefined, endpointSelector).server
+const server = expose(NAME, makeExecute(), undefined, endpointSelector).server
 
 export { NAME, makeConfig, makeExecute, server }
