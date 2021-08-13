f1f6c002f9c3754eef3e0ce9fef6a9f83c0358b6
bogos <andre.neyra@gmail.com>
2021-08-13 00:32:29 -0500

    chore(ethgaswatch): add gasprice endpoint data consumption, tests and documentation

---
+++ packages/sources/ethgaswatch/src/index.ts
@@ -0,0 +1,10 @@
+import { expose } from '@chainlink/ea-bootstrap'
+import { makeExecute, endpointSelector } from './adapter'
+import { makeConfig, NAME } from './config'
+
+export = {
+  NAME,
+  makeExecute,
+  makeConfig,
+  ...expose(NAME, makeExecute(), undefined, endpointSelector),
+}
