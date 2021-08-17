2552e97d9a2b27a1382bea3caf76fbc1d8f972dd
bogos <andre.neyra@gmail.com>
2021-08-17 00:10:12 -0500

    fix: add export to a schema definition

--- packages/sources/mycryptoapi/src/endpoint/gasprice.ts
+++ packages/sources/mycryptoapi/src/endpoint/gasprice.ts
@@ -3,7 +3,7 @@ import { Config, ExecuteWithConfig, InputParameters } from '@chainlink/types'
 
 export const supportedEndpoints = ['gasprice']
 
-interface ResponseSchema  {
+export interface ResponseSchema  {
     safeLow: number,
     standard: number,
     fast: number,
