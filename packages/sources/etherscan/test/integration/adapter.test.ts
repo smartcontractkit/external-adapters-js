a6f4291a0f5368e7689cd7a6b4cb28b498240d3f
bogos <andre.neyra@gmail.com>
2021-08-17 05:58:10 -0500

    chore: add gasprice endpoint data consumption, tests and documentation

---
+++ packages/sources/etherscan/test/integration/adapter.test.ts
@@ -0,0 +1,92 @@
+import { Requester } from '@chainlink/ea-bootstrap'
+import { assertError, assertSuccess } from '@chainlink/ea-test-helpers'
+import { AdapterRequest } from '@chainlink/types'
+import { makeExecute } from '../../src/adapter'
+
+describe('execute', () => {
+  const jobID = '1'
+  const execute = makeExecute()
+  process.env.API_KEY = process.env.API_KEY ?? 'test_api_key'
+
+  describe('successful calls @integration', () => {
+    const requests = [
+      {
+        name: 'id not supplied',
+        testData: {
+          data: {
+            speed: 'FastGasPrice',
+          },
+        },
+      },
+      {
+        name: 'speed is FastGasPrice',
+        testData: {
+          id: jobID,
+          data: { speed: 'FastGasPrice' },
+        },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        const data = await execute(req.testData as AdapterRequest)
+        assertSuccess({ expected: 200, actual: data.statusCode }, data, jobID)
+        expect(data.data.result).toBeGreaterThan(0)
+      })
+    })
+  })
+
+  describe('validation error', () => {
+    const requests = [
+      {
+        name: 'empty body',
+        testData: {},
+      },
+      {
+        name: 'empty data',
+        testData: { data: {} },
+      },
+      {
+        name: 'no speed param',
+        testData: {
+          id: jobID,
+          data: { endpoint: 'not_real' },
+        },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, error)
+          assertError({ expected: 'errored', actual: errorResp.status }, errorResp, jobID)
+        }
+      })
+    })
+  })
+
+  describe('error calls @integration', () => {
+    const requests = [
+      {
+        name: 'unknown speed',
+        testData: {
+          id: jobID,
+          data: { speed: 'not_real' },
+        },
+      },
+    ]
+
+    requests.forEach((req) => {
+      it(`${req.name}`, async () => {
+        try {
+          await execute(req.testData as AdapterRequest)
+        } catch (error) {
+          const errorResp = Requester.errored(jobID, error)
+          assertError({ expected: 500, actual: errorResp.statusCode }, errorResp, jobID)
+        }
+      })
+    })
+  })
+})
