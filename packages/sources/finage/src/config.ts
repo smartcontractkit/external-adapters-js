99a6b4fd5681f907b24101a0a745d912e1875699
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2021-08-20 10:40:19 -0700

    add ws support for the finage adapter (#812)

--- packages/sources/finage/src/config.ts
+++ packages/sources/finage/src/config.ts
@@ -1,12 +1,16 @@
-import { Requester } from '@chainlink/ea-bootstrap'
+import { Requester, util } from '@chainlink/ea-bootstrap'
 import { Config } from '@chainlink/types'
 
 export const NAME = 'FINAGE'
 
 export const DEFAULT_BASE_URL = 'https://api.finage.co.uk'
+export const DEFAULT_WS_API_ENDPOINT = 'wss://e4s39ar3mr.finage.ws:7002'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix, true)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
+  const wsUrl = config.api.baseWsURL || DEFAULT_WS_API_ENDPOINT
+  const socketKey = util.getRequiredEnv('WS_SOCKET_KEY')
+  config.api.baseWsURL = `${wsUrl}?token=${socketKey}`
   return config
 }
