59ab55d483e7c246d1e19929caa29e55051a9361
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-20 09:53:42 -0700

    prettify synthetix debt pool adapter files

--- packages/sources/synthetix-debt-pool/src/adapter.ts
+++ packages/sources/synthetix-debt-pool/src/adapter.ts
@@ -1,14 +1,13 @@
-import {
-  AdapterResponse,
-  AdapterContext,
-  Execute,
-  AdapterRequest,
-} from '@chainlink/types'
+import { AdapterResponse, AdapterContext, Execute, AdapterRequest } from '@chainlink/types'
 import { makeConfig, Config as AdapterConfig } from './config'
 import * as endpoints from './endpoint'
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 
-export const execute = async (request: AdapterRequest, context: AdapterContext, config: AdapterConfig): Promise<AdapterResponse> => {
+export const execute = async (
+  request: AdapterRequest,
+  context: AdapterContext,
+  config: AdapterConfig,
+): Promise<AdapterResponse> => {
   const validator = new Validator(request)
   if (validator.error) throw validator.error
   Requester.logConfig(config)
