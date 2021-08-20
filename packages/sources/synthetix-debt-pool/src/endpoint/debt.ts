f1661fc40e778dbcc0c27b01c6c1024eb02c8273
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2021-08-20 09:58:47 -0700

    prettify synthetix debt pool adapter files (#822)

--- packages/sources/synthetix-debt-pool/src/endpoint/debt.ts
+++ packages/sources/synthetix-debt-pool/src/endpoint/debt.ts
@@ -10,13 +10,13 @@ export const endpointResultPaths = {
 }
 
 interface CurrentDebtResults {
-  total: ethers.BigNumber,
+  total: ethers.BigNumber
   isInvalid: boolean
 }
 
 export interface ResponseSchema {
   data: {
-    total: string,
+    total: string
     isInvalid: boolean
   }
 }
@@ -30,43 +30,44 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
     data: {
       result: debt.total.toString(),
       total: debt.total.toString(),
-      isInvalid: debt.isInvalid
-    }
+      isInvalid: debt.isInvalid,
+    },
   }
   return Requester.success(jobRunID, result, config.verbose)
 }
 
 const DEBT_POOL_ABI = [
   {
-     "constant":true,
-     "inputs":[
-        
-     ],
-     "name":"currentDebt",
-     "outputs":[
-        {
-           "internalType":"uint256",
-           "name":"debt",
-           "type":"uint256"
-        },
-        {
-           "internalType":"bool",
-           "name":"anyRateIsInvalid",
-           "type":"bool"
-        }
-     ],
-     "payable":false,
-     "stateMutability":"view",
-     "type":"function"
+    constant: true,
+    inputs: [],
+    name: 'currentDebt',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'debt',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bool',
+        name: 'anyRateIsInvalid',
+        type: 'bool',
+      },
+    ],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
   },
 ]
 
-const getCurrentDebt = async (rpcUrl: string, debtPoolAddress: string): Promise<CurrentDebtResults> => {
+const getCurrentDebt = async (
+  rpcUrl: string,
+  debtPoolAddress: string,
+): Promise<CurrentDebtResults> => {
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
   const debtPool = new ethers.Contract(debtPoolAddress, DEBT_POOL_ABI, provider)
   const [totalDebt, isInvalid] = await debtPool.currentDebt()
   return {
     total: totalDebt,
-    isInvalid
+    isInvalid,
   }
 }
