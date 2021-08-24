67d5baa94f85f692a61b7207cb89b1e7a12bdbd6
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2021-08-24 15:46:45 -0400

    Format files

--- packages/sources/tiingo/src/index.ts
+++ packages/sources/tiingo/src/index.ts
@@ -1,7 +1,7 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler, endpointSelector } from './adapter'
 import { NAME, makeConfig } from './config'
-import * as types from "./endpoint"
+import * as types from './endpoint'
 
 const { server } = expose(NAME, makeExecute(), makeWSHandler(), endpointSelector)
 export { NAME, makeExecute, makeConfig, server, types }
