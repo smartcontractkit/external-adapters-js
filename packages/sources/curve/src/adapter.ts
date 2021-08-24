34dadce5c9cdadccdc628c392abd263b6d90701c
Jonas Hals <jonas@smartcontract.com>
2021-08-24 12:43:21 +0200

    refactor: avoid using generic defaults where possible

--- packages/sources/curve/src/adapter.ts
+++ packages/sources/curve/src/adapter.ts
@@ -4,11 +4,11 @@ import { makeConfig, Config } from './config'
 import * as endpoints from './endpoint'
 
 export const execute: ExecuteWithConfig<Config> = async (request, context, config) => {
-  return Builder.buildSelector<Config>(request, context, config, endpoints)
+  return Builder.buildSelector(request, context, config, endpoints)
 }
 
 export const endpointSelector = (request: AdapterRequest): APIEndpoint<Config> =>
-  Builder.selectEndpoint<Config>(request, makeConfig(), endpoints)
+  Builder.selectEndpoint(request, makeConfig(), endpoints)
 
 export const makeExecute: ExecuteFactory<Config> = (config) => {
   return async (request, context) => execute(request, context, config || makeConfig())
