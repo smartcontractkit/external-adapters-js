c24d2e4fb56dee0e5eef0ff224f33f100b8e5dbf
Jonas Hals <contact@jonashals.me>
2021-08-24 21:04:24 +0200

    feat(coinpaprika): crypto batch support (#797)
    * feat(coinpaprika): crypto batch support
    
    * feat(coinpaprika): bring back old endpoint and add `batchablePropertyPath`
    
    * test(coinpaprika): bring back deleted unit tests
    
    * style(coinpaprika): fix formatting

--- packages/sources/coinpaprika/src/util.ts
+++ packages/sources/coinpaprika/src/util.ts
@@ -1,7 +1,24 @@
 import { makeExecute } from './adapter'
 import { makeMiddleware, withMiddleware } from '@chainlink/ea-bootstrap'
+import { ResponseSchema } from './endpoint/crypto'
 import { CoinsResponse } from './endpoint/coins'
 import { AdapterContext } from '@chainlink/types'
+
+export const getCoin = (
+  data: ResponseSchema[],
+  symbol?: string,
+  coinId?: string,
+): ResponseSchema | undefined => {
+  if (coinId) {
+    return data.find(({ id }) => id.toLowerCase() === coinId.toLowerCase())
+  } else if (symbol) {
+    return data.find(
+      ({ symbol: coinSymbol, rank }) =>
+        coinSymbol.toLowerCase() === symbol.toLowerCase() && rank !== 0,
+    )
+  }
+  return undefined
+}
 
 export function getCoinIds(context: AdapterContext, id: string): Promise<CoinsResponse[]> {
   const execute = makeExecute()
