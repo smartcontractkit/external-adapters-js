867ba279f6a7133f1e87159587638355f18119b8
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-26 15:10:40 -0700

    mock ether library in apy finance test

--- packages/composites/apy-finance/test/integration/adapter.test.ts
+++ packages/composites/apy-finance/test/integration/adapter.test.ts
@@ -1,54 +1,8 @@
 import { AdapterRequest, Execute } from '@chainlink/types'
 import * as apyFinanceAdapter from "../../src/index"
-import { ethers } from 'ethers'
-import { mockTiingoResponse } from './fixtures'
-
-const mockData = {
-    '1': {
-        symbol: 'WETH',
-        balance: 100,
-        decimals: 18
-    },
-    '2': {
-        symbol: 'LINK',
-        balance: 300,
-        decimals: 18
-    }
-}
-
-const mockRegistry = {
-    chainlinkRegistryAddress: () => "mock-address",
-    getAssetAllocationIds: () => ['1', '2'],
-    symbolOf: (id: string) => mockData[id].symbol,
-    balanceOf: (id: string) => mockData[id].balance,
-    decimalsOf: (id: string) => mockData[id].decimals 
-}
-
-jest.mock("ethers", () => ({
-  ...(jest.requireActual("ethers")),
-  ethers: {
-    providers: {
-      JsonRpcProvider: function(_: string): ethers.provider.JsonRpcProvider { return {} } 
-    },
-    Contract: function() {
-      return mockRegistry
-    }
-  }
-}))
-
-
+import { mockTiingoResponse, mockEthereumCalls } from './fixtures'
 
 let oldEnv: NodeJS.ProcessEnv
-
-beforeAll(() => {
-  oldEnv = JSON.parse(JSON.stringify(process.env))
-  process.env.REGISTRY_ADDRESS = process.env.REGISTRY_ADDRESS || "fake-registry-address" 
-  process.env.TIINGO_DATA_PROVIDER_URL = process.env.TIINGO_DATA_PROVIDER_URL || "http://localhost:3000"
-})
-
-afterAll(() => {
-  process.env = oldEnv
-})
 
 describe('execute', () => {
   let execute: Execute 
@@ -56,10 +10,26 @@ describe('execute', () => {
 
   beforeAll(async () => {
     execute = await apyFinanceAdapter.makeExecute()
+    oldEnv = JSON.parse(JSON.stringify(process.env))
+    process.env.REGISTRY_ADDRESS = process.env.REGISTRY_ADDRESS || "0x7ec81b7035e91f8435bdeb2787dcbd51116ad303" 
+    process.env.TIINGO_DATA_PROVIDER_URL = process.env.TIINGO_DATA_PROVIDER_URL || "http://localhost:3000"
+    process.env.RPC_URL = process.env.RPC_URL || "https://geth-main.eth.devnet.tools"
+  })
+
+  afterAll(() => {
+    process.env = oldEnv
+    if (process.env.RECORD) {
+        nock.recorder.play()
+    }
+
+    nock.restore()
+    nock.cleanAll()
+    nock.enableNetConnect()
   })
 
   describe("with request properly formatted", () => {
     mockTiingoResponse()
+    mockEthereumCalls()
     const data: AdapterRequest = { 
       id,
       data: {
