ebbbf9c62edd3f25649ff575ea4b07d3a0c89d05
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-26 16:39:11 -0700

    format files using prettier

--- packages/core/bootstrap/src/lib/ws/utils.ts
+++ packages/core/bootstrap/src/lib/ws/utils.ts
@@ -1,28 +1,28 @@
 import { AdapterRequest } from '@chainlink/types'
 
 export const separateBatches = async (
-    curr: AdapterRequest,
-    input: AdapterRequest, 
-    dataFields: string[],
-    callback: (singleInput: AdapterRequest) => Promise<void>
+  curr: AdapterRequest,
+  input: AdapterRequest,
+  dataFields: string[],
+  callback: (singleInput: AdapterRequest) => Promise<void>,
 ): Promise<void> => {
-    if (dataFields.length === 0) {
-        await callback(curr)
-    } else {
-        let dataValues = input.data[dataFields[0]]
-        if (dataValues) {
-          dataValues = Array.isArray(dataValues) ? dataValues : [dataValues]
-          for (const val of dataValues) {
-            let updatedCurr = JSON.parse(JSON.stringify(curr))
-            updatedCurr = {
-              ...curr,
-              data: {
-                ...curr.data,
-                [dataFields[0]]: val
-              }
-            }
-            await separateBatches(updatedCurr, input, dataFields.slice(1), callback)
-          }
+  if (dataFields.length === 0) {
+    await callback(curr)
+  } else {
+    let dataValues = input.data[dataFields[0]]
+    if (dataValues) {
+      dataValues = Array.isArray(dataValues) ? dataValues : [dataValues]
+      for (const val of dataValues) {
+        let updatedCurr = JSON.parse(JSON.stringify(curr))
+        updatedCurr = {
+          ...curr,
+          data: {
+            ...curr.data,
+            [dataFields[0]]: val,
+          },
         }
-    } 
+        await separateBatches(updatedCurr, input, dataFields.slice(1), callback)
+      }
+    }
+  }
 }
