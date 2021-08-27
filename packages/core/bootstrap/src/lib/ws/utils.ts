96e69a9f2311687e91830b038d2695939b771a25
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-08-26 20:17:18 -0700

    Add documentation and test for splitIntoBatches function

--- packages/core/bootstrap/src/lib/ws/utils.ts
+++ packages/core/bootstrap/src/lib/ws/utils.ts
@@ -1,5 +1,11 @@
 import { AdapterRequest } from '@chainlink/types'
 
+/**
+ * Separates a batched request into indivdual requests and calls a callback function with the individual request passed in
+ * @param input The original batched request
+ * @param dataFields The input request data object's fields
+ * @param callback Callback function that is called after batching is complete
+ */
 export const separateBatches = async (
   input: AdapterRequest,
   dataFields: string[],
