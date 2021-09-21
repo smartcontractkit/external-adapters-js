8db071df0901ff3b29b8d79e889af0aad3fbaf6e
Thomas <thomas@smartcontract.com>
2021-09-21 12:16:00 -0500

    Require API_KEY
    Co-authored-by: Jonas Hals <contact@jonashals.me>

--- packages/sources/flightaware/src/config.ts
+++ packages/sources/flightaware/src/config.ts
@@ -7,7 +7,7 @@ export const DEFAULT_ENDPOINT = 'estimatedarrivaltime'
 export const DEFAULT_BASE_URL = 'https://flightxml.flightaware.com/json/FlightXML2'
 
 export const makeConfig = (prefix?: string): Config => {
-  const config = Requester.getDefaultConfig(prefix)
+  const config = Requester.getDefaultConfig(prefix, true)
   config.api.username = util.getRequiredEnv('API_USERNAME')
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
   config.defaultEndpoint = DEFAULT_ENDPOINT
