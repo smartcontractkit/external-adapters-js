47c9144f25c20563223bc9a9666591a93b21d32b
Victor Navascues <victor@linkpool.io>
2021-09-29 18:40:09 +0200

    feat(coinmetrics): burned endpoint logic amendments

--- packages/sources/coinmetrics/src/endpoint/burned.ts
+++ packages/sources/coinmetrics/src/endpoint/burned.ts
@@ -1,28 +1,20 @@
 import { Requester, Validator, AdapterError } from '@chainlink/ea-bootstrap'
 import { Config, ExecuteWithConfig, InputParameters } from '@chainlink/types'
-import { NAME as AdapterName } from '../config'
-
-export const DEFAULT_FREQUENCY = '1d'
+import { Decimal } from 'decimal.js'
+Decimal.set({ precision: 100 })
 
 export const supportedEndpoints = ['burned']
 
-export const DEFAULT_STARTDATE = '2021-06-13'
+const METRICS = 'FeeTotNtv,RevNtv,IssTotNtv'
+const ASSETS = 'eth'
+const FREQUENCY = '1d' // Common and safe frequency for total ETH burned via FeeTotNtv, RevNtv and IssTotNtv
+const DEFAULT_PAGE_SIZE = 10_000
+// NB: first API ETH metric is from the 2021-06-21. Any ETH metrics before DEFAULT_START_DATE report 0 ETH burned.
+const DEFAULT_START_DATE = '2021-08-05' // EIP-1559 release date
 
-export interface ResponseSchema {
-  data: [
-    {
-      asset: string
-      time: string
-      FeeTotNtv: string
-      IssTotNtv: string
-      RevNtv: string
-    },
-  ]
-  next_page_token: string
-  next_page_url: string
-}
+const URL = 'timeseries/asset-metrics'
 
-export interface Day {
+interface AssetMetrics {
   asset: string
   time: string
   FeeTotNtv: string
@@ -30,137 +22,128 @@ export interface Day {
   RevNtv: string
 }
 
+interface ResponseData {
+  data: AssetMetrics[]
+  next_page_token?: string
+  next_page_url?: string
+}
+
 export const inputParameters: InputParameters = {
-  asset: true,
-  frequency: false,
-  pageSize: false,
   startDate: false,
   endDate: false,
 }
 
-const throwAdapterError = (jobRunID: string, message: string, statusCode = 500) => {
-  throw new AdapterError({
-    jobRunID,
-    message,
-    statusCode,
-  })
-}
-
 const reDate = new RegExp(/^\d{4}-\d{2}-\d{2}$/)
+
+const calculateBurnedETH = (jobRunID: string, assetMetricsList: AssetMetrics[]): Decimal => {
+  let burnedETH = new Decimal(0)
+  assetMetricsList.forEach((assetMetrics: AssetMetrics) => {
+    let feeTotNTV
+    let revNtv
+    let issTotNtv
+    try {
+      feeTotNTV = new Decimal(assetMetrics.FeeTotNtv)
+      revNtv = new Decimal(assetMetrics.RevNtv)
+      issTotNtv = new Decimal(assetMetrics.IssTotNtv)
+    } catch (error) {
+      throw new AdapterError({
+        jobRunID,
+        message: `Unprocessable asset metrics: ${JSON.stringify(assetMetrics)}, due to: ${error}.`,
+        statusCode: 400,
+      })
+    }
+    burnedETH = burnedETH.plus(feeTotNTV.minus(revNtv.minus(issTotNtv)))
+  })
+  return burnedETH
+}
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
   const validator = new Validator(request, inputParameters)
   if (validator.error) throw validator.error
 
+  const today = new Date().toISOString().split('T')[0]
+
   const jobRunID = validator.validated.id
-  const asset = validator.overrideSymbol(AdapterName, validator.validated.data.asset)
-  const frequency = validator.validated.data.frequency || DEFAULT_FREQUENCY
-  const startDate = validator.validated.data.startDate || DEFAULT_STARTDATE
-  const endDate = validator.validated.data.endDate
+  const startDate = validator.validated.data.startDate || DEFAULT_START_DATE
+  const endDate = validator.validated.data.endDate || today
 
   if (startDate && !reDate.test(startDate)) {
-    throwAdapterError(
+    throw new AdapterError({
       jobRunID,
-      `Invalid 'startDate': ${startDate}. Expected formats is: YYYY-MM-DD`,
-      400,
-    )
+      message: `Invalid 'startDate': ${startDate}. Expected formats is: YYYY-MM-DD.`,
+      statusCode: 400,
+    })
   }
 
-  if (endDate && !reDate.test(endDate)) {
-    throwAdapterError(
+  if (Date.parse(startDate) > Date.parse(today)) {
+    throw new AdapterError({
       jobRunID,
-      `Invalid 'endDate': ${endDate}. Expected formats is: YYYY-MM-DD`,
-      400,
-    )
+      message: `Invalid 'startDate': ${startDate}. Is greater than today: ${today}.`,
+      statusCode: 400,
+    })
   }
 
-  if (startDate && endDate && Date.parse(startDate) > Date.parse(endDate)) {
-    throwAdapterError(
+  if (!reDate.test(endDate)) {
+    throw new AdapterError({
       jobRunID,
-      `'startDate': ${startDate} cannot be greater than 'endDate': ${endDate}`,
-      400,
-    )
+      message: `Invalid 'endDate': ${endDate}. Expected formats is: YYYY-MM-DD.`,
+      statusCode: 400,
+    })
   }
 
-  if (startDate && Date.parse(startDate) < Date.parse(DEFAULT_STARTDATE)) {
-    throwAdapterError(
+  if (Date.parse(endDate) > Date.parse(today)) {
+    throw new AdapterError({
       jobRunID,
-      `'startDate': ${startDate} should be greater or equal to ${DEFAULT_STARTDATE}`,
-      400,
-    )
+      message: `Invalid 'endDate': ${endDate}. Is greater than today: ${today}.`,
+      statusCode: 400,
+    })
   }
 
-  if (endDate && Date.parse(endDate) <= Date.parse(DEFAULT_STARTDATE)) {
-    throwAdapterError(
+  if (Date.parse(startDate) > Date.parse(endDate)) {
+    throw new AdapterError({
       jobRunID,
-      `'endDate': ${endDate} should be greater than ${DEFAULT_STARTDATE}`,
-      400,
-    )
+      message: `Invalid date range: 'startDate': ${startDate} cannot be greater than 'endDate': ${endDate}.`,
+      statusCode: 400,
+    })
   }
-
-  const url = 'timeseries/asset-metrics'
-  const metrics = 'FeeTotNtv,RevNtv,IssTotNtv'
-
-  let totalBurned = 0
-
-  let nextPageToken = ''
 
   const params = {
-    assets: (asset as string).toLowerCase(),
-    metrics,
-    frequency,
-    page_size: 10,
+    assets: ASSETS,
+    metrics: METRICS,
+    frequency: FREQUENCY,
+    page_size: DEFAULT_PAGE_SIZE,
     start_time: startDate,
     end_time: endDate,
     api_key: config.apiKey,
   }
 
-  const options = { ...config.api, params, url }
+  const options = { ...config.api, params, url: URL }
 
-  const response = await Requester.request<ResponseSchema>(options)
+  let totalBurnedETH = new Decimal(0)
+  let response
+  /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+  while (true) {
+    response = await Requester.request(options)
 
-  response.data.data.forEach((day: Day) => {
-    const burnedEth: number =
-      parseFloat(day.FeeTotNtv) - (parseFloat(day.RevNtv) - parseFloat(day.IssTotNtv))
-    totalBurned += burnedEth
-  })
-
-  if (response.data.next_page_token) {
-    let flag = 1
-
-    nextPageToken = response.data.next_page_token
-
-    while (flag == 1) {
-      const params = {
-        assets: (asset as string).toLowerCase(),
-        metrics,
-        frequency,
-        page_size: 10,
-        next_page_token: nextPageToken,
-        start_time: startDate,
-        end_time: endDate,
-        api_key: config.apiKey,
-      }
-
-      const options = { ...config.api, params, url }
-
-      const newResponse = await Requester.request<ResponseSchema>(options)
-
-      newResponse.data.data.forEach((day: Day) => {
-        const burnedEth: number =
-          parseFloat(day.FeeTotNtv) - (parseFloat(day.RevNtv) - parseFloat(day.IssTotNtv))
-        totalBurned += burnedEth
+    const responseData: ResponseData = response.data
+    const { data: assetMetricsList } = responseData
+    if (!Array.isArray(assetMetricsList)) {
+      throw new AdapterError({
+        jobRunID,
+        message: `Unexpected response: ${assetMetricsList}. 'data' expected to be an array.`,
+        statusCode: 400,
       })
-
-      if (!newResponse.data.next_page_token) {
-        flag = 0
-      } else {
-        nextPageToken = newResponse.data.next_page_token
-      }
     }
+    totalBurnedETH = totalBurnedETH.plus(calculateBurnedETH(jobRunID, assetMetricsList))
+
+    const nextPageToken = response.data.next_page_token
+    if (!nextPageToken || assetMetricsList.length < DEFAULT_PAGE_SIZE) break
+    options.params.next_page_token = nextPageToken
   }
 
-  const result = totalBurned
-
-  return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
+  return Requester.success(
+    jobRunID,
+    Requester.withResult(response, totalBurnedETH.toString()),
+    config.verbose,
+  )
 }
