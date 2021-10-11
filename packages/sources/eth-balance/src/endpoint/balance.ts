cc87a7a5e482ecb11ae66a1b72af13536d019aa6
karen-stepanyan <karen.stepanyan@altoros.com>
2021-10-12 00:55:51 +0400

    add basic validation, multiple address lookups

--- packages/sources/eth-balance/src/endpoint/balance.ts
+++ packages/sources/eth-balance/src/endpoint/balance.ts
@@ -1,37 +1,51 @@
-import { Validator, Requester } from '@chainlink/ea-bootstrap'
+import { Validator, Requester, AdapterError } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, InputParameters } from '@chainlink/types'
+import { Config } from '../config'
 import { utils } from 'ethers'
-// import { Config } from '../config'
 
-// This should be filled in with a lowercase name corresponding to the API endpoint
 export const supportedEndpoints = ['balance']
-
-export const endpointResultPaths = {
-  example: 'price',
-}
-
-export interface ResponseSchema {
-  data: {
-    // Some data
-  }
-}
-
-// const customError = (data: any) => data.Response === 'Error'
 
 export const inputParameters: InputParameters = {
   addresses: ['addresses'],
 }
 
-export const execute: ExecuteWithConfig<any> = async (request, _, config) => {
+interface AddressWithBalance {
+  address: string
+  balance: number | BigInteger
+}
+
+interface Address {
+  address: string
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
   const validator = new Validator(request, inputParameters)
   if (validator.error) throw validator.error
 
   const jobRunID = validator.validated.id
   const addresses = validator.validated.data.addresses
 
-  const response = await config.provider.getBalance(addresses)
-  console.log('response', utils.formatEther(response))
-  // const result = Requester.validateResultNumber(response.data,)
+  if (!addresses || !Array.isArray(addresses) || addresses.length === 0) {
+    throw new AdapterError({
+      jobRunID,
+      message: `Input, at 'address' path, must be a non-empty array.`,
+      statusCode: 400,
+    })
+  }
 
-  return Requester.success(jobRunID, response)
+  const balances = await Promise.all<AddressWithBalance>(
+    addresses.map((addr: Address) => getBalance(addr.address, config)),
+  )
+  const formattedBalances = balances.map((balance) => ({
+    ...balance,
+    balance: utils.formatEther(balance.balance),
+  }))
+  const result = { jobRunID, statusCode: 200, data: formattedBalances, result: formattedBalances }
+  console.log(result)
+  return Requester.success(jobRunID, result)
 }
+
+const getBalance: any = async (address: string, config: Config) => ({
+  address,
+  balance: await config.provider.getBalance(address),
+})
