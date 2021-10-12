f33f4ff74755ac7150d89db3698ccf84e4e56854
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-10-12 09:39:09 -0700

    add test for when response from ap election has multiple levels

--- packages/sources/ap-election/test/integration/election.test.ts
+++ packages/sources/ap-election/test/integration/election.test.ts
@@ -4,7 +4,13 @@ import process from 'process'
 import nock from 'nock'
 import http from 'http'
 import { server as startServer } from '../../src'
-import { mockStatusLevelResponse } from './fixtures'
+import {
+  mockResponseWithMultipleRaces,
+  mockResponseWithNationalAndState,
+  mockResponseWithNoRaces,
+  mockResponseWithStateAndDistrict,
+  mockStatusLevelResponse,
+} from './fixtures'
 
 let oldEnv: NodeJS.ProcessEnv
 
@@ -39,6 +45,58 @@ describe('execute', () => {
     server.close(done)
   })
 
+  describe('with national level response', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        date: '2020-11-08',
+        statePostal: 'VA',
+        level: 'state',
+        officeID: 'P',
+        raceType: 'G',
+      },
+    }
+
+    mockResponseWithNationalAndState()
+
+    it('should return success', async () => {
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('with response having both state and disrict reporting units', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        date: '2021-06-08',
+        statePostal: 'VA',
+        level: 'state',
+        officeID: 'A',
+        raceType: 'D',
+      },
+    }
+
+    mockResponseWithStateAndDistrict()
+
+    it('should return success', async () => {
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
   describe('with state level response', () => {
     const data: AdapterRequest = {
       id,
@@ -61,6 +119,58 @@ describe('execute', () => {
         .set('Content-Type', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('with no races', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        date: '2021-06-08',
+        statePostal: 'VA',
+        level: 'state',
+        officeID: 'A',
+        raceType: 'D',
+      },
+    }
+
+    mockResponseWithNoRaces()
+
+    it('should return error', async () => {
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('with multiple races', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        date: '2021-06-08',
+        statePostal: 'VA',
+        level: 'state',
+        officeID: 'A',
+        raceType: 'D',
+      },
+    }
+
+    mockResponseWithMultipleRaces()
+
+    it('should return error', async () => {
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500)
       expect(response.body).toMatchSnapshot()
     })
   })
