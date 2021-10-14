fa1680bfef4a0938a79dd1d9875faeec4fa9182b
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-10-14 10:27:22 -0700

    add nba endpoint to sportsdataio adapter

--- packages/sources/sportsdataio/src/config.ts
+++ packages/sources/sportsdataio/src/config.ts
@@ -10,11 +10,13 @@ export const DEFAULT_BASE_URL = 'https://fly.sportsdata.io/v3'
 export const ENV_NFL_SCORES_API_KEY = 'NFL_SCORES_API_KEY'
 export const ENV_MMA_STATS_API_KEY = 'MMA_STATS_API_KEY'
 export const ENV_CFB_SCORES_API_KEY = 'CFB_SCORES_API_KEY'
+export const ENV_NBA_API_KEY = 'NBA_API_KEY'
 
 export type Config = ChainlinkConfig & {
   nflScoresKey?: string
   mmaStatsKey?: string
   cfbScoresKey?: string
+  nbaKey?: string
 }
 
 export const makeConfig = (prefix?: string): Config => {
@@ -23,6 +25,7 @@ export const makeConfig = (prefix?: string): Config => {
     nflScoresKey: util.getEnv(ENV_NFL_SCORES_API_KEY, prefix),
     mmaStatsKey: util.getEnv(ENV_MMA_STATS_API_KEY, prefix),
     cfbScoresKey: util.getEnv(ENV_CFB_SCORES_API_KEY, prefix),
+    nbaKey: util.getEnv(ENV_NBA_API_KEY, prefix),
   }
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
 
---
+++ packages/sources/sportsdataio/src/sport/nba/endpoint/player-stats.ts
@@ -0,0 +1,173 @@
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { ExecuteWithConfig } from '@chainlink/types'
+import { Config } from '../../../config'
+import { utils } from 'ethers'
+
+export const NAME = 'player-stats'
+
+const DEFAULT_PLAYER_ID = 20002528 // LaMelo Ball
+
+const customParams = {
+  date: true,
+  playerID: false,
+}
+
+export interface ResponseSchema {
+  StatID: number
+  TeamID: number
+  PlayerID: number
+  SeasonType: number
+  Season: number
+  Name: string
+  Team: string
+  Position: string
+  Started: number
+  FanDuelSalary: number
+  DraftKingSalary: number
+  FantasyDuelSalary: number
+  YahooSalary: number | null
+  InjuryStatus: string | null
+  InjuryBodyPart: string | null
+  InjuryStartDate: string | null
+  InjuryNotes: string | null
+  FanDuelPosition: string
+  DraftKingPosition: string
+  YahooPosition: string | null
+  OpponentRank: number
+  OpponentPositionRank: number
+  GlobalTeamID: number
+  FantasyDraftSalary: number | null
+  FantasyDraftPosition: string | null
+  GameID: number
+  OpponentID: number
+  Opponent: string
+  Day: string
+  DateTime: string
+  HomeOrAway: string
+  isGameOver: true
+  GlobalGameID: number
+  GlobalOpponentID: number
+  Updated: string
+  Games: number
+  FantasyPoints: number
+  Minutes: number
+  Seconds: number
+  FieldGoalsMade: number
+  FieldGoalsAttempted: number
+  FieldGoalsPercentage: number
+  EffectiveFieldGoalsPercentage: number
+  TwoPointersMade: number
+  TwoPointersAttempted: number
+  TwoPointersPercentage: number
+  ThreePointersMade: number
+  ThreePointersAttempted: number
+  ThreePointersPercentage: number
+  FreeThrowsMade: number
+  FreeThrowsAttempted: number
+  FreeThrowPercentage: number
+  OffensiveRebounds: number
+  DefensiveRebounds: number
+  Rebounds: number
+  OffensiveReboundsPercentage: number
+  DefensiveReboundsPercenage: number
+  TotalReboundsPercentage: number
+  Assists: number
+  Steals: number
+  BlockedShots: number
+  Turnovers: number
+  PersonalFouls: number
+  Points: number
+  TrueShootingAttempts: number
+  TrueShootingPercentage: number
+  PlayerEfficiencyRating: number
+  AssistsPercentage: number
+  StealsPercentage: number
+  BlocksPercentage: number
+  TurnOversPercentage: number
+  UsageRatePercentage: number
+  FantasyPointsFanDuel: number
+  FantasyPointsDraftKings: number
+  FantasyPointsYahoo: number
+  PlusMinus: number
+  DoubleDoubles: number
+  TripleDoubles: number
+  FantasyPointsFantasyDraft: number
+  isClosed: boolean
+  LineupConfirmed: boolean
+  LineupStatus: string
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
+  const validator = new Validator(request, customParams)
+  if (validator.error) throw validator.error
+
+  const jobRunID = validator.validated.id
+  const { date, playerID } = validator.validated.data
+  const url = `/nba/stats/json/PlayerGameStatsByPlayer/${date}/${playerID || DEFAULT_PLAYER_ID}`
+
+  const params = {
+    key: config.nbaKey,
+  }
+
+  const options = { ...config.api, params, url }
+
+  const response = await Requester.request<ResponseSchema>(options)
+  return Requester.success(
+    jobRunID,
+    Requester.withResult(response, packResponse(response.data)),
+    config.verbose,
+  )
+}
+
+const packResponse = (response: ResponseSchema): string => {
+  const dataTypes = [
+    'uint16',
+    'uint32',
+    'bool',
+    'bool',
+    'uint32',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint8',
+    'uint16',
+  ]
+  const epochSeconds = new Date(response.DateTime).getTime() / 1000
+  const dataValues = [
+    response.Season,
+    epochSeconds,
+    response.HomeOrAway === 'HOME',
+    response.isGameOver,
+    response.GlobalGameID,
+    response.FieldGoalsMade,
+    response.FieldGoalsAttempted,
+    response.TwoPointersMade,
+    response.TwoPointersAttempted,
+    response.ThreePointersMade,
+    response.ThreePointersAttempted,
+    response.FreeThrowsMade,
+    response.FreeThrowsAttempted,
+    response.OffensiveRebounds,
+    response.DefensiveRebounds,
+    response.Rebounds,
+    response.Assists,
+    response.Steals,
+    response.BlockedShots,
+    response.DoubleDoubles,
+    response.TripleDoubles,
+    response.Points,
+  ]
+  return utils.solidityPack(dataTypes, dataValues)
+}
