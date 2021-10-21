3df96b32f09c815f5906e3aa2838090911f62118
karen-stepanyan <karen.stepanyan@altoros.com>
2021-10-22 01:01:20 +0400

    kaiko integration tests

--- packages/sources/kaiko/src/index.ts
+++ packages/sources/kaiko/src/index.ts
@@ -2,4 +2,5 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute } from './adapter'
 import { makeConfig, NAME } from './config'
 
-export = { NAME, makeExecute, makeConfig, ...expose(NAME, makeExecute()) }
+const { server } = expose(NAME, makeExecute(), undefined)
+export { NAME, makeExecute, makeConfig, server }
