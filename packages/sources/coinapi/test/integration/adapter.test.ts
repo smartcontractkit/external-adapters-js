fb70e42a151746a31377071177305623275faab0
Austin Born <austinborn212@gmail.com>
2021-10-21 13:24:04 -0700

    test: coin API Integration tests

--- packages/sources/coinapi/test/integration/adapter.test.ts
+++ packages/sources/coinapi/test/integration/adapter.test.ts
@@ -1,28 +1,29 @@
 import { AdapterRequest } from '@chainlink/types'
+import { util } from '@chainlink/ea-bootstrap'
 import http from 'http'
 import nock from 'nock'
 import request from 'supertest'
 import { server as startServer } from '../../src/index'
-import { mockCryptoEndpointFailure, mockCryptoEndpointSuccess } from './cryptoFixtures'
-
-let oldEnv: NodeJS.ProcessEnv
+import { mockCryptoEndpointSuccess } from './cryptoFixtures'
 
 describe('coinmarketcap', () => {
   let server: http.Server
+  const oldEnv: NodeJS.ProcessEnv = JSON.parse(JSON.stringify(process.env))
   const req = request('localhost:8080')
 
   beforeAll(async () => {
-    oldEnv = JSON.parse(JSON.stringify(process.env))
     process.env.CACHE_ENABLED = 'false'
-    process.env.API_KEY = process.env.API_KEY || 'mock-api-key'
-    if (process.env.RECORD) {
+
+    if (util.parseBool(process.env.RECORD)) {
       nock.recorder.rec()
+    } else {
+      process.env.API_KEY = 'mock-api-key'
     }
     server = await startServer()
   })
   afterAll((done) => {
     process.env = oldEnv
-    if (process.env.RECORD) {
+    if (util.parseBool(process.env.RECORD)) {
       nock.recorder.play()
     }
 
@@ -51,27 +52,6 @@ describe('coinmarketcap', () => {
           .set('Content-Type', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
-        expect(response.body).toMatchSnapshot()
-      })
-    })
-
-    describe('when sending request without quote asset', () => {
-      it('should reply with failure', async () => {
-        const cryptoRequest: AdapterRequest = {
-          id: '1',
-          data: {
-            endpoint: 'crypto',
-            base: 'ETH',
-          },
-        }
-        mockCryptoEndpointFailure()
-        const response = await req
-          .post('/')
-          .send(cryptoRequest)
-          .set('Accept', '*/*')
-          .set('Content-Type', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(400)
         expect(response.body).toMatchSnapshot()
       })
     })
