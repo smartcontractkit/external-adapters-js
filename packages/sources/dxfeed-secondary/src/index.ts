dc0301b4b815cf27b97e750abd9ab62f023d7429
Justin Kaseman <justinkaseman@live.com>
2021-10-22 14:19:21 -0700

    fix(dxfeed): fix root exports

--- packages/sources/dxfeed-secondary/src/index.ts
+++ packages/sources/dxfeed-secondary/src/index.ts
@@ -2,9 +2,7 @@ import { expose } from '@chainlink/ea-bootstrap'
 import * as dxfeed from '@chainlink/dxfeed-adapter'
 import { makeConfig, NAME } from './config'
 
-export = {
-  NAME,
-  makeExecute: dxfeed.makeExecute,
-  makeConfig,
-  ...expose(NAME, dxfeed.makeExecute(), dxfeed.makeWSHandler(), dxfeed.endpointSelector),
-}
+const makeExecute = dxfeed.makeExecute
+const server = expose(NAME, makeExecute(), dxfeed.makeWSHandler(), dxfeed.endpointSelector)
+
+export { NAME, makeExecute, makeConfig, server }
--- packages/sources/dxfeed/src/index.ts
+++ packages/sources/dxfeed/src/index.ts
@@ -2,11 +2,6 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { endpointSelector, makeExecute, makeWSHandler } from './adapter'
 import { makeConfig, NAME } from './config'
 
-export = {
-  NAME,
-  makeExecute,
-  makeWSHandler,
-  makeConfig,
-  endpointSelector,
-  ...expose(NAME, makeExecute(), makeWSHandler(), endpointSelector),
-}
+const server = expose(NAME, makeExecute(), makeWSHandler(), endpointSelector)
+
+export { NAME, makeExecute, makeWSHandler, makeConfig, endpointSelector, server }
