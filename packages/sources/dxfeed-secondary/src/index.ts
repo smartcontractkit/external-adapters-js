e1a468d084a2fdb110f13296b492b9ccec71740e
Christopher Dimitri Sastropranoto <christopher.sastropranoto@smartcontract.com>
2021-10-26 08:26:50 -1000

    fix dxfeed adapter exports (#1036)

--- packages/sources/dxfeed-secondary/src/index.ts
+++ packages/sources/dxfeed-secondary/src/index.ts
@@ -3,6 +3,6 @@ import * as dxfeed from '@chainlink/dxfeed-adapter'
 import { makeConfig, NAME } from './config'
 
 const makeExecute = dxfeed.makeExecute
-const server = expose(NAME, makeExecute(), dxfeed.makeWSHandler(), dxfeed.endpointSelector)
+const { server } = expose(NAME, makeExecute(), dxfeed.makeWSHandler(), dxfeed.endpointSelector)
 
 export { NAME, makeExecute, makeConfig, server }
