aa1c925744040fd7538e5c80dbbc8a86464e846f
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2021-10-26 10:20:00 -0700

    fix dxfeed adapter exports

--- packages/sources/dxfeed/src/index.ts
+++ packages/sources/dxfeed/src/index.ts
@@ -2,6 +2,6 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { endpointSelector, makeExecute, makeWSHandler } from './adapter'
 import { makeConfig, NAME } from './config'
 
-const server = expose(NAME, makeExecute(), makeWSHandler(), endpointSelector)
+const { server } = expose(NAME, makeExecute(), makeWSHandler(), endpointSelector)
 
 export { NAME, makeExecute, makeWSHandler, makeConfig, endpointSelector, server }
