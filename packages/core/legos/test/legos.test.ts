679d0da0f251e97c566d897830a6522515377fe6
Silas Lenihan <sjl58@duke.edu>
2021-11-01 15:40:48 -0400

    Created  test for Adapter names

---
+++ packages/core/legos/test/legos.test.ts
@@ -0,0 +1,9 @@
+import legos from '../src'
+
+describe('legos test', () => {
+  it('has valid adapter names', function () {
+    for (const source of legos.sources) {
+      expect(source.includes('-')).toBe(false)
+    }
+  })
+})
