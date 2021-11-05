edd29f46710bd04a3484f134f27d1e5bca3c4def
karen-stepanyan <karen.stepanyan@altoros.com>
2021-11-05 21:20:38 +0400

    add integration tests

--- packages/sources/tradermade/src/index.ts
+++ packages/sources/tradermade/src/index.ts
@@ -2,4 +2,5 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler } from './adapter'
 import { makeConfig, NAME } from './config'
 
-export = { NAME, makeExecute, makeConfig, ...expose(NAME, makeExecute(), makeWSHandler()) }
+const { server } = expose(NAME, makeExecute(), makeWSHandler())
+export { NAME, makeExecute, makeConfig, server }
