0779592a4f646c5e72e8674d8a3af6177ac33075
Silas Lenihan <sjl58@duke.edu>
2021-11-05 11:15:21 -0400

    Added more comprehensive tests

--- packages/core/legos/test/unit/legos.test.ts
+++ packages/core/legos/test/unit/legos.test.ts
@@ -1,9 +1,21 @@
 import legos from '../../src'
 
 describe('legos test', () => {
-  it('has valid adapter names', function () {
+  it('has no unnacceptable characters', function () {
     for (const source of legos.sources) {
-      expect(source.includes('-')).toBe(false)
+      const pattern = new RegExp(/[~`!#$%^&*+=\-[\]\\';,/{}|\\":<>?]/)
+      expect(pattern.test(source)).toBe(false)
+    }
+  })
+  it('doesnt begin with a digit', function () {
+    for (const source of legos.sources) {
+      const isDigit = source.charAt(0) >= '0' && source.charAt(0) <= '9'
+      expect(isDigit).toBe(false)
+    }
+  })
+  it('contains only uppercase letters', function () {
+    for (const source of legos.sources) {
+      expect(source.toUpperCase() === source).toBe(true)
     }
   })
 })
