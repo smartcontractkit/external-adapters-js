dbb1ef0580f09e9e4c1102dfb1cf38234456a911
karen-stepanyan <karen.stepanyan@altoros.com>
2021-11-09 23:55:56 +0400

    add integration tests

---
+++ packages/sources/taapi/test/integration/adapter.test.ts
@@ -0,0 +1,83 @@
+import { AdapterRequest } from '@chainlink/types'
+import request from 'supertest'
+import * as process from 'process'
+import { server as startServer } from '../../src'
+import * as nock from 'nock'
+import * as http from 'http'
+import { mockResponseSuccess } from './fixtures'
+
+describe('execute', () => {
+  const id = '1'
+  let server: http.Server
+  const req = request('localhost:8080')
+  beforeAll(async () => {
+    process.env.CACHE_ENABLED = 'false'
+    process.env.API_KEY = process.env.API_KEY || 'fake-api-key'
+    if (process.env.RECORD) {
+      nock.recorder.rec()
+    }
+    server = await startServer()
+  })
+  afterAll((done) => {
+    if (process.env.RECORD) {
+      nock.recorder.play()
+    }
+
+    nock.restore()
+    nock.cleanAll()
+    nock.enableNetConnect()
+    server.close(done)
+  })
+
+  describe('avgprice indicator api', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        base: 'BTC',
+        quote: 'USDT',
+        exchange: 'binance',
+        indicator: 'avgprice',
+        interval: '1h',
+      },
+    }
+
+    it('should return success', async () => {
+      mockResponseSuccess()
+
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('cci indicator api', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        base: 'BTC',
+        quote: 'USDT',
+        exchange: 'binance',
+        indicator: 'cci',
+        interval: '1h',
+      },
+    }
+
+    it('should return success', async () => {
+      mockResponseSuccess()
+
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+})
---
+++ packages/sources/taapi/test/integration/fixtures.ts
@@ -0,0 +1,30 @@
+import nock from 'nock'
+
+export const mockResponseSuccess = (): nock =>
+  nock('https://api.taapi.io', {
+    encodedQueryParams: true,
+  })
+    .get('/avgprice')
+    .query({ exchange: 'binance', symbol: 'BTC/USDT', interval: '1h', secret: 'fake-api-key' })
+    .reply(200, (_, request) => ({ value: 66470.04250000001 }), [
+      'Content-Type',
+      'application/json',
+      'Connection',
+      'close',
+      'Vary',
+      'Accept-Encoding',
+      'Vary',
+      'Origin',
+    ])
+    .get('/cci')
+    .query({ exchange: 'binance', symbol: 'BTC/USDT', interval: '1h', secret: 'fake-api-key' })
+    .reply(200, (_, request) => ({ value: -109.20727257685407 }), [
+      'Content-Type',
+      'application/json',
+      'Connection',
+      'close',
+      'Vary',
+      'Accept-Encoding',
+      'Vary',
+      'Origin',
+    ])
