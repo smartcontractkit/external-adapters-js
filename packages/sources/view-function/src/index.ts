7f31cce5e9ad2f78b5e8ae6f477bc38e4aed5f3a
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2021-11-12 00:42:59 +0400

    view-function integration tests (#1087)

--- packages/sources/view-function/src/index.ts
+++ packages/sources/view-function/src/index.ts
@@ -1,10 +1,7 @@
 import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, endpointSelector } from './adapter'
 import { makeConfig, NAME } from './config'
+import * as types from './endpoint'
 
-export = {
-  NAME,
-  makeExecute,
-  makeConfig,
-  ...expose(NAME, makeExecute(), undefined, endpointSelector),
-}
+const { server } = expose(NAME, makeExecute(), undefined, endpointSelector)
+export { NAME, makeExecute, makeConfig, server, types }
