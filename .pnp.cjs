4e9681bdec67185672c90ea5af81908527422b3a
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2021-11-12 00:44:55 +0400

    Twelvedata integration tests (#1089)

--- .pnp.cjs
+++ .pnp.cjs
@@ -6133,6 +6133,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@chainlink/types", "workspace:packages/core/types/@chainlink"],
             ["@types/jest", "npm:27.0.2"],
             ["@types/node", "npm:14.17.21"],
+            ["nock", "npm:13.1.3"],
+            ["supertest", "npm:6.1.6"],
             ["tslib", "npm:2.3.1"],
             ["typescript", "patch:typescript@npm%3A4.3.5#~builtin<compat/typescript>::version=4.3.5&hash=d8b4e7"]
           ],
