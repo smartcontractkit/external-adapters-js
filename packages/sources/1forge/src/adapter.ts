4fd0df8ea90621c168084386a2135d9704fbd1f4
karen-stepanyan <karen.stepanyan@altoros.com>
2021-11-16 19:50:47 +0400

    add makeWSHandler

--- packages/sources/1forge/src/adapter.ts
+++ packages/sources/1forge/src/adapter.ts
@@ -1,10 +1,11 @@
-import { Builder } from '@chainlink/ea-bootstrap'
+import { Builder, Requester, Validator } from '@chainlink/ea-bootstrap'
 import {
   Config,
   ExecuteWithConfig,
   ExecuteFactory,
   AdapterRequest,
   APIEndpoint,
+  MakeWSHandler,
 } from '@chainlink/types'
 import { makeConfig } from './config'
 import * as endpoints from './endpoint'
@@ -18,4 +19,38 @@ export const endpointSelector = (request: AdapterRequest): APIEndpoint =>
 
 export const makeExecute: ExecuteFactory<Config> = (config) => {
   return async (request, context) => execute(request, context, config || makeConfig())
+}
+
+export const makeWSHandler = (): MakeWSHandler => {
+  const getSubscription = (symbol?: string) => {
+    if (!symbol) return
+    return 'login'
+  }
+  const getSymbol = (input: AdapterRequest) => {
+    const validator = new Validator(input, endpoints.forex.inputParameters, {})
+    if (validator.error) return
+    const symbol = validator.validated.data.base.toUpperCase()
+    const convert = validator.validated.data.quote.toUpperCase()
+    return `${symbol.toLowerCase()}${convert.toLowerCase()}`
+  }
+  return () => {
+    return {
+      connection: {
+        url: 'wss://sockets.1forge.com/socket',
+      },
+      subscribe: (input) => getSubscription(getSymbol(input)),
+      noHttp: true,
+      unsubscribe: (input) => getSubscription(getSymbol(input)),
+      subsFromMessage: (message) => {
+        if (!message.s) return undefined
+        return getSubscription(`${message.s.toLowerCase()}`)
+      },
+      isError: (message: any) => message.type === 'error',
+      filter: () => false,
+      toResponse: (message: any) => {
+        const result = Requester.validateResultNumber(message, ['c'])
+        return Requester.success('1', { data: { result } })
+      },
+    }
+  }
 }
