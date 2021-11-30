bf14d6d9dfc2c9a17e407d732911c58619c80076
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2021-11-30 23:03:24 +0400

    Cryptomkt integration tests (#1209)

---
+++ packages/sources/cryptomkt/test/integration/fixtures.ts
@@ -0,0 +1,32 @@
+import nock from 'nock'
+
+export const mockResponseSuccess = (): nock =>
+  nock('https://api.exchange.cryptomkt.com/api/3', {
+    encodedQueryParams: true,
+  })
+    .get('/public/ticker/BTCARS')
+    .query((query) => true)
+    .reply(
+      200,
+      (_, request) => ({
+        ask: '12395990',
+        bid: '12339900',
+        last: '12396935',
+        low: '11716731',
+        high: '12403061',
+        open: '11845809',
+        volume: '1.62057',
+        volume_quote: '19483671.75328',
+        timestamp: '2021-11-25T16:27:54.000Z',
+      }),
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
