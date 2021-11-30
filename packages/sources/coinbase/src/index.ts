7a88772abc9d7eb71b09c42d2082099406db1b9f
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2021-11-30 22:59:15 +0400

    coinbase integration tests (#1141)

--- packages/sources/coinbase/src/index.ts
+++ packages/sources/coinbase/src/index.ts
@@ -1,10 +1,6 @@
 import { expose } from '@chainlink/ea-bootstrap'
-import { endpointSelector, makeExecute, makeWSHandler } from './adapter'
+import { makeExecute, makeWSHandler, endpointSelector } from './adapter'
 import { makeConfig, NAME } from './config'
 
-export = {
-  NAME,
-  makeExecute,
-  makeConfig,
-  ...expose(NAME, makeExecute(), makeWSHandler(), endpointSelector),
-}
+const { server } = expose(NAME, makeExecute(), makeWSHandler(), endpointSelector)
+export { NAME, makeExecute, makeConfig, server }
