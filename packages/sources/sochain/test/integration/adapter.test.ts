4d61c0d1a4063bc314e568055eef8ed65d5cab8e
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2021-11-30 23:14:01 +0400

    Sochain integration tests (#1235)

---
+++ packages/sources/sochain/test/integration/adapter.test.ts
@@ -0,0 +1,62 @@
+import { AdapterRequest } from '@chainlink/types'
+import request from 'supertest'
+import * as process from 'process'
+import { server as startServer } from '../../src'
+import * as nock from 'nock'
+import * as http from 'http'
+import { mockResponseSuccess } from './fixtures'
+
+describe('execute', () => {
+  const id = '1'
+  let server: http.Server
+  const req = request('localhost:8080')
+  beforeAll(async () => {
+    if (process.env.RECORD) {
+      nock.recorder.rec()
+    }
+    server = await startServer()
+  })
+  afterAll((done) => {
+    if (process.env.RECORD) {
+      nock.recorder.play()
+    }
+
+    nock.restore()
+    nock.cleanAll()
+    nock.enableNetConnect()
+    server.close(done)
+  })
+
+  describe('balance api', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        addresses: [
+          {
+            address: '3D8DJLwUXFfZvE8yJRu729MZ8uLy25SuLz',
+            coin: 'btc',
+          },
+          {
+            address: '38bzm6nhQMFJe71jJw1U7CbgNrVNpkonZF',
+            coin: 'btc',
+          },
+        ],
+        dataPath: 'addresses',
+        confirmations: 3,
+      },
+    }
+
+    it('should return success', async () => {
+      mockResponseSuccess()
+
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+})
