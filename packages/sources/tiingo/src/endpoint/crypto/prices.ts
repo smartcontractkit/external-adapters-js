5c7c82a397f520e45dedcdb14db5cefab020ffdf
karen-stepanyan <karen.stepanyan@altoros.com>
2021-12-21 20:17:13 +0400

    [Refactor] tiingo response types

--- packages/sources/tiingo/src/endpoint/crypto/prices.ts
+++ packages/sources/tiingo/src/endpoint/crypto/prices.ts
@@ -63,13 +63,8 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
     url,
   }
 
-  const response = await Requester.request(options, customError)
-  response.data.result = Requester.validateResultNumber(response.data as ResponseSchema[], [
-    0,
-    'priceData',
-    0,
-    resultPath,
-  ])
+  const response = await Requester.request<ResponseSchema[]>(options, customError)
+  const result = Requester.validateResultNumber(response.data, [0, 'priceData', 0, resultPath])
 
-  return Requester.success(jobRunID, response, config.verbose)
+  return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
 }
