09866bebd9cf709a78c3676dd2c6071406c3bae6
karen-stepanyan <karen.stepanyan@altoros.com>
2022-01-03 21:18:54 +0400

    [Refactor] ethgasstation types

--- packages/sources/ethgasstation/src/endpoint/gasprice.ts
+++ packages/sources/ethgasstation/src/endpoint/gasprice.ts
@@ -3,10 +3,23 @@ import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/types'
 
 export const supportedEndpoints = ['gasprice']
 
-const customError = (data: any) => data.Response === 'Error'
-
 export const inputParameters: InputParameters = {
   speed: false,
+}
+
+export interface ResponseSchema {
+  fast: number
+  fastest: number
+  safeLow: number
+  average: number
+  block_time: number
+  blockNum: number
+  speed: number
+  safeLowWait: number
+  avgWait: number
+  fastWait: number
+  fastestWait: number
+  gasPriceRange: Record<string, number>
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
@@ -22,7 +35,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
     url,
   }
 
-  const response = await Requester.request(options, customError)
+  const response = await Requester.request(options)
   response.data.result = Requester.validateResultNumber(response.data, [speed]) * 1e8
 
   return Requester.success(jobRunID, response, config.verbose)
