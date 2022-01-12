3125d9539bfe481c7933cd3af5b06b0b0e987712
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2022-01-12 10:19:29 -0800

    add api key support for coinpaprika

--- packages/sources/coinpaprika/src/config.ts
+++ packages/sources/coinpaprika/src/config.ts
@@ -1,4 +1,4 @@
-import { Requester } from '@chainlink/ea-bootstrap'
+import { Requester, util } from '@chainlink/ea-bootstrap'
 import { Config } from '@chainlink/types'
 
 export const NAME = 'COINPAPRIKA'
@@ -8,9 +8,17 @@ export const DEFAULT_API_ENDPOINT = 'https://api.coinpaprika.com'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
+
+  const headers: { [T: string]: string | boolean } = {}
+  if (config.apiKey) headers['Authorization'] = config.apiKey
+
+  const isInTestMode = util.getEnv('IS_TEST_MODE', prefix)
+  if (isInTestMode) headers['COINPAPRIKA-API-KEY-VERIFY'] = true
+
   config.api = {
     ...config.api,
     baseURL: config.api.baseURL || DEFAULT_API_ENDPOINT,
+    headers,
   }
   config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
