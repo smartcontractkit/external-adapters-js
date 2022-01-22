92dcb7c5a512bae0017e5985d8f1b7e6a20418dc
AndrN <andre.neyra@gmail.com>
2022-01-21 21:50:11 -0500

    [Refactor] sochain types (#1421)
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>

--- packages/sources/sochain/src/endpoint/balance.ts
+++ packages/sources/sochain/src/endpoint/balance.ts
@@ -8,6 +8,16 @@ export const supportedEndpoints = ['balance']
 
 export const inputParameters = balance.inputParameters
 
+interface ResponseSchema {
+  status: string
+  data: {
+    network: string
+    address: string
+    confirmed_balance: string
+    unconfirmed_balance: string | null
+  }
+}
+
 const getBalanceURI = (account: Account, confirmations: number) => {
   account.coin = account.coin?.toUpperCase()
   if (account.chain === 'testnet') account.coin = account.coin + 'TEST'
@@ -20,7 +30,7 @@ const getBalance: balance.GetBalance = async (account, config) => {
     url: getBalanceURI(account, config.confirmations as number),
   }
 
-  const response = await Requester.request(options)
+  const response = await Requester.request<ResponseSchema>(options)
   // Each BTC has 8 decimal places
   const balance = ethers.utils.parseUnits(response.data.data.confirmed_balance, 8).toString()
 
