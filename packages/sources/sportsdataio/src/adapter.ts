05674b7fb1fc2f3f2c43bec96fe0d7da4b91a1de
bogos <andre.neyra@gmail.com>
2022-02-02 23:08:50 -0500

    Refactor: add sportdataio endpoint folder structure

--- packages/sources/sportsdataio/src/adapter.ts
+++ packages/sources/sportsdataio/src/adapter.ts
@@ -1,45 +1,20 @@
-import { Requester, Validator, AdapterError } from '@chainlink/ea-bootstrap'
-import { Config, ExecuteWithConfig, ExecuteFactory } from '@chainlink/types'
-import { makeConfig, DEFAULT_SPORT } from './config'
-import { MMA, NFL, NCAA_FB, NBA, MLB } from './sport'
-
-const inputParams = {
-  sport: true,
-}
+import { Builder } from '@chainlink/ea-bootstrap'
+import {
+  Config,
+  ExecuteWithConfig,
+  ExecuteFactory,
+  AdapterRequest,
+  APIEndpoint,
+} from '@chainlink/types'
+import { makeConfig } from './config'
+import * as endpoints from './endpoint'
 
 export const execute: ExecuteWithConfig<Config> = async (request, context, config) => {
-  const validator = new Validator(request, inputParams)
-
-  Requester.logConfig(config)
-
-  const jobRunID = validator.validated.id
-  const sport = validator.validated.data.sport || DEFAULT_SPORT
-
-  switch (sport.toLowerCase()) {
-    case MMA.NAME: {
-      return await MMA.execute(request, context, config)
-    }
-    case NFL.NAME: {
-      return await NFL.execute(request, context, config)
-    }
-    case NCAA_FB.NAME: {
-      return await NCAA_FB.execute(request, context, config)
-    }
-    case NBA.NAME: {
-      return await NBA.execute(request, context, config)
-    }
-    case MLB.NAME: {
-      return await MLB.execute(request, context, config)
-    }
-    default: {
-      throw new AdapterError({
-        jobRunID,
-        message: `Sport ${sport} not supported.`,
-        statusCode: 400,
-      })
-    }
-  }
+  return Builder.buildSelector(request, context, config, endpoints)
 }
+
+export const endpointSelector = (request: AdapterRequest): APIEndpoint =>
+  Builder.selectEndpoint(request, makeConfig(), endpoints)
 
 export const makeExecute: ExecuteFactory<Config> = (config) => {
   return async (request, context) => execute(request, context, config || makeConfig())
