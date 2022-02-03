ab04b582cf129d26bd7460ea03b5ff36e9b44966
bogos <andre.neyra@gmail.com>
2022-02-02 23:47:38 -0500

    Refactor: add json-rpc endpoint folder structure

--- packages/sources/json-rpc/src/config.ts
+++ packages/sources/json-rpc/src/config.ts
@@ -5,12 +5,15 @@ export interface ExtendedConfig extends Config {
   RPC_URL?: string
 }
 
+export const NAME = 'JSON_RPC'
+export const DEFAULT_ENDPOINT = 'request'
 export const DEFAULT_BASE_URL = 'http://localhost:8545'
 
 export const makeConfig = (prefix?: string): ExtendedConfig => {
   const RPC_URL = util.getEnv('RPC_URL', prefix)
   return {
     ...Requester.getDefaultConfig(prefix),
+    defaultEndpoint: DEFAULT_ENDPOINT,
     RPC_URL,
   }
 }
