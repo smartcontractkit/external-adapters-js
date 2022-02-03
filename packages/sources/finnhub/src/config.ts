7e0d3af550d243f772bda8aabb46aeb5e6084462
bogos <andre.neyra@gmail.com>
2022-02-02 19:15:24 -0500

    Refactor: add finnhub endpoint folder structure

--- packages/sources/finnhub/src/config.ts
+++ packages/sources/finnhub/src/config.ts
@@ -3,11 +3,12 @@ import { Config } from '@chainlink/types'
 
 export const NAME = 'FINNHUB'
 
-export const DEFAULT_BASE_URL = 'https://finnhub.io/api/v1/'
 export const DEFAULT_ENDPOINT = 'quote'
+export const DEFAULT_BASE_URL = 'https://finnhub.io/api/v1/'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix, true)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
+  config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
 }
