961902b92801814697aa45b70bf58987c206adf4
jpeg <98284627+0xJPEG@users.noreply.github.com>
2022-02-04 17:57:10 -0500

    Set requireKey in config

--- packages/sources/jpegd/src/config.ts
+++ packages/sources/jpegd/src/config.ts
@@ -7,7 +7,7 @@ export const DEFAULT_ENDPOINT = 'punks'
 export const DEFAULT_BASE_URL = 'https://jpegapi.com'
 
 export const makeConfig = (prefix?: string): Config => {
-  const config = Requester.getDefaultConfig(prefix)
+  const config = Requester.getDefaultConfig(prefix, true)
   config.api.baseURL = config.api.baseURL || DEFAULT_BASE_URL
   config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
