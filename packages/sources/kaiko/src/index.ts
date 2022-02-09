d0d80736655445f131fe09a9f93c923612909b11
karen-stepanyan <karen.stepanyan@altoros.com>
2022-02-09 23:55:03 +0400

    format fix

--- packages/sources/kaiko/src/index.ts
+++ packages/sources/kaiko/src/index.ts
@@ -4,7 +4,7 @@ import { makeConfig, NAME } from './config'
 import * as types from './endpoint'
 import rateLimit from './config/limits.json'
 
-const adapterContext = {name: NAME, rateLimit}
+const adapterContext = { name: NAME, rateLimit }
 
 const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
 export { NAME, makeExecute, makeConfig, server, types }
--- packages/sources/renvm-address-set/src/index.ts
+++ packages/sources/renvm-address-set/src/index.ts
@@ -5,6 +5,5 @@ import * as types from './endpoint'
 
 const adapterContext = { name: NAME }
 
-
 const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
 export { NAME, makeExecute, makeConfig, server, types }
