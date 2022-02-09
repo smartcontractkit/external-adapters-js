4cb4a72dafcd7a340bd1cdd1c46132d365c53928
karen-stepanyan <karen.stepanyan@altoros.com>
2022-02-09 12:38:33 +0400

    add missing config folders for source adapters

--- packages/sources/intrinio/src/index.ts
+++ packages/sources/intrinio/src/index.ts
@@ -2,4 +2,11 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, makeWSHandler } from './adapter'
 import { makeConfig, NAME } from './config'
 
-export = { NAME, makeExecute, makeConfig, ...expose(NAME, makeExecute(), makeWSHandler()) }
+const adapterContext = { name: NAME }
+
+export = {
+  NAME,
+  makeExecute,
+  makeConfig,
+  ...expose(adapterContext, makeExecute(), makeWSHandler()),
+}
