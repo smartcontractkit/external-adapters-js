2926047622e81c53ddc9302e4fa740d7eb8bf5ff
karen-stepanyan <karen.stepanyan@altoros.com>
2022-02-09 23:15:02 +0400

    dxfeed-secondary and mock-ea config and presets

--- packages/sources/mock-ea/src/index.ts
+++ packages/sources/mock-ea/src/index.ts
@@ -2,6 +2,7 @@ import { expose } from '@chainlink/ea-bootstrap'
 import { makeExecute, endpointSelector } from './adapter'
 import { makeConfig, NAME } from './config'
 import * as types from './endpoint'
+const adapterContext = { name: NAME }
 
-const { server } = expose(NAME, makeExecute(), undefined, endpointSelector)
+const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
 export { NAME, makeExecute, makeConfig, server, types }
