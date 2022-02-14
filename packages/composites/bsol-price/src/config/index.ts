c94fba88be7d53b16093c9bf3e16329f251cfecd
karen-stepanyan <karen.stepanyan@altoros.com>
2022-02-15 00:41:15 +0400

    config and presets for solana-view-function and bsol-price

--- packages/composites/bsol-price/src/config/index.ts
+++ packages/composites/bsol-price/src/config/index.ts
@@ -3,6 +3,8 @@ import { Config as DefaultConfig } from '@chainlink/types'
 
 export const DEFAULT_ENDPOINT = 'price'
 
+export const NAME = 'BSOL_PRICE'
+
 export const DEFAULT_SOLIDO_ADDRESS = 'EMtjYGwPnXdtqK5SGL8CWGv4wgdBQN79UPoy53x9bBTJ'
 export const DEFAULT_STSOL_ADDRESS = 'BSGfVnE6q6KemspkugEERU8x7WbQwSKwvHT1cZZ4ACVN'
 export const DEFAULT_BSOL_ADDRESS = '3FMBoeddUhtqxepzkrxPrMUV3CL4bZM5QmMoLJfEpirz'
