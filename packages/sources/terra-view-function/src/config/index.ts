b3f3423e4d3d0a083062a3a730dac76f473e6997
silaslenihan <32529249+silaslenihan@users.noreply.github.com>
2022-02-28 18:02:06 -0500

    Refactored Terra RPC_URL environment variables (#1578)
    Co-authored-by: Silas Lenihan <sjl58@duke.edu>

--- packages/sources/terra-view-function/src/config/index.ts
+++ packages/sources/terra-view-function/src/config/index.ts
@@ -11,10 +11,11 @@ export type ChainId = typeof SUPPORTED_CHAIN_IDS[number]
 export const ENV_DEFAULT_CHAIN_ID = 'DEFAULT_CHAIN_ID'
 export const DEFAULT_CHAIN_ID = 'columbus-5'
 
+export const ENV_LCD_URL = 'LCD_URL'
 export const ENV_RPC_URL = 'RPC_URL'
 
 export type Config = BaseConfig & {
-  rpcUrls: Partial<Record<ChainId, string>>
+  lcdUrls: Partial<Record<ChainId, string>>
   defaultChainId: string
 }
 
@@ -23,26 +24,32 @@ export const makeConfig = (prefix?: string): Config => {
 
   return {
     ...baseConfig,
-    rpcUrls: buildRpcUrlMapping(),
+    lcdUrls: buildLcdUrlMapping(),
     defaultChainId: util.getEnv(ENV_DEFAULT_CHAIN_ID, prefix) || DEFAULT_CHAIN_ID,
     defaultEndpoint: DEFAULT_ENDPOINT,
   }
 }
 
-const buildRpcUrlMapping = () => {
+const buildLcdUrlMapping = () => {
   const output: Partial<Record<ChainId, string>> = {}
   let hasAtLeastOneURL = false
   for (const chainId of SUPPORTED_CHAIN_IDS) {
     // Underscore-ize and capitalize to format for environment variables
     const prefix = chainId.replace('-', '_').toUpperCase()
-    const envVar = util.getEnv(ENV_RPC_URL, prefix)
+    const envVar = util.getEnv(ENV_LCD_URL, prefix)
+    const legacyEnvVar = util.getEnv(ENV_RPC_URL, prefix)
     if (envVar) {
       output[chainId] = envVar
       hasAtLeastOneURL = true
+    } else if (legacyEnvVar) {
+      console.log(
+        'The RPC_URL environment variable is being deprecated, please use an LCD_URL instead. See README for more information.',
+      )
+      output[chainId] = legacyEnvVar
+      hasAtLeastOneURL = true
     }
   }
-
-  if (!hasAtLeastOneURL) throw new Error('At least one RPC URL must be defined')
+  if (!hasAtLeastOneURL) throw new Error('At least one LCD URL must be defined')
 
   return output
 }
