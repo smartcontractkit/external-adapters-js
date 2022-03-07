a9c24d086a8c9459e73d7954453a5e02e23c65fa
Matt Kelly <mkelly2@live.co.uk>
2022-03-07 17:48:06 +0800

    Updated the following adapters to use buildUrlPath: bitex, blockchain.com, blockchair, btc.com, coinapi, coinbase, coincodex, coinmetrics, coinpaprika, covidtracker, cryptoapis, cryptoapis-v2, cryptocompare, cryptomkt

---
+++ .changeset/afraid-books-tap.md
@@ -0,0 +1,46 @@
+---
+'@chainlink/ea-bootstrap': minor
+'@chainlink/accuweather-adapter': patch
+'@chainlink/amberdata-adapter': patch
+'@chainlink/ap-election-adapter': patch
+'@chainlink/bitex-adapter': patch
+'@chainlink/blockchain.com-adapter': patch
+'@chainlink/blockchair-adapter': patch
+'@chainlink/btc.com-adapter': patch
+'@chainlink/coinapi-adapter': patch
+'@chainlink/coinbase-adapter': patch
+'@chainlink/coincodex-adapter': patch
+'@chainlink/coinmetrics-adapter': patch
+'@chainlink/coinpaprika-adapter': patch
+'@chainlink/covid-tracker-adapter': patch
+'@chainlink/cryptoapis-adapter': patch
+'@chainlink/cryptoapis-v2-adapter': patch
+'@chainlink/cryptomkt-adapter': patch
+'@chainlink/eodhistoricaldata-adapter': patch
+'@chainlink/expert-car-broker-adapter': patch
+'@chainlink/fcsapi-adapter': patch
+'@chainlink/finage-adapter': patch
+'@chainlink/finnhub-adapter': patch
+'@chainlink/fmpcloud-adapter': patch
+'@chainlink/gemini-adapter': patch
+'@chainlink/geodb-adapter': patch
+'@chainlink/iex-cloud-adapter': patch
+'@chainlink/intrinio-adapter': patch
+'@chainlink/kaiko-adapter': patch
+'@chainlink/linkpool-adapter': patch
+'@chainlink/lition-adapter': patch
+'@chainlink/messari-adapter': patch
+'@chainlink/oilpriceapi-adapter': patch
+'@chainlink/paxos-adapter': patch
+'@chainlink/polygon-adapter': patch
+'@chainlink/satoshitango-adapter': patch
+'@chainlink/sochain-adapter': patch
+'@chainlink/sportsdataio-adapter': patch
+'@chainlink/therundown-adapter': patch
+'@chainlink/tiingo-adapter': patch
+'@chainlink/tradingeconomics-adapter': patch
+'@chainlink/uscpi-one-adapter': patch
+'@chainlink/xbto-adapter': patch
+---
+
+Added buildUrl & buildUrlPath methods to util. Updated source adapters to use these methods for building URLs with user input.
--- packages/core/bootstrap/src/lib/util.ts
+++ packages/core/bootstrap/src/lib/util.ts
@@ -373,3 +373,101 @@ export const getRequiredEnvWithFallback = (
 
   throw new RequiredEnvError(getEnvName(primary, prefix))
 }
+
+//  URL Encoding
+
+const charsToEncode = {
+  ':': '%3A',
+  '/': '%2F',
+  '?': '%3F',
+  '#': '%23',
+  '[': '%5B',
+  ']': '%5D',
+  '@': '%40',
+  '!': '%21',
+  $: '%24',
+  '&': '%26',
+  "'": '%27',
+  '(': '%28',
+  ')': '%29',
+  '*': '%2A',
+  '+': '%2B',
+  ',': '%2C',
+  ';': '%3B',
+  '=': '%3D',
+  '%': '%25',
+  ' ': '%20',
+  '"': '%22',
+  '<': '%3C',
+  '>': '%3E',
+  '{': '%7B',
+  '}': '%7D',
+  '|': '%7C',
+  '^': '%5E',
+  '`': '%60',
+  '\\': '%5C',
+}
+
+/**
+ * Check whether the given string contains characters in the given whitelist.
+ * @param str The string to check.
+ * @param whitelist The string array of whitelist entries. Returns true if any of these are found in 'str', otherwise returns false.
+ * @returns {boolean}
+ */
+const stringHasWhitelist = (str: string, whitelist: string[]): boolean =>
+  whitelist.some((el) => str.includes(el))
+
+/**
+ * Manually iterate through a given string and replace unsafe/reserved characters with encoded values (unless a character is whitelisted)
+ * @param str The string to encode.
+ * @param whitelist The string array of whitelist entries.
+ * @returns {string}
+ */
+const percentEncodeString = (str: string, whitelist: string[]): string =>
+  str
+    .split('')
+    .map((char) => {
+      const encodedValue = charsToEncode[char as keyof typeof charsToEncode]
+      return encodedValue && !whitelist.includes(char) ? encodedValue : char
+    })
+    .join('')
+
+/**
+ * Build a URL path using the given pathTemplate and params. If a param is found in pathTemplate, it will be inserted there; otherwise, it will be ignored.
+ * eg.) pathTemplate = "/from/:from/to/:to" and params = { from: "ETH", to: "BTC", note: "hello" } will become "/from/ETH/to/BTC"
+ * @param pathTemplate The path template for the URL path. Each param to include in the path should have a prefix of ':'.
+ * @param params The object containing keys & values to be added to the URL path.
+ * @param whitelist The list of characters to whitelist for the URL path (if a param contains one of your whitelisted characters, it will not be encoded).
+ * @returns {string}
+ */
+export const buildUrlPath = (pathTemplate = '', params = {}, whitelist = ''): string => {
+  const allowedChars = whitelist.split('')
+
+  for (const param in params) {
+    const value = params[param as keyof typeof params]
+    if (!value) continue
+
+    // If string contains a whitelisted character: manually replace any non-whitelisted characters with percent encoded values. Otherwise, encode the string as usual.
+    const encodedValue = stringHasWhitelist(value, allowedChars)
+      ? percentEncodeString(value, allowedChars)
+      : encodeURIComponent(value)
+
+    if (pathTemplate.includes(`:${param}`))
+      pathTemplate = pathTemplate.replace(`:${param}`, encodedValue)
+  }
+
+  return pathTemplate
+}
+
+/**
+ * Build a full URL using the given baseUrl, pathTemplate and params. Uses buildUrlPath to add path & params.
+ * @param baseUrl The base URL to add the pathTemplate & params to.
+ * @param pathTemplate The path template for the URL path. Leave empty if only searchParams are required.
+ * @param params The object containing keys & values to be added to the URL path.
+ * @param whitelist The list of characters to whitelist for the URL path.
+ * @returns {string}
+ */
+export const buildUrl = (baseUrl: string, pathTemplate = '', params = {}, whitelist = ''): string =>
+  new URL(buildUrlPath(pathTemplate, params, whitelist), baseUrl).toString()
+
+//  URL Encoding
