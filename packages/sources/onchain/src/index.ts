e488715a160ab97eb2a43d97b6484b23d2dd2d40
Justin Kaseman <justinkaseman@live.com>
2022-03-08 09:26:10 -0800

    Remove merge fragments

--- packages/sources/onchain/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { expose } from '@chainlink/ea-bootstrap'
-import { makeExecute } from './adapter'
-import { makeConfig, NAME } from './config'
-
-const { server } = expose(NAME, makeExecute())
-
-export { NAME, makeExecute, makeConfig, server }
