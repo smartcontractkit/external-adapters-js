c6d499af9dd3e9591128e56e20033776d23d6afe
Matt Kelly <mkelly2@live.co.uk>
2022-03-18 12:45:32 +0800

    Updated types for cache.gold

--- packages/sources/cache.gold/src/endpoint/index.ts
+++ packages/sources/cache.gold/src/endpoint/index.ts
@@ -1,1 +1,5 @@
+import type { TInputParameters as LockedGoldInputParameters } from './lockedGold'
+
+export type TInputParameters = LockedGoldInputParameters
+
 export * as lockedGold from './lockedGold'
