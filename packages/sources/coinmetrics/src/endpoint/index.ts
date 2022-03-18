e5d7c8db2d1f5a39ed13a6548ff404352ba5d741
Matt Kelly <mkelly2@live.co.uk>
2022-03-18 19:30:22 +0800

    Updated types for coinmetrics

--- packages/sources/coinmetrics/src/endpoint/index.ts
+++ packages/sources/coinmetrics/src/endpoint/index.ts
@@ -1,3 +1,9 @@
+import type { TInputParameters as PriceInputParameters } from './price'
+import type { TInputParameters as BurnedInputParameters } from './burned'
+import type { TInputParameters as TotalInputParameters } from './total-burned'
+
+export type TInputParameters = PriceInputParameters | BurnedInputParameters | TotalInputParameters
+
 export * as price from './price'
 export * as burned from './burned'
 export * as totalBurned from './total-burned'
