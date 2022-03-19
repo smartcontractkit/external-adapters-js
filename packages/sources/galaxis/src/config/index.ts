2bbc1c8b9d2d2115562f6c5aa2ac79b0b5f590ed
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2022-03-19 11:27:57 +0800

    refactor galaxis EA to map player and team IDs

--- packages/sources/galaxis/src/config/index.ts
+++ packages/sources/galaxis/src/config/index.ts
@@ -7,10 +7,12 @@ export const DEFAULT_ENDPOINT = 'nba'
 export const DEFAULT_EC_REGISTRY_ADDRESS = '0x163883263274e8Ef6332cFa84F35B23c6C51dF72'
 export const DEFAULT_CHAIN_BATCH_WRITE_ADAPTER_ADDRESS =
   '0xB57fba975C89492B016e0215E819B4d489F0fbcD'
-export const DEFAULT_LIMIT_IN_BYTES = 700
+export const DEFAULT_EC_REGISTRY_MAP_ADDRESS = '0x139B522955D54482E7662927653ABb0bFB6F19BA'
+export const DEFAULT_LIMIT_IN_BYTES = 800
 
 export interface ExtendedConfig extends Config {
   ecRegistryAddress: string
+  ecRegistryMapAddress: string
   batchWriterAddress: string
   maxEncodedCallsBytes: number
   rpcUrl: string
@@ -25,6 +27,8 @@ export const makeConfig = (prefix?: string): ExtendedConfig => {
     ...config,
     rpcUrl: util.getRequiredEnv('POLYGON_RPC_URL', prefix),
     ecRegistryAddress: util.getEnv('EC_REGISTRY_ADDRESS', prefix) || DEFAULT_EC_REGISTRY_ADDRESS,
+    ecRegistryMapAddress:
+      util.getEnv('EC_REGISTRY_MAP_ADDRESS', prefix) || DEFAULT_EC_REGISTRY_MAP_ADDRESS,
     batchWriterAddress:
       util.getEnv('CHAIN_BATCH_WRITE_ADAPTER_ADDRESS', prefix) ||
       DEFAULT_CHAIN_BATCH_WRITE_ADAPTER_ADDRESS,
--- packages/sources/galaxis/src/endpoint/nba.ts
+++ packages/sources/galaxis/src/endpoint/nba.ts
@@ -2,7 +2,7 @@ import { Requester, Validator } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, InputParameters } from '@chainlink/types'
 import { ExtendedConfig } from '../config'
 import { ethers } from 'ethers'
-import { EC_REGISTRY_ABI } from './abis'
+import { EC_REGISTRY_ABI, EC_REGISTRY_MAP_ABI } from './abis'
 import { ByteArray } from '@ethercards/ec-util'
 
 export const supportedEndpoints = ['nba']
@@ -17,6 +17,22 @@ export interface Achievement {
 export interface ResponseSchema {
   player_achievements: Achievement[]
   team_achievements: Achievement[]
+}
+
+export interface TeamStruct {
+  id: number
+  name: string
+  city: string
+  tricode: string
+  real_id: ethers.BigNumber
+}
+
+export interface PlayerStruct {
+  id: number
+  team_id: number
+  real_id: ethers.BigNumber
+  real_team_id: ethers.BigNumber
+  full_name: string
 }
 
 const customError = (data: Record<string, unknown>) => data.Response === 'Error'
@@ -54,6 +70,7 @@ export const getFilteredAchievements = async (
 ): Promise<string> => {
   const groupedAchievements = getGroupedAchievementsByID(response)
   const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl)
+  const { teams, players } = await getPlayerAndTeamMaps(provider, config)
   const ecRegistry = new ethers.Contract(config.ecRegistryAddress, EC_REGISTRY_ABI, provider)
   const calls: string[][] = []
 
@@ -65,10 +82,24 @@ export const getFilteredAchievements = async (
   while (!hasHitLimit && currAchievementIdIdx < achievementIDs.length) {
     const achievementID = achievementIDs[currAchievementIdIdx]
     if (await ecRegistry.addressCanModifyTrait(config.batchWriterAddress, achievementID)) {
-      const values = groupedAchievements[achievementID].map(({ value }) => value)
-      const ids = groupedAchievements[achievementID].map(
-        ({ team_id, player_id }) => team_id || player_id,
-      )
+      const values = groupedAchievements[achievementID].map(({ value }) => (value ? 1 : 0))
+      const ids = groupedAchievements[achievementID].map(({ team_id, player_id }) => {
+        if (team_id) {
+          const team = teams.find((t) => t.real_id.toString() === team_id.toString())
+          if (!team) {
+            throw new Error(`Cannot match team ID ${team_id} with response from ECRegistryMap`)
+          }
+          return team.id
+        } else if (player_id) {
+          const player = players.find((p) => p.real_id.toString() === player_id.toString())
+          if (!player) {
+            throw new Error(`Cannot match player ID ${team_id} with response from ECRegistryMap`)
+          }
+          return player.id
+        } else {
+          throw new Error('Missing player and team IDs')
+        }
+      })
       calls.push([
         config.ecRegistryAddress,
         ecRegistry.interface.encodeFunctionData('setData', [parseInt(achievementID), ids, values]),
@@ -81,7 +112,7 @@ export const getFilteredAchievements = async (
     }
     currAchievementIdIdx++
   }
-  return encodedCalls + ethers.utils.defaultAbiCoder.encode(['bool'], [hasHitLimit])
+  return encodedCalls + ethers.utils.defaultAbiCoder.encode(['bool'], [hasHitLimit]).substring(2) // Strip out 0x
 }
 
 export interface AchievementsByIDs {
@@ -139,4 +170,21 @@ const callLentoHex = (num: number): string => {
   const data = new ByteArray(Buffer.alloc(2))
   data.writeUnsignedShort(num / 2)
   return removeZeroX(data.toString('hex'))
+}
+
+export const getPlayerAndTeamMaps = async (
+  provider: ethers.providers.JsonRpcProvider,
+  config: ExtendedConfig,
+): Promise<{ teams: TeamStruct[]; players: PlayerStruct[] }> => {
+  const ecRegistryMap = new ethers.Contract(
+    config.ecRegistryMapAddress,
+    EC_REGISTRY_MAP_ABI,
+    provider,
+  )
+  const teams = await ecRegistryMap.getTeams()
+  const players = await ecRegistryMap.getPlayers()
+  return {
+    teams,
+    players,
+  }
 }
