bf1d906d215601c4eb8e0b57829b0969415d250f
Matt Kelly <mkelly2@live.co.uk>
2022-03-20 17:19:01 +0800

    Updated types for currencylayer

--- packages/sources/currencylayer/src/endpoint/index.ts
+++ packages/sources/currencylayer/src/endpoint/index.ts
@@ -1,2 +1,7 @@
+import type { TInputParameters as ConvertInputParameters } from './convert'
+import type { TInputParameters as LiveInputParameters } from './live'
+
+export type TInputParameters = ConvertInputParameters | LiveInputParameters
+
 export * as convert from './convert'
 export * as live from './live'
