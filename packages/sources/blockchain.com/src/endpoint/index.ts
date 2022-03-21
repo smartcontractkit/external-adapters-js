b0189768028a9b403aea7d63b58f1a65495e8869
Matt Kelly <mkelly2@live.co.uk>
2022-03-21 17:55:34 +0800

    Updated types for blockchain.com

--- packages/sources/blockchain.com/src/endpoint/index.ts
+++ packages/sources/blockchain.com/src/endpoint/index.ts
@@ -1,3 +1,12 @@
+import type { TInputParameters as BalanceInputParameters } from './balance'
+import type { TInputParameters as DifficultyInputParameters } from './difficulty'
+import type { TInputParameters as HeightInputParameters } from './height'
+
+export type TInputParameters =
+  | BalanceInputParameters
+  | DifficultyInputParameters
+  | HeightInputParameters
+
 export * as balance from './balance'
 export * as difficulty from './difficulty'
 export * as height from './height'
