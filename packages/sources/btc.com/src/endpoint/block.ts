2c775612d96dc4ed11e5cb4ce2435084108ce1fc
Matt Kelly <mkelly2@live.co.uk>
2022-03-22 11:52:46 +0800

    Updated types for btc.com

--- packages/sources/btc.com/src/endpoint/block.ts
+++ packages/sources/btc.com/src/endpoint/block.ts
@@ -49,13 +49,14 @@ export interface ResponseSchema {
   status: string
 }
 
-export const inputParameters: InputParameters = {}
+export type TInputParameters = Record<string, never>
+export const inputParameters: InputParameters<TInputParameters> = {}
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters)
+  const validator = new Validator<TInputParameters>(request, inputParameters)
 
   const jobRunID = validator.validated.id
-  const resultPath = validator.validated.data.resultPath
+  const resultPath = (validator.validated.data.resultPath || '').toString()
   const url = `/v3/block/latest`
 
   const options = {
