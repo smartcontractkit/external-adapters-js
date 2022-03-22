b8aedfff15e2d1f5eb28c6aafdd30898d14bdf58
Matt Kelly <mkelly2@live.co.uk>
2022-03-22 16:40:40 +0800

    Updated types for cryptoapis

--- packages/sources/cryptoapis/src/endpoint/index.ts
+++ packages/sources/cryptoapis/src/endpoint/index.ts
@@ -1,3 +1,12 @@
+import type { TInputParameters as BalanceInputParameters } from './balance'
+import type { TInputParameters as CryptoInputParameters } from './crypto'
+import type { TInputParameters as BcInfoInputParameters } from './bc_info'
+
+export type TInputParameters =
+  | BalanceInputParameters
+  | CryptoInputParameters
+  | BcInfoInputParameters
+
 export * as balance from './balance'
 export * as crypto from './crypto'
 export * as bc_info from './bc_info'
