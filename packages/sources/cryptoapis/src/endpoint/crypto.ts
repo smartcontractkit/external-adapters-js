c1f732307d17b71eaa995c157b94d2d3a72f9bfd
Matt Kelly <mkelly2@live.co.uk>
2022-03-22 16:40:40 +0800

    Updated types for cryptoapis

--- packages/sources/cryptoapis/src/endpoint/crypto.ts
+++ packages/sources/cryptoapis/src/endpoint/crypto.ts
@@ -6,7 +6,8 @@ export const supportedEndpoints = ['crypto', 'price']
 export const description =
   '**NOTE: the `price` endpoint is temporarily still supported, however, is being deprecated. Please use the `crypto` endpoint instead.**'
 
-export const inputParameters: InputParameters = {
+export type TInputParameters = { base: string; quote: string }
+export const inputParameters: InputParameters<TInputParameters> = {
   base: {
     aliases: ['from', 'coin'],
     description: 'The symbol of the currency to query',
@@ -33,7 +34,7 @@ export interface ResponseSchema {
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters)
+  const validator = new Validator<TInputParameters>(request, inputParameters)
 
   const jobRunID = validator.validated.id
   const coin = validator.validated.data.base
