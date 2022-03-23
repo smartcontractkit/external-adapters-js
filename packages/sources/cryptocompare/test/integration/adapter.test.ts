029fc5c658f36d822086229c28cc20e78efe2054
karen-stepanyan <karen.stepanyan@altoros.com>
2022-03-23 23:19:55 +0400

    use sleep from util

--- packages/sources/cryptocompare/test/integration/adapter.test.ts
+++ packages/sources/cryptocompare/test/integration/adapter.test.ts
@@ -4,6 +4,7 @@ import * as process from 'process'
 import { server as startServer } from '../../src'
 import * as nock from 'nock'
 import * as http from 'http'
+import { util } from '@chainlink/ea-bootstrap'
 import {
   mockPriceResponseFailure,
   mockPriceResponseSuccess,
@@ -19,10 +20,6 @@ import {
 } from '@chainlink/ea-test-helpers'
 import { WebSocketClassProvider } from '@chainlink/ea-bootstrap/dist/lib/middleware/ws/recorder'
 import { DEFAULT_WS_API_ENDPOINT } from '../../src/config'
-
-const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
 
 describe('execute', () => {
   const id = '1'
@@ -236,7 +233,7 @@ describe('websocket', () => {
 
       // This final request should disable the cache warmer, sleep is used to make sure that the data is  pulled from the websocket
       // populated cache entries.
-      await sleep(10)
+      await util.sleep(10)
       const response = await makeRequest()
 
       expect(response.body).toEqual({
