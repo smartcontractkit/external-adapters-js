2db0ae3bd8cb6b1d519c1ae49aec25ea892d4178
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2022-03-24 09:48:42 -0500

    Setting RPC environment var instead of using fallback RPC node

--- packages/composites/lido/test/integration/adapter.test.ts
+++ packages/composites/lido/test/integration/adapter.test.ts
@@ -7,7 +7,7 @@ import { AddressInfo } from 'net'
 import { mockContractCallResponseSuccess } from './fixtures'
 
 describe('LIDO', () => {
-  process.env.RPC_URL = 'http://localhost:8545'
+  process.env.ETHEREUM_RPC_URL = 'http://localhost:8545'
   mockContractCallResponseSuccess()
 
   let server: http.Server
