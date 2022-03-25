9532057fdd2cc5cf210c7848f1904a93178ac2b0
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2022-03-25 07:12:03 +0700

    fix integration tests

--- packages/sources/galaxis/test/integration/fixtures.ts
+++ packages/sources/galaxis/test/integration/fixtures.ts
@@ -1,6 +1,6 @@
 import nock from 'nock'
 
-export const MOCK_GALAXIS_API_RESP = {
+export const MOCK_BOOLEAN_ONLY_GALAXIS_API_RESP = {
   team_achievements: [
     {
       team_id: 1610612737,
@@ -17,20 +17,100 @@ export const MOCK_GALAXIS_API_RESP = {
     {
       player_id: 203991,
       achievement_id: 15,
-      value: 5,
+      value: false,
     },
     {
       player_id: 203992,
       achievement_id: 17,
-      value: 4,
+      value: true,
     },
   ],
 }
 
-export function mockGalaxisApiResp(apiEndpoint: string): void {
+export function mockBooleanOnlyGalaxisApiResp(apiEndpoint: string): void {
   nock(apiEndpoint, { encodedQueryParams: true })
     .get('/')
-    .reply(200, MOCK_GALAXIS_API_RESP, [
+    .reply(200, MOCK_BOOLEAN_ONLY_GALAXIS_API_RESP, [
+      'Connection',
+      'close',
+      'Content-Length',
+      '4902',
+      'Cache-Control',
+      'max-age=300',
+      'Content-Security-Policy',
+      "default-src 'none'; style-src 'unsafe-inline'; sandbox",
+      'Content-Type',
+      'text/plain; charset=utf-8',
+      'ETag',
+      '"285d7a1190ad122b5ad61b7b75b0569828e3ed075dd27b7db469feb3fbdbe862"',
+      'Strict-Transport-Security',
+      'max-age=31536000',
+      'X-Content-Type-Options',
+      'nosniff',
+      'X-Frame-Options',
+      'deny',
+      'X-XSS-Protection',
+      '1; mode=block',
+      'X-GitHub-Request-Id',
+      '9A9C:6C33:13142D:2167A8:6236DB22',
+      'Accept-Ranges',
+      'bytes',
+      'Date',
+      'Sun, 20 Mar 2022 07:43:30 GMT',
+      'Via',
+      '1.1 varnish',
+      'X-Served-By',
+      'cache-qpg1231-QPG',
+      'X-Cache',
+      'MISS',
+      'X-Cache-Hits',
+      '0',
+      'X-Timer',
+      'S1647762210.020453,VS0,VE325',
+      'Vary',
+      'Authorization,Accept-Encoding,Origin',
+      'Access-Control-Allow-Origin',
+      '*',
+      'X-Fastly-Request-ID',
+      '37ccd72bbaeacb26a5fc47518bfc54fd0b98aff3',
+      'Expires',
+      'Sun, 20 Mar 2022 07:48:30 GMT',
+      'Source-Age',
+      '0',
+    ])
+}
+
+export const MOCK_NON_BOOLEAN_ONLY_GALAXIS_API_RESP = {
+  team_achievements: [
+    {
+      team_id: 1610612737,
+      achievement_id: 2,
+      value: 1,
+    },
+    {
+      team_id: 1610612738,
+      achievement_id: 8,
+      value: 10,
+    },
+  ],
+  player_achievements: [
+    {
+      player_id: 203991,
+      achievement_id: 15,
+      value: 4,
+    },
+    {
+      player_id: 203992,
+      achievement_id: 17,
+      value: 7,
+    },
+  ],
+}
+
+export function mockNonBooleanOnlyGalaxisApiResp(apiEndpoint: string): void {
+  nock(apiEndpoint, { encodedQueryParams: true })
+    .get('/')
+    .reply(200, MOCK_NON_BOOLEAN_ONLY_GALAXIS_API_RESP, [
       'Connection',
       'close',
       'Content-Length',
--- packages/sources/galaxis/test/integration/nba.test.ts
+++ packages/sources/galaxis/test/integration/nba.test.ts
@@ -6,13 +6,16 @@ import request, { SuperTest, Test } from 'supertest'
 
 import { ethers } from 'ethers'
 import { AddressInfo } from 'net'
-import { mockGalaxisApiResp } from './fixtures'
+import { mockBooleanOnlyGalaxisApiResp, mockNonBooleanOnlyGalaxisApiResp } from './fixtures'
 import { Interface } from 'ethers/lib/utils'
-import { EC_REGISTRY_ABI } from '../../src/endpoint/abis'
+import { EC_REGISTRY_ABI, TRAIT_IMPLEMENTER_ABI } from '../../src/abis'
 
-const fakeApiEndpoint = 'http://galaxis.com/achievements/'
+const fakeBooleanOnlyEndpoint = 'http://galaxis.com/achievements/'
+const fakeNonBooleanOnlyEndpoint = 'http://galaxis-fake-non-boolean.com/achievements/'
 
 const mockECRegistryInterface = new Interface(EC_REGISTRY_ABI)
+const mockTraitImplementerInterface = new Interface(TRAIT_IMPLEMENTER_ABI)
+const mockImplementerAddress = 'test-implementer-address'
 
 jest.mock('ethers', () => {
   const actualModule = jest.requireActual('ethers')
@@ -25,48 +28,48 @@ jest.mock('ethers', () => {
           return {}
         },
       },
-      Contract: function () {
+      Contract: function (address: string) {
         return {
-          interface: mockECRegistryInterface,
-          addressCanModifyTrait: (_, achievementID: string): boolean => {
+          interface:
+            address === mockImplementerAddress
+              ? mockTraitImplementerInterface
+              : mockECRegistryInterface,
+          traits: jest.fn().mockReturnValue({ implementer: 'test-implementer-address' }),
+          addressCanModifyTrait: jest.fn().mockImplementation((_, achievementID: string) => {
             return achievementID === '2' || achievementID === '15'
-          },
-          getTeams: () => {
-            return [
-              {
-                id: 1,
-                name: 'team-1',
-                city: 'Seattle',
-                tricode: 'SEA',
-                real_id: 1610612737,
-              },
-              {
-                id: 2,
-                name: 'team-1',
-                city: 'Seattle',
-                tricode: 'SEA',
-                real_id: 1610612738,
-              },
-            ]
-          },
-          getPlayers: () => {
-            return [
-              {
-                id: 3,
-                team_id: 1,
-                real_id: 203991,
-                real_team_id: 1610612738,
-                name: 'Lebron James',
-              },
-              {
-                id: 4,
-                team_id: 1,
-                real_id: 203991,
-                real_team_id: 1610612738,
-                name: 'Steph Curry',
-              },
-            ]
-          },
+          }),
+          getTeams: jest.fn().mockReturnValue([
+            {
+              id: 1,
+              name: 'team-1',
+              city: 'Seattle',
+              tricode: 'SEA',
+              real_id: 1610612737,
+            },
+            {
+              id: 2,
+              name: 'team-1',
+              city: 'Seattle',
+              tricode: 'SEA',
+              real_id: 1610612738,
+            },
+          ]),
+          getPlayers: jest.fn().mockReturnValue([
+            {
+              id: 3,
+              team_id: 1,
+              real_id: 203991,
+              real_team_id: 1610612738,
+              name: 'Lebron James',
+            },
+            {
+              id: 4,
+              team_id: 1,
+              real_id: 203991,
+              real_team_id: 1610612738,
+              name: 'Steph Curry',
+            },
+          ]),
         }
       },
     },
@@ -80,12 +83,11 @@ let req: SuperTest<Test>
 const jobID = '1'
 
 describe('nba', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
     server = await startServer()
     req = request(`localhost:${(server.address() as AddressInfo).port}`)
     process.env.POLYGON_RPC_URL = process.env.POLYGON_RPC_URL || 'fake-polygon-rpc-url'
-    process.env.API_ENDPOINT = process.env.API_ENDPOINT || fakeApiEndpoint
     process.env.CACHE_ENABLED = 'false'
 
     if (process.env.RECORD) {
@@ -93,14 +95,41 @@ describe('nba', () => {
     }
   })
 
-  afterEach((done) => {
+  afterAll((done) => {
     process.env = oldEnv
     server.close(done)
+    nock.restore()
+    nock.cleanAll()
+    nock.enableNetConnect()
   })
 
-  describe('successful calls', () => {
-    it('return success when fetching the achievements and returns the correct result', async () => {
-      mockGalaxisApiResp(fakeApiEndpoint)
+  afterEach(() => {
+    delete process.env.API_ENDPOINT
+    nock.cleanAll()
+  })
+
+  describe('successful call for successful calls', () => {
+    it('return success when fetching the achievements and returns the correct result for boolean only achievements', async () => {
+      process.env.API_ENDPOINT = process.env.API_ENDPOINT || fakeBooleanOnlyEndpoint
+      mockBooleanOnlyGalaxisApiResp(fakeBooleanOnlyEndpoint)
+      const data: AdapterRequest = {
+        id: jobID,
+        data: {},
+      }
+
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+
+    it('return success when fetching the achievements and returns the correct result for non boolean only achievements', async () => {
+      process.env.API_ENDPOINT = process.env.API_ENDPOINT || fakeNonBooleanOnlyEndpoint
+      mockNonBooleanOnlyGalaxisApiResp(fakeNonBooleanOnlyEndpoint)
       const data: AdapterRequest = {
         id: jobID,
         data: {},
