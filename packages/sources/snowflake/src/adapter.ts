53f426c98afd0f6b3924cc58ab85784627142343
Matt Kelly <mkelly2@live.co.uk>
2022-03-28 23:36:51 +0800

    Updated types for snowflake

--- packages/sources/snowflake/src/adapter.ts
+++ packages/sources/snowflake/src/adapter.ts
@@ -8,13 +8,30 @@ import {
 import { makeConfig, SnowflakeConfig } from './config'
 import * as endpoints from './endpoint'
 
-export const execute: ExecuteWithConfig<SnowflakeConfig> = async (request, context, config) => {
-  return Builder.buildSelector(request, context, config, endpoints)
+export const execute: ExecuteWithConfig<SnowflakeConfig, endpoints.TInputParameters> = async (
+  request,
+  context,
+  config,
+) => {
+  return Builder.buildSelector<SnowflakeConfig, endpoints.TInputParameters>(
+    request,
+    context,
+    config,
+    endpoints,
+  )
 }
 
-export const endpointSelector = (request: AdapterRequest): APIEndpoint<SnowflakeConfig> =>
-  Builder.selectEndpoint(request, makeConfig(), endpoints)
+export const endpointSelector = (
+  request: AdapterRequest,
+): APIEndpoint<SnowflakeConfig, endpoints.TInputParameters> =>
+  Builder.selectEndpoint<SnowflakeConfig, endpoints.TInputParameters>(
+    request,
+    makeConfig(),
+    endpoints,
+  )
 
-export const makeExecute: ExecuteFactory<SnowflakeConfig> = (config) => {
+export const makeExecute: ExecuteFactory<SnowflakeConfig, endpoints.TInputParameters> = (
+  config,
+) => {
   return async (request, context) => execute(request, context, config || makeConfig())
 }
