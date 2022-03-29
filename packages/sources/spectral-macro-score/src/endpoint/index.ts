8f3d420e04e01fdd807ffb6e4a605a88ede697e0
Matt Kelly <mkelly2@live.co.uk>
2022-03-29 15:20:45 +0800

    Updated types for spectral-macro-score

--- packages/sources/spectral-macro-score/src/endpoint/index.ts
+++ packages/sources/spectral-macro-score/src/endpoint/index.ts
@@ -1,1 +1,5 @@
+import type { TInputParameters as MacroScoreInputParameters } from './macro-score-api'
+
+export type TInputParameters = MacroScoreInputParameters
+
 export * as MacroScoreAPI from './macro-score-api'
