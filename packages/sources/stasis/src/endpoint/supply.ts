44f0e633753e0591542e7e0a63e9cb8998f8afa0
Matt Kelly <mkelly2@live.co.uk>
2022-03-29 18:03:48 +0800

    Updated types for stasis

--- packages/sources/stasis/src/endpoint/supply.ts
+++ packages/sources/stasis/src/endpoint/supply.ts
@@ -1,12 +1,22 @@
-import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/ea-bootstrap'
+import { ExecuteWithConfig, Config, InputParameters, AxiosResponse } from '@chainlink/ea-bootstrap'
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 
 export const supportedEndpoints = ['supply']
 
-export const inputParameters: InputParameters = {}
+export type TInputParameters = Record<string, never>
+export const inputParameters: InputParameters<TInputParameters> = {}
+
+export interface ResponseSchema {
+  accounts: {
+    nexpay: { amount: string }
+    xnt: { amount: string }
+    ext: { amount: string }
+  }
+  summary: { amount: string }
+}
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters)
+  const validator = new Validator<TInputParameters>(request, inputParameters)
 
   const jobRunID = validator.validated.id
   const url = '/transparency/eurs-statement'
@@ -16,7 +26,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
     url,
   }
 
-  const response = await Requester.request(options)
-  response.data.result = Requester.validateResultNumber(response.data, ['amount'])
-  return Requester.success(jobRunID, response)
+  const response: AxiosResponse = await Requester.request<ResponseSchema>(options)
+  const result = Requester.validateResultNumber(response.data, ['summary', 'amount'])
+  return Requester.success(jobRunID, Requester.withResult(response, result))
 }
