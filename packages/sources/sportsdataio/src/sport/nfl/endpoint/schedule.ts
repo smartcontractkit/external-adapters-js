1d3883b74fd54d3a30b04b46555252695b5607fb
Matt Kelly <mkelly2@live.co.uk>
2022-03-29 17:44:58 +0800

    Updated types for sportsdataio

--- packages/sources/sportsdataio/src/sport/nfl/endpoint/schedule.ts
+++ packages/sources/sportsdataio/src/sport/nfl/endpoint/schedule.ts
@@ -1,15 +1,19 @@
-import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { AxiosResponse, InputParameters, Requester, Validator } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig } from '@chainlink/ea-bootstrap'
 import { Config } from '../../../config'
 
 export const NAME = 'schedule'
 
-const customParams = {
-  season: true,
+export type TInputParameters = { season: string | number }
+export const customParams: InputParameters<TInputParameters> = {
+  season: {
+    required: true,
+    description: 'The season to get events from',
+  },
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, customParams)
+  const validator = new Validator<TInputParameters>(request, customParams)
 
   const jobRunID = validator.validated.id
   const season = validator.validated.data.season
@@ -21,7 +25,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
 
   const options = { ...config.api, params, url }
 
-  const response = await Requester.request(options)
+  const response: AxiosResponse = await Requester.request(options)
   response.data.result = response.data
 
   return Requester.success(jobRunID, response, config.verbose)
