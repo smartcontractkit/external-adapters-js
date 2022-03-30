db663459de69fbe7074395d5ccbee91a3e291478
Austin Born <austinborn212@gmail.com>
2022-03-30 11:18:14 -0700

    Add qa:dependencies script to fetch composite dependencies

--- packages/scripts/src/shared/docGenTypes.d.ts
+++ packages/scripts/src/shared/docGenTypes.d.ts
@@ -61,4 +61,12 @@ export type Schema = {
   description?: string
   properties?: EnvVars
   required?: string[]
+  allOf?: (
+    | {
+        $ref: string
+      }
+    | {
+        anyOf: { required: string[] }[]
+      }
+  )[]
 }
