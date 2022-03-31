0f7c80bd3ee524b8173b227682719a195a8441b0
Matt Kelly <mkelly2@live.co.uk>
2022-03-31 15:35:49 +0800

    Updated types for example adapter

--- packages/examples/source/src/endpoint/example.ts
+++ packages/examples/source/src/endpoint/example.ts
@@ -11,18 +11,20 @@ export const endpointResultPaths = {
 }
 
 export interface ResponseSchema {
+  Response?: string // for errors
   data: {
     // Some data
   }
 }
 
-const customError = (data: Record<string, unknown>) => data.Response === 'Error'
+const customError = (data: ResponseSchema) => data.Response === 'Error'
 
 // The description string is used to explain how the endpoint works, and is used for part of the endpoint's README section
 export const description = 'This is an example endpoint description for example adapter.'
 
 // The inputParameters object must be present for README generation.
-export const inputParameters: InputParameters = {
+export type TInputParameters = { base: string; quote: string }
+export const inputParameters: InputParameters<TInputParameters> = {
   // See InputParameters type for more config options
   base: {
     aliases: ['from', 'coin'],
@@ -37,10 +39,10 @@ export const inputParameters: InputParameters = {
 }
 
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters)
+  const validator = new Validator<TInputParameters>(request, inputParameters)
 
   const jobRunID = validator.validated.id
-  const base = validator.overrideSymbol(AdapterName)
+  const base = validator.overrideSymbol(AdapterName, validator.validated.data.base)
   const quote = validator.validated.data.quote
   const url = `price`
   const resultPath = validator.validated.data.resultPath
@@ -54,7 +56,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
   const options = { ...config.api, params, url }
 
   const response = await Requester.request<ResponseSchema>(options, customError)
-  const result = Requester.validateResultNumber(response.data, [resultPath])
+  const result = Requester.validateResultNumber(response.data, resultPath)
 
   return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
 }
