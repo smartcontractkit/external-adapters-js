3ef057ac428fc544c047ef7acd37d909c485b7d8
Matt Kelly <mkelly2@live.co.uk>
2022-03-31 19:46:53 +0800

    Updated types for dydx-stark

--- packages/targets/dydx-stark/src/adapter.ts
+++ packages/targets/dydx-stark/src/adapter.ts
@@ -1,14 +1,23 @@
-import { Requester, Validator, AdapterError } from '@chainlink/ea-bootstrap'
+import { Requester, Validator, AdapterError, InputParameters } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, ExecuteFactory } from '@chainlink/ea-bootstrap'
 import { Config, makeConfig, DEFAULT_ENDPOINT } from './config'
-import { send } from './endpoint'
+import { send, TInputParameters as EndpointInputParams } from './endpoint'
 
-const inputParams = {
-  endpoint: false,
+export type TInputParameters = { endpoint: string }
+const inputParams: InputParameters<TInputParameters> = {
+  endpoint: {
+    type: 'string',
+  },
 }
 
-export const execute: ExecuteWithConfig<Config> = async (request, context, config) => {
-  const validator = new Validator(request, inputParams)
+export type TInputParams = EndpointInputParams & TInputParameters
+
+export const execute: ExecuteWithConfig<Config, TInputParameters> = async (
+  request,
+  context,
+  config,
+) => {
+  const validator = new Validator<TInputParameters>(request, inputParams)
 
   Requester.logConfig(config)
 
@@ -29,6 +38,6 @@ export const execute: ExecuteWithConfig<Config> = async (request, context, confi
   }
 }
 
-export const makeExecute: ExecuteFactory<Config> = (config) => {
+export const makeExecute: ExecuteFactory<Config, TInputParams> = (config) => {
   return async (request, context) => execute(request, context, config || makeConfig())
 }
