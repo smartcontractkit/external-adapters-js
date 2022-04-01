e4833bb938e00ea0d51b608db942afaded774139
Justin Kaseman <justinkaseman@live.com>
2022-03-31 21:27:45 -0700

    Bitcoin-JSON-RPC types

--- packages/composites/bitcoin-json-rpc/src/endpoint/scantxoutset.ts
+++ packages/composites/bitcoin-json-rpc/src/endpoint/scantxoutset.ts
@@ -1,12 +1,12 @@
 import * as JSONRPC from '@chainlink/json-rpc-adapter'
 import type {
-  Config,
   ExecuteWithConfig,
   AdapterRequest,
   AdapterContext,
   InputParameters,
 } from '@chainlink/ea-bootstrap'
 import { Validator, Requester, Logger, util } from '@chainlink/ea-bootstrap'
+import { ExtendedConfig } from '../config'
 
 export const NAME = 'scantxoutset'
 export const supportedEndpoints = [NAME, 'scan', 'scanobject']
@@ -14,7 +14,12 @@ export const description = `Calls \`"method": "scantxoutset"\` on the Bitcoin no
 
 **NOTE:** Requests to this endpoint may exceed the configured \`API_TIMEOUT\`. If a scan in progress, the adapter will wait an additional \`API_TIMEOUT\` period for the in-progress scan to complete. If the timeout is hit while a scan is in progress, a request to abort the scan is sent with an additional 1s timeout. This makes the theoretically maximum timeout for requests to this endpoint \`2 x API_TIMEOUT + 1000\` ms.`
 
-const inputParameters: InputParameters = {
+export type TInputParameters = {
+  scanobjects: string[]
+  confirmations?: string
+}
+
+export const inputParameters: InputParameters<TInputParameters> = {
   scanobjects: {
     aliases: ['addresses'],
     required: true,
@@ -24,8 +29,13 @@ const inputParameters: InputParameters = {
   },
 }
 
-export const execute: ExecuteWithConfig<Config> = async (request, context, config) => {
-  const validator = new Validator(request, inputParameters)
+type Params = {
+  action: string
+  scanobjects: string[]
+}
+
+export const execute: ExecuteWithConfig<ExtendedConfig> = async (request, context, config) => {
+  const validator = new Validator<TInputParameters>(request, inputParameters)
   const jobRunID = validator.validated.id
   const scanobjects = validator.validated.data.scanobjects.map((address: string) => {
     // Addresses must be formatted as addr(39e7mxbeNmRRnjfy1qkphv1TiMcztZ8VuE)
@@ -33,7 +43,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, context, confi
     return `addr(${address})`
   })
 
-  const params = {
+  const params: Params = {
     action: 'start',
     scanobjects,
   }
@@ -47,29 +57,29 @@ export const execute: ExecuteWithConfig<Config> = async (request, context, confi
 }
 
 const scanWithRetries = async (
-  params: Record<string, unknown>,
+  params: Params,
   request: AdapterRequest,
   context: AdapterContext,
-  config: Config,
+  config: ExtendedConfig,
 ) => {
   const requestData = {
     ...request,
-    data: { ...request.data, method: NAME, params },
+    data: { ...(request.data as JSONRPC.types.request.TInputParameters), method: NAME, params },
   }
 
-  const deadline = Date.now() + config.api.timeout
-  const _execute: ExecuteWithConfig<Config> = JSONRPC.makeExecute()
+  const deadline = Date.now() + (config.api?.timeout ?? 0)
+  const _execute = JSONRPC.makeExecute(config)
 
   while (Date.now() + 1000 <= deadline) {
     try {
-      return await _execute(requestData, context, config)
+      return await _execute(requestData, context)
     } catch (e) {
       if (e.cause?.response?.data?.error?.code === -8) {
         Logger.debug('scan is already in progress, waiting 1s...')
         Logger.debug(`time left to wait: ${deadline - Date.now()}ms`)
         await util.sleep(1000)
         continue
-      } else if (e.message === `timeout of ${config.api.timeout}ms exceeded`) {
+      } else if (e.message === `timeout of ${config.api?.timeout}ms exceeded`) {
         // Highly experimental:
         // If a timeout error was hit, we try to abort the scan that we initiated
         // However there is a race condition where:
@@ -82,7 +92,7 @@ const scanWithRetries = async (
         config.api.timeout = 1000 // We expect this action to be quick, and we do not want to hold up the request on this
         Logger.debug('timeout reached, aborting scan in progress')
         try {
-          await _execute(requestData, context, config)
+          await _execute(requestData, context)
         } catch (e) {
           Logger.error(`failed to abort scan in progress: ${e.message}`)
         }
