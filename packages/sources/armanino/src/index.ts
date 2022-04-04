6ed1e1ed53c3aea91bbbc36b96167fbcb4e1d9a7
Justin Kaseman <justinkaseman@live.com>
2022-04-04 16:53:27 -0700

    Add Armanino EA

---
+++ packages/sources/armanino/src/index.ts
@@ -0,0 +1,11 @@
+import { expose } from '@chainlink/ea-bootstrap'
+import { makeExecute, endpointSelector } from './adapter'
+import * as endpoints from './endpoint'
+import { makeConfig, NAME } from './config'
+import type * as types from './endpoint'
+import * as rateLimit from './config/limits.json'
+
+const adapterContext = { name: NAME, rateLimit }
+
+const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
+export { NAME, makeExecute, makeConfig, server, types, endpoints }
