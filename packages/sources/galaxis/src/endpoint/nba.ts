bcabb0bc037defcbbd5035718acf757292f5c64e
Christopher Dimitri Sastropranoto <c.sastropranoto@gmail.com>
2022-04-20 21:18:27 +0700

    update how how Galaxis API URL is formed

--- packages/sources/galaxis/src/endpoint/nba.ts
+++ packages/sources/galaxis/src/endpoint/nba.ts
@@ -65,7 +65,8 @@ const getDate = async (config: ExtendedConfig, paramsDate?: string): Promise<str
 
 const getURL = async (config: ExtendedConfig, date: string): Promise<string> => {
   if (isDateNotSet(date)) return config.api.baseURL
-  return `${config.api.baseURL}/nightly_achievements_${date}.json` // YYYY-MM-DD
+  const year = date.split('-')[0]
+  return `${config.api.baseURL}/${year}/nightly_achievements_${date}.json` // YYYY-MM-DD
 }
 
 export const isDateNotSet = (date: string): boolean =>
