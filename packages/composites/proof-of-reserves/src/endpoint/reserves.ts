753f927585521ae102ed9fb6ef563fd320b1622b
Morgan Kuphal <morgan.kuphal@smartcontract.com>
2022-05-04 11:00:54 -0500

    Addressed PR notes from @boxhock

--- packages/composites/proof-of-reserves/src/endpoint/reserves.ts
+++ packages/composites/proof-of-reserves/src/endpoint/reserves.ts
@@ -1,4 +1,5 @@
 import { Validator } from '@chainlink/ea-bootstrap'
+import type { AdapterResponse } from '@chainlink/types'
 import { Config, ExecuteWithConfig, InputParameters } from '@chainlink/types'
 import { makeOptions } from '../config'
 import { Indexer, runBalanceAdapter } from '../utils/balance'
@@ -36,15 +37,17 @@ const inputParameters: InputParameters = {
   },
   disableAddressValidation: {
     required: false,
-    type: 'boolean',
+    type: 'string',
     description: 'Gives the option to disable address validation before the balances are fetched.',
+    default: 'false',
   },
   disableDuplicateAddressFiltering: {
     required: false,
-    type: 'boolean',
+    type: 'string',
     description:
       'Gives the option to disabled the filtering of duplicate addresses in a request. ' +
       'If this is set to `true` and a duplicate address is contained in the request, the balance of that address will be counted twice.',
+    default: 'false',
   },
 }
 
@@ -54,14 +57,32 @@ export const execute: ExecuteWithConfig<Config> = async (input, context, config)
   const protocol = validator.validated.data.protocol.toUpperCase()
   const indexer: Indexer = validator.validated.data.indexer.toUpperCase()
   const confirmations = validator.validated.data.confirmations
-
   const protocolOutput = await runProtocolAdapter(jobRunID, context, protocol, input.data, config)
+  const validatedAddresses = getValidAddresses(protocolOutput, validator)
+  const balanceOutput = await runBalanceAdapter(
+    indexer,
+    context,
+    confirmations,
+    config,
+    validatedAddresses,
+  )
+  const reduceOutput = await runReduceAdapter(indexer, context, balanceOutput)
+  return reduceOutput
+}
+
+const getValidAddresses = (
+  protocolOutput: AdapterResponse,
+  validator: Validator,
+): AdapterResponse => {
   const validatedInput = { ...protocolOutput }
   if (
     !validator.validated.data.disableAddressValidation ||
     validator.validated.data.disableAddressValidation !== 'true'
   ) {
-    validatedInput.result = validateAddresses(indexer, validatedInput.result)
+    validatedInput.result = validateAddresses(
+      validator.validated.data.indexer,
+      validatedInput.result,
+    )
   }
   if (
     !validator.validated.data.disableDuplicateAddressFiltering ||
@@ -70,13 +91,5 @@ export const execute: ExecuteWithConfig<Config> = async (input, context, config)
     validatedInput.result = filterDuplicates(validatedInput.result)
   }
   validatedInput.data.result = validatedInput.result
-  const balanceOutput = await runBalanceAdapter(
-    indexer,
-    context,
-    confirmations,
-    config,
-    validatedInput,
-  )
-  const reduceOutput = await runReduceAdapter(indexer, context, balanceOutput)
-  return reduceOutput
+  return validatedInput
 }
