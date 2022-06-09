406ea9b50ed53a18909943ec598535e2d201c513
Matt Kelly <mkelly2@live.co.uk>
2022-06-09 16:57:17 +0100

    Added nft-floor-price endpoint to coinbase adapter

--- packages/sources/coinbase/src/config/index.ts
+++ packages/sources/coinbase/src/config/index.ts
@@ -1,15 +1,20 @@
-import { Requester } from '@chainlink/ea-bootstrap'
+import { Requester, util } from '@chainlink/ea-bootstrap'
 import { Config } from '@chainlink/types'
 
 export const NAME = 'COINBASE'
 
 export const DEFAULT_ENDPOINT = 'crypto'
-export const DEFAULT_API_ENDPOINT = 'https://api.coinbase.com'
+export const DEFAULT_API_ENDPOINT = 'https://api.coinbase.comm'
+export const DEFAULT_NFT_API_ENDPOINT = 'https://nft-api.coinbase.com'
 export const DEFAULT_WS_API_ENDPOINT = 'wss://ws-feed.pro.coinbase.com'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
   config.api.baseURL = config.api.baseURL || DEFAULT_API_ENDPOINT
+  config.adapterSpecificParams = {
+    nftBaseURL: util.getEnv('NFT_API_ENDPOINT', prefix) || DEFAULT_NFT_API_ENDPOINT,
+    nftApiAuthHeader: util.getEnv('NFT_API_AUTH_HEADER', prefix) || '',
+  }
   config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
 }
