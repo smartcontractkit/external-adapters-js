53826725ba508eeb91cfbc6c1b96d7962843e5b4
Tigran Karapetyan <t.karapetyan@mixbytes.io>
2022-06-15 18:36:17 +0300

    Update price calculation

--- packages/sources/lido-kusama/src/config/index.ts
+++ packages/sources/lido-kusama/src/config/index.ts
@@ -2,14 +2,17 @@ import { Requester, util } from '@chainlink/ea-bootstrap'
 import { Config } from '@chainlink/types'
 
 export const NAME = 'LIDO_KUSAMA'
+export const LIDO_ADDRESS = '0xFfc7780C34B450d917d557E728f033033CB4fA8C'
+export const WITHDRAWAL_ADDRESS = '0x8d24EF5B84d99b1871Eef84b4999b97B96eCfba7'
 export const KSM_AGGREGATOR_PROXY = '0x6e0513145FCE707Cd743528DB7C1cAB537DE9d1B'
-export const STKSM_RATE_PROVIDER = '0x77D4b212770A7cA26ee70b1E0f27fC36da191c53'
 export const DEFAULT_ENDPOINT = 'stksm'
 export const OUTPUT_DECIMALS = 8
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
-  config.api.baseURL ||= util.getEnv('MOONRIVER_RPC_URL') || ''
+  config.api.baseURL ||=
+    util.getEnv('MOONRIVER_RPC_URL') || 'https://rpc.moonriver.moonbeam.network/'
+  config.api.relayURL ||= util.getEnv('KUSAMA_RPC_URL') || 'wss://kusama-rpc.polkadot.io/'
   config.defaultEndpoint = DEFAULT_ENDPOINT
   return config
 }
--- packages/sources/lido-kusama/src/endpoint/stksm.ts
+++ packages/sources/lido-kusama/src/endpoint/stksm.ts
@@ -1,9 +1,12 @@
 import { Requester, Validator } from '@chainlink/ea-bootstrap'
 import { Config, ExecuteWithConfig, InputParameters, AdapterResponse } from '@chainlink/types'
-import { KSM_AGGREGATOR_PROXY, STKSM_RATE_PROVIDER, OUTPUT_DECIMALS } from '../config'
-import rateProviderAbi from '../abi/RateProvider.json'
+import { LIDO_ADDRESS, WITHDRAWAL_ADDRESS, KSM_AGGREGATOR_PROXY, OUTPUT_DECIMALS } from '../config'
+import lidoAbi from '../abi/Lido.json'
+import ledgerAbi from '../abi/Ledger.json'
+import withdrawalAbi from '../abi/Withdrawal.json'
 import ksmAggregatorAbi from '../abi/KsmAggregator.json'
 import { ethers } from 'ethers'
+import { ApiPromise, Keyring, WsProvider } from '@polkadot/api'
 
 export const supportedEndpoints = ['stksm']
 
@@ -20,25 +23,52 @@ export const execute: ExecuteWithConfig<Config> = async (
   const jobRunID = validator.validated.id
   const provider = new ethers.providers.JsonRpcProvider(config.api.baseURL)
 
-  const rateProvider = new ethers.Contract(STKSM_RATE_PROVIDER, rateProviderAbi, provider)
+  const wsProvider = new WsProvider(config.api.relayURL)
+  const api = await ApiPromise.create({ provider: wsProvider })
 
-  const kusamaPrice = new ethers.Contract(KSM_AGGREGATOR_PROXY, ksmAggregatorAbi, provider)
+  const keyring = new Keyring()
+  keyring.setSS58Format(2)
+
+  const lido = new ethers.Contract(LIDO_ADDRESS, lidoAbi, provider)
+
+  const withdrawal = new ethers.Contract(WITHDRAWAL_ADDRESS, withdrawalAbi, provider)
+
+  const ksmAggregator = new ethers.Contract(KSM_AGGREGATOR_PROXY, ksmAggregatorAbi, provider)
 
   const values = await Promise.all([
-    rateProvider.getVirtualPrice(),
-    kusamaPrice.latestRoundData(),
-    kusamaPrice.decimals(),
+    lido.getLedgerAddresses(),
+    lido.totalSupply(),
+    ksmAggregator.latestRoundData(),
+    ksmAggregator.decimals(),
   ])
 
-  const rate = values[0].div(ethers.utils.parseUnits('1', 10))
-  const ksmUsd = values[1].answer
-  const proxyDecimals = values[2]
+  let freeBalanceSum = ethers.utils.parseUnits('0', 12)
+
+  for (let ledgerAddress of values[0]) {
+    const ledgerContract = new ethers.Contract(ledgerAddress, ledgerAbi, provider)
+    const stashAccount = await ledgerContract.stashAccount()
+    const hexPair = keyring.addFromAddress(stashAccount)
+    // Workaround to bypass strict
+    const relayAccount = (await api.query.system.account(hexPair.address)).toHuman()
+    let freeBalance = JSON.stringify(relayAccount)
+    freeBalance = JSON.parse(freeBalance).data.free
+    freeBalanceSum = freeBalanceSum.add(freeBalance.replace(/,/g, ''))
+  }
+
+  const bufferedDeposits = await lido.bufferedDeposits()
+  const bufferedRedeems = await lido.bufferedRedeems()
+  const losses = await withdrawal.totalBalanceForLosses()
+  const totalKsm = bufferedDeposits.add(freeBalanceSum).sub(bufferedRedeems).sub(losses)
+
+  const totalStKsm = values[1]
+  const ksmUsd = values[2].answer
+  const proxyDecimals = values[3]
 
   const result = ksmUsd
-    .mul(ethers.utils.parseUnits('1', 8))
-    .div(rate)
     .mul(ethers.utils.parseUnits('1', OUTPUT_DECIMALS))
     .div(ethers.utils.parseUnits('1', proxyDecimals))
+    .mul(totalKsm)
+    .div(totalStKsm)
     .toNumber()
 
   const response = {
