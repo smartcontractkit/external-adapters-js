3c26e3ba6f6d6fd0d691dc6374bd83946c4da6cf
Justin Kaseman <justinkaseman@live.com>
2022-06-15 23:39:03 -0700

    Clean up Durin folder

--- packages/sources/durin/src/endpoint/index.ts
+++
@@ -1,5 +0,0 @@
-import type { TInputParameters as OptimismInputParameters } from './optimism-gateway'
-
-export type TInputParameters = OptimismInputParameters
-
-export * as optimismGateway from './optimism-gateway'
