5a69fc4b73f2a64d0cf7b2f68835a98522c5eaf6
Matthew Kelly <mkelly2@live.co.uk>
2022-06-17 03:46:54 +0800

    42073 - Coinbase NFT floor price (#1948)
    * Added nft-floor-price endpoint to coinbase adapter
    
    * Updated config
    
    * Added tests & changeset
    
    * Updated schema
    
    * Updated date handling, updated README
    
    * Updated schema
    
    * Updated README
    
    * Updated coinbase nftFloorPrice request params to match updated API docs
    
    Co-authored-by: Justin Kaseman <justinkaseman@live.com>
    Co-authored-by: cl-ea <93770670+cl-ea@users.noreply.github.com>

---
+++ packages/sources/coinbase/src/endpoint/nftFloorPrice.ts
@@ -0,0 +1,77 @@
+import { Requester, Validator } from '@chainlink/ea-bootstrap'
+import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/types'
+
+export const supportedEndpoints = ['nft-floor', 'nft-floor-price']
+
+export const inputParameters: InputParameters = {
+  network: {
+    required: false,
+    description: 'The blockchain network to get data from',
+    default: 'ethereum-mainnet',
+    type: 'string',
+    options: ['ethereum-mainnet', 'polygon-mainnet'],
+  },
+  contractAddress: {
+    description: 'The NFT contract address',
+    type: 'string',
+    required: true,
+  },
+  start: {
+    required: true,
+    description: 'The beginning of the time window (inclusive, yyyy-mm-dd hh:mm:ss)',
+    type: 'string',
+  },
+  end: {
+    required: true,
+    description: 'The end of the time window (inclusive, yyyy-mm-dd hh:mm:ss)',
+    type: 'string',
+  },
+}
+
+export interface ResponseSchema {
+  floorPriceDailyValue: [
+    {
+      date: string
+      multiplier: number
+      priceStdDev: number
+      logFloorPrice: number
+      adjustedFloorPrice: number
+    },
+  ]
+}
+
+export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
+  const validator = new Validator(request, inputParameters)
+
+  const jobRunID = validator.validated.id
+  const network = validator.validated.data.network
+  const contractAddress = validator.validated.data.contractAddress
+  const start = validator.validated.data.start
+  const end = validator.validated.data.end
+
+  const baseURL = `${config.adapterSpecificParams?.nftBaseURL}/api/nft/v1/GetFloorPriceEstimate`
+  const params = {
+    networkName: network,
+    contractAddress: contractAddress,
+    startDay: start,
+    endDay: end,
+  }
+
+  const options = {
+    ...config.api,
+    headers: {
+      ...config.api.headers,
+      'CB-NFT-API-TOKEN': config.adapterSpecificParams?.nftApiAuthHeader,
+    },
+    baseURL,
+    params,
+  }
+
+  const response = await Requester.request<ResponseSchema>(options)
+  const result = Requester.validateResultNumber(response.data, [
+    'floorPriceDailyValue',
+    0,
+    'adjustedFloorPrice',
+  ])
+  return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
+}
