e88d6de35f9fc84f43f8feddc91a6e1f88d6f6cb
Justin Kaseman <justinkaseman@live.com>
2022-06-23 15:47:11 +0200

    Add BITCOIN_TESTNET environment variable

--- packages/composites/por-indexer/src/config/index.ts
+++ packages/composites/por-indexer/src/config/index.ts
@@ -5,17 +5,20 @@ export const NAME = 'POR_INDEXER'
 
 export interface ExtendedConfig extends Config {
   BITCOIN_MAINNET_POR_INDEXER_URL?: string
+  BITCOIN_TESTNET_POR_INDEXER_URL?: string
   DOGECOIN_MAINNET_POR_INDEXER_URL?: string
   DOGECOIN_TESTNET_POR_INDEXER_URL?: string
 }
 
 export const makeConfig = (prefix?: string): ExtendedConfig => {
   const BITCOIN_MAINNET_POR_INDEXER_URL = util.getEnv('BITCOIN_MAINNET_POR_INDEXER_URL', prefix)
+  const BITCOIN_TESTNET_POR_INDEXER_URL = util.getEnv('BITCOIN_TESTNET_POR_INDEXER_URL', prefix)
   const DOGECOIN_MAINNET_POR_INDEXER_URL = util.getEnv('DOGECOIN_MAINNET_POR_INDEXER_URL', prefix)
   const DOGECOIN_TESTNET_POR_INDEXER_URL = util.getEnv('DOGECOIN_TESTNET_POR_INDEXER_URL', prefix)
   return {
     ...Requester.getDefaultConfig(prefix),
     BITCOIN_MAINNET_POR_INDEXER_URL,
+    BITCOIN_TESTNET_POR_INDEXER_URL,
     DOGECOIN_MAINNET_POR_INDEXER_URL,
     DOGECOIN_TESTNET_POR_INDEXER_URL,
   }
