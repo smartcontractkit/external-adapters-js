75976c67c080fb70b1cd00c34bc09834266a2b77
bogos <andre.neyra@gmail.com>
2022-06-23 11:47:24 -0500

    fix problem with integration test

--- packages/sources/finage/test/integration/adapter.test.ts
+++ packages/sources/finage/test/integration/adapter.test.ts
@@ -169,93 +169,6 @@ describe('websocket', () => {
   beforeAll(async () => {
     if (!process.env.RECORD) {
       process.env.API_KEY = 'fake-api-key'
-      process.env.WS_SOCKET_KEY = 'fake-api-key'
-      process.env.CRYPTO_WS_API_ENDPOINT = DEFAULT_CRYPTO_WS_API_ENDPOINT
-
-      mockedWsServer = mockWebSocketServer(process.env.CRYPTO_WS_API_ENDPOINT)
-      mockWebSocketProvider(WebSocketClassProvider)
-    }
-
-    oldEnv = JSON.parse(JSON.stringify(process.env))
-    process.env.WS_ENABLED = 'true'
-    process.env.WS_SUBSCRIPTION_TTL = '300'
-
-    fastify = await startServer()
-    req = request(`localhost:${(fastify.server.address() as AddressInfo).port}`)
-  })
-
-  afterAll((done) => {
-    process.env = oldEnv
-    nock.restore()
-    nock.cleanAll()
-    nock.enableNetConnect()
-    fastify.close(done)
-  })
-
-  describe('crypto endpoint', () => {
-    const jobID = '1'
-
-    it('should return success', async () => {
-      const data: AdapterRequest = {
-        id: jobID,
-        data: {
-          endpoint: 'crypto',
-          base: 'BTC',
-          quote: 'USD',
-        },
-      }
-
-      let flowFulfilled: Promise<boolean>
-      if (!process.env.RECORD) {
-        mockResponseSuccess() // For the first response
-
-        flowFulfilled = mockWebSocketFlow(mockedWsServer, [
-          mockCryptoSubscribeResponse,
-          mockCryptoUnsubscribeResponse,
-          // Double mocks in flow are needed to get rid of timing issues
-          mockCryptoSubscribeResponse,
-          mockCryptoUnsubscribeResponse,
-        ])
-      }
-
-      const makeRequest = () =>
-        req
-          .post('/')
-          .send(data)
-          .set('Accept', '*/*')
-          .set('Content-Type', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-
-      // We don't care about the first response, coming from http request
-      // This first request will start both batch warmer & websocket
-      await makeRequest()
-
-      await util.sleep(100)
-      // This final request should disable the cache warmer
-      const response = await makeRequest()
-      expect(response.body).toEqual({
-        jobRunID: '1',
-        result: 43682.66306523,
-        statusCode: 200,
-        maxAge: 30000,
-        data: { result: 43682.66306523 },
-      })
-
-      await flowFulfilled
-    }, 30000)
-  })
-})
-
-describe('websocket', () => {
-  let mockedWsServer: InstanceType<typeof MockWsServer>
-  let fastify: FastifyInstance
-  let req: SuperTest<Test>
-
-  let oldEnv: NodeJS.ProcessEnv
-  beforeAll(async () => {
-    if (!process.env.RECORD) {
-      process.env.API_KEY = 'fake-api-key'
       process.env.WS_SOCKET_KEY = process.env.WS_SOCKET_KEY || 'fake-api-key'
       process.env.STOCK_WS_API_ENDPOINT =
         process.env.STOCK_WS_API_ENDPOINT || DEFAULT_STOCK_WS_API_ENDPOINT
@@ -414,6 +327,93 @@ describe('websocket', () => {
         statusCode: 200,
         maxAge: 30000,
         data: { result: 1.331345 },
+      })
+
+      await flowFulfilled
+    }, 30000)
+  })
+})
+
+describe('websocket', () => {
+  let mockedWsServer: InstanceType<typeof MockWsServer>
+  let fastify: FastifyInstance
+  let req: SuperTest<Test>
+
+  let oldEnv: NodeJS.ProcessEnv
+  beforeAll(async () => {
+    if (!process.env.RECORD) {
+      process.env.API_KEY = 'fake-api-key'
+      process.env.WS_SOCKET_KEY = 'fake-api-key'
+      process.env.CRYPTO_WS_API_ENDPOINT = DEFAULT_CRYPTO_WS_API_ENDPOINT
+
+      mockedWsServer = mockWebSocketServer(process.env.CRYPTO_WS_API_ENDPOINT)
+      mockWebSocketProvider(WebSocketClassProvider)
+    }
+
+    oldEnv = JSON.parse(JSON.stringify(process.env))
+    process.env.WS_ENABLED = 'true'
+    process.env.WS_SUBSCRIPTION_TTL = '300'
+
+    fastify = await startServer()
+    req = request(`localhost:${(fastify.server.address() as AddressInfo).port}`)
+  })
+
+  afterAll((done) => {
+    process.env = oldEnv
+    nock.restore()
+    nock.cleanAll()
+    nock.enableNetConnect()
+    fastify.close(done)
+  })
+
+  describe('crypto endpoint', () => {
+    const jobID = '1'
+
+    it('should return success', async () => {
+      const data: AdapterRequest = {
+        id: jobID,
+        data: {
+          endpoint: 'crypto',
+          base: 'BTC',
+          quote: 'USD',
+        },
+      }
+
+      let flowFulfilled: Promise<boolean>
+      if (!process.env.RECORD) {
+        mockResponseSuccess() // For the first response
+
+        flowFulfilled = mockWebSocketFlow(mockedWsServer, [
+          mockCryptoSubscribeResponse,
+          mockCryptoUnsubscribeResponse,
+          // Double mocks in flow are needed to get rid of timing issues
+          mockCryptoSubscribeResponse,
+          mockCryptoUnsubscribeResponse,
+        ])
+      }
+
+      const makeRequest = () =>
+        req
+          .post('/')
+          .send(data)
+          .set('Accept', '*/*')
+          .set('Content-Type', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+
+      // We don't care about the first response, coming from http request
+      // This first request will start both batch warmer & websocket
+      await makeRequest()
+
+      await util.sleep(100)
+      // This final request should disable the cache warmer
+      const response = await makeRequest()
+      expect(response.body).toEqual({
+        jobRunID: '1',
+        result: 43682.66306523,
+        statusCode: 200,
+        maxAge: 30000,
+        data: { result: 43682.66306523 },
       })
 
       await flowFulfilled
