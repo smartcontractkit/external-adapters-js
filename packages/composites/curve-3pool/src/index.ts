a736ec8f833953155e0793abe8dfbdcb9dd26966
Chan-Ho Suh <chanhosuh@gmail.com>
2022-07-05 13:16:17 -0400

    Curve 3pool (#1946)

---
+++ packages/composites/curve-3pool/src/index.ts
@@ -0,0 +1,8 @@
+import { expose } from '@chainlink/ea-bootstrap'
+import { endpointSelector, makeExecute } from './adapter'
+import { makeConfig, NAME } from './config'
+
+const adapterContext = { name: NAME }
+
+const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
+export { NAME, makeConfig, makeExecute, server }
