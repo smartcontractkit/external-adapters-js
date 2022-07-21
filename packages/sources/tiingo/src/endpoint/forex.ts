b1987740916f9dce1bd3d7ed54ac7e9d28d13686
amit-momin <amit.momin@smartcontract.com>
2022-07-21 13:00:50 -0500

    Expanded Tiingo adapter to allow configuration of inverses

--- packages/sources/tiingo/src/endpoint/forex.ts
+++ packages/sources/tiingo/src/endpoint/forex.ts
@@ -1,6 +1,7 @@
-import { Requester, util, Validator } from '@chainlink/ea-bootstrap'
+import { IncludePair, Requester, util, Validator } from '@chainlink/ea-bootstrap'
 import { ExecuteWithConfig, Config, InputParameters } from '@chainlink/ea-bootstrap'
-import { NAME } from '../config'
+import { NAME as AdapterName } from '../config'
+import includes from '../config/includes.json'
 import overrides from '../config/symbols.json'
 
 export const supportedEndpoints = ['forex', 'fx', 'commodities']
@@ -37,15 +38,38 @@ interface ResponseSchema {
   midPrice: number
 }
 
+export type TOptions = {
+  url: string
+  inverse?: boolean
+}
+
+const getUrl = (from: string, to: string) => ({
+  url: util.buildUrlPath('/tiingo/fx/:ticker/top', { ticker: `${from}${to}`.toLowerCase() }),
+})
+
+const getIncludesOptions = (
+  //@ts-expect-error no-unused-vars
+  validator: Validator<TInputParameters>,
+  include: IncludePair,
+) => {
+  return {
+    ...getUrl(include.from, include.to),
+    inverse: include.inverse,
+  }
+}
+
 export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters, {}, { overrides })
+  const validator = new Validator(request, inputParameters, {}, { includes, overrides })
 
   const jobRunID = validator.validated.id
-  const base = validator.overrideSymbol(NAME, validator.validated.data.base)
-  const quote = validator.validated.data.quote
-  const ticker = `${base}${quote}`.toLowerCase()
   const resultPath = (validator.validated.data.resultPath || '').toString()
-  const url = util.buildUrlPath('/tiingo/fx/:ticker/top', { ticker })
+
+  const { url, inverse } = util.getPairOptions<TOptions, TInputParameters>(
+    AdapterName,
+    validator,
+    getIncludesOptions,
+    getUrl,
+  ) as TOptions // If base and quote cannot be batched, getPairOptions will return TOptions
 
   const reqConfig = {
     ...config.api,
@@ -56,7 +80,7 @@ export const execute: ExecuteWithConfig<Config> = async (request, _, config) => 
   }
 
   const response = await Requester.request<ResponseSchema[]>(reqConfig)
-  const result = Requester.validateResultNumber(response.data, [0, resultPath])
+  const result = Requester.validateResultNumber(response.data, [0, resultPath], { inverse })
 
   return Requester.success(jobRunID, Requester.withResult(response, result), config.verbose)
 }
