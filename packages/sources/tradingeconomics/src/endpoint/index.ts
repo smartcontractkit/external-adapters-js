e743603c84baf63b2b25c03c8107a22960068d44
Austin Born <austinborn212@gmail.com>
2022-07-29 05:50:38 -0700

    Export price-ws from tradingeconomics

--- packages/sources/tradingeconomics/src/endpoint/index.ts
+++ packages/sources/tradingeconomics/src/endpoint/index.ts
@@ -3,3 +3,4 @@ import type { TInputParameters as PriceInputParameters } from './price'
 export type TInputParameters = PriceInputParameters
 
 export * as price from './price'
+export * as price_ws from './price-ws'
