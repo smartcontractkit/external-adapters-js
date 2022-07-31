3e5006f2f6ee7bbc496b8e8b9907e06ce3924bd5
Austin Born <austinborn212@gmail.com>
2022-07-30 23:53:49 -0700

    Other endpoint TODOs

--- packages/composites/linear-finance/src/endpoint/allocations.ts
+++ packages/composites/linear-finance/src/endpoint/allocations.ts
@@ -5,11 +5,11 @@ import moment from 'moment-timezone'
 
 const endpointConfig: { [index: string]: { url: string; symbolKey: 'symbol' | 'slug' } } = {
   xlci: {
-    url: '/v1/index/xangle-largecap',
+    url: '/v1/index/xangle-largecap', //TODO remove support? https://pro-api.xangle.io/#section/Version-History
     symbolKey: 'symbol',
   },
   xbci: {
-    url: '/v1/index/xangle-bluechip',
+    url: '/v1/index/xangle-bluechip', //TODO remove support? https://pro-api.xangle.io/#section/Version-History
     symbolKey: 'symbol',
   },
   x30: {
