5c0a56f19b72dd90bb5234829d1ffa63fd734a94
Matt Kelly <mkelly2@live.co.uk>
2022-08-01 22:08:57 +0800

    Added inverse pairs for ncfx

--- packages/sources/ncfx/src/adapter.ts
+++ packages/sources/ncfx/src/adapter.ts
@@ -1,4 +1,4 @@
-import { Requester, Validator, Builder } from '@chainlink/ea-bootstrap'
+import { Requester, Validator, Builder, IncludePair, util } from '@chainlink/ea-bootstrap'
 import {
   Config,
   ExecuteWithConfig,
@@ -9,6 +9,9 @@ import {
 } from '@chainlink/ea-bootstrap'
 import { makeConfig } from './config'
 import * as endpoints from './endpoint'
+import { NAME as AdapterName } from './config'
+import { TInputParameters } from './endpoint'
+import includes from './config/includes.json'
 
 export const execute: ExecuteWithConfig<Config, endpoints.TInputParameters> = async (
   request,
@@ -50,26 +53,64 @@ interface ForexMessage {
 
 type Message = CryptoMessage | ForexMessage
 
+export type TOptions = {
+  from: string
+  to: string
+  inverse: boolean
+}
+
+interface Pair {
+  pair: string
+  inverse: boolean
+}
+
+const getIncludesOptions = (
+  //@ts-expect-error no-unused-vars
+  validator: Validator<TInputParameters>,
+  include: IncludePair,
+): TOptions | undefined => {
+  return {
+    from: include.from,
+    to: include.to,
+    inverse: include.inverse || false,
+  }
+}
+
+const getPair = (input: AdapterRequest): Pair => {
+  const validator = new Validator(
+    input,
+    endpoints.crypto.inputParameters,
+    {},
+    { includes, shouldThrowError: false },
+  )
+  if (validator.error) return { inverse: false, pair: '' }
+  const endpoint = input.data.endpoint
+
+  const { from, to, inverse } = util.getPairOptions<TOptions, TInputParameters>(
+    AdapterName,
+    validator,
+    getIncludesOptions,
+    (base: string, quote: string) => ({
+      from: base,
+      to: quote,
+      inverse: false,
+    }),
+  ) as TOptions
+
+  return {
+    inverse,
+    pair:
+      !!endpoint && endpoints.forex.supportedEndpoints.indexOf(endpoint) !== -1
+        ? `${from}${to}`
+        : `${from}/${to}`,
+  }
+}
+
 export const makeWSHandler = (
   config?: Config,
 ): MakeWSHandler<
   Message | any // TODO: WS message types
 > => {
-  const getPair = (input: AdapterRequest) => {
-    const validator = new Validator(
-      input,
-      endpoints.crypto.inputParameters,
-      {},
-      { shouldThrowError: false },
-    )
-    if (validator.error) return ''
-    const base = validator.validated.data.base.toUpperCase()
-    const quote = validator.validated.data.quote.toUpperCase()
-    const endpoint = input.data.endpoint
-    return !!endpoint && endpoints.forex.supportedEndpoints.indexOf(endpoint) !== -1
-      ? `${base}${quote}`
-      : `${base}/${quote}`
-  }
   const getSubscription = (request: 'subscribe' | 'unsubscribe', pair?: string) => {
     if (!pair) return ''
     return { request, ccy: pair }
@@ -100,9 +141,11 @@ export const makeWSHandler = (
       subscribe: (input: AdapterRequest) =>
         isForexEndpoint(input.data.endpoint)
           ? 'ncfx_forex'
-          : getSubscription('subscribe', getPair(input)),
+          : getSubscription('subscribe', getPair(input).pair),
       unsubscribe: (input: AdapterRequest) =>
-        isForexEndpoint(input.data.endpoint) ? '' : getSubscription('unsubscribe', getPair(input)),
+        isForexEndpoint(input.data.endpoint)
+          ? ''
+          : getSubscription('unsubscribe', getPair(input).pair),
       subsFromMessage: (message: Message, subscriptionMsg: any, input: AdapterRequest) => {
         if (isForexEndpoint(input.data.endpoint)) return 'ncfx_forex'
         if (Array.isArray(message) && message.length > 0) {
@@ -117,7 +160,7 @@ export const makeWSHandler = (
       filter: (message: Message) =>
         (Array.isArray(message) && message.length > 0) || Object.keys(message).length > 0,
       toResponse: (message: any, input: any) => {
-        const pair = getPair(input)
+        const { pair, inverse } = getPair(input)
         const pairMessage = Array.isArray(message)
           ? message.find((m: Message) => m.currencyPair === pair)
           : message[pair]
@@ -127,7 +170,7 @@ export const makeWSHandler = (
         }
         const endpoint = input.data.endpoint
         const resultField = isForexEndpoint(endpoint) ? 'price' : 'mid'
-        const result = Requester.validateResultNumber(pairMessage, [resultField])
+        const result = Requester.validateResultNumber(pairMessage, [resultField], { inverse })
         return Requester.success('1', { data: { ...pairMessage, result } }, defaultConfig.verbose)
       },
       onConnect: (input: AdapterRequest) => {
