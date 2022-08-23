9bccadc4f68245888e0acc4653a979aebef913c2
Austin Born <austinborn212@gmail.com>
2022-08-23 04:40:11 -0700

    Add integration tests

--- packages/sources/elwood/test/integration/adapter.test.ts
+++ packages/sources/elwood/test/integration/adapter.test.ts
@@ -1,8 +1,80 @@
-// import { AdapterRequest } from '@chainlink/ea-bootstrap'
-// import { AddressInfo } from 'net'
-// import nock from 'nock'
-// import request, { SuperTest, Test } from 'supertest'
-// import { server as startServer } from '../../src'
-// import { mockPriceSuccess } from './fixtures'
+import { AdapterRequest, FastifyInstance } from '@chainlink/ea-bootstrap'
+import { AddressInfo } from 'net'
+import request, { SuperTest, Test } from 'supertest'
+import { server as startServer } from '../../src'
+import {
+  mockSubscribeWSResponse,
+  mockUnsubscribeWSResponse,
+  mockSubscribeResponse,
+  mockUnsubscribeResponse,
+} from './fixtures'
+import {
+  mockWebSocketFlow,
+  mockWebSocketProvider,
+  mockWebSocketServer,
+  MockWsServer,
+} from '@chainlink/ea-test-helpers'
+import { WebSocketClassProvider } from '@chainlink/ea-bootstrap/dist/lib/middleware/ws/recorder'
+import * as nock from 'nock'
 
-//TODO WS Mock test
+describe('websocket', () => {
+  let mockedWsServer: InstanceType<typeof MockWsServer>
+  let fastify: FastifyInstance
+  let req: SuperTest<Test>
+  let oldEnv: NodeJS.ProcessEnv
+
+  beforeAll(async () => {
+    oldEnv = JSON.parse(JSON.stringify(process.env))
+    process.env.WS_ENABLED = 'true'
+    process.env.API_KEY = process.env.API_KEY ?? 'test-api-key'
+    process.env.WS_SUBSCRIPTION_TTL = '3000'
+
+    if (!process.env.RECORD) {
+      mockedWsServer = mockWebSocketServer('wss://api.chk.elwood.systems/v1/stream')
+      mockWebSocketProvider(WebSocketClassProvider)
+    } else {
+      nock.recorder.rec()
+    }
+
+    fastify = await startServer()
+    req = request(`localhost:${(fastify.server.address() as AddressInfo).port}`)
+  })
+
+  afterAll((done) => {
+    process.env = oldEnv
+    fastify.close(done)
+  })
+
+  describe('price endpoint', () => {
+    it('should return success', async () => {
+      mockSubscribeResponse()
+      mockUnsubscribeResponse()
+      const data: AdapterRequest = {
+        id: '1',
+        data: {
+          base: 'ETH',
+          quote: 'USD',
+        },
+      }
+
+      let flowFulfilled: Promise<boolean>
+      if (!process.env.RECORD) {
+        flowFulfilled = mockWebSocketFlow(mockedWsServer, [
+          mockSubscribeWSResponse,
+          mockUnsubscribeWSResponse,
+        ])
+      }
+
+      const response = await req
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+      expect(response.body).toMatchSnapshot()
+      await flowFulfilled
+    }, 10_000)
+  })
+})
--- packages/sources/elwood/test/integration/fixtures.ts
+++ packages/sources/elwood/test/integration/fixtures.ts
@@ -1,1 +1,42 @@
-//TODO WS Fixtures
+import nock from 'nock'
+
+export const mockSubscribeResponse = (): nock => {
+  nock('https://api.chk.elwood.systems:443', { encodedQueryParams: true })
+    .persist()
+    .post('/v1/stream', {
+      action: 'subscribe',
+      stream: 'index',
+      symbol: 'ETH-USD',
+      index_freq: 1000,
+    })
+    .reply(200, {}, [])
+}
+
+export const mockUnsubscribeResponse = (): nock => {
+  nock('https://api.chk.elwood.systems:443', { encodedQueryParams: true })
+    .persist()
+    .post('/v1/stream', {
+      action: 'unsubscribe',
+      stream: 'index',
+      symbol: 'ETH-USD',
+      index_freq: 1000,
+    })
+    .reply(200, {}, [])
+}
+
+export const mockSubscribeWSResponse = {
+  request: { action: 'subscribe', stream: 'index', symbol: 'ETH-USD', index_freq: 1000 },
+  response: [
+    { type: 'heartbeat', data: '2022-08-22T10:58:30.537993108Z', sequence: 1 },
+    {
+      type: 'Index',
+      data: { price: '10000.000', symbol: 'ETH-USD', timestamp: '2022-08-22T10:58:34.31724727Z' },
+      sequence: 2,
+    },
+  ],
+}
+
+export const mockUnsubscribeWSResponse = {
+  request: { action: 'unsubscribe', stream: 'index', symbol: 'ETH-USD', index_freq: 1000 },
+  response: [{ type: 'heartbeat', data: '2022-08-22T10:58:30.537993108Z', sequence: 3 }],
+}
