c707c7c6b670dbef2f25a03ce94cbd383f89042d
Justin Kaseman <justinkaseman@live.com>
2022-09-28 18:53:41 -0700

    Modify Proof-of-Reserves composite to allow stader-address-list as a protocol

--- packages/composites/proof-of-reserves/src/utils/protocol.ts
+++ packages/composites/proof-of-reserves/src/utils/protocol.ts
@@ -9,6 +9,7 @@ import { makeRequestFactory, callAdapter } from '.'
 
 // protocol adapters
 import * as swellList from '@chainlink/swell-address-list-adapter'
+import * as staderList from '@chainlink/stader-address-list-adapter'
 import * as renVM from '@chainlink/renvm-address-set-adapter'
 import * as wBTC from '@chainlink/wbtc-address-set-adapter'
 import * as Gemini from '@chainlink/gemini-adapter'
@@ -27,6 +28,7 @@ export const adapters: AdapterImplementation[] = [
   chainReserveWallets as unknown as AdapterImplementation,
   wrapped as unknown as AdapterImplementation,
   swellList as unknown as AdapterImplementation,
+  staderList as unknown as AdapterImplementation,
 ]
 
 export type Protocol = typeof adapters[number]['NAME']
