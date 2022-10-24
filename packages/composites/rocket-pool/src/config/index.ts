b3c2f4a3323fe44bfeab1d3ccb8ff8393c990903
Austin Born <austinborn212@gmail.com>
2022-10-24 08:58:51 -0700

    Network address comment

--- packages/composites/rocket-pool/src/config/index.ts
+++ packages/composites/rocket-pool/src/config/index.ts
@@ -5,6 +5,9 @@ import { ethers } from 'ethers'
 export const NAME = 'ROCKET_POOL'
 
 export const DEFAULT_ENDPOINT = 'reth'
+/*
+Note: if we know the addresses ahead of time, we could set up a map to the network provided
+*/
 export const DEFAULT_ETH_USD_PROXY_ADDRESS = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419'
 export const DEFAULT_RETH_STORAGE_ADDRESS = '0xae78736Cd615f374D3085123A210448E74Fc6393'
 
