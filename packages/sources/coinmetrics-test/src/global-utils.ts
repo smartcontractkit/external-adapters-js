dee737aeff93364df14620ec8621b915624be247
Matt Kelly <mkelly2@live.co.uk>
2022-11-03 17:18:40 +0800

    Updated validateResultNumber in coinmetrics-test

--- packages/sources/coinmetrics-test/src/global-utils.ts
+++ packages/sources/coinmetrics-test/src/global-utils.ts
@@ -1,4 +1,44 @@
+import { makeLogger } from '@chainlink/external-adapter-framework/util'
+import { AdapterDataProviderError } from '@chainlink/external-adapter-framework/validation/error'
+import { ResponseSchema } from './endpoint/price'
+
+const logger = makeLogger('Coinmetrics Global Util')
+
+const missingDataErrorMsg = 'Data provider response empty'
+const missingResultsErrorMsg =
+  'Result could not be found in path or is empty. This is likely an issue with the data provider or the input params/overrides.'
+const invalidResultError =
+  'Invalid result received. This is likely an issue with the data provider or the input params/overrides.'
+
 // Placeholder. Will replace this when v3 framework is updated
-export const validateResultNumber = (data: unknown, metric: string): number => {
-  return (data as any).data[0][metric]
+export const validateResultNumber = (data: ResponseSchema, metric: string): number => {
+  if (!data.data || !data.data.length) {
+    logger.error(missingDataErrorMsg, { data, metric })
+    throw new AdapterDataProviderError({
+      message: missingDataErrorMsg,
+      statusCode: 502,
+    })
+  }
+
+  const result = data.data[0][metric as 'ReferenceRateUSD' | 'ReferenceRateEUR']
+
+  if (typeof result === 'undefined' || result === null) {
+    logger.error(missingResultsErrorMsg, { data, metric })
+    throw new AdapterDataProviderError({
+      message: missingResultsErrorMsg,
+      statusCode: 502,
+    })
+  }
+
+  const num = Number(result)
+
+  if (num === 0 || isNaN(num)) {
+    logger.error(invalidResultError, { data, metric })
+    throw new AdapterDataProviderError({
+      message: invalidResultError,
+      statusCode: 400,
+    })
+  }
+
+  return num
 }
