0dbaa8a4dbaecb8be267b0c1b73f1e1659a10e02
Matt Kelly <mkelly2@live.co.uk>
2022-11-03 21:25:59 +0800

    Added integration tests for coinmetrics-test

--- packages/sources/coinmetrics-test/test/integration/adapter.test.ts
+++ packages/sources/coinmetrics-test/test/integration/adapter.test.ts
@@ -1,42 +1,31 @@
-import { AdapterRequest, FastifyInstance } from '@chainlink/ea-bootstrap'
-import { AddressInfo } from 'net'
-import nock from 'nock'
-import request, { SuperTest, Test } from 'supertest'
-import { server as startServer } from '../../src'
-import { mockPriceSuccess } from './fixtures'
+import { mockBurnedSuccess, mockPriceSuccess, mockTotalBurnedSuccess } from './fixtures'
+import { SuperTest, Test } from 'supertest'
+import { setupExternalAdapterTest, SuiteContext } from './setup'
+import { ServerInstance } from '@chainlink/external-adapter-framework'
 
 describe('execute', () => {
   const id = '1'
-  let fastify: FastifyInstance
-  let req: SuperTest<Test>
-  let oldEnv: NodeJS.ProcessEnv
 
-  beforeAll(async () => {
-    oldEnv = JSON.parse(JSON.stringify(process.env))
+  const context: SuiteContext = {
+    req: null,
+    server: async () => {
+      process.env['RATE_LIMIT_CAPACITY_SECOND'] = '6'
+      process.env['METRICS_ENABLED'] = 'false'
+      const server = (await import('../../src')).server
+      return server() as Promise<ServerInstance>
+    },
+  }
 
-    process.env.CACHE_ENABLED = 'false'
-    if (process.env.RECORD) {
-      nock.recorder.rec()
-    }
-    fastify = await startServer()
-    req = request(`localhost:${(fastify.server.address() as AddressInfo).port}`)
-  })
+  const envVariables = {
+    CACHE_ENABLED: 'false',
+    API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:18081',
+    API_KEY: process.env.API_KEY || 'test_key',
+  }
 
-  afterAll((done) => {
-    process.env = oldEnv
-
-    if (process.env.RECORD) {
-      nock.recorder.play()
-    }
-
-    nock.restore()
-    nock.cleanAll()
-    nock.enableNetConnect()
-    fastify.close(done)
-  })
+  setupExternalAdapterTest(envVariables, context)
 
   describe('price api', () => {
-    const data: AdapterRequest = {
+    const data = {
       id,
       data: {
         base: 'ETH',
@@ -47,7 +36,58 @@ describe('execute', () => {
     it('should return success', async () => {
       mockPriceSuccess()
 
-      const response = await req
+      const response = await (context.req as SuperTest<Test>)
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('total-burned api', () => {
+    const data = {
+      id,
+      data: {
+        asset: 'eth',
+        frequency: '1d',
+        pageSize: 10000,
+        startTime: '2021-09-20',
+        endTime: '2021-09-25',
+        endpoint: 'total-burned',
+      },
+    }
+
+    it('should return success', async () => {
+      mockTotalBurnedSuccess()
+
+      const response = await (context.req as SuperTest<Test>)
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('burned api', () => {
+    const data = {
+      id,
+      data: {
+        asset: 'eth',
+        frequency: '1d',
+        endpoint: 'burned',
+      },
+    }
+
+    it('should return success', async () => {
+      mockBurnedSuccess()
+
+      const response = await (context.req as SuperTest<Test>)
         .post('/')
         .send(data)
         .set('Accept', '*/*')
--- packages/sources/coinmetrics-test/test/integration/fixtures.ts
+++ packages/sources/coinmetrics-test/test/integration/fixtures.ts
@@ -4,18 +4,144 @@ export const mockPriceSuccess = (): nock.Scope =>
   nock('http://localhost:18081', {
     encodedQueryParams: true,
   })
-    .get('/price')
+    .get('/timeseries/asset-metrics')
     .query({
-      base: 'ETH',
-      quote: 'USD',
+      assets: 'ETH',
+      metrics: 'ReferenceRateUSD',
+      frequency: '1s',
+      api_key: 'test_key',
+      page_size: '1',
     })
-    .reply(200, () => ({ price: 10 }), [
-      'Content-Type',
-      'application/json',
-      'Connection',
-      'close',
-      'Vary',
-      'Accept-Encoding',
-      'Vary',
-      'Origin',
-    ])
+    .reply(
+      200,
+      {
+        data: [
+          { asset: 'eth', time: '2022-11-03T09:31:12.000000000Z', ReferenceRateUSD: '1552.8' },
+        ],
+        next_page_token: 'abc',
+        next_page_url: 'abc',
+      },
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
+
+export const mockTotalBurnedSuccess = (): nock.Scope =>
+  nock('http://localhost:18081', {
+    encodedQueryParams: true,
+  })
+    .get('/timeseries/asset-metrics')
+    .query({
+      assets: 'eth',
+      metrics: 'FeeTotNtv%2CRevNtv%2CIssTotNtv',
+      frequency: '1d',
+      page_size: '10000',
+      api_key: 'test_key',
+      start_time: '2021-09-20',
+      end_time: '2021-09-25',
+    })
+    .reply(
+      200,
+      {
+        data: [
+          {
+            asset: 'eth',
+            time: '2021-09-20T00:00:00.000000000Z',
+            FeeTotNtv: '9331.617399578292365165',
+            IssTotNtv: '13465.375',
+            RevNtv: '14903.076551063217245019',
+          },
+          {
+            asset: 'eth',
+            time: '2021-09-21T00:00:00.000000000Z',
+            FeeTotNtv: '10725.893521006011986668',
+            IssTotNtv: '13569.625',
+            RevNtv: '15125.559184739053811959',
+          },
+          {
+            asset: 'eth',
+            time: '2021-09-22T00:00:00.000000000Z',
+            FeeTotNtv: '7241.077830167154483706',
+            IssTotNtv: '13445.875',
+            RevNtv: '14506.07882008361914748',
+          },
+          {
+            asset: 'eth',
+            time: '2021-09-23T00:00:00.000000000Z',
+            FeeTotNtv: '9503.848309834219302852',
+            IssTotNtv: '13372.8125',
+            RevNtv: '16274.553315636682282599',
+          },
+          {
+            asset: 'eth',
+            time: '2021-09-24T00:00:00.000000000Z',
+            FeeTotNtv: '7640.784022848909291801',
+            IssTotNtv: '13449.25',
+            RevNtv: '14517.956543257905308913',
+          },
+          {
+            asset: 'eth',
+            time: '2021-09-25T00:00:00.000000000Z',
+            FeeTotNtv: '5019.537962541415576967',
+            IssTotNtv: '13721.25',
+            RevNtv: '14418.533987808854584078',
+          },
+        ],
+      },
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
+
+export const mockBurnedSuccess = (): nock.Scope =>
+  nock('http://localhost:18081', {
+    encodedQueryParams: true,
+  })
+    .get('/timeseries/asset-metrics')
+    .query({
+      assets: 'eth',
+      metrics: 'FeeTotNtv%2CRevNtv%2CIssTotNtv',
+      frequency: '1d',
+      page_size: '1',
+      api_key: 'test_key',
+    })
+    .reply(
+      200,
+      {
+        data: [
+          {
+            asset: 'eth',
+            time: '2022-11-02T00:00:00.000000000Z',
+            FeeTotNtv: '2161.966589993882262224',
+            IssTotNtv: '0',
+            RevNtv: '581.529288070705439454',
+          },
+        ],
+        next_page_token: 'abc',
+        next_page_url: 'abc',
+      },
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
