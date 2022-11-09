e728ba3894aab53e75a68f281126b77289df63a5
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2022-11-09 12:34:58 +0400

    uncomment rateLimit config

--- packages/sources/coinpaprika-test/src/index.ts
+++ packages/sources/coinpaprika-test/src/index.ts
@@ -16,29 +16,29 @@ export const adapter = new Adapter({
   defaultEndpoint: 'crypto',
   name: 'COINPAPRIKA',
   overrides: overrides['coinpaprika'],
-  // rateLimiting: {
-  //   tiers: {
-  //     "free": {
-  //       "rateLimit1s": 10,
-  //       "rateLimit1h": 69.44,
-  //       "note": "50k/mo for free"
-  //     },
-  //     "pro": {
-  //       "rateLimit1s": 10,
-  //       "rateLimit1h": 347.22,
-  //       "note": "250k/mo for pro"
-  //     },
-  //     "business": {
-  //       "rateLimit1s": 10,
-  //       "rateLimit1h": 1388.888,
-  //       "note": "1mil/mo for business"
-  //     },
-  //     "enterprise": {
-  //       "rateLimit1s": 10,
-  //       "note": "unlimited per month"
-  //     }
-  //   }
-  // },
+  rateLimiting: {
+    tiers: {
+      "free": {
+        "rateLimit1s": 10,
+        "rateLimit1h": 69.44,
+        "note": "50k/mo for free"
+      },
+      "pro": {
+        "rateLimit1s": 10,
+        "rateLimit1h": 347.22,
+        "note": "250k/mo for pro"
+      },
+      "business": {
+        "rateLimit1s": 10,
+        "rateLimit1h": 1388.888,
+        "note": "1mil/mo for business"
+      },
+      "enterprise": {
+        "rateLimit1s": 10,
+        "note": "unlimited per month"
+      }
+    }
+  },
   endpoints: [
     coins,
     dominance,
