d252afb4c346b5e847bd4777c41e8597ab4ba178
karen-stepanyan <karen.stepanyan@protofire.io>
2022-11-15 19:47:47 +0400

    add overrides and limits

--- packages/sources/kaiko-test/src/index.ts
+++ packages/sources/kaiko-test/src/index.ts
@@ -3,17 +3,27 @@ import { PriceAdapter } from '@chainlink/external-adapter-framework/adapter'
 import { customSettings, DEFAULT_API_ENDPOINT } from './config'
 import { trades } from './endpoint'
 import includes from './config/includes.json'
+import overrides from './config/overrides.json'
 
 export const adapter = new PriceAdapter({
   defaultEndpoint: trades.name,
   name: 'KAIKO',
   endpoints: [trades],
   customSettings,
+  rateLimiting: {
+    tiers: {
+      default: {
+        rateLimit1h: -1,
+        note: 'claims no rate limits',
+      },
+    },
+  },
   envDefaultOverrides: {
     API_TIMEOUT: 10000,
     API_ENDPOINT: DEFAULT_API_ENDPOINT,
   },
   includes,
+  overrides: overrides['kaiko'],
 })
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
