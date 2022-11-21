b836b5fb37e995309f868da8e508d440504a287d
Ivan Savin <ivan.savin@smartcontract.com>
2022-11-21 11:16:03 +0000

    cleanup

--- packages/k6/src/config/types.ts
+++ packages/k6/src/config/types.ts
@@ -5,15 +5,6 @@ export interface Payload {
   data: string
 }
 
-/** Example of assertion:
- {
-    "request": {"endpoint": "/crypto", "data": { "base": "ETH", "quote": "USD" }},
-    "expectedResponse": {
-        "result": {"greaterThan": 0, "minPrecision": 10}
-    }
-} 
-Assertions are copied from {pathToAdapterPackage}/output-assertions.json
-*/
 export type ExpectedResponse = Record<string, Record<string, string | number>>
 
 export type Assertion = {
