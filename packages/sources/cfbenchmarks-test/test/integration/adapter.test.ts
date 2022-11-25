6abb413ab31ddc9aab29a1aaefd9855d2b338933
Alejo Berardino <alejoberardino@gmail.com>
2022-11-25 16:25:34 -0300

    Bump CFBenchmarks v3 framework (#2358)
    * Bump CFBenchmarks v3 framework
    
    * Remove unused package

--- packages/sources/cfbenchmarks-test/test/integration/adapter.test.ts
+++ packages/sources/cfbenchmarks-test/test/integration/adapter.test.ts
@@ -7,6 +7,7 @@ import { AdapterRequestBody, sleep } from '@chainlink/external-adapter-framework
 import { mockWebSocketProvider, mockWebSocketServer, setEnvVariables } from './setup'
 // import { mockResponseSuccess } from './fixtures'
 import { createAdapter } from './setup'
+import { Server } from 'mock-socket'
 
 // describe('rest', () => {
 //   jest.setTimeout(10000)
@@ -61,6 +62,8 @@ import { createAdapter } from './setup'
 describe('websocket', () => {
   let fastify: ServerInstance | undefined
   let req: SuperTest<Test>
+  let spy: jest.SpyInstance
+  let mockWsServer: Server | undefined
   const wsEndpoint = 'ws://localhost:9090'
 
   jest.setTimeout(10000)
@@ -83,9 +86,12 @@ describe('websocket', () => {
     process.env['API_USERNAME'] = 'fake-api-username'
     process.env['API_PASSWORD'] = 'fake-api-password'
 
+    const mockDate = new Date('2022-05-10T16:09:27.193Z')
+    spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
+
     // Start mock web socket server
     mockWebSocketProvider(WebSocketClassProvider)
-    mockWebSocketServer(wsEndpoint)
+    mockWsServer = mockWebSocketServer(wsEndpoint)
 
     fastify = await expose(createAdapter())
     req = request(`http://localhost:${(fastify?.server.address() as AddressInfo).port}`)
@@ -96,7 +102,9 @@ describe('websocket', () => {
   })
 
   afterAll((done) => {
+    spy.mockRestore()
     setEnvVariables(oldEnv)
+    mockWsServer?.close()
     fastify?.close(done())
   })
 
@@ -109,86 +117,68 @@ describe('websocket', () => {
           .set('Accept', '*/*')
           .set('Content-Type', 'application/json')
           .expect('Content-Type', /json/)
-      let response = await makeRequest()
+      const response = await makeRequest()
       expect(response.body).toEqual({
         result: 40067,
         statusCode: 200,
         data: { result: 40067 },
+        timestamps: {
+          providerDataReceived: 1652198967193,
+          providerDataStreamEstablished: 1652198967193,
+          providerIndicatedTime: 1645203822000,
+        },
       })
-      await sleep(5000)
-      // WS subscription and cache should be expired by now
-      response = await makeRequest()
-      expect(response.statusCode).toEqual(504)
     }, 30000)
   })
-})
 
-describe('input validation', () => {
-  let fastify: ServerInstance | undefined
-  let req: SuperTest<Test>
-  let oldEnv: NodeJS.ProcessEnv
+  describe('input validation', () => {
+    it('should return error (empty body)', async () => {
+      const makeRequest = () =>
+        req
+          .post('/')
+          .send({})
+          .set('Accept', '*/*')
+          .set('Content-Type', 'application/json')
+          .expect('Content-Type', /json/)
 
-  beforeAll(async () => {
-    oldEnv = JSON.parse(JSON.stringify(process.env))
-    process.env['METRICS_ENABLED'] = 'false'
-    process.env['API_USERNAME'] = 'fake-api-username'
-    process.env['API_PASSWORD'] = 'fake-api-password'
+      const response = await makeRequest()
+      expect(response.statusCode).toEqual(400)
+    }, 30000)
+    it('should return error (empty data)', async () => {
+      const makeRequest = () =>
+        req
+          .post('/')
+          .send({ data: {} })
+          .set('Accept', '*/*')
+          .set('Content-Type', 'application/json')
+          .expect('Content-Type', /json/)
 
-    fastify = await expose(createAdapter())
-    req = request(`http://localhost:${(fastify?.server.address() as AddressInfo).port}`)
+      const response = await makeRequest()
+      expect(response.statusCode).toEqual(400)
+    }, 30000)
+    it('should return error (empty base)', async () => {
+      const makeRequest = () =>
+        req
+          .post('/')
+          .send({ data: { quote: 'BTC' } })
+          .set('Accept', '*/*')
+          .set('Content-Type', 'application/json')
+          .expect('Content-Type', /json/)
+
+      const response = await makeRequest()
+      expect(response.statusCode).toEqual(400)
+    }, 30000)
+    it('should return error (empty quote)', async () => {
+      const makeRequest = () =>
+        req
+          .post('/')
+          .send({ data: { base: 'ETH' } })
+          .set('Accept', '*/*')
+          .set('Content-Type', 'application/json')
+          .expect('Content-Type', /json/)
+
+      const response = await makeRequest()
+      expect(response.statusCode).toEqual(400)
+    }, 30000)
   })
-
-  afterAll((done) => {
-    setEnvVariables(oldEnv)
-    fastify?.close(done())
-  })
-
-  it('should return error (empty body)', async () => {
-    const makeRequest = () =>
-      req
-        .post('/')
-        .send({})
-        .set('Accept', '*/*')
-        .set('Content-Type', 'application/json')
-        .expect('Content-Type', /json/)
-
-    const response = await makeRequest()
-    expect(response.statusCode).toEqual(400)
-  }, 30000)
-  it('should return error (empty data)', async () => {
-    const makeRequest = () =>
-      req
-        .post('/')
-        .send({ data: {} })
-        .set('Accept', '*/*')
-        .set('Content-Type', 'application/json')
-        .expect('Content-Type', /json/)
-
-    const response = await makeRequest()
-    expect(response.statusCode).toEqual(400)
-  }, 30000)
-  it('should return error (empty base)', async () => {
-    const makeRequest = () =>
-      req
-        .post('/')
-        .send({ data: { quote: 'BTC' } })
-        .set('Accept', '*/*')
-        .set('Content-Type', 'application/json')
-        .expect('Content-Type', /json/)
-
-    const response = await makeRequest()
-    expect(response.statusCode).toEqual(400)
-  }, 30000)
-  it('should return error (empty quote)', async () => {
-    const makeRequest = () =>
-      req
-        .post('/')
-        .send({ data: { base: 'ETH' } })
-        .set('Accept', '*/*')
-        .set('Content-Type', 'application/json')
-        .expect('Content-Type', /json/)
-
-    const response = await makeRequest()
-    expect(response.statusCode).toEqual(400)
-  }, 30000)
 })
