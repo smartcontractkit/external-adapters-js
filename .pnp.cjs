87cb0bc669dd54fe6c22ebbf377c95b7cfdcd14b
Alejo Berardino <alejoberardino@gmail.com>
2022-11-25 15:10:42 -0300

    Bump CFBenchmarks v3 framework

--- .pnp.cjs
+++ .pnp.cjs
@@ -4573,7 +4573,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/cfbenchmarks-test/",\
           "packageDependencies": [\
             ["@chainlink/cfbenchmarks-test-adapter", "workspace:packages/sources/cfbenchmarks-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.11.1"],\
+            ["@chainlink/external-adapter-framework", "npm:0.13.0"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
