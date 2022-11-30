cab074ed31374a8d0615f5d19170cece0546568c
Matt Kelly <mkelly2@live.co.uk>
2022-11-30 11:56:34 +0800

    Added integration tests for DAR

---
+++ packages/sources/dar/test/integration/fixtures.ts
@@ -0,0 +1,31 @@
+import nock from 'nock'
+
+export const mockPriceResponse = {
+  darAssetID: '12345',
+  darAssetTicker: 'ETH',
+  quoteCurrency: 'USD',
+  price: 1272.12,
+  publishedAt: '2022-11-30T11:46:28.2323078Z',
+  errors: '',
+}
+
+export const mockTokenResponse = (): nock.Scope =>
+  nock('https://test-url.com', {
+    encodedQueryParams: true,
+  })
+    .post('/token-auth')
+    .reply(
+      200,
+      () => ({ access_token: '111111111111111111111111111', expires_in: 12345, token_type: 'abc' }),
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
+    .persist(true)
