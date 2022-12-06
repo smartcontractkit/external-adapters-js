9dd7692b1b3dd4ab17508dd19be1ebf8c9043a09
ad0ll <ad0ll@users.noreply.github.com>
2022-12-06 14:38:44 -0800

    Cherry pick changesets, update yarn lock

---
+++ .changeset/three-eyes-guess.md
@@ -0,0 +1,5 @@
+---
+'@chainlink/dar-adapter': patch
+---
+
+Update framework version
--- .pnp.cjs
+++ .pnp.cjs
@@ -4573,7 +4573,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/cfbenchmarks-test/",\
           "packageDependencies": [\
             ["@chainlink/cfbenchmarks-test-adapter", "workspace:packages/sources/cfbenchmarks-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.13.2"],\
+            ["@chainlink/external-adapter-framework", "npm:0.14.3"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
