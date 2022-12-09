f4a2f483b763b027ed1c3c554f30914747a6942f
ad0ll <20057155+ad0ll@users.noreply.github.com>
2022-12-09 11:22:56 -0800

    chore/PDI-166: Deprecate v2 Galaxy adapter for v3 (#2403)
    * Drop v2 galaxy
    
    * Move galaxy-test to galaxy
    
    * Add changeset
    
    Co-authored-by: ad0ll <ad0ll@users.noreply.github.com>
    Co-authored-by: amit-momin <108959691+amit-momin@users.noreply.github.com>

--- packages/core/legos/src/sources.ts
+++ packages/core/legos/src/sources.ts
@@ -76,7 +76,6 @@ import * as fluent_finance from '@chainlink/fluent-finance-adapter'
 import * as fmpcloud from '@chainlink/fmpcloud-adapter'
 import * as galaxis from '@chainlink/galaxis-adapter'
 import * as galaxy from '@chainlink/galaxy-adapter'
-import * as galaxy_test from '@chainlink/galaxy-test-adapter'
 import * as gemini from '@chainlink/gemini-adapter'
 import * as genesis_adapter from '@chainlink/genesis-volatility-adapter'
 import * as geodb from '@chainlink/geodb-adapter'
@@ -225,7 +224,6 @@ export default {
   fmpcloud,
   galaxis,
   galaxy,
-  galaxy_test,
   gemini,
   genesis_adapter,
   geodb,
