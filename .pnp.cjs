05fa5c9ff40f937fe4a0d5b26143f2be4c3b8626
bogos <andre.neyra@gmail.com>
2023-01-16 07:36:49 -0500

    update v0.19.0

--- .pnp.cjs
+++ .pnp.cjs
@@ -9350,7 +9350,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/tradingeconomics-test/",\
           "packageDependencies": [\
             ["@chainlink/tradingeconomics-test-adapter", "workspace:packages/sources/tradingeconomics-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.18.0"],\
+            ["@chainlink/external-adapter-framework", "npm:0.19.0"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
