eaab4ee145c047d7d36c71694b60bf197ae7acb8
karen-stepanyan <karen.stepanyan@protofire.io>
2023-01-17 18:49:02 +0400

    update ea framework version

--- .pnp.cjs
+++ .pnp.cjs
@@ -6942,7 +6942,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/nomics-test/",\
           "packageDependencies": [\
             ["@chainlink/nomics-test-adapter", "workspace:packages/sources/nomics-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.15.1"],\
+            ["@chainlink/external-adapter-framework", "npm:0.19.0"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
