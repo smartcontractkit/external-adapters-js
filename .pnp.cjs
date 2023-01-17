06cdfbe5ccad1d6b4a9a65e8f88c822acf104f13
karen-stepanyan <karen.stepanyan@protofire.io>
2023-01-17 19:02:09 +0400

    update ea framework version

--- .pnp.cjs
+++ .pnp.cjs
@@ -5276,7 +5276,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/dxfeed-test/",\
           "packageDependencies": [\
             ["@chainlink/dxfeed-test-adapter", "workspace:packages/sources/dxfeed-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.18.0"],\
+            ["@chainlink/external-adapter-framework", "npm:0.19.0"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
