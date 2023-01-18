bfa059479ba010952882f451e8d2818ca0ce874b
ad0ll <ad0ll@users.noreply.github.com>
2023-01-18 10:15:18 -0800

    Change bank frick, expect run against kank frick only

--- packages/sources/bank-frick/src/index.ts
+++ packages/sources/bank-frick/src/index.ts
@@ -3,6 +3,7 @@ import { accountsRestEndpoint } from './endpoint/accounts'
 import { customSettings } from './config'
 import { expose } from '@chainlink/external-adapter-framework'
 
+//TODO (REVERT ME) Comment to trigger build
 export const adapter = new Adapter({
   name: 'BANK_FRICK',
   defaultEndpoint: 'accounts',
