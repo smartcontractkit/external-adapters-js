23a1bd4b38c7f04b2257c7b3a010ac57ce48c780
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2023-01-24 20:54:47 +0400

    add coinpaprika-test v3 ea (#2303)
    * add coinpaprika-test v3 ea
    
    * remove old unit tests
    
    * uncomment rateLimit config
    
    * fix format issues
    
    * remove unused deps
    
    * run yarn install
    
    * remove crypto single endpoint
    
    * bump framework version
    
    * fix vwap endpoint
    
    * refactor error handling
    
    * fix integration tests failing with workaround
    
    * refactor endpoint and headers
    
    * chunked request for crypto endpoint
    
    * small refactor
    
    * fix batch requests to use up to 3 different quotes
    
    * update ea framework version
    
    * update ea-framework version
    
    * regenerate test snapshots
    
    Co-authored-by: amit-momin <108959691+amit-momin@users.noreply.github.com>

---
+++ packages/sources/coinpaprika-test/src/endpoint/dominance.ts
@@ -0,0 +1,31 @@
+import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { ProviderResult } from '@chainlink/external-adapter-framework/util'
+import { HttpTransport } from '@chainlink/external-adapter-framework/transports'
+import {
+  buildGlobalRequestBody,
+  constructEntry,
+  GlobalEndpointTypes,
+  inputParameters,
+} from '../global-utils'
+
+const httpTransport = new HttpTransport<GlobalEndpointTypes>({
+  prepareRequests: (params, config) => {
+    return buildGlobalRequestBody(params, config)
+  },
+  parseResponse: (params, res): ProviderResult<GlobalEndpointTypes>[] => {
+    const entries = [] as ProviderResult<GlobalEndpointTypes>[]
+    for (const requestPayload of params) {
+      const entry = constructEntry(res.data, requestPayload, '_dominance_percentage')
+      if (entry) {
+        entries.push(entry)
+      }
+    }
+    return entries
+  },
+})
+
+export const endpoint = new AdapterEndpoint({
+  name: 'dominance',
+  transport: httpTransport,
+  inputParameters,
+})
