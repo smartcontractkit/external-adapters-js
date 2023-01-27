ad69c4669e49338ef5a05ae5ec63cff2a110e8fb
amit-momin <amit.momin@smartcontract.com>
2023-01-26 18:25:45 -0600

    Bumped framework to latest version

--- .pnp.cjs
+++ .pnp.cjs
@@ -4213,7 +4213,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/bank-frick/",\
           "packageDependencies": [\
             ["@chainlink/bank-frick-adapter", "workspace:packages/sources/bank-frick"],\
-            ["@chainlink/external-adapter-framework", "npm:0.21.3"],\
+            ["@chainlink/external-adapter-framework", "npm:0.22.0"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
@@ -6002,10 +6002,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],\
           "linkType": "HARD"\
         }],\
-        ["npm:0.21.3", {\
-          "packageLocation": "./.yarn/cache/@chainlink-external-adapter-framework-npm-0.21.3-da497b2e24-9ca29b2517.zip/node_modules/@chainlink/external-adapter-framework/",\
+        ["npm:0.22.0", {\
+          "packageLocation": "./.yarn/cache/@chainlink-external-adapter-framework-npm-0.22.0-9fbb5d3512-d53b5b3192.zip/node_modules/@chainlink/external-adapter-framework/",\
           "packageDependencies": [\
-            ["@chainlink/external-adapter-framework", "npm:0.21.3"],\
+            ["@chainlink/external-adapter-framework", "npm:0.22.0"],\
             ["ajv", "npm:8.11.0"],\
             ["axios", "npm:0.27.2"],\
             ["eventsource", "npm:2.0.2"],\
