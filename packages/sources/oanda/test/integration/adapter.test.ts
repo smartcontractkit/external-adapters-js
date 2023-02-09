e40145a7358d8a52e9ded140c960e7e2eb0ab78c
Austin Born <austinborn212@gmail.com>
2023-02-09 02:28:58 -0800

    Add test payload

--- packages/sources/oanda/test/integration/adapter.test.ts
+++ packages/sources/oanda/test/integration/adapter.test.ts
@@ -1,90 +1,229 @@
-// import { ServerInstance } from '@chainlink/external-adapter-framework'
-// import { WebSocketClassProvider } from '@chainlink/external-adapter-framework/transports'
+// import test from 'ava'
+// import FakeTimers from '@sinonjs/fake-timers'
+// import axios, { AxiosError, AxiosRequestConfig } from 'axios'
+// import { SettingsMap } from '../../src/config'
 // import { AddressInfo } from 'net'
-// import * as nock from 'nock'
-// import request, { SuperTest, Test } from 'supertest'
-// import { mockSubscribeError, mockSubscribeResponse, mockUnsubscribeResponse } from './fixtures'
-// import { mockWebSocketProvider, mockWebSocketServer } from './setup'
+// import { expose } from '../../src'
+// import { Adapter, AdapterEndpoint } from '../../src/adapter'
+// import { SseTransport, SSEConfig } from '../../src/transports'
+// import { ProviderResult, SingleNumberResultResponse } from '../../src/util'
+// import { InputParameters } from '../../src/validation'
+// import { assertEqualResponses, MockCache, runAllUntilTime } from '../util'
+// import nock from 'nock'
+// const { MockEvent, EventSource } = require('mocksse') // eslint-disable-line
+// const URL = 'http://test.com'
 
-// describe('websocket', () => {
-//   let fastify: ServerInstance
-//   let req: SuperTest<Test>
-//   let oldEnv: NodeJS.ProcessEnv
-//   let spy: jest.SpyInstance
-//   const apiKey: string = process.env['API_KEY'] ?? 'test-api-key'
+// interface AdapterRequestParams {
+//   base: string
+//   quote: string
+// }
 
-//   beforeAll(async () => {
-//     oldEnv = JSON.parse(JSON.stringify(process.env))
-//     process.env['API_KEY'] = apiKey
-//     process.env['WS_SUBSCRIPTION_TTL'] = '3000'
-//     process.env['METRICS_ENABLED'] = 'false'
-//     process.env['RATE_LIMIT_CAPACITY_SECOND'] = '10'
+// export const inputParameters: InputParameters = {
+//   base: {
+//     type: 'string',
+//     required: true,
+//   },
+//   quote: {
+//     type: 'string',
+//     required: true,
+//   },
+// }
 
-//     const mockDate = new Date('2022-05-10T16:09:27.193Z')
-//     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
+// type StreamEndpointTypes = {
+//   Request: {
+//     Params: AdapterRequestParams
+//   }
+//   Response: SingleNumberResultResponse
+//   CustomSettings: SettingsMap
+//   Provider: {
+//     RequestBody: never
+//   }
+// }
 
-//     if (!process.env['RECORD']) {
-//       mockWebSocketServer(`wss://api.chk.elwood.systems/v1/stream?apiKey${process.env['API_KEY']}`)
-//       mockWebSocketProvider(WebSocketClassProvider)
-//     } else {
-//       nock.recorder.rec()
+// export const sseTransport: SseTransport<StreamEndpointTypes> = new SseTransport({
+//   prepareSSEConnectionConfig: (): SSEConfig => {
+//     return { url: URL }
+//   },
+//   prepareKeepAliveRequest: (): AxiosRequestConfig<never> => {
+//     const axiosRequestConfig: AxiosRequestConfig<never> = {
+//       method: 'POST',
+//       url: `${URL}/ping`,
 //     }
-
-//     const { server } = await import('../../src')
-//     const api = await server()
-//     if (!api) {
-//       throw 'API did not start'
+//     return axiosRequestConfig
+//   },
+//   prepareSubscriptionRequest: (): AxiosRequestConfig<never> => {
+//     const axiosConfig: AxiosRequestConfig<never> = {
+//       method: 'POST',
+//       url: `${URL}/sub`,
 //     }
-//     fastify = api
-//     req = request(`localhost:${(fastify.server.address() as AddressInfo).port}`)
-//   })
-
-//   afterAll((done) => {
-//     spy.mockRestore()
-//     process.env = oldEnv
-//     fastify.close(done)
-//   })
-
-//   describe('price endpoint', () => {
-//     it('should return success', async () => {
-//       mockSubscribeResponse(apiKey)
-//       mockUnsubscribeResponse(apiKey)
-//       const data = {
-//         id: '1',
-//         data: {
-//           base: 'ETH',
-//           quote: 'USD',
-//         },
-//       }
-
-//       const response = await req
-//         .post('/')
-//         .send(data)
-//         .set('Accept', '*/*')
-//         .set('Content-Type', 'application/json')
-//         .expect('Content-Type', /json/)
-//         .expect(200)
-
-//       expect(response.body).toMatchSnapshot()
-//     }, 10_000)
-//     it('should return cached subscribe error', async () => {
-//       mockSubscribeError(apiKey)
-//       const data = {
-//         data: {
-//           base: 'XXX',
-//           quote: 'USD',
-//         },
-//       }
-
-//       const response = await req
-//         .post('/')
-//         .send(data)
-//         .set('Accept', '*/*')
-//         .set('Content-Type', 'application/json')
-//         .expect('Content-Type', /json/)
-//         .expect(400)
-
-//       expect(response.body).toMatchSnapshot()
-//     }, 10_000)
-//   })
+//     return axiosConfig
+//   },
+//   prepareUnsubscriptionRequest: (): AxiosRequestConfig<never> => {
+//     const axiosConfig: AxiosRequestConfig<never> = {
+//       method: 'POST',
+//       url: `${URL}/unsub`,
+//     }
+//     return axiosConfig
+//   },
+//   eventListeners: [
+//     {
+//       type: 'price',
+//       parseResponse: (evt: MessageEvent): ProviderResult<StreamEndpointTypes>[] => {
+//         return [
+//           {
+//             params: { base: evt.data.base, quote: evt.data.quote },
+//             response: {
+//               data: {
+//                 result: evt.data.price,
+//               },
+//               result: evt.data.price,
+//             },
+//           },
+//         ]
+//       },
+//     },
+//   ],
 // })
+
+// export const sseEndpoint = new AdapterEndpoint({
+//   name: 'test',
+//   transport: sseTransport,
+//   inputParameters,
+// })
+
+// const CACHE_MAX_AGE = 4000
+// const BACKGROUND_EXECUTE_MS_SSE = 5000
+
+// // Disable retries to make the testing flow easier
+// process.env['CACHE_POLLING_MAX_RETRIES'] = '0'
+
+// const adapter = new Adapter({
+//   name: 'TEST',
+//   defaultEndpoint: 'test',
+//   endpoints: [sseEndpoint],
+//   envDefaultOverrides: {
+//     CACHE_MAX_AGE,
+//     BACKGROUND_EXECUTE_MS_SSE,
+//   },
+// })
+
+// test('connects to EventSource, subscribes, gets message, unsubscribes and handles misconfigured subscription', async (t) => {
+//   const clock = FakeTimers.install()
+
+//   // Mocks SSE events which are handled by the mock EventListener dependency
+//   mockSSE()
+//   mockHTTP()
+
+//   const base = 'ETH'
+//   const quote = 'USD'
+//   const price = 111
+
+//   // Create mocked cache so we can listen when values are set
+//   // This is a more reliable method than expecting precise clock timings
+//   // Also use the mock event source provided by mocksee
+//   const mockCache = new MockCache(adapter.config.CACHE_MAX_ITEMS)
+//   // Start up adapter
+//   const api = await expose(adapter, {
+//     cache: mockCache,
+//     eventSource: EventSource,
+//   })
+//   const address = `http://localhost:${(api?.server?.address() as AddressInfo)?.port}`
+
+//   const makeRequest = () =>
+//     axios.post(address, {
+//       data: {
+//         base,
+//         quote,
+//       },
+//     })
+
+//   // Expect the first response to time out
+//   // The polling behavior is tested in the cache tests, so this is easier here.
+//   // Start the request:
+//   const earlyErrorPromise: Promise<AxiosError | undefined> = t.throwsAsync(makeRequest)
+//   // Advance enough time for the initial request async flow
+//   // clock.tickAsync(10)
+//   // Wait for the failed cache get -> instant 504
+//   const earlyError = await earlyErrorPromise
+//   t.is(earlyError?.response?.status, 504)
+
+//   // Advance clock so that the batch warmer executes once again and wait for the cache to be set
+//   const cacheValueSetPromise = mockCache.waitForNextSet()
+//   await runAllUntilTime(clock, BACKGROUND_EXECUTE_MS_SSE + 10)
+//   await cacheValueSetPromise
+
+//   // Second request should find the response in the cache
+//   const response = await makeRequest()
+
+//   t.is(response.status, 200)
+//   assertEqualResponses(t, response.data, {
+//     data: { result: 111 },
+//     result: price,
+//     statusCode: 200,
+//   })
+
+//   // Handles misconfigured subscription
+
+//   // Make a request for an unsupported ticker symbol
+//   const makeBadRequest = () =>
+//     axios.post(address, {
+//       data: {
+//         base: 'NONE',
+//         quote: 'USD',
+//       },
+//     })
+
+//   await clock.tickAsync(BACKGROUND_EXECUTE_MS_SSE + 10)
+
+//   // Expect the request to fail since the token is invalid
+//   const errorPromise: Promise<AxiosError | undefined> = t.throwsAsync(makeBadRequest)
+//   // Advance enough time for the initial request async flow
+//   // clock.tickAsync(10)
+//   // Wait for the failed cache get -> instant 504
+//   const error = await errorPromise
+//   t.is(error?.response?.status, 504)
+
+//   // Wait until the cache expires, and the subscription is out
+//   await clock.tickAsync(11000)
+
+//   // Now that the cache is out and the subscription no longer there, this should time out
+//   const error2: AxiosError | undefined = await t.throwsAsync(makeRequest)
+//   t.is(error2?.response?.status, 504)
+
+//   clock.uninstall()
+// })
+
+// const mockSSE = () => {
+//   const mock = new MockEvent({
+//     url: URL,
+//     setInterval: 10,
+//     responses: [
+//       {
+//         type: 'price',
+//         data: { base: 'ETH', quote: 'USD', price: 111 },
+//         lastEventId: '0000000',
+//         origin: URL,
+//       },
+//     ],
+//   })
+//   return mock
+// }
+
+// const mockHTTP = () => {
+//   nock('http://test.com')
+//     .post('/sub')
+//     .times(2)
+//     .reply(200, {
+//       message: 'Successfully subscribed to ETH/USD',
+//     })
+//     .post('/unsub')
+//     .times(2)
+//     .reply(200, {
+//       message: 'Successfully unsubscribed from ETH/USD',
+//     })
+//     .post('/ping')
+//     .times(9999999)
+//     .reply(200, {
+//       message: 'Pong',
+//     })
+// }
