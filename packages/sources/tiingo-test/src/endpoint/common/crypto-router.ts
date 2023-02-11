4ebd7154411aee29bb2ab4732f2e6007a92345d7
amit-momin <amit.momin@smartcontract.com>
2023-02-10 21:08:41 -0600

    Bumped v3 framework version

--- packages/sources/tiingo-test/src/endpoint/common/crypto-router.ts
+++ packages/sources/tiingo-test/src/endpoint/common/crypto-router.ts
@@ -1,5 +1,5 @@
 import { RoutingTransport } from '@chainlink/external-adapter-framework/transports/meta'
-import { PriceEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { CryptoPriceEndpoint } from '@chainlink/external-adapter-framework/adapter'
 import { CryptoEndpointTypes, inputParameters } from '../../crypto-utils'
 import { httpTransport } from '../http/crypto'
 import { wsTransport } from '../ws/crypto'
@@ -12,7 +12,7 @@ export const routingTransport = new RoutingTransport<CryptoEndpointTypes>(
   (_, adapterConfig) => (adapterConfig?.WS_ENABLED ? 'WS' : 'HTTP'),
 )
 
-export const endpoint = new PriceEndpoint<CryptoEndpointTypes>({
+export const endpoint = new CryptoPriceEndpoint<CryptoEndpointTypes>({
   name: 'crypto',
   aliases: ['price', 'prices', 'crypto-synth'],
   transport: routingTransport,
