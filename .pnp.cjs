bbd060f8132ea80f40ef83a473829c12b8cfb4e3
Martin Liu <martin.liu@smartcontract.com>
2023-02-16 11:53:38 +1100

    Upgrade EA framework to 0.23.2

--- .pnp.cjs
+++ .pnp.cjs
@@ -7883,7 +7883,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/twosigma/",\
           "packageDependencies": [\
             ["@chainlink/twosigma-adapter", "workspace:packages/sources/twosigma"],\
-            ["@chainlink/external-adapter-framework", "npm:0.21.3"],\
+            ["@chainlink/external-adapter-framework", "npm:0.23.2"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
