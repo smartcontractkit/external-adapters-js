86e75c8f47d9bc7d32f85c20f82f944ef5c1c817
karen-stepanyan <karen.stepanyan@protofire.io>
2023-02-17 19:53:30 +0400

    add validation of missing websocket key

--- packages/sources/tradermade-test/src/endpoint/forex-router.ts
+++ packages/sources/tradermade-test/src/endpoint/forex-router.ts
@@ -1,12 +1,31 @@
 import { RoutingTransport } from '@chainlink/external-adapter-framework/transports/meta'
 import { PriceEndpoint } from '@chainlink/external-adapter-framework/adapter'
-import { BatchEndpointTypes } from '../price-utils'
+import { BatchEndpointTypes, BatchRequestParams } from '../price-utils'
 import { httpTransport, inputParameters } from './forex'
 import { wsTransport } from './forex-ws'
+import { AdapterConfig } from '@chainlink/external-adapter-framework/config'
+import { customSettings } from '../config'
+import {
+  AdapterError,
+  AdapterInputError,
+} from '@chainlink/external-adapter-framework/validation/error'
 
 const transports = {
   ws: wsTransport,
   rest: httpTransport,
+}
+
+function customInputValidation(
+  params: BatchRequestParams,
+  config: AdapterConfig<typeof customSettings>,
+): AdapterError | undefined {
+  if (params.transport === 'ws' && !config.WS_API_KEY) {
+    return new AdapterInputError({
+      statusCode: 400,
+      message: 'WS_API_KEY is not set',
+    })
+  }
+  return
 }
 
 export const routingTransport = new RoutingTransport<BatchEndpointTypes>(transports, {
@@ -18,4 +37,5 @@ export const endpoint = new PriceEndpoint<BatchEndpointTypes>({
   aliases: ['batch'],
   transport: routingTransport,
   inputParameters: inputParameters,
+  customInputValidation,
 })
