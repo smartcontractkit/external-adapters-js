82f1f36586a3165a4b41f2546e36f761edb6a0b4
Ivan Savin <ivan.savin@smartcontract.com>
2023-02-27 14:00:56 +0000

    app pod logs and update liveness delay

--- packages/scripts/src/ephemeral-adapters/lib.ts
+++ packages/scripts/src/ephemeral-adapters/lib.ts
@@ -194,7 +194,9 @@ export const deployAdapter = (config: Inputs): void => {
   }
 
   // const k8sEvents = new Shell().exec(`kubectl describe pods -n adapters`)
-  const k8sEvents = new Shell().exec(`kubectl logs redis-master-0 -n adapters`)
+  const k8sEvents = new Shell().exec(
+    `kubectl logs -l app.kubernetes.io/name=qa-ea-oanda-2512 --all-containers -n adapters`,
+  )
   log(blue.bold(`k8sEvents\n ${k8sEvents}`))
 
   if (exec_result) {
