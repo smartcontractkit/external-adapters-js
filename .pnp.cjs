fcfa8c4f64a8e69002ae4d6d1ebe119e746f4055
bogos <andre.neyra@gmail.com>
2023-03-09 14:30:06 -0500

    add missing files

--- .pnp.cjs
+++ .pnp.cjs
@@ -7465,22 +7465,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["ws", "virtual:1dec7dbd5a4c81f9d324d14923601bbaeec0be79c9b024d3ffbd58bc01936b6e0f705c25f56c7cc2cd81695309929e12db6472661bf6db8d727b07794d3bc5e4#npm:8.9.0"]\
           ],\
           "linkType": "HARD"\
-        }],\
-        ["npm:0.27.4", {\
-          "packageLocation": "./.yarn/cache/@chainlink-external-adapter-framework-npm-0.27.4-a8b2b83028-7879f95ee6.zip/node_modules/@chainlink/external-adapter-framework/",\
-          "packageDependencies": [\
-            ["@chainlink/external-adapter-framework", "npm:0.27.4"],\
-            ["ajv", "npm:8.11.0"],\
-            ["axios", "npm:1.3.4"],\
-            ["eventsource", "npm:2.0.2"],\
-            ["fastify", "npm:4.9.2"],\
-            ["ioredis", "npm:5.2.3"],\
-            ["pino", "npm:8.6.1"],\
-            ["pino-pretty", "npm:9.1.0"],\
-            ["prom-client", "npm:13.2.0"],\
-            ["ws", "virtual:9bcb395e33af1476676205024eaeeb9ff3cc20552a16175c13c3f5040d89bbd447d2875194b826fc04ceb29c5066a6fa008e709abaa30eec3185f667e7043ce2#npm:8.9.0"]\
-          ],\
-          "linkType": "HARD"\
         }]\
       ]],\
       ["@chainlink/external-adapters-js", [\
