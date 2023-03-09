dfc717db013a57a87f7f72b1ab6a64d5b4d162fd
bogos <andre.neyra@gmail.com>
2023-03-09 14:24:24 -0500

    update framework v0.27.4

--- .pnp.cjs
+++ .pnp.cjs
@@ -5174,7 +5174,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/sources/alphavantage-test/",\
           "packageDependencies": [\
             ["@chainlink/alphavantage-test-adapter", "workspace:packages/sources/alphavantage-test"],\
-            ["@chainlink/external-adapter-framework", "npm:0.27.3"],\
+            ["@chainlink/external-adapter-framework", "npm:0.27.4"],\
             ["@types/jest", "npm:27.5.2"],\
             ["@types/node", "npm:16.11.51"],\
             ["@types/supertest", "npm:2.0.12"],\
