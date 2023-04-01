c6943ea82456086d117d6cbbbe54f0a40ea07e00
JoshC2k <josh.brown@smartcontract.com>
2023-04-01 01:38:16 -0500

    Feature/DF-18698 Tiingo Liquidity Spread (#2596)

--- packages/sources/tiingo-test/src/index.ts
+++ packages/sources/tiingo-test/src/index.ts
@@ -2,13 +2,13 @@ import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
 import { PriceAdapter } from '@chainlink/external-adapter-framework/adapter'
 import { config } from './config'
 import includes from './config/includes.json'
-import { crypto, eod, forex, iex, top, volume, vwap } from './endpoint'
+import { crypto, cryptolwba, eod, forex, iex, top, volume, vwap } from './endpoint'
 
 export const adapter = new PriceAdapter({
   defaultEndpoint: crypto.name,
   name: 'TIINGO',
   config,
-  endpoints: [crypto, volume, top, eod, iex, forex, vwap],
+  endpoints: [crypto, volume, top, eod, iex, forex, vwap, cryptolwba],
   includes,
   rateLimiting: {
     tiers: {
