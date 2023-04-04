c71f22e9f8103197b1eef73493b49180e4bf06bf
Ivan Savin <ivan.savin@smartcontract.com>
2023-04-04 16:34:17 +0100

    add all types of pairs

--- packages/sources/tradermade/src/index.ts
+++ packages/sources/tradermade/src/index.ts
@@ -3,8 +3,6 @@ import { endpointSelector, makeExecute, makeWSHandler } from './adapter'
 import { makeConfig, NAME } from './config'
 import rateLimit from './config/limits.json'
 
-// test change to trigger soak testing - TODO remove
-
 const adapterContext = { name: NAME, rateLimit }
 
 const { server } = expose(adapterContext, makeExecute(), makeWSHandler(), endpointSelector)
