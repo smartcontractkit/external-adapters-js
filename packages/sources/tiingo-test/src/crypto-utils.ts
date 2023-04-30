7e95b90d4d4ddddf27e7b52e866c451c5095c6d2
Alejo Berardino <alejoberardino@gmail.com>
2023-04-30 17:47:54 -0300

    Remove unused type

--- packages/sources/tiingo-test/src/crypto-utils.ts
+++ packages/sources/tiingo-test/src/crypto-utils.ts
@@ -1,7 +1,6 @@
 import { priceEndpointInputParametersDefinition } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
-import { TypeFromDefinition } from '@chainlink/external-adapter-framework/validation/input-params'
 import { config } from './config'
 
 export const inputParameters = new InputParameters(priceEndpointInputParametersDefinition)
@@ -27,8 +26,6 @@ export interface ProviderResponseBody {
     tradesDone: number
   }[]
 }
-
-export type PriceEndpointParams = TypeFromDefinition<typeof inputParameters.definition>
 
 export type CryptoEndpointTypes = {
   Parameters: typeof inputParameters.definition
