6794e45149f10ff00331c96297a0af75bf1ec0fd
amit-momin <108959691+amit-momin@users.noreply.github.com>
2023-05-05 20:34:00 -0500

    Add overrides to Tradermade v3 live endpoint (#2696)
    * Added overrides to Tradermade v3 live endpoint
    
    * Added override integration test

---
+++ .changeset/short-olives-explain.md
@@ -0,0 +1,5 @@
+---
+'@chainlink/tradermade-test-adapter': minor
+---
+
+Added overrides to the live endpoint
--- packages/sources/tradermade-test/test/integration/fixtures.ts
+++ packages/sources/tradermade-test/test/integration/fixtures.ts
@@ -65,3 +65,33 @@ export const mockForexResponse = (): nock.Scope =>
         'Origin',
       ],
     )
+    .get('/api/v1/live')
+    .query({ api_key: 'fake-api-key', currency: 'OILUSD' })
+    .reply(
+      200,
+      () => {
+        return {
+          endpoint: 'live',
+          quotes: [
+            {
+              ask: 71.292,
+              bid: 71.27,
+              instrument: 'OILUSD',
+              mid: 71.281,
+            },
+          ],
+          requested_time: 'Fri, 05 May 2023 21:56:43 GMT',
+          timestamp: 1683323804,
+        }
+      },
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
