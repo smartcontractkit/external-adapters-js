047001bbf74fe55f50634b9b902119199395512b
Alexandr Yepishev <alexandr.yepishev@smartcontract.com>
2023-05-24 12:46:26 +0100

    Add integration with Sonar

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ module.exports = {
   coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['json', 'text'],
+  coverageReporters: ['lcov', 'json', 'text'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
