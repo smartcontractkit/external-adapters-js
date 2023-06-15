f2afd2b9d6ccdef254df5dd76e367c808b14d7eb
amit-momin <amit.momin@smartcontract.com>
2023-06-15 12:08:06 -0500

    Updated trueusd default endpoint

--- packages/sources/trueusd/src/config/index.ts
+++ packages/sources/trueusd/src/config/index.ts
@@ -4,7 +4,7 @@ import { Config } from '@chainlink/ea-bootstrap'
 export const NAME = 'TRUEUSD'
 
 export const DEFAULT_ENDPOINT = 'trueusd'
-export const DEFAULT_BASE_URL = 'https://api.real-time-attest.trustexplorer.io'
+export const DEFAULT_BASE_URL = 'https://api.real-time-reserves.ledgerlens.io/v1/'
 
 export const makeConfig = (prefix?: string): Config => {
   const config = Requester.getDefaultConfig(prefix)
