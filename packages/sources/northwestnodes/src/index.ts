4155e34c3718248a384304fbe945d75db296f297
Eric H. den Boer <eric@northwestnodes.com>
2023-06-17 13:14:02 +0400

    added rate limiting, set at 1 req/sec, 10 req/min, 60 req/hr.

--- packages/sources/northwestnodes/src/index.ts
+++ packages/sources/northwestnodes/src/index.ts
@@ -8,6 +8,16 @@ export const adapter = new Adapter({
   endpoints: [staking_ethereum_epoch_single, staking_ethereum_epoch_list],
   defaultEndpoint: staking_ethereum_epoch_single.name,
   config,
+  rateLimiting: {
+    tiers: {
+      staking_ethereum_apr_chainlink_nop: {
+        rateLimit1s: 1,
+        rateLimit1m: 10,
+        rateLimit1h: 60,
+        note: 'Default Chainlink NOP rate limiting tier for the Ethereum staking APR data',
+      },
+    },
+  },
 })
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
