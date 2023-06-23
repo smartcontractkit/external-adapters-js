6062242a82f09d78164e6c13c42620251f65e2df
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2023-06-23 21:33:54 +0400

    Standardize v3 adapter structure pt 2 (#2846)
    * standardize v3 adapter structure
    
    * changeset
    
    * remove 'router' suffix from router endpoint file names
    
    ---------
    
    Co-authored-by: amit-momin <108959691+amit-momin@users.noreply.github.com>

--- packages/sources/blocksize-capital/src/endpoint/price.ts
+++ packages/sources/blocksize-capital/src/endpoint/price.ts
@@ -1,134 +1,23 @@
 import {
   CryptoPriceEndpoint,
-  EndpointContext,
   priceEndpointInputParametersDefinition,
 } from '@chainlink/external-adapter-framework/adapter'
-import { WebsocketReverseMappingTransport } from '@chainlink/external-adapter-framework/transports/websocket'
-import {
-  ProviderResult,
-  SingleNumberResultResponse,
-  makeLogger,
-} from '@chainlink/external-adapter-framework/util'
+import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
-
-const logger = makeLogger('BlocksizeCapitalWebsocketEndpoint')
-interface BaseMessage {
-  jsonrpc: string
-  id?: string | number | null
-}
-
-export interface Message extends BaseMessage {
-  method: 'vwap'
-  params: {
-    updates: {
-      ticker: string
-      price?: number
-      size?: number
-      volume?: number
-      ts: number
-    }[]
-  }
-}
+import { transport } from '../transport/price'
 
 const inputParameters = new InputParameters(priceEndpointInputParametersDefinition)
 
-export type EndpointTypes = {
+export type BaseEndpointTypes = {
   Parameters: typeof inputParameters.definition
   Settings: typeof config.settings
   Response: SingleNumberResultResponse
-  Provider: {
-    WsMessage: Message
-  }
 }
 
-export const websocketTransport: WebsocketReverseMappingTransport<EndpointTypes, string> =
-  new WebsocketReverseMappingTransport<EndpointTypes, string>({
-    url: ({ adapterSettings: { WS_API_ENDPOINT } }) => {
-      return WS_API_ENDPOINT
-    },
-    handlers: {
-      open: (connection: WebSocket, context: EndpointContext<EndpointTypes>) => {
-        return new Promise((resolve, reject) => {
-          connection.addEventListener('message', (event: MessageEvent<any>) => {
-            const parsed = JSON.parse(event.data.toString())
-            if (parsed.result?.user_id) {
-              logger.debug('Got logged in response, connection is ready')
-              resolve()
-            } else {
-              reject(new Error('Failed to make WS connection'))
-            }
-          })
-          const options = {
-            jsonrpc: '2.0',
-            method: 'authentication_logon',
-            params: { api_key: context.adapterSettings.API_KEY },
-          }
-          connection.send(JSON.stringify(options))
-        })
-      },
-      message: (message) => {
-        if (message.method !== 'vwap') return []
-        const updates = message.params.updates
-        const results: ProviderResult<EndpointTypes>[] = []
-        for (const update of updates) {
-          const params = websocketTransport.getReverseMapping(update.ticker)
-          if (!params) {
-            continue
-          }
-          if (!update.price) {
-            const errorMessage = `The data provider didn't return any value for ${params.base}/${params.quote}`
-            logger.info(errorMessage)
-            results.push({
-              params,
-              response: {
-                statusCode: 502,
-                errorMessage,
-              },
-            })
-          } else {
-            results.push({
-              params,
-              response: {
-                result: update.price,
-                data: {
-                  result: update.price,
-                },
-                timestamps: {
-                  providerIndicatedTimeUnixMs: update.ts,
-                },
-              },
-            })
-          }
-        }
-        return results
-      },
-    },
-    builders: {
-      subscribeMessage: (params) => {
-        const pair = `${params.base}${params.quote}`.toUpperCase()
-        websocketTransport.setReverseMapping(pair, params)
-        return {
-          jsonrpc: '2.0',
-          method: 'vwap_subscribe',
-          params: { tickers: [pair] },
-        }
-      },
-
-      unsubscribeMessage: (params) => {
-        const pair = `${params.base}${params.quote}`.toUpperCase()
-        return {
-          jsonrpc: '2.0',
-          method: 'vwap_unsubscribe',
-          params: { tickers: [pair] },
-        }
-      },
-    },
-  })
-
-export const endpoint = new CryptoPriceEndpoint<EndpointTypes>({
+export const endpoint = new CryptoPriceEndpoint({
   name: 'price',
   aliases: ['crypto'],
-  transport: websocketTransport,
+  transport,
   inputParameters,
 })
---
+++ packages/sources/coinpaprika/src/transport/global.ts
@@ -0,0 +1,87 @@
+import { getApiEndpoint, getApiHeaders } from '../config'
+import { BaseEndpointTypes } from '../endpoint/global'
+import { HttpTransport } from '@chainlink/external-adapter-framework/transports'
+import { makeLogger } from '@chainlink/external-adapter-framework/util/logger'
+
+const logger = makeLogger('CoinPaprika Global Batched')
+
+export interface GlobalResponseBody {
+  market_cap_usd: number
+  volume_24h_usd: number
+  bitcoin_dominance_percentage: number
+  cryptocurrencies_number: number
+  market_cap_ath_value: number
+  market_cap_ath_date: string
+  volume_24h_ath_value: number
+  volume_24h_ath_date: string
+  market_cap_change_24h: number
+  volume_24h_change_24h: number
+  last_updated: number
+}
+
+export type HttpTransportTypes = BaseEndpointTypes & {
+  Provider: {
+    RequestBody: never
+    ResponseBody: GlobalResponseBody
+  }
+}
+
+const marketMap: { [key: string]: string } = {
+  BTC: 'bitcoin',
+}
+export const transport = new HttpTransport<HttpTransportTypes>({
+  prepareRequests: (params, settings) => {
+    return {
+      params,
+      request: {
+        baseURL: getApiEndpoint(settings),
+        url: '/v1/global',
+        method: 'GET',
+        headers: getApiHeaders(settings),
+      },
+    }
+  },
+  parseResponse: (params, res) => {
+    const data = res.data
+
+    return params.map((p) => {
+      if (!data) {
+        logger.warn(`The data provider did not send any value for this request`)
+        return {
+          params: p,
+          response: {
+            errorMessage: `The data provider did not send any value when requesting the global endpoint`,
+            statusCode: 502,
+          },
+        }
+      }
+
+      const propertyPath =
+        p.resultPath === '_dominance_percentage'
+          ? `${marketMap[p.market.toUpperCase()]}${p.resultPath}`
+          : `${p.resultPath}${p.market.toLowerCase()}`
+
+      const rawResult = data[propertyPath as keyof GlobalResponseBody]
+      if (!rawResult) {
+        return {
+          params: p,
+          response: {
+            errorMessage: `A value for "${propertyPath}" was not found in the provider response`,
+            statusCode: 502,
+          },
+        }
+      }
+
+      const result = Number(rawResult)
+      return {
+        params: p,
+        response: {
+          result,
+          data: {
+            result,
+          },
+        },
+      }
+    })
+  },
+})
