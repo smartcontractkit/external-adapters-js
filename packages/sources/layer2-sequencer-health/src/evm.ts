799f45423ecbf1711ba57de344a95d95b0a9959a
karen-stepanyan <karen.stepanyan@protofire.io>
2023-06-29 16:16:12 +0400

    fix lastSeenBlock saved value

--- packages/sources/layer2-sequencer-health/src/evm.ts
+++ packages/sources/layer2-sequencer-health/src/evm.ts
@@ -53,23 +53,32 @@ export const sendEVMDummyTransaction = async (
   })
 }
 
+const lastSeenBlock: Record<EVMNetworks, { block: number; timestamp: number }> = {
+  [Networks.Arbitrum]: {
+    block: 0,
+    timestamp: 0,
+  },
+  [Networks.Optimism]: {
+    block: 0,
+    timestamp: 0,
+  },
+  [Networks.Metis]: {
+    block: 0,
+    timestamp: 0,
+  },
+}
 export const checkOptimisticRollupBlockHeight = (
   network: EVMNetworks,
 ): ((config: ExtendedConfig) => Promise<boolean>) => {
-  let lastSeenBlock: { block: number; timestamp: number } = {
-    block: 0,
-    timestamp: 0,
-  }
-
-  const _isPastBlock = (block: number) => block <= lastSeenBlock.block
+  const _isPastBlock = (block: number) => block <= lastSeenBlock[network].block
   const _isStaleBlock = (block: number, delta: number): boolean => {
-    return _isPastBlock(block) && Date.now() - lastSeenBlock.timestamp >= delta
+    return _isPastBlock(block) && Date.now() - lastSeenBlock[network].timestamp >= delta
   }
   // If the request hit a replica node that fell behind, the block could be previous to the last seen. Including a deltaBlocks range to consider this case.
   const _isValidBlock = (block: number, deltaBlocks: number) =>
-    lastSeenBlock.block - block <= deltaBlocks
+    lastSeenBlock[network].block - block <= deltaBlocks
   const _updateLastSeenBlock = (block: number): void => {
-    lastSeenBlock = {
+    lastSeenBlock[network] = {
       block,
       timestamp: Date.now(),
     }
@@ -83,21 +92,21 @@ export const checkOptimisticRollupBlockHeight = (
     })
     if (!_isValidBlock(block, deltaBlocks))
       throw new AdapterResponseInvalidError({
-        message: `Block found #${block} is previous to last seen #${lastSeenBlock.block} with more than ${deltaBlocks} difference`,
+        message: `Block found #${block} is previous to last seen #${lastSeenBlock[network].block} with more than ${deltaBlocks} difference`,
       })
     if (!_isStaleBlock(block, delta)) {
       if (!_isPastBlock(block)) _updateLastSeenBlock(block)
       Logger.info(
         `Block #${block} is not considered stale at ${Date.now()}. Last seen block #${
-          lastSeenBlock.block
-        } was at ${lastSeenBlock.timestamp}`,
+          lastSeenBlock[network].block
+        } was at ${lastSeenBlock[network].timestamp}`,
       )
       return true
     }
     Logger.warn(
       `Block #${block} is considered stale at ${Date.now()}. Last seen block #${
-        lastSeenBlock.block
-      } was at ${lastSeenBlock.timestamp}, more than ${delta} milliseconds ago.`,
+        lastSeenBlock[network].block
+      } was at ${lastSeenBlock[network].timestamp}, more than ${delta} milliseconds ago.`,
     )
     return false
   }
