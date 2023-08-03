e603020a6f24df61080b2a3a1fd48191451cabf9
Alec Gard <alec.gard@chainlinklabs.com>
2023-08-03 09:15:17 +0100

    Finnhub EA's: Support inverse pairs.
    * Adds support for inverse pairs to Finnhub EA's.
    * If provided with a pair marked as inverse (e.g. JPY-USD), we
    will fetch the inverse (e.g. USD-JPY) from the DP and invert the
    result.
    * This replaces the previous FX overrides which used the inverted
    pair, but did not calculate the inverted response. This means we
    no longer support single bases (e.g. 'EUR'), and require a base &
    quote for FX.
      * Non-FX overrides still are supported, e.g. for index funds.
    * This commit also adds a defaultExchange, so that `finnhub` and
    `finnhub-secondary` can use a different default exchange if none
    is provided ('FHFX' and 'OANDA' respectively).

---
+++ .changeset/pretty-bugs-heal.md
@@ -0,0 +1,8 @@
+---
+'@chainlink/finnhub-adapter': minor
+'@chainlink/finnhub-secondary-adapter': minor
+---
+
+Add inverse config to Finnhub EA's.
+
+- Add's support for inverse pairs to Finnhub EAs.
--- packages/sources/finnhub/test/integration/adapter-rest.test.ts
+++ packages/sources/finnhub/test/integration/adapter-rest.test.ts
@@ -55,16 +55,6 @@ describe('rest', () => {
       expect(response.json()).toMatchSnapshot()
     })
 
-    it('should return success for requests overriden by default adapter overrides', async () => {
-      const data = {
-        base: 'EUR',
-      }
-      mockResponseSuccess()
-      const response = await testAdapter.request(data)
-      expect(response.statusCode).toBe(200)
-      expect(response.json()).toMatchSnapshot()
-    })
-
     it('should return success for requests with base and quote', async () => {
       const data = {
         base: 'EUR',
@@ -96,6 +86,28 @@ describe('rest', () => {
       }
       const response = await testAdapter.request(data)
       expect(response.statusCode).toBe(400)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return success for standard pairs, when pair has inverse config', async () => {
+      const data = {
+        base: 'USD',
+        quote: 'JPY',
+      }
+      mockResponseSuccess()
+      const response = await testAdapter.request(data)
+      expect(response.statusCode).toBe(200)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return success for inverted pairs', async () => {
+      const data = {
+        base: 'JPY',
+        quote: 'USD',
+      }
+      mockResponseSuccess()
+      const response = await testAdapter.request(data)
+      expect(response.statusCode).toBe(200)
       expect(response.json()).toMatchSnapshot()
     })
   })
