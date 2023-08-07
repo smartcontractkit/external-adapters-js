2fa317bfbac430d915b12a92955df7079daf4167
root <root@Austins-Surface.localdomain>
2023-08-07 16:10:01 -0400

    ion.au v3

---
+++ packages/sources/ion.au/src/endpoint/index.ts
@@ -0,0 +1,1 @@
+export { endpoint as total_reserve } from './total_reserve' 
---
+++ packages/sources/ion.au/src/transport/total_reserve.ts
@@ -0,0 +1,55 @@
+import { HttpTransport } from '@chainlink/external-adapter-framework/transports'
+import { BaseEndpointTypes } from '../endpoint/total_reserve'
+
+export interface ResponseSchema {
+    total_reserve: number
+    errorMessage?: string
+}
+
+export type HttpTransportTypes = BaseEndpointTypes & {
+  Provider: {
+    RequestBody: never
+    ResponseBody: ResponseSchema
+  }
+}
+export const httpTransport = new HttpTransport<HttpTransportTypes>({
+  prepareRequests: (params, config) => {
+    return params.map((param) => {
+      return {
+        params: [param],
+        request: {
+          baseURL: config.API_ENDPOINT,
+          url: '/',
+          headers: {},
+          params: {},
+        },
+      }
+    })
+  },
+  parseResponse: (params, response) => {
+    if (!response.data) {
+      return params.map((param) => {
+        return {
+          params: param,
+          response: {
+            errorMessage: `The data provider didn't return any value for total_reserve`,
+            statusCode: 502,
+          },
+        }
+      })
+    }
+
+    return params.map((param) => {
+      const result = response.data.total_reserve
+      return {
+        params: param,
+        response: {
+          result,
+          data: {
+            result
+          }
+        },
+      }
+    })
+  },
+})
