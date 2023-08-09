4965649ed589f0c28b5c1438d22b2c0168ca2e39
austpryb <47420717+austpryb@users.noreply.github.com>
2023-08-09 12:40:03 -0400

    Update packages/sources/ion.au/src/index.ts
    Co-authored-by: amit-momin <108959691+amit-momin@users.noreply.github.com>

--- packages/sources/ion.au/src/index.ts
+++ packages/sources/ion.au/src/index.ts
@@ -8,6 +8,14 @@ export const adapter = new Adapter({
   name: 'ION.AU',
   config,
   endpoints: [total_reserve],
+  rateLimiting: {
+    tiers: {
+      default: {
+        rateLimit1s: 50,
+        note: 'Considered unlimited tier, but setting reasonable limits',
+      },
+    },
+  },
 })
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
