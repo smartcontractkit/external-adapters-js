5195fd9f09899943d0a85b4bc9b8ab6885b2430a
karen-stepanyan <karen.stepanyan@protofire.io>
2023-08-11 10:41:00 +0400

    add rate limits

--- packages/sources/trueusd/src/index.ts
+++ packages/sources/trueusd/src/index.ts
@@ -8,6 +8,14 @@ export const adapter = new Adapter({
   name: 'TRUEUSD',
   config,
   endpoints: [trueusd],
+  rateLimiting: {
+    tiers: {
+      default: {
+        rateLimit1s: 5,
+        note: 'Considered unlimited tier, but setting reasonable limits',
+      },
+    },
+  },
 })
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
