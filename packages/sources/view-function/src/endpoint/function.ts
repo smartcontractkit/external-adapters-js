3c58594e4f7c30f80a8f6bea4f6eebe15974e76d
karen-stepanyan <karen.stepanyan@protofire.io>
2023-08-14 22:04:52 +0400

    upgrade view-function to v3

--- packages/sources/view-function/src/endpoint/function.ts
+++ packages/sources/view-function/src/endpoint/function.ts
@@ -1,13 +1,11 @@
-import { Requester, Validator } from '@chainlink/ea-bootstrap'
-import { ExecuteWithConfig, InputParameters } from '@chainlink/ea-bootstrap'
-import { Config } from '../config'
-import { utils } from 'ethers'
+import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { InputParameters } from '@chainlink/external-adapter-framework/validation'
+import { config } from '../config'
+import { functionTransport } from '../transport/function'
 
-export const supportedEndpoints = ['function']
-
-export type TInputParameters = { signature: string; address: string; inputParams: string[] }
-export const inputParameters: InputParameters<TInputParameters> = {
+export const inputParameters = new InputParameters({
   signature: {
+    type: 'string',
     aliases: ['function'],
     required: true,
     description:
@@ -20,38 +18,25 @@ export const inputParameters: InputParameters<TInputParameters> = {
     type: 'string',
   },
   inputParams: {
-    required: false,
+    array: true,
     description: 'Array of function parameters in order',
-    type: 'array',
+    type: 'string',
   },
-}
+})
 
-export const execute: ExecuteWithConfig<Config> = async (request, _, config) => {
-  const validator = new Validator(request, inputParameters)
-
-  const jobRunID = validator.validated.id
-  const address = validator.validated.data.address
-  const fnSignature = validator.validated.data.signature
-  const params = validator.validated.data.inputParams || []
-
-  const iface = new utils.Interface([fnSignature])
-  const fnName = iface.functions[Object.keys(iface.functions)[0]].name
-
-  const encoded = iface.encodeFunctionData(fnName, [...params])
-
-  const result = await config.provider.call({
-    to: address,
-    data: encoded,
-  })
-
-  const response = {
-    jobRunID,
-    status: 200,
-    statusText: 'OK',
-    headers: {},
-    config: {},
-    data: result,
+export type BaseEndpointTypes = {
+  Parameters: typeof inputParameters.definition
+  Response: {
+    Data: {
+      result: string
+    }
+    Result: string
   }
-
-  return Requester.success(jobRunID, Requester.withResult(response, result))
+  Settings: typeof config.settings
 }
+
+export const endpoint = new AdapterEndpoint({
+  name: 'function',
+  transport: functionTransport,
+  inputParameters,
+})
--- packages/sources/view-function/src/index.ts
+++ packages/sources/view-function/src/index.ts
@@ -1,10 +1,13 @@
-import { expose } from '@chainlink/ea-bootstrap'
-import { makeExecute, endpointSelector } from './adapter'
-import * as endpoints from './endpoint'
-import { makeConfig, NAME } from './config'
-import * as types from './endpoint'
+import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
+import { Adapter } from '@chainlink/external-adapter-framework/adapter'
+import { config } from './config'
+import { functionEndpoint } from './endpoint'
 
-const adapterContext = { name: NAME }
+export const adapter = new Adapter({
+  defaultEndpoint: functionEndpoint.name,
+  name: 'VIEW_FUNCTION',
+  config,
+  endpoints: [functionEndpoint],
+})
 
-const { server } = expose(adapterContext, makeExecute(), undefined, endpointSelector)
-export { NAME, makeExecute, makeConfig, server, types, endpoints }
+export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
