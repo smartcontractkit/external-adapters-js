eff77a22105d04247c675e73261bc8ae293a5540
Matthew McAllister <matthew.mcallister@smartcontract.com>
2023-09-08 14:44:26 -0400

    update to use framework LWBA types

--- packages/sources/gsr/src/endpoint/price.ts
+++ packages/sources/gsr/src/endpoint/price.ts
@@ -1,5 +1,7 @@
 import {
   CryptoPriceEndpoint,
+  DEFAULT_LWBA_ALIASES,
+  LwbaResponseDataFields,
   priceEndpointInputParametersDefinition,
 } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
@@ -9,23 +11,17 @@ import { transport } from '../transport/price'
 
 const inputParameters = new InputParameters(priceEndpointInputParametersDefinition)
 
-type EndpointResponse = SingleNumberResultResponse & {
-  Data: {
-    mid: number
-    ask: number
-    bid: number
-  }
-}
+type OmitResultFromLwba = Omit<LwbaResponseDataFields, 'Result'>
 
 export type BaseEndpointTypes = {
   Parameters: typeof inputParameters.definition
   Settings: typeof config.settings
-  Response: EndpointResponse
+  Response: OmitResultFromLwba & SingleNumberResultResponse
 }
 
 export const endpoint = new CryptoPriceEndpoint({
   name: 'price',
-  aliases: ['price-ws', 'crypto', 'crypto-lwba', 'cryptolwba', 'crypto_lwba'],
+  aliases: ['price-ws', 'crypto', ...DEFAULT_LWBA_ALIASES],
   transport,
   inputParameters,
 })
