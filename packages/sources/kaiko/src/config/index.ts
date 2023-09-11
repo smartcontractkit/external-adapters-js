e5520b60c89f74f09a3bb2bad6556c6d2d4969e6
Matthew McAllister <matthew.mcallister@smartcontract.com>
2023-09-11 13:12:38 -0400

    remove env var extra spaces

--- packages/sources/kaiko/src/config/index.ts
+++ packages/sources/kaiko/src/config/index.ts
@@ -3,13 +3,13 @@ import { AdapterConfig } from '@chainlink/external-adapter-framework/config'
 export const config = new AdapterConfig(
   {
     API_KEY: {
-      description: 'API KEY for  KAIKO',
+      description: 'API KEY for KAIKO',
       type: 'string',
       required: true,
       sensitive: true,
     },
     API_ENDPOINT: {
-      description: 'API endpoint for  KAIKO',
+      description: 'API endpoint for KAIKO',
       type: 'string',
       default: 'https://us.market-api.kaiko.io/v2/data/trades.v2',
     },
