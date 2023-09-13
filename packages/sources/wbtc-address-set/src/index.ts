d5cbf5821d771ed6a4cdf75fb7a97ee3b696772d
Alejo Berardino <alejoberardino@gmail.com>
2023-09-13 01:21:44 -0300

    Add workaround to disable validation on CACHE_MAX_AGE and WARMUP_SUBSCRIPTION_TTL

--- packages/sources/wbtc-address-set/src/index.ts
+++ packages/sources/wbtc-address-set/src/index.ts
@@ -1,5 +1,6 @@
 import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
 import { PoRAdapter } from '@chainlink/external-adapter-framework/adapter/por'
+import { BaseSettingsDefinition } from '@chainlink/external-adapter-framework/config'
 import { config } from './config'
 import { addresses, members } from './endpoint'
 
@@ -17,5 +18,11 @@ export const adapter = new PoRAdapter({
     },
   },
 })
+
+// Remove validations for these specific env vars.
+// This is a very ugly workaround, and might not work with slightly breaking changes in the framework.
+// It is only meant to serve as a temporary measure because the provider API is not reliable.
+;(BaseSettingsDefinition.CACHE_MAX_AGE.validate as unknown) = undefined
+;(BaseSettingsDefinition.WARMUP_SUBSCRIPTION_TTL.validate as unknown) = undefined
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
