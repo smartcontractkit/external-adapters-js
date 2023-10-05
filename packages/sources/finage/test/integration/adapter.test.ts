622c4a3fadced3913db47e3c36318c735e667bae
Subarna-Singh <subarnasingh11@gmail.com>
2023-10-05 11:40:56 +0530

    tests updated

--- packages/sources/finage/test/integration/adapter.test.ts
+++ packages/sources/finage/test/integration/adapter.test.ts
@@ -48,12 +48,36 @@ describe('execute', () => {
 
   setupExternalAdapterTest(envVariables, context)
 
-  describe('uk etf api', () => {
+  describe('uk-etf endpoint api', () => {
     const data: AdapterRequest = {
       id,
       data: {
         endpoint: 'uk_etf',
         base: 'IBTA',
+      },
+    }
+
+    it('should return success', async () => {
+      mockResponseSuccess()
+
+      const response = await (context.req as SuperTest<Test>)
+        .post('/')
+        .send(data)
+        .set('Accept', '*/*')
+        .set('Content-Type', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+      expect(response.body).toMatchSnapshot()
+    })
+  })
+
+  describe('etf api using country:uk ', () => {
+    const data: AdapterRequest = {
+      id,
+      data: {
+        endpoint: 'etf',
+        base: 'IBTA',
+        country: 'uk',
       },
     }
 
@@ -600,7 +624,7 @@ describe('websocket', () => {
     fastify.close(done)
   })
 
-  describe('uk etf endpoint', () => {
+  describe('uk-etf endpoint', () => {
     const jobID = '1'
 
     it('should return success', async () => {
