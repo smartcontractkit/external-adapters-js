2dc0ce8ed4de60bff8f07f17e3abf6fceeced5ed
Subarna-Singh <subarnasingh11@gmail.com>
2023-11-16 20:39:58 +0530

    finage v3 inverse bahavior

--- packages/sources/finage-test/src/endpoint/forex.ts
+++ packages/sources/finage-test/src/endpoint/forex.ts
@@ -4,6 +4,35 @@ import overrides from '../config/overrides.json'
 import { httpTransport } from '../transport/forex-http'
 import { ForexBaseEndpointTypes, forexPriceInputParameters } from './utils'
 import { wsTransport } from '../transport/forex-ws'
+import { AdapterRequest } from '@chainlink/external-adapter-framework/util'
+import { PriceAdapterRequest } from '../index'
+
+/* 
+excludesMap includes currency pairs {quote: [base ...]} which should not be inverted.
+*/
+const excludesMap: Record<string, string[]> = {
+  USD: ['EUR', 'GBP', 'AUD', 'NZD', 'XAG', 'XAU', 'XPT', 'XPD', 'XCU'],
+}
+
+export const forexReqTransformer = (req: AdapterRequest<any>): void => {
+  if (req.requestContext.endpointName == 'forex') {
+    const priceRequest = req as PriceAdapterRequest<any>
+
+    const quote = req.requestContext.data.quote
+    const base = req.requestContext.data.base
+    let inverse
+    if (excludesMap?.[quote].includes(base)) inverse = false
+    else {
+      inverse = true
+      priceRequest.requestContext.data.base = quote
+      priceRequest.requestContext.data.quote = base
+    }
+
+    priceRequest.requestContext.priceMeta = {
+      inverse,
+    }
+  }
+}
 
 export const endpoint = new ForexPriceEndpoint({
   name: 'forex',
@@ -16,4 +45,5 @@ export const endpoint = new ForexPriceEndpoint({
   },
   inputParameters: forexPriceInputParameters,
   overrides: overrides.finage,
+  requestTransforms: [forexReqTransformer],
 })
--- packages/sources/finage-test/src/index.ts
+++ packages/sources/finage-test/src/index.ts
@@ -1,9 +1,66 @@
 import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
+import { IncludesFile } from '@chainlink/external-adapter-framework/adapter'
 import { PriceAdapter } from '@chainlink/external-adapter-framework/adapter'
 import { config } from './config'
 import { commodities, crypto, eod, forex, stock, ukEtf } from './endpoint'
+import { PriceEndpointInputParametersDefinition } from '@chainlink/external-adapter-framework/adapter/price'
+import { AdapterParams } from '@chainlink/external-adapter-framework/adapter/types'
+import {
+  AdapterRequest,
+  AdapterRequestContext,
+  AdapterResponse,
+} from '@chainlink/external-adapter-framework/util'
 
-export const adapter = new PriceAdapter({
+export type PriceAdapterRequest<T> = AdapterRequest<T> & {
+  requestContext: AdapterRequestContext<T> & {
+    priceMeta: {
+      inverse: boolean
+    }
+  }
+}
+
+class FinageAdapter extends PriceAdapter<any> {
+  constructor(
+    params: AdapterParams<any> & {
+      includes?: IncludesFile
+    },
+  ) {
+    super(params)
+
+    if (params.includes) {
+      throw Error(
+        'Includes.json cannot be used for inverse behavior due to alternate implementation. refer https://smartcontract-it.atlassian.net/browse/DF-18867',
+      )
+    }
+  }
+
+  override async handleRequest(
+    req: PriceAdapterRequest<PriceEndpointInputParametersDefinition>,
+    replySent: Promise<unknown>,
+  ): Promise<AdapterResponse> {
+    const response = await super.handleRequest(req, replySent)
+
+    if (req.requestContext.endpointName == 'forex') {
+      if (req.requestContext.priceMeta.inverse) {
+        // Deep clone the response, as it may contain objects which won't be cloned by simply destructuring
+        const cloneResponse = JSON.parse(JSON.stringify(response))
+
+        const inverseResult = 1 / (cloneResponse.result as number)
+        cloneResponse.result = inverseResult
+        // Check if response data has a result within it
+        const data = cloneResponse.data as { result: number } | null
+        if (data?.result) {
+          data.result = inverseResult
+        }
+        return cloneResponse
+      }
+    }
+
+    return response
+  }
+}
+
+export const adapter = new FinageAdapter({
   defaultEndpoint: stock.name,
   name: 'FINAGE',
   config,
