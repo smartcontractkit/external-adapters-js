1313ad72d39badb9c4ecb45607c73c55452cf1f3
karen-stepanyan <karen.stepanyan@protofire.io>
2023-11-24 09:53:10 +0400

    change forex ws auth flow

--- packages/sources/ncfx/src/config/index.ts
+++ packages/sources/ncfx/src/config/index.ts
@@ -4,20 +4,14 @@ export const config = new AdapterConfig({
   API_USERNAME: {
     description: 'Username for the NCFX API',
     type: 'string',
-    required: true,
   },
   API_PASSWORD: {
     description: 'Password for the NCFX API',
     type: 'string',
-    required: true,
     sensitive: true,
   },
-  FOREX_WS_USERNAME: {
-    description: 'Username for Forex websocket endpoint',
-    type: 'string',
-  },
-  FOREX_WS_PASSWORD: {
-    description: 'Password for the Forex websocket endpoint',
+  FOREX_WS_API_KEY: {
+    description: 'API key for Forex websocket endpoint',
     type: 'string',
     sensitive: true,
   },
@@ -29,6 +23,6 @@ export const config = new AdapterConfig({
   FOREX_WS_API_ENDPOINT: {
     type: 'string',
     description: 'The WS API endpoint to use for the forex endpoint',
-    default: 'wss://fiat-ws.eu-west-2.apingxelb.v1.newchangefx.com/sub/fiat/ws/ref',
+    default: 'wss://fiat.ws.newchangefx.com/sub/fiat/ws/ref',
   },
 })
