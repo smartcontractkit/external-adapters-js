816452beb81d624bce0430ba03ea0f08a53e7a16
mmcallister-cll <139181225+mmcallister-cll@users.noreply.github.com>
2024-01-11 18:13:45 -0500

    Feature/df 18971 gsr lwba endpoint (#2938)
    * GSR LWBA endpoint
    
    * add changeset
    
    * update integration test
    
    * update tests
    
    * updates to separate price and lwba endpoints following discussion with GSR & associated changes
    
    * back to price endpoint alias due to GSR DP decision
    
    * review fix
    
    * update to use framework LWBA types
    
    * update readme endpoints
    
    * number readability
    
    ---------
    
    Co-authored-by: cl-ea <93770670+cl-ea@users.noreply.github.com>

--- packages/sources/gsr/src/endpoint/price.ts
+++ packages/sources/gsr/src/endpoint/price.ts
@@ -1,5 +1,7 @@
 import {
   CryptoPriceEndpoint,
+  DEFAULT_LWBA_ALIASES,
+  LwbaResponseDataFields,
   priceEndpointInputParametersDefinition,
 } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
@@ -14,15 +16,17 @@ const inputParameters = new InputParameters(priceEndpointInputParametersDefiniti
   },
 ])
 
+type OmitResultFromLwba = Omit<LwbaResponseDataFields, 'Result'>
+
 export type BaseEndpointTypes = {
   Parameters: typeof inputParameters.definition
   Settings: typeof config.settings
-  Response: SingleNumberResultResponse
+  Response: OmitResultFromLwba & SingleNumberResultResponse
 }
 
 export const endpoint = new CryptoPriceEndpoint({
   name: 'price',
-  aliases: ['price-ws', 'crypto'],
+  aliases: ['price-ws', 'crypto', ...DEFAULT_LWBA_ALIASES],
   transport,
   inputParameters,
 })
