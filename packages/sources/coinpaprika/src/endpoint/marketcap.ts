aec5abee212a0a2cfbf31d0888f41c50928b4234
karen-stepanyan <karen.stepanyan@protofire.io>
2024-01-24 17:27:40 +0400

    add WS_ENABLED env var support

--- packages/sources/coinpaprika/src/endpoint/marketcap.ts
+++ packages/sources/coinpaprika/src/endpoint/marketcap.ts
@@ -2,14 +2,19 @@ import { PriceEndpoint } from '@chainlink/external-adapter-framework/adapter'
 import overrides from '../config/overrides.json'
 import { TransportRoutes } from '@chainlink/external-adapter-framework/transports'
 import { buildCryptoHttpTransport, buildWebsocketTransport } from '../transport/utils'
-import { BaseEndpointTypes, cryptoInputParameters, customInputValidation } from './utils'
+import {
+  BaseEndpointTypes,
+  cryptoInputParameters,
+  customInputValidation,
+  customRouter,
+} from './utils'
 
 export const endpoint = new PriceEndpoint({
   name: 'marketcap',
   transportRoutes: new TransportRoutes<BaseEndpointTypes>()
     .register('ws', buildWebsocketTransport('m'))
     .register('rest', buildCryptoHttpTransport('market_cap')),
-  defaultTransport: 'rest',
+  customRouter,
   inputParameters: cryptoInputParameters,
   overrides: overrides.coinpaprika,
   customInputValidation,
