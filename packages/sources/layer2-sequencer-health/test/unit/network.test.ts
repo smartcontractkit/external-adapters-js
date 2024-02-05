5e0fdda15d150d181ca4e274c5d47340d5ad5e77
Chris De Leon <christopher.deleon@smartcontract.com>
2024-02-05 11:02:54 -0800

    fixes unit tests

--- packages/sources/layer2-sequencer-health/test/unit/network.test.ts
+++ packages/sources/layer2-sequencer-health/test/unit/network.test.ts
@@ -14,7 +14,7 @@ describe('network', () => {
       describe('when dummy contract initialized', () => {
         it('returns true', async () => {
           jest.spyOn(starkware, 'sendDummyStarkwareTransaction').mockRejectedValue({
-            errorCode:
+            message:
               'RPC: starknet_addInvokeTransaction with params {"invoke_transaction":{"sender_address":"0x009cf509ef7a55ee8e487787003d47a704b4c7b6cc5469d7cd319d27bd753566","calldata":["0x1","0x9cf509ef7a55ee8e487787003d47a704b4c7b6cc5469d7cd319d27bd753566","0x79dc0da7c54b95f10aa182ad0a46400db63156920adb65eca2654c0945a463","0x2","0x1ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca","0x0"],"type":"INVOKE","max_fee":"0x0","version":"0x1","signature":["0x1034048c548de23a36d5da5ba0f8fa125d1cede29f7ffa166680aee25165cc","0x7ccd1ffdf427cfa2e4e760c58455e5dcecc7246ef2fd8d3b0c095ddb69a849f"],"nonce":"0x3"}}\n An unexpected error occurred: {"error":"StarknetError { code: Known(OutOfRangeFee), message: "Transaction must commit to pay a positive amount on fee." }"}',
           })
           expect(await network.getStatusByTransaction(Networks.Starkware, config)).toBe(true)
@@ -24,7 +24,7 @@ describe('network', () => {
       describe('when dummy contract not initialized', () => {
         it('returns true', async () => {
           jest.spyOn(starkware, 'sendDummyStarkwareTransaction').mockRejectedValue({
-            errorCode:
+            message:
               'RPC: starknet_getNonce with params {"contract_address":"0x1","block_id":"pending"}\n 20: Contract not found: undefined',
           })
           expect(await network.getStatusByTransaction(Networks.Starkware, config)).toBe(true)
