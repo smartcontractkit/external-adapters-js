2e46100fee7c48c856749c4db93f3d45b0c9e421
karen-stepanyan <91897037+karen-stepanyan@users.noreply.github.com>
2024-02-12 22:45:57 +0400

    Added group batching calls to Por-address-list (#3185)
    * add group batching calls
    
    * change batchGroupSize input param to config
    
    * fix unit tests
    
    * change BATCH_GROUP_SIZE to GROUP_SIZE

---
+++ .changeset/good-fishes-shake.md
@@ -0,0 +1,5 @@
+---
+'@chainlink/por-address-list-adapter': patch
+---
+
+Added GROUP_SIZE config
--- packages/sources/por-address-list/src/config/index.ts
+++ packages/sources/por-address-list/src/config/index.ts
@@ -12,6 +12,12 @@ export const config = new AdapterConfig({
     type: 'number',
     default: 1,
   },
+  GROUP_SIZE: {
+    description:
+      'The number of concurrent batched contract calls to make at a time. Setting this lower than the default may result in lower performance from the adapter.',
+    type: 'number',
+    default: 10,
+  },
   BACKGROUND_EXECUTE_MS: {
     description:
       'The amount of time the background execute should sleep before performing the next request',
