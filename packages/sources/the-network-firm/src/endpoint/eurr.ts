d98044705abb8180464fda61b03b22a509ce817d
karen-stepanyan <karen.stepanyan@protofire.io>
2024-03-19 21:03:58 +0400

    bump ea-framework version

--- packages/sources/the-network-firm/src/endpoint/eurr.ts
+++ packages/sources/the-network-firm/src/endpoint/eurr.ts
@@ -1,21 +1,18 @@
-import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import {
+  PoRProviderEndpoint,
+  PoRProviderResponse,
+} from '@chainlink/external-adapter-framework/adapter/por'
 import { config } from '../config'
 import { httpTransport } from '../transport/eurr'
 import { EmptyInputParameters } from '@chainlink/external-adapter-framework/validation/input-params'
 
 export type BaseEndpointTypes = {
   Parameters: EmptyInputParameters
-  Response: {
-    Result: number
-    Data: {
-      result: number
-      ripcord: boolean
-    }
-  }
+  Response: PoRProviderResponse
   Settings: typeof config.settings
 }
 
-export const endpoint = new AdapterEndpoint({
+export const endpoint = new PoRProviderEndpoint({
   name: 'eurr',
   transport: httpTransport,
 })
