9744caee6220300f97c8f5b393afd12d42df17e9
Alec Gard <alec.gard@chainlinklabs.com>
2024-06-12 11:37:56 +0100

    Add integration tests, pin csv-parse library

--- packages/sources/s3reader/test/integration/adapter.test.ts
+++ packages/sources/s3reader/test/integration/adapter.test.ts
@@ -2,17 +2,44 @@ import {
   TestAdapter,
   setEnvVariables,
 } from '@chainlink/external-adapter-framework/util/testing-utils'
-import * as nock from 'nock'
-import { mockResponseSuccess } from './fixtures'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
+
+jest.mock('@aws-sdk/client-s3', () => {
+  const originalModule = jest.requireActual('@aws-sdk/client-s3')
+  return {
+    ...originalModule,
+    S3Client: jest.fn().mockImplementation(() => {
+      return {
+        send: jest.fn().mockImplementation(async (command: GetObjectCommand) => {
+          if (command.input.Bucket == 's3_bucket' && command.input.Key == 'correct/path/file.csv') {
+            return {
+              Body: {
+                transformToString: () =>
+                  'Date,01/01/01,\nName,Value,Other\nRowOne,123,Other1\nRowTwo,456,Other2\nRowThree,789,Other3',
+              },
+            }
+          } else {
+            return {
+              Body: undefined,
+            }
+          }
+        }),
+      }
+    }),
+  }
+})
 
 describe('execute', () => {
   let spy: jest.SpyInstance
   let testAdapter: TestAdapter
   let oldEnv: NodeJS.ProcessEnv
 
+  jest.setTimeout(10000)
+
   beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
     process.env.API_KEY = process.env.API_KEY ?? 'fake-api-key'
+    process.env.BACKGROUND_EXECUTE_MS = '1000'
 
     const mockDate = new Date('2001-01-01T11:11:11.111Z')
     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
@@ -27,23 +54,93 @@ describe('execute', () => {
   afterAll(async () => {
     setEnvVariables(oldEnv)
     await testAdapter.api.close()
-    nock.restore()
-    nock.cleanAll()
     spy.mockRestore()
+    jest.clearAllMocks()
   })
 
   describe('csv endpoint', () => {
     it('should return success', async () => {
-      const data = {
+      const request = {
         endpoint: 'csv',
-        bucket: 's3bucket',
-        path: 'path/to/file.csv',
-        row: '1',
-        column: 'A',
+        bucket: 's3_bucket',
+        key: 'correct/path/file.csv',
+        headerRow: 2,
+        resultField: 'Value',
+        matcherField: 'Name',
+        matcherValue: 'RowTwo',
       }
-      mockResponseSuccess()
-      const response = await testAdapter.request(data)
+
+      const response = await testAdapter.request(request)
+
       expect(response.statusCode).toBe(200)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return error for unrecognised file path', async () => {
+      const request = {
+        endpoint: 'csv',
+        bucket: 's3_bucket',
+        key: 'incorrect/path/file.csv',
+        headerRow: 2,
+        resultField: 'Value',
+        matcherField: 'Name',
+        matcherValue: 'RowTwo',
+      }
+
+      const response = await testAdapter.request(request)
+
+      expect(response.statusCode).toBe(502)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return error for invalid result field', async () => {
+      const request = {
+        endpoint: 'csv',
+        bucket: 's3_bucket',
+        key: 'correct/path/file.csv',
+        headerRow: 2,
+        resultField: 'Invalid',
+        matcherField: 'Name',
+        matcherValue: 'RowTwo',
+      }
+
+      const response = await testAdapter.request(request)
+
+      expect(response.statusCode).toBe(502)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return error for invalid matcher field', async () => {
+      const request = {
+        endpoint: 'csv',
+        bucket: 's3_bucket',
+        key: 'correct/path/file.csv',
+        headerRow: 2,
+        resultField: 'Value',
+        matcherField: 'Invalid',
+        matcherValue: 'RowTwo',
+      }
+
+      const response = await testAdapter.request(request)
+
+      expect(response.statusCode).toBe(502)
+      expect(response.json()).toMatchSnapshot()
+    })
+
+    it('should return error for invalid matcher value', async () => {
+      const request = {
+        endpoint: 'csv',
+        bucket: 's3_bucket',
+        key: 'correct/path/file.csv',
+        headerRow: 2,
+        resultField: 'Value',
+        matcherField: 'Name',
+        matcherValue: 'Invalid',
+      }
+
+      const response = await testAdapter.request(request)
+
+      expect(response.statusCode).toBe(502)
       expect(response.json()).toMatchSnapshot()
     })
   })
