892bdea2fce9adb43071956eda788bc54234ab1f
Matt Kelly <mkelly2@live.co.uk>
2024-07-11 18:31:01 +0900

    Add LWBA customOutputValidation to elwood. Update integration tests with LWBA validation test

--- packages/sources/elwood/src/endpoint/crypto.ts
+++ packages/sources/elwood/src/endpoint/crypto.ts
@@ -3,11 +3,13 @@ import {
   DEFAULT_LWBA_ALIASES,
   LwbaResponseDataFields,
   priceEndpointInputParametersDefinition,
+  validateLwbaResponse,
 } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
 import { transport } from '../transport/crypto'
+import { AdapterLWBAError } from '@chainlink/external-adapter-framework/validation/error'
 
 const inputParameters = new InputParameters(priceEndpointInputParametersDefinition, [
   {
@@ -29,4 +31,14 @@ export const cryptoEndpoint = new CryptoPriceEndpoint({
   aliases: ['crypto', ...DEFAULT_LWBA_ALIASES],
   inputParameters,
   transport,
+  customOutputValidation: (output) => {
+    const data = output.data as LwbaResponseDataFields['Data']
+    const error = validateLwbaResponse(data.bid, data.mid, data.ask)
+
+    if (error) {
+      throw new AdapterLWBAError({ statusCode: 500, message: error })
+    }
+
+    return undefined
+  },
 })
