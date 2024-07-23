aefb32db8ae1fea8966517cc289567a667ed2de2
Matt Kelly <mkelly2@live.co.uk>
2024-07-23 12:10:02 +0900

    Update GSRPriceAdapter to extend from PriceAdapter

--- packages/sources/gsr/src/adapter.ts
+++ packages/sources/gsr/src/adapter.ts
@@ -1,129 +1,14 @@
-import {
-  Adapter,
-  AdapterParams,
-  DEFAULT_LWBA_ALIASES,
-  IncludesFile,
-  PriceEndpoint,
-  PriceEndpointGenerics,
-  PriceEndpointInputParametersDefinition,
-} from '@chainlink/external-adapter-framework/adapter'
+import { DEFAULT_LWBA_ALIASES, PriceAdapter } from '@chainlink/external-adapter-framework/adapter'
 import { SettingsDefinitionMap } from '@chainlink/external-adapter-framework/config'
-import {
-  AdapterRequest,
-  AdapterRequestContext,
-  AdapterResponse,
-} from '@chainlink/external-adapter-framework/util'
+import { AdapterRequest, AdapterResponse } from '@chainlink/external-adapter-framework/util'
 import { AdapterError } from '@chainlink/external-adapter-framework/validation/error'
-import {
-  EmptyInputParameters,
-  TypeFromDefinition,
-} from '@chainlink/external-adapter-framework/validation/input-params'
-
-type IncludeDetails = {
-  from: string
-  to: string
-  inverse: boolean
-}
-type IncludesMap = Record<string, Record<string, IncludeDetails>>
-
-type PriceAdapterRequest<T> = AdapterRequest<T> & {
-  requestContext: AdapterRequestContext<T> & {
-    priceMeta: {
-      inverse: boolean
-    }
-  }
-}
-
-const buildIncludesMap = (includesFile: IncludesFile) => {
-  const includesMap: IncludesMap = {}
-
-  for (const { from, to, includes } of includesFile) {
-    if (!includesMap[from]) {
-      includesMap[from] = {}
-    }
-    includesMap[from][to] = includes[0]
-  }
-
-  return includesMap
-}
+import { EmptyInputParameters } from '@chainlink/external-adapter-framework/validation/input-params'
 
 export class GSRPriceAdapter<
   CustomSettingsDefinition extends SettingsDefinitionMap,
-> extends Adapter<CustomSettingsDefinition> {
-  includesMap?: IncludesMap
-
-  constructor(
-    params: AdapterParams<CustomSettingsDefinition> & {
-      includes?: IncludesFile
-    },
-  ) {
-    const priceEndpoints = params.endpoints.filter(
-      (e) => e instanceof PriceEndpoint,
-    ) as PriceEndpoint<PriceEndpointGenerics>[]
-    if (!priceEndpoints.length) {
-      throw new Error(
-        `This PriceAdapter's list of endpoints does not contain a valid PriceEndpoint`,
-      )
-    }
-
-    super(params)
-
-    if (params.includes) {
-      // Build includes map for constant lookups
-      this.includesMap = buildIncludesMap(params.includes)
-
-      const requestTransform = (req: AdapterRequest<EmptyInputParameters>) => {
-        const priceRequest = req as PriceAdapterRequest<
-          TypeFromDefinition<PriceEndpointInputParametersDefinition>
-        >
-
-        const requestData = priceRequest.requestContext.data
-        if (!requestData.base || !requestData.quote) {
-          return
-        }
-        const includesDetails = this.includesMap?.[requestData.base]?.[requestData.quote]
-
-        if (includesDetails) {
-          requestData.base = includesDetails.from || requestData.base
-          requestData.quote = includesDetails.to || requestData.quote
-        }
-
-        const inverse = includesDetails?.inverse || false
-        priceRequest.requestContext.priceMeta = {
-          inverse,
-        }
-      }
-
-      for (const endpoint of priceEndpoints) {
-        endpoint.requestTransforms?.push(requestTransform)
-      }
-    }
-  }
-
-  override async handleRequest(
-    req: PriceAdapterRequest<PriceEndpointInputParametersDefinition>,
-    replySent: Promise<unknown>,
-  ): Promise<AdapterResponse> {
-    const response = await super.handleRequest(req, replySent)
-
-    if (this.includesMap && req.requestContext.priceMeta?.inverse) {
-      // We need to search in the reverse order (quote -> base) because the request transform will have inverted the pair
-
-      // Deep clone the response, as it may contain objects which won't be cloned by simply destructuring
-      const cloneResponse = JSON.parse(JSON.stringify(response))
-
-      const inverseResult = 1 / (cloneResponse.result as number)
-      cloneResponse.result = inverseResult
-      // Check if response data has a result within it
-      const data = cloneResponse.data as { result: number } | null
-      if (data?.result) {
-        data.result = inverseResult
-      }
-      return cloneResponse
-    }
-
-    return response
-  }
+> extends PriceAdapter<CustomSettingsDefinition> {
+  // List of endpoints to apply validation to. Add any others we need to perform custom validation on here
+  endpointsToValidate = [...DEFAULT_LWBA_ALIASES]
 
   // Custom implementation of validateOutput to execute only for LWBA endpoints
   // This is because we need to use the same transport and cache keys for both price and LWBA endpoints due to the way GSR's API works
@@ -133,9 +18,9 @@ export class GSRPriceAdapter<
   ): AdapterError | undefined {
     const endpointObj = this.endpointsMap[req.requestContext.endpointName]
     const { endpoint = '' } = req.body.data
-    const isLwba = DEFAULT_LWBA_ALIASES.includes(endpoint.toLowerCase())
+    const shouldValidateOutput = this.endpointsToValidate.includes(endpoint.toLowerCase())
 
-    if (endpointObj.customOutputValidation && isLwba) {
+    if (endpointObj.customOutputValidation && shouldValidateOutput) {
       return endpointObj.customOutputValidation(output)
     }
     return undefined
