abc239b59092bb3e0c3acdcc5a660607f7392552
Michael Xiao <michael.xiao@smartcontract.com>
2024-08-09 08:09:39 -0400

    Make Cryptocompare EA default to WS (#3380)

---
+++ .changeset/purple-experts-dream.md
@@ -0,0 +1,6 @@
+---
+'@chainlink/cryptocompare-adapter': minor
+'@chainlink/deep-blue-adapter': patch
+---
+
+Use WS as default transport
--- packages/sources/cryptocompare/test/integration/adapter.test.ts
+++ packages/sources/cryptocompare/test/integration/adapter.test.ts
@@ -13,6 +13,7 @@ describe('execute', () => {
   beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
     process.env['API_KEY'] = 'API_KEY'
+    process.env['WS_ENABLED'] = 'false'
     const mockDate = new Date('2022-01-01T11:11:11.111Z')
     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
 
