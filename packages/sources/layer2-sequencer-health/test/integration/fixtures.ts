f59b2b67afb967b6ee3b0fd3f273baa39038060a
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2024-08-27 00:36:35 +0200

    Adds L2EP zkSync integration

--- packages/sources/layer2-sequencer-health/test/integration/fixtures.ts
+++ packages/sources/layer2-sequencer-health/test/integration/fixtures.ts
@@ -98,6 +98,19 @@ export const mockResponseSuccessBlock = (): void => {
       'Vary',
       'Origin',
     ])
+
+  nock('https://mainnet.era.zksync.io')
+    .post('/', { jsonrpc: '2.0', method: 'eth_blockNumber', params: [], id: /^\d+$/ })
+    .reply(200, () => ({ jsonrpc: '2.0', id: 1, result: '0x42d293' }), [
+      'Content-Type',
+      'application/json',
+      'Connection',
+      'close',
+      'Vary',
+      'Accept-Encoding',
+      'Vary',
+      'Origin',
+    ])
 }
 
 export const mockResponseSuccessRollup = (): void => {
@@ -192,6 +205,19 @@ export const mockResponseFailureBlock = (): void => {
     ])
 
   nock('https://rpc.scroll.io')
+    .post('/', { jsonrpc: '2.0', method: 'eth_blockNumber', params: [], id: /^\d+$/ })
+    .reply(200, () => ({ jsonrpc: '2.0', id: 1, result: '0x00' }), [
+      'Content-Type',
+      'application/json',
+      'Connection',
+      'close',
+      'Vary',
+      'Accept-Encoding',
+      'Vary',
+      'Origin',
+    ])
+
+  nock('https://mainnet.era.zksync.io')
     .post('/', { jsonrpc: '2.0', method: 'eth_blockNumber', params: [], id: /^\d+$/ })
     .reply(200, () => ({ jsonrpc: '2.0', id: 1, result: '0x00' }), [
       'Content-Type',
