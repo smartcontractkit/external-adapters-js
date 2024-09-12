897d46f1207b645e1fc71acff8e56a7bec5dd5cd
Karen Stepanyan <karen.stepanyan@smartcontract.com>
2024-09-12 23:01:12 +0400

    remove multi api key support, add enterprise id support

--- packages/sources/bitgo/test/integration/adapter.test.ts
+++ packages/sources/bitgo/test/integration/adapter.test.ts
@@ -12,8 +12,8 @@ describe('execute', () => {
 
   beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
-    process.env.BTC_API_KEY = process.env.API_KEY ?? 'fake-api-key'
-    process.env.BTC_API_ENDPOINT = process.env.API_ENDPOINT ?? 'https://localhost:2322'
+    process.env.API_KEY = process.env.API_KEY ?? 'fake-api-key'
+    process.env.API_ENDPOINT = process.env.API_ENDPOINT ?? 'https://localhost:2322'
     process.env.BACKGROUND_EXECUTE_MS = process.env.BACKGROUND_EXECUTE_MS ?? '0'
     const mockDate = new Date('2001-01-01T11:11:11.111Z')
     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
@@ -39,8 +39,8 @@ describe('execute', () => {
         coin: 'tbtc',
         chainId: 'testnet',
         network: 'bitcoin',
-        reserve: 'BTC',
         endpoint: 'wallet',
+        enterpriseId: '66d72170014fe3e546d3d10a4fc93149',
       }
       mockResponseSuccess()
       const response = await testAdapter.request(data)
