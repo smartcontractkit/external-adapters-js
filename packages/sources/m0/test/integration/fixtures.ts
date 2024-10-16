f3c07e441f994e75ee28b2c4f92e782c2d9e93f6
Adam Stibal <adam.stibal@m0.xyz>
2024-10-16 15:15:28 +0200

    testing

--- packages/sources/m0/test/integration/fixtures.ts
+++ packages/sources/m0/test/integration/fixtures.ts
@@ -1,15 +1,15 @@
 import nock from 'nock'
 
 export const mockResponseSuccess = (): nock.Scope =>
-  nock('https://dataproviderapi.com', {
+  nock('https://api.m0.xyz', {
     encodedQueryParams: true,
   })
-    .get('/cryptocurrency/price')
-    .query({
-      symbol: 'ETH',
-      convert: 'USD',
-    })
-    .reply(200, () => ({ ETH: { price: 10000 } }), [
+    .post('/api/dashboard-backend-api/api/rpc', {"method":"navDetails"})
+    .reply(200, () => ({
+      "totalCollateral": 54716821540000,
+      "totalOwedM": 42847433836220,
+      "collateralisation": 0.7830760743407144
+    }), [
       'Content-Type',
       'application/json',
       'Connection',
