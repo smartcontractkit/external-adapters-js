fa34fe96b09c1e99b73ddadebdba9da0e6529c0d
martin-cll <121895364+martin-cll@users.noreply.github.com>
2024-10-23 14:53:42 +1100

    market-status: Use NCFX as primary for forex and metals markets (#3498)

--- packages/composites/market-status/src/config/adapters.ts
+++ packages/composites/market-status/src/config/adapters.ts
@@ -3,9 +3,17 @@ export const adapterNames = ['NCFX', 'TRADINGHOURS'] as const
 export type AdapterName = (typeof adapterNames)[number]
 
 // Mapping from market to primary and secondary adapters.
-export const marketAdapters: Record<string, Record<'primary' | 'secondary', AdapterName>> = {
+export const marketAdapters: Record<string, { primary: AdapterName; secondary: AdapterName }> = {
   __default: {
     primary: 'TRADINGHOURS',
     secondary: 'NCFX',
+  },
+  forex: {
+    primary: 'NCFX',
+    secondary: 'TRADINGHOURS',
+  },
+  metals: {
+    primary: 'NCFX',
+    secondary: 'TRADINGHOURS',
   },
 }
