8bd801daca510bd92089c8dd734672cf7268a2f2
Michael Xiao <michael.xiao@smartcontract.com>
2024-12-09 10:53:54 -0500

    Remove user agent from BX (#3612)

--- packages/sources/bx-digital/src/transport/price.ts
+++ packages/sources/bx-digital/src/transport/price.ts
@@ -22,7 +22,6 @@ export const httpTransport = new HttpTransport<HttpTransportTypes>({
           baseURL: config.API_ENDPOINT,
           headers: {
             'API-key': config.API_KEY,
-            'User-Agent': config.API_USER_AGENT,
           },
         },
       }
