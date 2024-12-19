74e3c136da3ca30a486fca5c4a64845e2ca633e8
Michael Xiao <michael.xiao@smartcontract.com>
2024-12-19 14:05:14 -0500

    Lombard PoR address

---
+++ packages/sources/por-address-list/src/transport/addressManager.ts
@@ -0,0 +1,83 @@
+import { ethers } from 'ethers'
+import { PoRAddress } from '@chainlink/external-adapter-framework/adapter/por'
+
+export abstract class AddressManager<T> {
+  contract: ethers.Contract
+
+  constructor(
+    contractAddress: string,
+    abi: ethers.ContractInterface,
+    provider: ethers.providers.JsonRpcProvider,
+  ) {
+    this.contract = new ethers.Contract(contractAddress, abi, provider)
+  }
+
+  async fetchAddressList(
+    latestBlockNum: number,
+    confirmations = 0,
+    batchSize = 10,
+    batchGroupSize = 10,
+  ): Promise<T[]> {
+    const blockTag = latestBlockNum - confirmations
+    const numAddresses = await this.contract.getPoRAddressListLength({ blockTag })
+    let totalRequestedAddressesCount = 0
+    let startIdx = ethers.BigNumber.from(0)
+    const addresses: T[] = []
+    let batchRequests: Promise<T>[] = []
+
+    while (totalRequestedAddressesCount < numAddresses.toNumber()) {
+      const nextEndIdx = startIdx.add(batchSize)
+      const endIdx = nextEndIdx.gte(numAddresses) ? numAddresses.sub(1) : nextEndIdx
+      const batchCall = this.getPoRAddressListCall(startIdx, endIdx, blockTag)
+      batchRequests.push(batchCall)
+      // element at endIdx is included in result
+      const addressesRequested: number = endIdx.sub(startIdx).add(1).toNumber()
+      totalRequestedAddressesCount += addressesRequested
+      startIdx = endIdx.add(1)
+
+      if (
+        batchRequests.length >= batchGroupSize ||
+        totalRequestedAddressesCount >= numAddresses.toNumber()
+      ) {
+        addresses.push(...(await Promise.all(batchRequests)))
+        batchRequests = []
+      }
+    }
+    return addresses
+  }
+
+  abstract getPoRAddressListCall(start: ethers.BigNumber, end: number, blockTag: number): Promise<T>
+
+  abstract processPoRAddressList(result: T[], network: string, chainId: string): PoRAddress[]
+}
+
+export class DefaultAddressManager extends AddressManager<string[]> {
+  getPoRAddressListCall(start: ethers.BigNumber, end: number, blockTag: number) {
+    return this.contract.getPoRAddressList(start, end, { blockTag })
+  }
+
+  processPoRAddressList(result: string[][], network: string, chainId: string) {
+    return result.flat().map((address) => ({
+      address,
+      network,
+      chainId,
+    }))
+  }
+}
+
+export class LombardAddressManager extends AddressManager<string[][]> {
+  getPoRAddressListCall(start: ethers.BigNumber, end: number, blockTag: number) {
+    return this.contract.getPoRAddressSignatureMessages(start.toNumber(), end, { blockTag })
+  }
+
+  processPoRAddressList(result: string[][][], network: string, chainId: string) {
+    return result
+      .flatMap((r) => r[0])
+      .filter((address) => address != '')
+      .map((address) => ({
+        address: address.replace("'", ''),
+        network: network,
+        chainId: chainId,
+      }))
+  }
+}
