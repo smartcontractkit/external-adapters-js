0c8c3e60d084aa8abbc075e0b5a9b4c6da32108d
Michael Xiao <michael.xiao@smartcontract.com>
2025-01-09 13:35:13 -0500

    Add Solv addresses

--- packages/sources/por-address-list/src/transport/solvBTC.ts
+++ packages/sources/por-address-list/src/transport/solvBTC.ts
@@ -1,19 +1,23 @@
 import { HttpTransport } from '@chainlink/external-adapter-framework/transports'
 import { BaseEndpointTypes } from '../endpoint/solvBTC'
+import { PoRAddress } from '@chainlink/external-adapter-framework/adapter/por'
 
 interface ResponseSchema {
   accountName: string
   result: {
     id: number
-    address: string
-    symbol: string
-    addressType: string
-    walletName: string
+    // BTC
+    address?: string
+    symbol?: string
+    addressType?: string
+    walletName?: string
+    // JUP on CEFFU
+    mirrorXLinkId?: string
+    label?: string
   }[]
   count: number
   lastUpdatedAt: string
 }
-
 export type HttpTransportTypes = BaseEndpointTypes & {
   Provider: {
     RequestBody: never
@@ -23,11 +27,23 @@ export type HttpTransportTypes = BaseEndpointTypes & {
 export const solvHttpTransport = new HttpTransport<HttpTransportTypes>({
   prepareRequests: (params, config) => {
     return params.map((param) => {
+      const baseURL = (function () {
+        switch (param.type) {
+          case 'BTC':
+            return config.SOLVBTC_API_ENDPOINT
+          case 'BBN':
+            return config.SOLVBTC_BBN_API_ENDPOINT
+          case 'ENA':
+            return config.SOLVBTC_ENA_API_ENDPOINT
+          case 'CORE':
+            return config.SOLVBTC_CORE_API_ENDPOINT
+          case 'JUP':
+            return config.SOLVBTC_JUP_API_ENDPOINT
+        }
+      })()
       return {
         params: [param],
-        request: {
-          baseURL: config.SOLVBTC_API_ENDPOINT,
-        },
+        request: { baseURL },
       }
     })
   },
@@ -41,7 +57,7 @@ export const solvHttpTransport = new HttpTransport<HttpTransportTypes>({
         {
           params: params[0],
           response: {
-            errorMessage: `The data provider didn't return any address for solvBTC`,
+            errorMessage: `The data provider didn't return any address for solvBTCs ${params[0].type}`,
             statusCode: 502,
             timestamps: timestamps,
           },
@@ -50,11 +66,21 @@ export const solvHttpTransport = new HttpTransport<HttpTransportTypes>({
     }
 
     const addresses = response.data.result
-      .map((r) => ({
-        address: r.address,
-        network: 'bitcoin',
-        chainId: 'mainnet',
-      }))
+      .map((r) => {
+        if (params[0].type == 'JUP') {
+          return {
+            address: r.mirrorXLinkId,
+            network: 'solana',
+            chainId: 'mainnet',
+          } as PoRAddress
+        } else {
+          return {
+            address: r.address,
+            network: 'bitcoin',
+            chainId: 'mainnet',
+          } as PoRAddress
+        }
+      })
       .sort()
 
     return [
