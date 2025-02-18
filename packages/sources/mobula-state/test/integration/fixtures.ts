de512be597161bd1bf66f37072232b5bb31825c0
Martin Liu <martin.liu@smartcontract.com>
2025-02-18 19:27:46 +1100

    New funding-rate endpoint for mobula EA

--- packages/sources/mobula-state/test/integration/fixtures.ts
+++ packages/sources/mobula-state/test/integration/fixtures.ts
@@ -4,7 +4,7 @@ export const mockWebsocketServer = (URL: string): MockWebsocketServer => {
   const mockWsServer = new MockWebsocketServer(URL, { mock: false })
   mockWsServer.on('connection', (socket) => {
     socket.on('message', () => {
-      return socket.send(
+      socket.send(
         JSON.stringify({
           timestamp: 1726648165000,
           price: 2325.847186068699,
@@ -13,6 +13,26 @@ export const mockWebsocketServer = (URL: string): MockWebsocketServer => {
           volume24h: 230120379.9751866,
           baseSymbol: 'ETH',
           quoteSymbol: 'USD',
+        }),
+      )
+
+      socket.send(
+        JSON.stringify({
+          binanceFundingRate: {
+            symbol: 'BTCUSDC',
+            fundingTime: 1740441600000,
+            fundingRate: 0.009854,
+            marketPrice: '91505.25655876',
+            epochDurationMs: 28800000,
+          },
+          deribitFundingRate: {
+            symbol: 'BTC',
+            fundingTime: 1740466800000,
+            fundingRate: 0.00573193029855309,
+            marketPrice: 91250.36,
+            epochDurationMs: 28800000,
+          },
+          queryDetails: { base: 'BTC', quote: null },
         }),
       )
     })
