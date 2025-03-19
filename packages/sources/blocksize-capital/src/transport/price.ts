1249648d2a35f8dad5ceaeea9605fddcef65b5c9
Subarna-Singh <subarnasingh11@gmail.com>
2025-03-19 14:45:04 +0530

    add error handling

--- packages/sources/blocksize-capital/src/transport/price.ts
+++ packages/sources/blocksize-capital/src/transport/price.ts
@@ -1,4 +1,5 @@
 import { BaseEndpointTypes } from '../endpoint/price'
+import { makeLogger } from '@chainlink/external-adapter-framework/util'
 import { WebsocketReverseMappingTransport } from '@chainlink/external-adapter-framework/transports/websocket'
 import {
   BaseMessage,
@@ -9,7 +10,15 @@ import {
   VwapUpdate,
 } from './utils'
 
-export interface VwapMessage extends BaseMessage {
+const logger = makeLogger('BlocksizeCapitalTransportPrice')
+
+export interface PriceMessage extends BaseMessage {
+  result?: {
+    snapshot: string[]
+  }
+}
+
+export interface VwapMessage extends PriceMessage {
   method: 'vwap'
   params: {
     updates: VwapUpdate[]
@@ -29,7 +38,15 @@ export const transport: WebsocketReverseMappingTransport<WsTransportTypes, strin
       open: (connection, context) =>
         blocksizeDefaultWebsocketOpenHandler(connection, context.adapterSettings.API_KEY),
       message: (message) => {
-        if (message.method !== 'vwap') return []
+        if (message.method !== 'vwap') {
+          if (message?.result?.snapshot && JSON.stringify(message.result.snapshot) === '[]') {
+            logger.error(`Pair does not exist`)
+            logger.warn(`Possible Solutions:
+              1. Confirm you are using the same symbol found in the job spec with the correct case.
+              2. There maybe an issue with the job spec or the Data Provider may have delisted the asset. Reach out to Chainlink Labs.`)
+          }
+          return []
+        }
         const updates = message.params.updates
         return handlePriceUpdates(updates, transport)
       },
