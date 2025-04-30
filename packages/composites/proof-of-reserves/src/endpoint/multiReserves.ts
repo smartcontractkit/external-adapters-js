91c4e363eb6b06702639533df4826dedfce1583f
Matthew McAllister <matthew.mcallister@smartcontract.com>
2025-04-30 12:44:26 -0400

    review fixes

--- packages/composites/proof-of-reserves/src/endpoint/multiReserves.ts
+++ packages/composites/proof-of-reserves/src/endpoint/multiReserves.ts
@@ -51,7 +51,11 @@ export const execute: ExecuteWithConfig<Config> = async (input, context, config)
           ...result,
         })
       } else {
-        return scale(BigInt(result.result.toString()), 18, result.data.decimals?.toString())
+        // Scaling everything up to 18 to preserve precision
+        return scale(BigInt(result.result.toString()), {
+          valueDecimal: result.data.decimals?.toString(),
+          targetDecimal: 18,
+        })
       }
     })
     .reduce((s, e) => s + e)
@@ -59,7 +63,10 @@ export const execute: ExecuteWithConfig<Config> = async (input, context, config)
   // Possible for result to aggregate multiple values > outputDecimals from the separate singleExecutes
   // Therefore, we scale everything to 18 and then down to outputDecimals, which should preserve
   // more precision by performing possible division last
-  const scaledResult = scale(result, outputDecimals, '18').toString()
+  const scaledResult = scale(result, {
+    valueDecimal: '18',
+    targetDecimal: outputDecimals,
+  }).toString()
 
   return {
     jobRunID,
@@ -78,14 +85,15 @@ export const execute: ExecuteWithConfig<Config> = async (input, context, config)
   }
 }
 
-const scale = (value: bigint, outputDecimals: number, decimal?: string) => {
-  if (decimal) {
-    // Scale to outputDecimals decimals (default 18)
-    if (outputDecimals >= Number(decimal)) {
-      return value * 10n ** (BigInt(outputDecimals) - BigInt(decimal))
+// Scale value with valueDecimal to targetDecimal (default 18)
+const scale = (value: bigint, decimals: { valueDecimal?: string; targetDecimal: number }) => {
+  const { valueDecimal, targetDecimal } = decimals
+  if (valueDecimal) {
+    if (targetDecimal >= Number(valueDecimal)) {
+      return value * 10n ** (BigInt(targetDecimal) - BigInt(valueDecimal))
     } else {
-      // NOTE: this condition may lead to precision loss
-      return value / 10n ** (BigInt(decimal) - BigInt(outputDecimals))
+      // NOTE: this condition can lead to precision loss
+      return value / 10n ** (BigInt(valueDecimal) - BigInt(targetDecimal))
     }
   } else {
     return value
