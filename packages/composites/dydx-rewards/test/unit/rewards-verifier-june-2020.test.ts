b9a2192d61b76950d24ad66bb9243ecee5afc988
David de Kloet <david.dekloet@smartcontract.com>
2025-05-15 16:53:35 +0200

    run test on GHA

--- packages/composites/dydx-rewards/test/unit/rewards-verifier-june-2020.test.ts
+++ packages/composites/dydx-rewards/test/unit/rewards-verifier-june-2020.test.ts
@@ -1,9 +1,9 @@
+import * as bn from 'bignumber.js'
 import { calcMarketMakerRewards } from '../../src/method/formulas/initial'
 import { calcTraderRewards } from '../../src/method/formulas/june-2022'
+import expectedRewards from '../mock-data/expected-result-2.json'
 import previousMerkleTreeRewardsData from '../mock-data/previous-test-data-2.json'
 import sampleOracleData from '../mock-data/rewards-test-data-4.json'
-import expectedRewards from '../mock-data/expected-result-2.json'
-import * as bn from 'bignumber.js'
 
 import {
   calcCumulativeRewards,
@@ -14,6 +14,7 @@ import {
 describe('rewards-verifier', () => {
   describe('rewards calculations', () => {
     it('calculateExpectedOracleResult', () => {
+      console.log('dskloetx test 1')
       const rewards = {}
 
       calcTraderRewards(sampleOracleData, rewards, new bn.BigNumber(3_835_616).shiftedBy(18))
@@ -28,6 +29,7 @@ describe('rewards-verifier', () => {
       const result = constructJsonTree(rewards)
 
       expect(result).toEqual(expectedRewards)
+      console.log('dskloetx test 2')
     })
   })
 })
