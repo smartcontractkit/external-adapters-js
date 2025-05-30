1e23f1177ecd2344e78696ead4a3f8c35de99047
Subarna-Singh <subarnasingh11@gmail.com>
2025-05-30 18:05:55 +0530

    Correct unit test

--- packages/sources/token-balance/test/unit/utils.test.ts
+++ packages/sources/token-balance/test/unit/utils.test.ts
@@ -43,7 +43,6 @@ describe('transport/utils.ts', () => {
     const groupSize = 3
     const tokenContractAddress = '0x0123'
     const priceOracleAddress = '0x4567'
-    const tokenSymbol = 'TBILL'
 
     it('should create a token Contract', () => {
       const groupedProvider = new GroupedProvider(mockProvider, groupSize)
@@ -97,7 +96,7 @@ describe('transport/utils.ts', () => {
       ethersNewContract.mockReturnValueOnce(mockTokenContract)
       const groupedTokenContract = groupedProvider.createTokenContract(tokenContractAddress)
 
-      expect(await groupedTokenContract.getWithdrawalQueueLength(tokenSymbol)).toBe(queueLength)
+      expect(await groupedTokenContract.getWithdrawalQueueLength()).toBe(queueLength)
       expect(mockTokenContract.getWithdrawalQueueLength).toBeCalledTimes(1)
     })
 
@@ -261,7 +260,7 @@ describe('transport/utils.ts', () => {
       groupedPriceOracleContract.decimals()
       groupedTokenContract.balanceOf('0x1234')
 
-      groupedTokenContract.getWithdrawalQueueLength(tokenSymbol)
+      groupedTokenContract.getWithdrawalQueueLength()
       groupedTokenContract.getWithdrawalQueueInfo(0)
       groupedPriceOracleContract.latestRoundData()
 
