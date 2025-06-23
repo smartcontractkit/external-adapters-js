d761e84d36743959460590f9abab856663bfa68e
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-06-23 16:14:38 +0400

    Adds wisdomtree EA

---
+++ packages/sources/wisdomtree/src/endpoint/nav.ts
@@ -0,0 +1,35 @@
+import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
+import { InputParameters } from '@chainlink/external-adapter-framework/validation'
+import { config } from '../config'
+import overrides from '../config/overrides.json'
+import { httpTransport } from '../transport/nav'
+
+export const inputParameters = new InputParameters(
+  {
+    ticker: {
+      aliases: ['symbol', 'fundId'],
+      required: true,
+      type: 'string',
+      description: 'The symbol of the fund ticker to query',
+    },
+  },
+  [
+    {
+      ticker: 'WTGXX',
+    },
+  ],
+)
+export type BaseEndpointTypes = {
+  Parameters: typeof inputParameters.definition
+  Response: SingleNumberResultResponse
+  Settings: typeof config.settings
+}
+
+export const endpoint = new AdapterEndpoint({
+  name: 'nav',
+  aliases: [],
+  transport: httpTransport,
+  inputParameters,
+  overrides: overrides['wisdomtree'],
+})
---
+++ packages/sources/wisdomtree/test/integration/fixtures.ts
@@ -0,0 +1,36 @@
+import nock from 'nock'
+
+export const mockResponseSuccess = (): nock.Scope =>
+  nock('https://dataspanapi.wisdomtree.com', {
+    encodedQueryParams: true,
+  })
+    .get('/funddetails/nav')
+    .query({
+      ticker: 'WTGXX',
+    })
+    .reply(
+      200,
+      () => ({
+        ticker: 'WTGXX',
+        relatedTicker: null,
+        name: 'WisdomTree Government Money Market Digital Fund',
+        dt: '2025-06-18',
+        nav: 1.0,
+        sharesOutstanding: 320324457.111,
+        aum: 320324.45711,
+        navPrevious: 1.0,
+        navDelta: 0.0,
+        navDeltaPCT: 0.0,
+      }),
+      [
+        'Content-Type',
+        'application/json',
+        'Connection',
+        'close',
+        'Vary',
+        'Accept-Encoding',
+        'Vary',
+        'Origin',
+      ],
+    )
+    .persist()
