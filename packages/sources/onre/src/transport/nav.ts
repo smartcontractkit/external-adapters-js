1aa2e4118d69153e4a0cb7bd1f794933f298bd8e
Subarna Singh <subarnasingh11@gmail.com>
2025-06-25 03:51:46 +0530

    DF-21398 OnRe NAV EA (#3911)
    * DF-21398 OnRe NAV EA
    
    * update test-payload
    
    * add rateLimit
    
    * yarn lock file
    
    * .pnp.cjs file
    
    * yarn cache
    
    * fixing tests
    
    * update package versions
    
    * fix dependencies
    
    * Changes - Review 1
    
    * fix test
    
    * fix date to include UTC time
    
    * Modify output; add ripcord
    
    * Review 20 June - Part 1
    
    * Update test snapshot
    
    * OnRe EA - correct precision
    
    * Change Response Type
    
    * update snapshot
    
    * refactor
    
    * Update packages/sources/onre/src/endpoint/nav.ts
    
    ---------
    
    Co-authored-by: mmcallister-cll <139181225+mmcallister-cll@users.noreply.github.com>

---
+++ packages/sources/onre/src/transport/nav.ts
@@ -0,0 +1,107 @@
+import { HttpTransport } from '@chainlink/external-adapter-framework/transports'
+import { BaseEndpointTypes } from '../endpoint/nav'
+import { parseDateToTimestamp, sanityCheckData } from './utils'
+
+interface FundNavData {
+  net_asset_value_date: string
+  net_asset_value: string
+  assets_under_management: string
+  fund_name: string
+  fund_id: number
+}
+
+type ProviderResponseSchema = FundNavData[]
+
+export type FundNavDataWithTimestamp = Omit<FundNavData, 'net_asset_value_date'> & {
+  net_asset_value_date: number
+}
+
+export type HttpTransportTypes = BaseEndpointTypes & {
+  Provider: {
+    RequestBody: never
+    ResponseBody: ProviderResponseSchema
+  }
+}
+
+export const httpTransport = new HttpTransport<HttpTransportTypes>({
+  prepareRequests: (params, config) => {
+    return params.map((param) => {
+      return {
+        params: [param],
+        request: {
+          baseURL: config.API_ENDPOINT,
+        },
+      }
+    })
+  },
+
+  parseResponse: (params, response) => {
+    if (!response.data) {
+      return params.map((param) => {
+        return {
+          params: param,
+          response: {
+            errorMessage: `The data provider didn't return any value for ${param}`,
+            statusCode: 502,
+          },
+        }
+      })
+    }
+
+    return params.map((param) => {
+      const filteredData = response.data.filter((item) => item.fund_id === param.fundId)
+      if (filteredData.length === 0) {
+        return {
+          params: param,
+          response: {
+            errorMessage: `The data provider didn't return any value for fundId: ${param.fundId}`,
+            statusCode: 502,
+          },
+        }
+      }
+
+      const dataWithDateMs = filteredData.map((item) => {
+        return {
+          ...item,
+          net_asset_value_date: parseDateToTimestamp(item.net_asset_value_date),
+        }
+      })
+
+      const dataWithDateMsFiltered = dataWithDateMs.filter((item) =>
+        Number.isFinite(item.net_asset_value_date),
+      ) as FundNavDataWithTimestamp[]
+      const sortedData = dataWithDateMsFiltered.sort(
+        (a, b) => b.net_asset_value_date - a.net_asset_value_date,
+      )
+      const latestData = sortedData[0]
+
+      if (!sanityCheckData(latestData)) {
+        return {
+          params: param,
+          response: {
+            errorMessage: `The data by data provider for ${param} is null in value`,
+            statusCode: 502,
+          },
+        }
+      }
+
+      const resultData = {
+        navPerShare: Number(latestData.net_asset_value),
+        navDate: String(latestData.net_asset_value_date),
+        currency: 'USD',
+        aum: latestData.assets_under_management,
+        fundId: latestData.fund_id,
+      }
+
+      const result = Number(latestData.net_asset_value)
+
+      return {
+        params: param,
+        response: {
+          result: result,
+          data: resultData,
+        },
+      }
+    })
+  },
+})
