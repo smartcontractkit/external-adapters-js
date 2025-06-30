46f102b1e6020d45a9e6125bb37b3eebd03e75af
Subarna Singh <subarnasingh11@gmail.com>
2025-06-30 18:15:38 +0530

    DF21398 OnRe NAV EA (#3921)
    * DF-21398 OnRe NAV EA
    
    * update test-payload
    
    * add rateLimit
    
    * yarn lock file
    
    * .pnp.cjs file
    
    * yarn cache
    
    * fixing tests
    
    * update package versions
    
    * fix dependencies
    
    * Changes - Review 1
    
    * fix test
    
    * fix date to include UTC time
    
    * Modify output; add ripcord
    
    * Review 20 June - Part 1
    
    * Update test snapshot
    
    * OnRe EA - correct precision
    
    * Change Response Type
    
    * update snapshot
    
    * refactor
    
    * Add changeset
    
    * Fix linter; Fix Tests
    
    * remove circuit breaking ripcord
    
    * remove circuit breaking ripcord
    
    * fix failing test
    
    * fix typo
    
    * Update snapshot
    
    * Update ResponseSchema field type

--- packages/sources/onre/test/integration/fixtures.ts
+++ packages/sources/onre/test/integration/fixtures.ts
@@ -1,31 +1,35 @@
 import nock from 'nock'
 
 export const mockResponseSuccess = (): nock.Scope =>
-  nock('https://onre-api-prod.ew.r.appspot.com/nav', {
+  nock('https://onre-api-prod.ew.r.appspot.com/data/nav', {
     encodedQueryParams: true,
   })
     .get('')
-    .reply(200, () => [
-      {
-        net_asset_value_date: '06/14/2025',
-        net_asset_value: '2.01702150000035000',
-        assets_under_management: '15172254.24630367655644876519110000',
-        fund_name: 'ONe',
-        fund_id: 1,
-      },
-      {
-        net_asset_value_date: '06/13/2025',
-        net_asset_value: '1.01654803794770000',
-        assets_under_management: '12165140.98716837796116030716880000',
-        fund_name: 'ONe',
-        fund_id: 1,
-      },
-      {
-        net_asset_value_date: '06/12/2025',
-        net_asset_value: '1.01607457589505000',
-        assets_under_management: '12159421.51526390254394172217410000',
-        fund_name: 'ONe',
-        fund_id: 1,
-      },
-    ])
+    .reply(200, () => ({
+      data: [
+        {
+          net_asset_value_date: '06/14/2025',
+          net_asset_value: '2.01702150000035000',
+          assets_under_management: '15172254.24630367655644876519110000',
+          fund_name: 'ONe',
+          fund_id: 1,
+        },
+        {
+          net_asset_value_date: '06/13/2025',
+          net_asset_value: '1.01654803794770000',
+          assets_under_management: '12165140.98716837796116030716880000',
+          fund_name: 'ONe',
+          fund_id: 1,
+        },
+        {
+          net_asset_value_date: '06/12/2025',
+          net_asset_value: '1.01607457589505000',
+          assets_under_management: '12159421.51526390254394172217410000',
+          fund_name: 'ONe',
+          fund_id: 1,
+        },
+      ],
+      ripcord: false,
+      ripcordDetails: null,
+    }))
     .persist()
