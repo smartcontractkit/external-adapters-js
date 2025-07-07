cc32c8a26774aeff4a647675042a901f72268061
Vasco Figueira <vasco.figueira@smartcontract.com>
2025-07-07 10:46:01 +0100

    DF-21369 ICE adapter end-to-end tests
    Makes use of netdania double (via xhr mocking)

--- packages/sources/ice/test/integration/adapter.test.ts
+++ packages/sources/ice/test/integration/adapter.test.ts
@@ -1,18 +1,52 @@
 import {
-  TestAdapter,
   setEnvVariables,
+  TestAdapter,
 } from '@chainlink/external-adapter-framework/util/testing-utils'
-import * as nock from 'nock'
-import { mockResponseSuccess } from './fixtures'
+import { NetDaniaDouble } from './netdania.double'
+import { config } from '../../src/config'
+import { window } from '../../src/transport/netdania/jsApi/jsapi-nodejs'
+import { MockXhrServer, newServer } from 'mock-xmlhttprequest'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const JsApi = window.NetDania.JsApi
 
 describe('execute', () => {
   let spy: jest.SpyInstance
   let testAdapter: TestAdapter
+  let netDaniaDouble: NetDaniaDouble
+  let server: MockXhrServer
+  let savedJXhr: () => XMLHttpRequest // the original jXHR from the NetDania client, to be restored after the test
   let oldEnv: NodeJS.ProcessEnv
 
   beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
+    const baseUri = 'https://mockedq102948.com'
+    const unreachable = 'https://m2039oienoien8.com'
+    const path = '/StreamingServer/StreamingServer'
+    const mockedApiEndpoint = baseUri + path
+    process.env.API_ENDPOINT = mockedApiEndpoint
+    process.env.API_ENDPOINT_FAILOVER_1 = unreachable + path
     process.env.NETDANIA_PASSWORD = process.env.NETDANIA_PASSWORD ?? 'fake-api-key'
+
+    config.initialize()
+    config.validate()
+
+    netDaniaDouble = NetDaniaDouble.getInstance(config.settings)
+
+    server = newServer({
+      get: [
+        (uri: string) => uri.startsWith(mockedApiEndpoint),
+        (request) => {
+          const { status, headers, body } = netDaniaDouble.streaming(request.url)
+          request.respond(status, headers, body)
+        },
+      ],
+    })
+
+    // keep for restore
+    savedJXhr = JsApi.mkXHR
+    JsApi.mkXHR = server.xhrFactory
 
     const mockDate = new Date('2001-01-01T11:11:11.111Z')
     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
@@ -27,8 +61,7 @@ describe('execute', () => {
   afterAll(async () => {
     setEnvVariables(oldEnv)
     await testAdapter.api.close()
-    nock.restore()
-    nock.cleanAll()
+    JsApi.mkXHR = savedJXhr
     spy.mockRestore()
   })
 
@@ -40,10 +73,13 @@ describe('execute', () => {
         endpoint: 'price',
         transport: 'rest',
       }
-      mockResponseSuccess()
       const response = await testAdapter.request(data)
       expect(response.statusCode).toBe(200)
-      expect(response.json()).toMatchSnapshot()
+      expect(response.json()).toMatchSnapshot({
+        timestamps: {
+          providerIndicatedTimeUnixMs: expect.closeTo(1750333483000, -4),
+        },
+      })
     })
   })
 })
