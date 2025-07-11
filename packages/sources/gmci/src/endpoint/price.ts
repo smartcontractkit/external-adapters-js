d98103a054105f28c6a0aebd69200224ef6bd007
Subarna-Singh <subarnasingh11@gmail.com>
2025-07-11 16:35:03 +0530

    remove override functionality

--- packages/sources/gmci/src/endpoint/price.ts
+++ packages/sources/gmci/src/endpoint/price.ts
@@ -1,7 +1,6 @@
 import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
-import overrides from '../config/overrides.json'
 import { transport } from '../transport/price'
 
 export const inputParameters = new InputParameters(
@@ -41,5 +40,4 @@ export const endpoint = new AdapterEndpoint({
   aliases: [],
   transport: transport,
   inputParameters,
-  overrides: overrides['gmci'],
 })
