f80198a344a88f580ccf5d4ce1d373b203602e31
Subarna-Singh <subarnasingh11@gmail.com>
2025-07-16 17:41:40 +0530

    Unit test: for price transport

--- packages/sources/gmci/test/unit/price.test.ts
+++ packages/sources/gmci/test/unit/price.test.ts
@@ -1,6 +1,13 @@
 import { EndpointContext } from '@chainlink/external-adapter-framework/adapter'
 import { LoggerFactoryProvider } from '@chainlink/external-adapter-framework/util'
-import { options, WsTransportTypes } from '../../src/transport/price'
+import {
+  GmciWebsocketTransport,
+  options,
+  PriceMessage,
+  RebalanceMessage,
+  WsResponse,
+  WsTransportTypes,
+} from '../../src/transport/price'
 import { convertTimetoUnixMs } from '../../src/transport/util'
 
 // Initialize logger to avoid errors during test
@@ -41,5 +48,138 @@ describe('builders.unsubscribeMessage', () => {
     expect(message.args).toContain('price.gmci30')
     expect(message.args).toContain('rebalance_status.gmci30')
     expect(message.args.length).toBe(2)
+  })
+})
+
+describe('GmciWebsocketTransport', () => {
+  let transport: GmciWebsocketTransport
+
+  const mockWrite = jest.fn()
+  const now = Date.now()
+
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(now)
+    transport = new GmciWebsocketTransport({} as any)
+
+    // Mock the responseCache
+    transport.responseCache = {
+      write: mockWrite,
+    } as any
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+    jest.useRealTimers()
+  })
+
+  it('processes price and rebalance messages and writes combined result to cache', async () => {
+    const priceMsg: PriceMessage = {
+      symbol: 'GMCI30',
+      last_updated: '2025-07-14T12:00:00Z',
+      price: 184,
+    }
+
+    const rebalanceMsg: RebalanceMessage = {
+      symbol: 'GMCI30',
+      start_time: '2025-07-14T12:00:00Z',
+      end_time: '2025-07-14T13:00:00Z',
+      status: 'open',
+    }
+
+    transport.price_cache.set(
+      'GMCI30',
+      {
+        params: { index: 'GMCI30', type: 'price' },
+        response: {
+          result: 184,
+          data: {
+            result: 184,
+            symbol: 'GMCI30',
+          },
+          timestamps: {
+            providerIndicatedTimeUnixMs: 1752494400000,
+          },
+        },
+      },
+      90000,
+    )
+
+    transport.rebalance_status_cache.set(
+      'GMCI30',
+      {
+        params: { index: 'GMCI30', type: 'rebalance_status' },
+        response: {
+          data: {
+            symbol: 'GMCI30',
+            status: 'open',
+            start_time: '2025-07-14T12:00:00Z',
+            end_time: '2025-07-14T13:00:00Z',
+          },
+        },
+      },
+      90000,
+    )
+
+    const priceWsMessage: WsResponse = {
+      success: true,
+      topic: 'price',
+      data: [priceMsg],
+    }
+
+    const rebalanceWsMessage: WsResponse = {
+      success: true,
+      topic: 'rebalance_status',
+      data: [rebalanceMsg],
+    }
+
+    await transport.processMessage(transport, priceWsMessage)
+    await transport.processMessage(transport, rebalanceWsMessage)
+
+    expect(mockWrite).toHaveBeenCalledTimes(2)
+    const [callArg] = mockWrite.mock.calls[0]
+    expect(callArg).toBe(transport.name)
+
+    const [result] = mockWrite.mock.calls[0][1]
+    expect(result.params.index).toBe('GMCI30')
+    expect(result.response.data.symbol).toBe('GMCI30')
+    expect(result.response.data.status).toBe('open')
+    expect(result.response.data.result).toBe(184)
+    expect(result.response.timestamps.providerIndicatedTimeUnixMs).toBe(1752494400000)
+    expect(result.response.timestamps.providerDataReceivedUnixMs).toBe(now)
+  })
+
+  it('skips writing to cache if any data is missing', async () => {
+    const priceWsMessage: WsResponse = {
+      success: true,
+      topic: 'price',
+      data: [
+        {
+          symbol: 'GMCI30',
+          last_updated: '2025-07-14T12:00:00Z',
+          price: 184,
+        },
+      ],
+    }
+
+    transport.price_cache.set(
+      'GMCI30',
+      {
+        params: { index: 'GMCI30', type: 'price' },
+        response: {
+          result: 184,
+          data: {
+            result: 184,
+            symbol: 'GMCI30',
+          },
+          timestamps: {
+            providerIndicatedTimeUnixMs: 1752494400000,
+          },
+        },
+      },
+      90000,
+    )
+
+    await transport.processMessage(transport, priceWsMessage)
+    expect(mockWrite).not.toHaveBeenCalled()
   })
 })
