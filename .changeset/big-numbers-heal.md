83c779664ede47d9504284bc041688f31bc8e716
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-07-28 17:20:33 -0400

    Adds Coinmetric LWBA EA (#3935)
    * Adds Coinmetric LWBA EA
    
    * Adds coinmetrics-adapter to workspace
    
    * Fix tsconfig
    
    * Adds dependency reference to test tsconfig
    
    * Clone instead of import
    
    * Removes ratelimit allocation

---
+++ .changeset/big-numbers-heal.md
@@ -0,0 +1,6 @@
+---
+'@chainlink/coinmetrics-lwba-adapter': major
+'@chainlink/coinmetrics-adapter': minor
+---
+
+Adds Coinmetrics LWBA EA
---
+++ packages/sources/coinmetrics-lwba/src/index.ts
@@ -0,0 +1,39 @@
+import { makeConfig } from '@chainlink/coinmetrics-adapter/config'
+import { endpoint } from '@chainlink/coinmetrics-adapter/endpoint/lwba'
+import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
+import { Adapter } from '@chainlink/external-adapter-framework/adapter'
+
+export const config = makeConfig({
+  NAME: 'COINMETRICS_LWBA',
+  API_KEY: {
+    description: 'Unused in LWBA',
+    type: 'string',
+    required: false,
+  },
+  API_ENDPOINT: {
+    description: 'Unused in LWBA',
+    type: 'string',
+    required: false,
+  },
+})
+
+const newEndpoint = Object.assign(Object.create(Object.getPrototypeOf(endpoint)), endpoint)
+
+export const adapter = new Adapter({
+  defaultEndpoint: newEndpoint.name,
+  name: 'COINMETRICS_LWBA',
+  config,
+  endpoints: [newEndpoint],
+  rateLimiting: {
+    tiers: {
+      community: {
+        rateLimit1m: 100,
+      },
+      paid: {
+        rateLimit1s: 300,
+      },
+    },
+  },
+})
+
+export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
