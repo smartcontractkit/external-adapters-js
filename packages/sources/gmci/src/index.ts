bfbd60e451fb1d2baf7ca89287388db2f5befa4b
Subarna-Singh <subarnasingh11@gmail.com>
2025-07-28 13:17:39 +0530

    MERC-6932 Wintermute

--- packages/sources/gmci/src/index.ts
+++ packages/sources/gmci/src/index.ts
@@ -1,13 +1,13 @@
 import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
 import { Adapter } from '@chainlink/external-adapter-framework/adapter'
 import { config } from './config'
-import { price } from './endpoint'
+import { price, wintermute } from './endpoint'
 
 export const adapter = new Adapter({
   defaultEndpoint: price.name,
   name: 'GMCI',
   config,
-  endpoints: [price],
+  endpoints: [price, wintermute],
 })
 
 export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
