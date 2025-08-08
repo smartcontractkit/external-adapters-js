a2e4bedfdf1ef5987cbaeb821f9c9b5427b60a04
David de Kloet <david.dekloet@smartcontract.com>
2025-08-08 11:56:33 +0200

    add workflow

---
+++ .github/scripts/packages-to-ignore.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+eas_to_include="$(echo "$*" | sed -e 's/ *, */ /g' | tr ' ' '\n')"
+packages_to_include="$(echo "$eas_to_include" | tr ' ' '\n' | sed -e 's|.*|@chainlink/&-adapter|')"
+
+all_packages=$(yarn workspaces list --json | jq -r '.name' | grep -v '@chainlink/external-adapters-js')
+
+packages_to_ignore="$(echo "$all_packages" | grep -vFf <(echo "$packages_to_include" | tr ' ' '\n'))"
+
+echo "$packages_to_ignore"
