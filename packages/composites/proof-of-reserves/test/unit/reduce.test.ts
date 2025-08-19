520631543d45b20b3821abf88924f98b7a4b7655
David de Kloet <david.dekloet@smartcontract.com>
2025-08-19 17:54:08 +0200

    Add unit test for proof-of-reserve with ethereum-cl-indexer

---
+++ packages/composites/proof-of-reserves/test/unit/reduce.test.ts
@@ -0,0 +1,51 @@
+import { AdapterContext, AdapterResponse } from '@chainlink/ea-bootstrap'
+import { makeStub } from '@chainlink/external-adapter-framework/util/testing-utils'
+import { runReduceAdapter } from '../../src/utils/reduce'
+
+describe('reduce', () => {
+  describe('ethereum-cl-indexer', () => {
+    describe('etherFiBalance endpoint', () => {
+      it('should get totalBalance', async () => {
+        const jobRunID = '45'
+        const totalBalance = '123000000000000000000'
+        const context = makeStub('context', {} as AdapterContext)
+        const input = makeStub('input', {
+          jobRunID,
+          data: {
+            isValid: true,
+            totalBalance,
+          },
+        } as AdapterResponse)
+
+        const response = await runReduceAdapter('ETHEREUM_CL_INDEXER', context, input)
+
+        expect(response).toEqual({
+          jobRunID,
+          result: totalBalance,
+          statusCode: 200,
+          data: {
+            result: totalBalance,
+            statusCode: 200,
+          },
+        })
+      })
+
+      it('should throw if result is not valid', async () => {
+        const jobRunID = '45'
+        const totalBalance = '123000000000000000000'
+        const context = makeStub('context', {} as AdapterContext)
+        const input = makeStub('input', {
+          jobRunID,
+          data: {
+            isValid: false,
+            totalBalance,
+          },
+        } as AdapterResponse)
+
+        await expect(() => runReduceAdapter('ETHEREUM_CL_INDEXER', context, input)).rejects.toThrow(
+          `ETHEREUM_CL_INDEXER ripcord is true: ${JSON.stringify(input.data)}`,
+        )
+      })
+    })
+  })
+})
