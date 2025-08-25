cb9cc0e9c99d8c5d52caabc7dcd306bfbe88d6f8
chray-zhang <chray.zhang@smartcontract.com>
2025-08-25 17:18:59 -0400

    index.ts

---
+++ packages/sources/ftse-sftp/src/index.ts
@@ -0,0 +1,3 @@
+import { Adapter } from '@chainlink/external-adapter-framework/adapter'
+import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
+import { config } from './config'
