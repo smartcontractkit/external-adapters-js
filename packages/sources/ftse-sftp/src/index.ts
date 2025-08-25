8f77a1d084d0de4d2152280f10a4773057723963
chray-zhang <chray.zhang@smartcontract.com>
2025-08-25 17:29:42 -0400

    Empty source for just the transport utils

--- packages/sources/ftse-sftp/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { Adapter } from '@chainlink/external-adapter-framework/adapter'
-import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
-import { config } from './config'
