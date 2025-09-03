9f0f62eab458c897c7402652095275db94d88a0a
David de Kloet <david.dekloet@smartcontract.com>
2025-09-03 09:11:14 +0200

    Remove SSH dep

--- .pnp.cjs
+++ .pnp.cjs
@@ -7228,13 +7228,11 @@ const RAW_RUNTIME_STATE =
           ["@sinonjs/fake-timers", "npm:14.0.0"],\
           ["@types/jest", "npm:29.5.14"],\
           ["@types/node", "npm:22.14.1"],\
-          ["@types/ssh2-sftp-client", "npm:9.0.5"],\
           ["csv-parse", "npm:5.5.6"],\
           ["decimal.js", "npm:10.5.0"],\
           ["jest", "virtual:532fdf4c5364453a90c37bb5287483fa331782ecfc41fc8a238ae4c5b9ecbfa540f6b4c584d3930549b0f5a0f6dd6c0d866dbdf0879570565599d51c3b649109#npm:29.7.0"],\
           ["jest-util", "npm:29.7.0"],\
           ["nock", "npm:13.5.6"],\
-          ["ssh2-sftp-client", "npm:10.0.3"],\
           ["ts-jest", "virtual:574dc4ff24172c9d1b6014b09ac4f0f8a617e1a95145ad46aecd2ebd04a4f75de4ab8a41492c5d05a29c48f79f7f8b9ebb3a7bb1b88d778e9d134d1f21b3f7cf#npm:29.4.1"],\
           ["tslib", "npm:2.8.1"],\
           ["typescript", "patch:typescript@npm%3A5.8.3#optional!builtin<compat/typescript>::version=5.8.3&hash=5786d5"]\
@@ -16830,14 +16828,6 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:18.19.123", {\
-        "packageLocation": "./.yarn/cache/@types-node-npm-18.19.123-a6051ce4a3-b5e6c524d9.zip/node_modules/@types/node/",\
-        "packageDependencies": [\
-          ["@types/node", "npm:18.19.123"],\
-          ["undici-types", "npm:5.26.5"]\
-        ],\
-        "linkType": "HARD"\
-      }],\
       ["npm:22.14.1", {\
         "packageLocation": "./.yarn/cache/@types-node-npm-22.14.1-ff7e0a29d7-561b1ad98e.zip/node_modules/@types/node/",\
         "packageDependencies": [\
@@ -17125,26 +17115,6 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/@types-sinonjs__fake-timers-npm-8.1.5-c35b400174-3a0b285fcb.zip/node_modules/@types/sinonjs__fake-timers/",\
         "packageDependencies": [\
           ["@types/sinonjs__fake-timers", "npm:8.1.5"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
-    ["@types/ssh2", [\
-      ["npm:1.15.5", {\
-        "packageLocation": "./.yarn/cache/@types-ssh2-npm-1.15.5-f4fbed4d10-dd6f29f4e9.zip/node_modules/@types/ssh2/",\
-        "packageDependencies": [\
-          ["@types/ssh2", "npm:1.15.5"],\
-          ["@types/node", "npm:18.19.123"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
-    ["@types/ssh2-sftp-client", [\
-      ["npm:9.0.5", {\
-        "packageLocation": "./.yarn/cache/@types-ssh2-sftp-client-npm-9.0.5-19c71d94cb-a88f442bf6.zip/node_modules/@types/ssh2-sftp-client/",\
-        "packageDependencies": [\
-          ["@types/ssh2-sftp-client", "npm:9.0.5"],\
-          ["@types/ssh2", "npm:1.15.5"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -19791,15 +19761,6 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
-    ["buildcheck", [\
-      ["npm:0.0.6", {\
-        "packageLocation": "./.yarn/cache/buildcheck-npm-0.0.6-46e0f23ecf-194ee8d3b0.zip/node_modules/buildcheck/",\
-        "packageDependencies": [\
-          ["buildcheck", "npm:0.0.6"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
     ["builtins", [\
       ["npm:1.0.3", {\
         "packageLocation": "./.yarn/cache/builtins-npm-1.0.3-f09d2d57f2-8f756616bd.zip/node_modules/builtins/",\
@@ -20893,17 +20854,6 @@ const RAW_RUNTIME_STATE =
           ["typedarray", "npm:0.0.6"]\
         ],\
         "linkType": "HARD"\
-      }],\
-      ["npm:2.0.0", {\
-        "packageLocation": "./.yarn/cache/concat-stream-npm-2.0.0-8bb2ad5aa0-250e576d06.zip/node_modules/concat-stream/",\
-        "packageDependencies": [\
-          ["concat-stream", "npm:2.0.0"],\
-          ["buffer-from", "npm:1.1.2"],\
-          ["inherits", "npm:2.0.4"],\
-          ["readable-stream", "npm:3.6.2"],\
-          ["typedarray", "npm:0.0.6"]\
-        ],\
-        "linkType": "HARD"\
       }]\
     ]],\
     ["config-chain", [\
@@ -21059,18 +21009,6 @@ const RAW_RUNTIME_STATE =
           ["parse-json", "npm:5.2.0"],\
           ["path-type", "npm:4.0.0"],\
           ["yaml", "npm:1.10.2"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
-    ["cpu-features", [\
-      ["npm:0.0.10", {\
-        "packageLocation": "./.yarn/unplugged/cpu-features-npm-0.0.10-7200b22ae6/node_modules/cpu-features/",\
-        "packageDependencies": [\
-          ["cpu-features", "npm:0.0.10"],\
-          ["buildcheck", "npm:0.0.6"],\
-          ["nan", "npm:2.23.0"],\
-          ["node-gyp", "npm:10.2.0"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -31714,14 +31652,6 @@ const RAW_RUNTIME_STATE =
           ["node-gyp", "npm:10.2.0"]\
         ],\
         "linkType": "HARD"\
-      }],\
-      ["npm:2.23.0", {\
-        "packageLocation": "./.yarn/unplugged/nan-npm-2.23.0-b3b2a3ae9b/node_modules/nan/",\
-        "packageDependencies": [\
-          ["nan", "npm:2.23.0"],\
-          ["node-gyp", "npm:10.2.0"]\
-        ],\
-        "linkType": "HARD"\
       }]\
     ]],\
     ["nanoid", [\
@@ -36585,31 +36515,6 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
-    ["ssh2", [\
-      ["npm:1.17.0", {\
-        "packageLocation": "./.yarn/unplugged/ssh2-npm-1.17.0-b973b284c6/node_modules/ssh2/",\
-        "packageDependencies": [\
-          ["ssh2", "npm:1.17.0"],\
-          ["asn1", "npm:0.2.6"],\
-          ["bcrypt-pbkdf", "npm:1.0.2"],\
-          ["cpu-features", "npm:0.0.10"],\
-          ["nan", "npm:2.23.0"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
-    ["ssh2-sftp-client", [\
-      ["npm:10.0.3", {\
-        "packageLocation": "./.yarn/cache/ssh2-sftp-client-npm-10.0.3-c85f979cec-87933bbb7e.zip/node_modules/ssh2-sftp-client/",\
-        "packageDependencies": [\
-          ["ssh2-sftp-client", "npm:10.0.3"],\
-          ["concat-stream", "npm:2.0.0"],\
-          ["promise-retry", "npm:2.0.1"],\
-          ["ssh2", "npm:1.17.0"]\
-        ],\
-        "linkType": "HARD"\
-      }]\
-    ]],\
     ["sshpk", [\
       ["npm:1.18.0", {\
         "packageLocation": "./.yarn/cache/sshpk-npm-1.18.0-e75427668c-858339d43e.zip/node_modules/sshpk/",\
@@ -38309,13 +38214,6 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["undici-types", [\
-      ["npm:5.26.5", {\
-        "packageLocation": "./.yarn/cache/undici-types-npm-5.26.5-de4f7c7bb9-0097779d94.zip/node_modules/undici-types/",\
-        "packageDependencies": [\
-          ["undici-types", "npm:5.26.5"]\
-        ],\
-        "linkType": "HARD"\
-      }],\
       ["npm:6.19.8", {\
         "packageLocation": "./.yarn/cache/undici-types-npm-6.19.8-9f12285b7a-cf0b48ed4f.zip/node_modules/undici-types/",\
         "packageDependencies": [\
