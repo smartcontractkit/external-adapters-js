28cc03f676a71786f088e980b7d9e3f5172f21bb
chray-zhang <chray.zhang@smartcontract.com>
2025-09-15 14:35:53 -0400

    Added Adapter to pass deploy test

---
+++ packages/sources/ftse-sftp/src/index.ts
@@ -0,0 +1,13 @@
+import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
+import { Adapter } from '@chainlink/external-adapter-framework/adapter'
+import { config } from './config'
+import * as endpoints from './endpoint'
+
+export const adapter = new Adapter({
+  defaultEndpoint: 'ftse_sftp',
+  name: 'FTSE_SFTP',
+  config,
+  endpoints: [endpoints.sftp.endpoint],
+})
+
+export const server = (): Promise<ServerInstance | undefined> => expose(adapter)
