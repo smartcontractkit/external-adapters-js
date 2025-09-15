53607c8c8d408d453f80de50c14efb589be35e90
David de Kloet <david.dekloet@smartcontract.com>
2025-09-15 16:23:47 +0200

    Solana-balance unit test

--- packages/sources/token-balance/test/unit/solana-balance.test.ts
+++ packages/sources/token-balance/test/unit/solana-balance.test.ts
@@ -1,10 +1,10 @@
 import { EndpointContext } from '@chainlink/external-adapter-framework/adapter'
-import { calculateHttpRequestKey } from '@chainlink/external-adapter-framework/cache'
 import { TransportDependencies } from '@chainlink/external-adapter-framework/transports'
 import { deferredPromise, LoggerFactoryProvider } from '@chainlink/external-adapter-framework/util'
 import { makeStub } from '@chainlink/external-adapter-framework/util/testing-utils'
-import { BaseEndpointTypes, inputParameters } from '../../src/endpoint/xrp'
-import { XrpTransport } from '../../src/transport/xrp'
+import { PublicKey } from '@solana/web3.js'
+import { BaseEndpointTypes } from '../../src/endpoint/solana-balance'
+import { SolanaBalanceTransport } from '../../src/transport/solana-balance'
 
 const originalEnv = { ...process.env }
 
@@ -17,6 +17,30 @@ const restoreEnv = () => {
     }
   }
 }
+
+const RESULT_DECIMALS = 9
+
+const connectionGetAccountInfo = jest.fn()
+
+const createFakePublicKey = (address: string) =>
+  ({
+    publicKey: address,
+  } as unknown as PublicKey)
+
+const makeSolanaWeb3 = () => {
+  return {
+    PublicKey: function (address: string): PublicKey {
+      return createFakePublicKey(address)
+    },
+    Connection: class {
+      async getAccountInfo(publicKey: PublicKey) {
+        return connectionGetAccountInfo(publicKey)
+      }
+    },
+  }
+}
+
+jest.mock('@solana/web3.js', () => makeSolanaWeb3())
 
 const log = jest.fn()
 const logger = {
@@ -32,15 +56,16 @@ const loggerFactory = { child: () => logger }
 
 LoggerFactoryProvider.set(loggerFactory)
 
-describe('XrpTransport', () => {
+describe('SolanaBalanceTransport', () => {
   const transportName = 'default_single_transport'
-  const endpointName = 'xrp'
-  const XRPL_RPC_URL = 'https://xrpl.rpc.url'
+  const endpointName = 'solana-balance'
+  const SOLANA_RPC_URL = 'https://solana.rpc.url'
   const BACKGROUND_EXECUTE_MS = 1500
   const GROUP_SIZE = 3
 
   const adapterSettings = makeStub('adapterSettings', {
-    XRPL_RPC_URL,
+    SOLANA_RPC_URL,
+    SOLANA_COMMITMENT: 'finalized',
     WARMUP_SUBSCRIPTION_TTL: 10_000,
     BACKGROUND_EXECUTE_MS,
     GROUP_SIZE,
@@ -51,80 +76,23 @@ describe('XrpTransport', () => {
     adapterSettings,
   } as EndpointContext<BaseEndpointTypes>)
 
-  const requester = makeStub('requester', {
-    request: jest.fn(),
-  })
-
   const responseCache = {
     write: jest.fn(),
   }
 
   const dependencies = makeStub('dependencies', {
-    requester,
     responseCache,
     subscriptionSetFactory: {
       buildSet: jest.fn(),
     },
   } as unknown as TransportDependencies<BaseEndpointTypes>)
 
-  let transport: XrpTransport
+  let transport: SolanaBalanceTransport
 
-  type RequestConfig = {
-    baseURL: string
-    method: 'POST'
-    data: {
-      method: 'account_info'
-      params: [
-        {
-          account: string
-          ledger_index: 'validated'
-        },
-      ]
-    }
-  }
-
-  const requestConfigForAddresses = ({ address }: { address: string }): RequestConfig => ({
-    baseURL: adapterSettings.XRPL_RPC_URL,
-    method: 'POST',
-    data: {
-      method: 'account_info',
-      params: [
-        {
-          account: address,
-          ledger_index: 'validated',
-        },
-      ],
-    },
-  })
-
-  const requestKeyForConfig = (requestConfig: RequestConfig) => {
-    const requestKey = calculateHttpRequestKey<BaseEndpointTypes>({
-      context: {
-        adapterSettings,
-        inputParameters,
-        endpointName,
-      },
-      data: requestConfig.data,
-      transportName,
-    })
-    expect(log).toBeCalledWith(`Generated HTTP request queue key: "${requestKey}"`)
-    expect(log).toBeCalledTimes(1)
-    log.mockClear()
-    return requestKey
-  }
-
-  const mockAccountInfo = (balance: string | Promise<string>) => {
-    requester.request.mockImplementationOnce(async () => {
+  const mockAccountInfo = (balance: number | Promise<number>) => {
+    connectionGetAccountInfo.mockImplementationOnce(async () => {
       return {
-        response: {
-          data: {
-            result: {
-              account_data: {
-                Balance: await balance,
-              },
-            },
-          },
-        },
+        lamports: await balance,
       }
     })
   }
@@ -134,7 +102,7 @@ describe('XrpTransport', () => {
     jest.resetAllMocks()
     jest.useFakeTimers()
 
-    transport = new XrpTransport()
+    transport = new SolanaBalanceTransport()
 
     await transport.initialize(dependencies, adapterSettings, endpointName, transportName)
   })
@@ -160,7 +128,7 @@ describe('XrpTransport', () => {
       const address = 'r101'
       const balance = 123
 
-      mockAccountInfo(balance.toString())
+      mockAccountInfo(balance)
 
       const param = makeStub('param', {
         addresses: [{ address }],
@@ -178,7 +146,7 @@ describe('XrpTransport', () => {
         statusCode: 200,
         result: null,
         data: {
-          decimals: 6,
+          decimals: RESULT_DECIMALS,
           result: expectedResult,
         },
         timestamps: {
@@ -195,10 +163,6 @@ describe('XrpTransport', () => {
         },
       ])
       expect(responseCache.write).toBeCalledTimes(1)
-
-      expect(log).toBeCalledWith(expect.stringContaining('Generated HTTP request queue key:'))
-      expect(log).toBeCalledTimes(1)
-      log.mockClear()
     })
   })
 
@@ -209,8 +173,8 @@ describe('XrpTransport', () => {
       const balance1 = 100
       const balance2 = 200
 
-      mockAccountInfo(balance1.toString())
-      mockAccountInfo(balance2.toString())
+      mockAccountInfo(balance1)
+      mockAccountInfo(balance2)
 
       const param = makeStub('param', {
         addresses: [{ address: address1 }, { address: address2 }],
@@ -231,7 +195,7 @@ describe('XrpTransport', () => {
         statusCode: 200,
         result: null,
         data: {
-          decimals: 6,
+          decimals: RESULT_DECIMALS,
           result: expectedResult,
         },
         timestamps: {
@@ -240,24 +204,13 @@ describe('XrpTransport', () => {
           providerIndicatedTimeUnixMs: undefined,
         },
       })
-
-      expect(log).toHaveBeenNthCalledWith(
-        1,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toHaveBeenNthCalledWith(
-        2,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toBeCalledTimes(2)
-      log.mockClear()
     })
 
     it('should record received timestamp separate from requested timestamp', async () => {
       const address = 'r101'
       const balance = 100
 
-      const [balancePromise, resolveBalance] = deferredPromise<string>()
+      const [balancePromise, resolveBalance] = deferredPromise<number>()
       mockAccountInfo(balancePromise)
 
       const param = makeStub('param', {
@@ -270,7 +223,7 @@ describe('XrpTransport', () => {
       const responseTimestamp = Date.now()
       expect(responseTimestamp).toBeGreaterThan(requestTimestamp)
 
-      resolveBalance(balance.toString())
+      resolveBalance(balance)
 
       const expectedResult = [
         {
@@ -282,7 +235,7 @@ describe('XrpTransport', () => {
         statusCode: 200,
         result: null,
         data: {
-          decimals: 6,
+          decimals: RESULT_DECIMALS,
           result: expectedResult,
         },
         timestamps: {
@@ -300,18 +253,8 @@ describe('XrpTransport', () => {
     it('should return the token balance of multiple addresses', async () => {
       const address1 = 'r101'
       const address2 = 'r102'
-      mockAccountInfo('100.0')
-      mockAccountInfo('200.0')
-
-      const expectedRequestConfig1 = requestConfigForAddresses({
-        address: address1,
-      })
-      const expectedRequestKey1 = requestKeyForConfig(expectedRequestConfig1)
-
-      const expectedRequestConfig2 = requestConfigForAddresses({
-        address: address2,
-      })
-      const expectedRequestKey2 = requestKeyForConfig(expectedRequestConfig2)
+      mockAccountInfo(100)
+      mockAccountInfo(200)
 
       const balances = await transport.getTokenBalances([
         { address: address1 },
@@ -319,39 +262,20 @@ describe('XrpTransport', () => {
       ])
 
       expect(balances).toEqual([
-        { address: address1, balance: '100.0' },
-        { address: address2, balance: '200.0' },
+        { address: address1, balance: '100' },
+        { address: address2, balance: '200' },
       ])
 
-      expect(requester.request).toHaveBeenNthCalledWith(
-        1,
-        expectedRequestKey1,
-        expectedRequestConfig1,
-      )
-      expect(requester.request).toHaveBeenNthCalledWith(
-        2,
-        expectedRequestKey2,
-        expectedRequestConfig2,
-      )
-      expect(requester.request).toBeCalledTimes(2)
-
-      expect(log).toHaveBeenNthCalledWith(
-        1,
-        `Generated HTTP request queue key: "${expectedRequestKey1}"`,
-      )
-      expect(log).toHaveBeenNthCalledWith(
-        2,
-        `Generated HTTP request queue key: "${expectedRequestKey2}"`,
-      )
-      expect(log).toBeCalledTimes(2)
-      log.mockClear()
+      expect(connectionGetAccountInfo).toHaveBeenNthCalledWith(1, createFakePublicKey(address1))
+      expect(connectionGetAccountInfo).toHaveBeenNthCalledWith(2, createFakePublicKey(address2))
+      expect(connectionGetAccountInfo).toBeCalledTimes(2)
     })
 
     it('should wait for the first group of RPCs to finish before sending the next group', async () => {
-      const [balance1, resolveLines1] = deferredPromise<string>()
-      const [balance2, resolveLines2] = deferredPromise<string>()
-      const [balance3, resolveLines3] = deferredPromise<string>()
-      const [balance4, resolveLines4] = deferredPromise<string>()
+      const [balance1, resolveLines1] = deferredPromise<number>()
+      const [balance2, resolveLines2] = deferredPromise<number>()
+      const [balance3, resolveLines3] = deferredPromise<number>()
+      const [balance4, resolveLines4] = deferredPromise<number>()
 
       const address1 = 'r101'
       const address2 = 'r102'
@@ -372,93 +296,63 @@ describe('XrpTransport', () => {
       await jest.runAllTimersAsync()
 
       // Only 3 of the 4 requests were made because GROUP_SIZE is 3
-      expect(requester.request).toBeCalledTimes(3)
+      expect(connectionGetAccountInfo).toBeCalledTimes(3)
 
-      resolveLines1('101.0')
-      resolveLines2('102.0')
-
-      await jest.runAllTimersAsync()
-      expect(requester.request).toBeCalledTimes(3)
-
-      resolveLines3('103.0')
+      resolveLines1(101.0)
+      resolveLines2(102.0)
 
       await jest.runAllTimersAsync()
-      expect(requester.request).toBeCalledTimes(4)
+      expect(connectionGetAccountInfo).toBeCalledTimes(3)
 
-      resolveLines4('104.0')
+      resolveLines3(103.0)
+
+      await jest.runAllTimersAsync()
+      expect(connectionGetAccountInfo).toBeCalledTimes(4)
+
+      resolveLines4(104.0)
 
       expect(await balancePromise).toEqual([
-        { address: address1, balance: '101.0' },
-        { address: address2, balance: '102.0' },
-        { address: address3, balance: '103.0' },
-        { address: address4, balance: '104.0' },
+        { address: address1, balance: '101' },
+        { address: address2, balance: '102' },
+        { address: address3, balance: '103' },
+        { address: address4, balance: '104' },
       ])
-
-      expect(log).toHaveBeenNthCalledWith(
-        1,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toHaveBeenNthCalledWith(
-        2,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toHaveBeenNthCalledWith(
-        3,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toHaveBeenNthCalledWith(
-        4,
-        expect.stringContaining('Generated HTTP request queue key:'),
-      )
-      expect(log).toBeCalledTimes(4)
-      log.mockClear()
     })
   })
 
   describe('getTokenBalance', () => {
     const address = 'r123'
 
-    const expectedRequestConfig = requestConfigForAddresses({
-      address,
-    })
-
-    const requestKey = requestKeyForConfig(expectedRequestConfig)
-
-    const expectRequesterRequest = () => {
-      expect(requester.request).toBeCalledWith(requestKey, expectedRequestConfig)
-      expect(requester.request).toBeCalledTimes(1)
-
-      expect(log).toBeCalledWith(`Generated HTTP request queue key: "${requestKey}"`)
-      expect(log).toBeCalledTimes(1)
-      log.mockClear()
-    }
-
     it('should return the token balance', async () => {
-      const expectedBalance = '1234.56'
+      const expectedBalance = 1234.56
 
       mockAccountInfo(expectedBalance)
 
       const balance = await transport.getTokenBalance(address)
 
       expect(balance).toEqual(expectedBalance)
-      expectRequesterRequest()
+      expect(connectionGetAccountInfo).toBeCalledWith(createFakePublicKey(address))
+      expect(connectionGetAccountInfo).toBeCalledTimes(1)
     })
 
-    it('should throw if XRPL_RPC_URL is missing', async () => {
-      transport = new XrpTransport()
+    it('should throw if SOLANA_RPC_URL is missing', async () => {
+      transport = new SolanaBalanceTransport()
       await transport.initialize(
         dependencies,
         {
           ...adapterSettings,
-          XRPL_RPC_URL: '',
+          SOLANA_RPC_URL: '',
         },
         endpointName,
         transportName,
       )
 
       await expect(() => transport.getTokenBalance(address)).rejects.toThrow(
-        'Environment variable XRPL_RPC_URL is missing',
+        'Environment variable SOLANA_RPC_URL is missing',
       )
+      expect(log).toBeCalledWith('SOLANA_RPC_URL is missing')
+      expect(log).toBeCalledTimes(1)
+      log.mockClear()
     })
   })
 })
