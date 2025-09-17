8b0ae0213eabc45f618e68a9e593275de53839ca
David de Kloet <david.dekloet@smartcontract.com>
2025-09-17 10:23:50 +0200

    New totalBalance endpoint boilerplate

---
+++ packages/sources/avalanche-platform/src/endpoint/totalBalance.ts
@@ -0,0 +1,43 @@
+import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
+import { InputParameters } from '@chainlink/external-adapter-framework/validation'
+import { config } from '../config'
+import overrides from '../config/overrides.json'
+import { customSubscriptionTransport } from '../transport/totalBalance'
+
+export const inputParameters = new InputParameters(
+  {
+    base: {
+      aliases: ['from', 'coin', 'symbol', 'market'],
+      required: true,
+      type: 'string',
+      description: 'The symbol of symbols of the currency to query',
+    },
+    quote: {
+      aliases: ['to', 'convert'],
+      required: true,
+      type: 'string',
+      description: 'The symbol of the currency to convert to',
+    },
+  },
+  [
+    {
+      base: 'BTC',
+      quote: 'USD',
+    },
+  ],
+)
+
+export type BaseEndpointTypes = {
+  Parameters: typeof inputParameters.definition
+  Response: SingleNumberResultResponse
+  Settings: typeof config.settings
+}
+
+export const endpoint = new AdapterEndpoint({
+  name: 'totalBalance',
+  aliases: [],
+  transport: customSubscriptionTransport,
+  inputParameters,
+  overrides: overrides['avalanche-platform'],
+})
---
+++ packages/sources/avalanche-platform/src/transport/totalBalance.ts
@@ -0,0 +1,88 @@
+import { EndpointContext } from '@chainlink/external-adapter-framework/adapter'
+import { ResponseCache } from '@chainlink/external-adapter-framework/cache/response'
+import { TransportDependencies } from '@chainlink/external-adapter-framework/transports'
+import { SubscriptionTransport } from '@chainlink/external-adapter-framework/transports/abstract/subscription'
+import { AdapterResponse, makeLogger, sleep } from '@chainlink/external-adapter-framework/util'
+import { Requester } from '@chainlink/external-adapter-framework/util/requester'
+import { AdapterError } from '@chainlink/external-adapter-framework/validation/error'
+import { BaseEndpointTypes, inputParameters } from '../endpoint/totalBalance'
+
+const logger = makeLogger('CustomTransport')
+
+type RequestParams = typeof inputParameters.validated
+
+export type CustomTransportTypes = BaseEndpointTypes & {
+  Provider: {
+    RequestBody: never
+    ResponseBody: any
+  }
+}
+export class CustomTransport extends SubscriptionTransport<CustomTransportTypes> {
+  name!: string
+  responseCache!: ResponseCache<CustomTransportTypes>
+  requester!: Requester
+
+  async initialize(
+    dependencies: TransportDependencies<CustomTransportTypes>,
+    adapterSettings: CustomTransportTypes['Settings'],
+    endpointName: string,
+    transportName: string,
+  ): Promise<void> {
+    await super.initialize(dependencies, adapterSettings, endpointName, transportName)
+    this.requester = dependencies.requester
+  }
+  async backgroundHandler(
+    context: EndpointContext<CustomTransportTypes>,
+    entries: RequestParams[],
+  ) {
+    await Promise.all(entries.map(async (param) => this.handleRequest(param)))
+    await sleep(context.adapterSettings.BACKGROUND_EXECUTE_MS)
+  }
+
+  async handleRequest(param: RequestParams) {
+    let response: AdapterResponse<CustomTransportTypes['Response']>
+    try {
+      response = await this._handleRequest(param)
+    } catch (e) {
+      const errorMessage = e instanceof Error ? e.message : 'Unknown error occurred'
+      logger.error(e, errorMessage)
+      response = {
+        statusCode: (e as AdapterError)?.statusCode || 502,
+        errorMessage,
+        timestamps: {
+          providerDataRequestedUnixMs: 0,
+          providerDataReceivedUnixMs: 0,
+          providerIndicatedTimeUnixMs: undefined,
+        },
+      }
+    }
+    await this.responseCache.write(this.name, [{ params: param, response }])
+  }
+
+  async _handleRequest(
+    _: RequestParams,
+  ): Promise<AdapterResponse<CustomTransportTypes['Response']>> {
+    const providerDataRequestedUnixMs = Date.now()
+
+    // custom transport logic
+
+    return {
+      data: {
+        result: 2000,
+      },
+      statusCode: 200,
+      result: 2000,
+      timestamps: {
+        providerDataRequestedUnixMs,
+        providerDataReceivedUnixMs: Date.now(),
+        providerIndicatedTimeUnixMs: undefined,
+      },
+    }
+  }
+
+  getSubscriptionTtlFromConfig(adapterSettings: CustomTransportTypes['Settings']): number {
+    return adapterSettings.WARMUP_SUBSCRIPTION_TTL
+  }
+}
+
+export const customSubscriptionTransport = new CustomTransport()
