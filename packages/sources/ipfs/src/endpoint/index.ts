0084b5bc37c1f651e93140ae67c16d434c7fc553
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-10-04 01:34:03 +0300

    Delete IPFS EA (#4066)
    * Deletes IPFS EA

--- packages/sources/ipfs/src/endpoint/index.ts
+++
@@ -1,7 +0,0 @@
-import type { TInputParameters as ReadInputParameters } from './read'
-import type { TInputParameters as WriteInputParameters } from './write'
-
-export type TInputParameters = ReadInputParameters | WriteInputParameters
-
-export * as read from './read'
-export * as write from './write'
