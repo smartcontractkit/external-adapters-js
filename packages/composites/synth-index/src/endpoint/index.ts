63f7d3241a8a16124727153d7616c3ff61eff7c8
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-10-04 02:00:49 +0300

    Delete synth-index EA (#4067)
    * Deletes Synth-index EA

--- packages/composites/synth-index/src/endpoint/index.ts
+++
@@ -1,5 +0,0 @@
-import * as value from './value'
-
-export type TInputParameters = value.TInputParameters
-
-export * as value from './value'
--- packages/composites/synth-index/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { expose } from '@chainlink/ea-bootstrap'
-import { makeExecute } from './adapter'
-import { NAME, makeConfig } from './config'
-
-const adapterContext = { name: NAME }
-
-const { server } = expose(adapterContext, makeExecute())
-export { NAME, makeConfig, makeExecute, server }
