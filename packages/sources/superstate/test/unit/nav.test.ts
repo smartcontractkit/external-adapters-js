704b111c1e280def9190c86abeb1d192af7eb8a9
Michael Xiao <michael.xiao@smartcontract.com>
2025-10-03 10:04:01 -0400

    Add tests

--- packages/sources/superstate/test/unit/nav.test.ts
+++ packages/sources/superstate/test/unit/nav.test.ts
@@ -1,34 +1,25 @@
 import { LoggerFactoryProvider } from '@chainlink/external-adapter-framework/util'
 import { makeStub } from '@chainlink/external-adapter-framework/util/testing-utils'
 
-jest.mock('../../src/transport/utils', () => ({
-  ...jest.requireActual('../../src/transport/utils'),
-  isInTimeRange: jest.fn(() => true),
-}))
-
 import { NavTransport } from '../../src/transport/nav'
 
 LoggerFactoryProvider.set()
 
 describe('navTransport', () => {
-  beforeEach(() => {
-    jest.useFakeTimers()
-  })
-
   afterEach(() => {
     jest.useRealTimers()
   })
 
   it('runScheduler should trigger periodically', async () => {
+    jest.useFakeTimers({ now: new Date('2024-01-01T10:00:00.000-05:00') }) // 9:08 <-> 12:00
+
     const transport = new NavTransport()
 
     const mockExecute = jest.spyOn(transport, 'execute').mockResolvedValue(undefined)
-
     const mockDependencies = makeStub('dependencies', {
       responseCache: { write: jest.fn() },
       requester: { request: jest.fn() },
     } as any)
-
     const mockSettings = makeStub('settings', { NAV_CRON_INTERVAL_MIN: 1 } as any)
 
     await transport.initialize(mockDependencies, mockSettings, 'nav', 'test-transport')
@@ -39,8 +30,29 @@ describe('navTransport', () => {
 
     jest.advanceTimersByTime(60 * 1000)
     expect(mockExecute).toHaveBeenCalledTimes(1)
-
     jest.advanceTimersByTime(60 * 1000)
     expect(mockExecute).toHaveBeenCalledTimes(2)
+  })
+
+  it('runScheduler should not trigger outside hours', async () => {
+    jest.useFakeTimers({ now: new Date('2024-01-01T15:00:00.000-05:00') }) // 9:08 <-> 12:00
+
+    const transport = new NavTransport()
+
+    const mockExecute = jest.spyOn(transport, 'execute').mockResolvedValue(undefined)
+    const mockDependencies = makeStub('dependencies', {
+      responseCache: { write: jest.fn() },
+      requester: { request: jest.fn() },
+    } as any)
+    const mockSettings = makeStub('settings', { NAV_CRON_INTERVAL_MIN: 1 } as any)
+
+    await transport.initialize(mockDependencies, mockSettings, 'nav', 'test-transport')
+
+    await transport.registerRequest({
+      requestContext: { data: { fundId: 123, reportValue: 'nav' } },
+    } as any)
+
+    jest.advanceTimersByTime(60 * 1000)
+    expect(mockExecute).toHaveBeenCalledTimes(0)
   })
 })
