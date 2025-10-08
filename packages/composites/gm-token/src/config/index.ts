11c143f90cd0b4e3a207f8457868951a82aa895c
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-10-08 03:09:56 +0300

    Refactors GM-token to support Botanix and dynamic fetching of tokens (#4077)
    * Refactors to add Botanix chain and dynamic fetching of tokens

--- packages/composites/gm-token/src/config/index.ts
+++ packages/composites/gm-token/src/config/index.ts
@@ -14,17 +14,56 @@ export const config = new AdapterConfig(
       required: true,
       default: 42161,
     },
+    BOTANIX_RPC_URL: {
+      description: 'RPC url of Botanix node',
+      type: 'string',
+    },
+    BOTANIX_CHAIN_ID: {
+      description: 'The chain id to connect to',
+      type: 'number',
+      default: 3637,
+    },
     DATASTORE_CONTRACT_ADDRESS: {
       description: 'Address of Data Store contract',
       type: 'string',
       required: true,
       default: '0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8',
     },
+    BOTANIX_DATASTORE_CONTRACT_ADDRESS: {
+      description: 'Address of Data Store contract',
+      type: 'string',
+      required: true,
+      default: '0xA23B81a89Ab9D7D89fF8fc1b5d8508fB75Cc094d',
+    },
     READER_CONTRACT_ADDRESS: {
       description: 'Address of Reader contract',
       type: 'string',
       required: true,
       default: '0xf60becbba223EEA9495Da3f606753867eC10d139',
+    },
+    BOTANIX_READER_CONTRACT_ADDRESS: {
+      description: 'Address of Reader contract',
+      type: 'string',
+      required: true,
+      default: '0xa254B60cbB85a92F6151B10E1233639F601f2F0F',
+    },
+    ARBITRUM_TOKENS_INFO_URL: {
+      description: 'URL to token meta data supported by GMX on Arbitrum',
+      type: 'string',
+      required: true,
+      default: 'https://arbitrum-api.gmxinfra.io/tokens',
+    },
+    BOTANIX_TOKENS_INFO_URL: {
+      description: 'URL to token meta data supported by GMX on Botanix',
+      type: 'string',
+      required: true,
+      default: 'https://botanix-api.gmxinfra.io/tokens',
+    },
+    GMX_TOKENS_CACHE_MS: {
+      description: 'TTL in milliseconds for GMX tokens cache',
+      type: 'number',
+      required: true,
+      default: 300_000,
     },
     PNL_FACTOR_TYPE: {
       description:
