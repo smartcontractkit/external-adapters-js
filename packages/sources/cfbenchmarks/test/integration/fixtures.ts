a77b4f8ae534effa98dcc4ff0ec200c21f06dfdc
Geert <117188496+cll-gg@users.noreply.github.com>
2025-10-15 13:43:45 +0100

    DF-21713 Remove unused rest transport for cfbenchmarks (#4102)

--- packages/sources/cfbenchmarks/test/integration/fixtures.ts
+++ packages/sources/cfbenchmarks/test/integration/fixtures.ts
@@ -1,17 +1,4 @@
 import { MockWebsocketServer } from '@chainlink/external-adapter-framework/util/testing-utils'
-import nock from 'nock'
-
-export const mockResponseSuccess = (): nock.Scope =>
-  nock('https://www.cfbenchmarks.com/api')
-    .get('/v1/values?id=BRTI')
-    .reply(200, {
-      serverTime: '2022-02-18T16:53:55.772Z',
-      payload: [
-        { value: '39829.42', time: 1645199636000 },
-        { value: '39829.30', time: 1645199637000 },
-      ],
-    })
-    .persist()
 
 export const mockWebSocketServer = (URL: string): MockWebsocketServer => {
   const mockWsServer = new MockWebsocketServer(URL, { mock: false })
