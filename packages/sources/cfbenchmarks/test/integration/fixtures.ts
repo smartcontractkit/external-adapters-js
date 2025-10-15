e9fc26796bfae82b9c6ca000ef14db2c2b7cc0f8
Geert G <117188496+cll-gg@users.noreply.github.com>
2025-10-15 12:27:21 +0100

    Also remove `mockResponseSuccess` fixture

--- packages/sources/cfbenchmarks/test/integration/fixtures.ts
+++ packages/sources/cfbenchmarks/test/integration/fixtures.ts
@@ -1,17 +1,5 @@
-import nock from 'nock'
 import { MockWebsocketServer } from '@chainlink/external-adapter-framework/util/testing-utils'
-
-export const mockResponseSuccess = (): nock.Scope =>
-  nock('https://www.cfbenchmarks.com/api')
-    .get('/v1/values?id=BRTI')
-    .reply(200, {
-      serverTime: '2022-02-18T16:53:55.772Z',
-      payload: [
-        { value: '39829.42', time: 1645199636000 },
-        { value: '39829.30', time: 1645199637000 },
-      ],
-    })
-    .persist()
+import nock from 'nock'
 
 export const mockBircResponseSuccess = (): nock.Scope => {
   const currentDayIsoString = new Date().toISOString()
