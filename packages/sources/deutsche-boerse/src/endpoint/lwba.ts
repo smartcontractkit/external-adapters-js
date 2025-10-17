46524fd803d2a644235a756ab2e14208205135b8
chray-zhang <chray.zhang@smartcontract.com>
2025-10-17 12:48:04 -0400

    removed timestamps

--- packages/sources/deutsche-boerse/src/endpoint/lwba.ts
+++ packages/sources/deutsche-boerse/src/endpoint/lwba.ts
@@ -45,8 +45,6 @@ export interface LwbaLatestPriceResponse {
   Result: number | null
   Data: {
     latestPrice: number
-    quoteProviderIndicatedTimeUnixMs: number
-    tradeProviderIndicatedTimeUnixMs: number
   }
 }
 
