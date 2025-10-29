eddced51b0767123ecf9ef300dd4890db2daf1b3
Subarna-Singh <subarnasingh11@gmail.com>
2025-10-29 17:26:20 +0530

    FIX: incorrect testcase

--- packages/sources/view-function-multi-chain/test/integration/adapter.test.ts
+++ packages/sources/view-function-multi-chain/test/integration/adapter.test.ts
@@ -62,7 +62,7 @@ describe('execute', () => {
       expect(response.json()).toMatchSnapshot()
     })
 
-    it('should return success with additional data requests ', async () => {
+    it.only('should return success with additional data requests ', async () => {
       const data = {
         contract: '0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c',
         function: 'function latestAnswer() external view returns (int256)',
