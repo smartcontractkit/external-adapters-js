c3dc2af5125e6996a8ef889a8e84d062bd2171c2
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2025-10-31 02:40:06 +0400

    Adds input validation for price endpoint (#4173)

--- packages/sources/deutsche-boerse/src/endpoint/price.ts
+++ packages/sources/deutsche-boerse/src/endpoint/price.ts
@@ -1,7 +1,34 @@
 import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
+import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
 import { priceProtobufWsTransport } from '../transport/price'
-import { inputParameters } from './lwba'
+import { MARKET_EUREX_MICRO, MARKET_TRADEGATE, MARKET_XETRA_ETFETP } from './lwba'
+
+export const MARKETS = [MARKET_XETRA_ETFETP, MARKET_TRADEGATE, MARKET_EUREX_MICRO] as const
+
+export const inputParameters = new InputParameters(
+  {
+    isin: {
+      aliases: ['instrument', 'ISIN'],
+      required: true,
+      type: 'string',
+      description: 'The ISIN identifier of the instrument to query',
+    },
+    market: {
+      aliases: ['stream'],
+      required: true,
+      type: 'string',
+      description: 'The market identifier of the stream to query',
+      options: [...MARKETS],
+    },
+  },
+  [
+    {
+      market: 'md-xetraetfetp',
+      isin: 'IE00B53L3W79',
+    },
+  ],
+)
 
 export interface PriceResponse {
   Result: number | null
