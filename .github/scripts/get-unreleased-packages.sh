4198824587ff9044d61fa08f2d0df0f9a05af2e6
Michael Xiao <michael.xiao@smartcontract.com>
2025-11-07 12:32:09 -0500

    Rename

--- .github/scripts/get-unreleased-packages.sh
+++ .github/scripts/get-unreleased-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Return a list of EA that is pending release
+# Return a list of packages that is pending release
 git grep -Eh "^'@chainlink/[^']*': (major|minor|patch)$" .changeset |
   sed -E "s#^'(@chainlink/[^']*)': (major|minor|patch).*\$#- \\1#" |
   sort -u
