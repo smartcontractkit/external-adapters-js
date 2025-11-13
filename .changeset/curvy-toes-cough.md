02675878a35abc6c0cd85f41942b17525612f73e
David de Kloet <david.dekloet@smartcontract.com>
2025-11-13 21:36:22 +0100

    Remove additionalRequests parameter from function endpoint

---
+++ .changeset/curvy-toes-cough.md
@@ -0,0 +1,5 @@
+---
+'@chainlink/view-function-multi-chain-adapter': patch
+---
+
+Remove unused additionalRequests parameter from function endpoint
--- packages/sources/view-function-multi-chain/test/integration/adapter.test.ts
+++ packages/sources/view-function-multi-chain/test/integration/adapter.test.ts
@@ -62,42 +62,6 @@ describe('execute', () => {
       expect(response.json()).toMatchSnapshot()
     })
 
-    it('should return success with additional data requests ', async () => {
-      const data = {
-        contract: '0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c',
-        function: 'function latestAnswer() external view returns (int256)',
-        network: 'ethereum_mainnet',
-        additionalRequests: [
-          {
-            name: 'decimals',
-            signature: 'function decimals() view returns (uint8)',
-          },
-        ],
-      }
-      mockETHMainnetContractCallResponseSuccess()
-      const response = await testAdapter.request(data)
-      expect(response.statusCode).toBe(200)
-      expect(response.json()).toMatchSnapshot()
-    })
-
-    it('should fail additional data requests in case of missing signature', async () => {
-      const data = {
-        contract: '0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c',
-        function: 'function latestAnswer() external view returns (int256)',
-        network: 'ethereum_mainnet',
-        additionalRequests: [
-          {
-            name: 'decimals',
-            signature: '',
-          },
-        ],
-      }
-      mockETHMainnetContractCallResponseSuccess()
-      const response = await testAdapter.request(data)
-      expect(response.statusCode).toBe(502)
-      expect(response.json()).toMatchSnapshot()
-    })
-
     it('should return success for different network', async () => {
       const data = {
         contract: '0x779877a7b0d9e8603169ddbd7836e478b4624789',
