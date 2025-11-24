303b5eb47624c330ae40ef96e02d7c6e7f4cf8b9
Eric Fornaciari <eric.fornaciari@smartcontract.com>
2025-11-24 13:55:33 -0800

    DF-22512: Adds tooling to generate EA fields to a CSV for review

---
+++ packages/scripts/src/generate-adapter-field-csv/index.ts
@@ -0,0 +1,160 @@
+import commandLineArgs from 'command-line-args'
+import commandLineUsage from 'command-line-usage'
+import { writeFileSync } from 'fs'
+import { Blacklist, BooleanMap } from '../shared/docGenTypes'
+import { getJsonFile } from '../shared/docGenUtils'
+import { getWorkspaceAdapters, getWorkspacePackages } from '../workspace'
+import { AdapterFieldGenerator } from './generator'
+
+const pathToBlacklist = 'packages/scripts/src/generate-readme/readmeBlacklist.json'
+
+const findTypeAndName = new RegExp(
+  // For example, packages/(sources)/(coinbase)
+  /packages\/(sources|composites|examples|targets|non-deployable)\/(.*)/,
+)
+export async function main(): Promise<void | string> {
+  try {
+    // Define CLI options
+    const commandLineOptions = [
+      {
+        name: 'all',
+        alias: 'a',
+        type: Boolean,
+        description: 'Generate READMEs for all source EAs not in blacklist',
+      },
+      {
+        name: 'verbose',
+        alias: 'v',
+        type: Boolean,
+        description: 'Include extra logs for each generation process',
+      },
+      {
+        name: 'testPath',
+        alias: 't',
+        type: String,
+        description: 'Run script as test for EA along given path',
+      },
+      {
+        name: 'adapters',
+        multiple: true,
+        defaultOption: true,
+        description: 'Generate READMEs for all source EAs given by name',
+      },
+      { name: 'help', alias: 'h', type: Boolean, description: 'Display usage guide' },
+    ]
+    const options = commandLineArgs(commandLineOptions)
+
+    // Generate usage guide
+    if (options.help) {
+      const usage = commandLineUsage([
+        {
+          header: 'README Generator Script',
+          content:
+            'This script is run from the root of the external-adapter-js/ repo to generate READMEs for supported external adapters. This functionality is currently limited to a subset of source adapters only.',
+        },
+        {
+          header: 'Options',
+          optionList: commandLineOptions,
+        },
+        {
+          content:
+            'Source code: {underline https://github.com/smartcontractkit/external-adapters-\njs/packages/scripts/src/generate-readme/}',
+        },
+      ])
+      console.log(usage)
+      return
+    }
+
+    const shouldBuildAll =
+      options.all ||
+      getWorkspacePackages(process.env['UPSTREAM_BRANCH']).find(
+        (p) => p.type === 'core' || p.type === 'scripts',
+      )
+
+    let adapters = shouldBuildAll
+      ? getWorkspaceAdapters()
+      : getWorkspaceAdapters([], process.env['UPSTREAM_BRANCH'])
+
+    // Test setting
+    if (options.testPath) {
+      const adapter = adapters.find((a) => a.location === options.testPath)
+      if (!adapter) {
+        console.error(`Adapter at ${options.testPath} was not found`)
+        return
+      }
+      const generator = new AdapterFieldGenerator(adapter, options.verbose)
+      await generator.loadAdapterContent()
+      const csv = generator.buildCSV().join('\n')
+      writeFileSync(`${adapter.descopedName}.csv`, csv)
+      return
+    }
+
+    options.verbose &&
+      console.log(
+        `Adapters being considered for readme generation: `,
+        adapters.map((a) => `${a.name}: ${a.location}`),
+      )
+
+    const initialAdapterLength = adapters.length
+
+    // If specific adapters are passed to the command line, only select those
+    if (options.adapters?.length) {
+      options.verbose &&
+        console.log(`Reducing list of adapters to ones specified on the command line`)
+      adapters = adapters.filter((p) => {
+        return (
+          (options.adapters as string[]).includes(p.descopedName) || // p.descopedName example: "coinbase-adapter"
+          (options.adapters as string[]).includes(p.descopedName.replace(/-adapter$/, '')) // "coinbase" (without "-adapter")
+        )
+      })
+    }
+
+    const blacklist = (getJsonFile(pathToBlacklist) as Blacklist).blacklist
+    const adapterInBlacklist = blacklist.reduce((map: BooleanMap, a) => {
+      const adapterName = `@chainlink/${a}-adapter`
+      map[adapterName] = true
+      return map
+    }, {})
+    options.verbose && console.log(`Removing blacklisted and non-source adapters from the list`)
+    adapters = adapters
+      .filter((a) => !adapterInBlacklist[a.name]) // Remove blacklisted adapters
+      .filter((p) => p.type === 'sources' || p.type === 'composites') // Remove non-source and non-composite adapters
+
+    options.verbose &&
+      console.log(`Filtered ${initialAdapterLength - adapters.length} adapters from the list`) // Verbose because this message is confusing if you're not familiar with generate-readme
+    console.log(
+      'Generating README(s) for the following adapters: ',
+      adapters.map((a) => a.name),
+    )
+
+    // Collect CSVs from each generator
+    const csvQueue = await Promise.all(
+      adapters
+        .map(async (adapter) => {
+          const generator = new AdapterFieldGenerator(adapter, options.verbose)
+          await generator.loadAdapterContent()
+          return generator
+        })
+        .filter(async (generator) => {
+          return (await generator).frameworkVersion === 'v3'
+        })
+        .map(async (generator) => {
+          return (await generator).buildCSV().join('\n')
+        }),
+    )
+
+    // Combine all CSVs into one
+    const combinedCSV = csvQueue.join('\n')
+
+    // Save the combined CSV to a file
+    writeFileSync('combined_adapters.csv', combinedCSV)
+    console.log(`Combined CSV generated with ${csvQueue.length} adapters.`)
+
+    process.exit(0)
+  } catch (error) {
+    console.error({ error: error.message, stack: error.stack })
+    process.exit(1)
+  }
+}
+
+main()
