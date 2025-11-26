e4b22b6025a39bb89e040c685a2f457ce5e149d2
Michael Xiao <michael.xiao@smartcontract.com>
2025-11-26 16:37:46 -0500

    Improve finnhub resources

--- packages/sources/finnhub/src/transport/stock-quotes.ts
+++ packages/sources/finnhub/src/transport/stock-quotes.ts
@@ -46,49 +46,51 @@ export const httpTransport = new HttpTransport<HttpTransportTypes>({
       })
     }
 
-    return res.data.map((data) => {
-      if (
-        data.a < 0 ||
-        data.av < 0 ||
-        data.b < 0 ||
-        data.bv < 0 ||
-        data.t == 0 ||
-        (data.av == 0 && data.bv == 0)
-      ) {
+    return res.data
+      .filter((d) => params.some((p) => p.base === d.s))
+      .map((data) => {
+        if (
+          data.a < 0 ||
+          data.av < 0 ||
+          data.b < 0 ||
+          data.bv < 0 ||
+          data.t == 0 ||
+          (data.av == 0 && data.bv == 0)
+        ) {
+          return {
+            params: { base: data.s },
+            response: {
+              statusCode: 502,
+              errorMessage: `In-valid data ${JSON.stringify(data)} received for ${data.s}`,
+            },
+          }
+        }
+
+        let midPrice: number
+        if (data.b == 0) {
+          midPrice = data.a
+        } else if (data.a == 0) {
+          midPrice = data.b
+        } else {
+          midPrice = (data.b * data.bv + data.a * data.av) / (data.bv + data.av)
+        }
+
         return {
           params: { base: data.s },
           response: {
-            statusCode: 502,
-            errorMessage: `In-valid data ${JSON.stringify(data)} received for ${data.s}`,
+            result: null,
+            data: {
+              mid_price: midPrice,
+              bid_price: data.b,
+              bid_volume: data.bv,
+              ask_price: data.a,
+              ask_volume: data.av,
+            },
+            timestamps: {
+              providerIndicatedTimeUnixMs: data.t,
+            },
           },
         }
-      }
-
-      let midPrice: number
-      if (data.b == 0) {
-        midPrice = data.a
-      } else if (data.a == 0) {
-        midPrice = data.b
-      } else {
-        midPrice = (data.b * data.bv + data.a * data.av) / (data.bv + data.av)
-      }
-
-      return {
-        params: { base: data.s },
-        response: {
-          result: null,
-          data: {
-            mid_price: midPrice,
-            bid_price: data.b,
-            bid_volume: data.bv,
-            ask_price: data.a,
-            ask_volume: data.av,
-          },
-          timestamps: {
-            providerIndicatedTimeUnixMs: data.t,
-          },
-        },
-      }
-    })
+      })
   },
 })
