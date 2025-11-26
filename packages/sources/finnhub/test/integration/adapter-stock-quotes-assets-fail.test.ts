7b916bf6ab3b69daa0dbb5e043265cc0fd1a262b
Michael Xiao <michael.xiao@smartcontract.com>
2025-11-26 14:21:05 -0500

    Add stock quotes to Finnhub (#4322)
    * Add stock quotes to Finnhub
    
    * Rename tests
    
    * Exclude invalid quotes

---
+++ packages/sources/finnhub/test/integration/adapter-stock-quotes-assets-fail.test.ts
@@ -0,0 +1,46 @@
+import {
+  TestAdapter,
+  setEnvVariables,
+} from '@chainlink/external-adapter-framework/util/testing-utils'
+import * as nock from 'nock'
+import { mockStockQuoteResponseFailure } from './fixtures'
+
+describe('rest', () => {
+  let spy: jest.SpyInstance
+  let testAdapter: TestAdapter
+  let oldEnv: NodeJS.ProcessEnv
+
+  beforeAll(async () => {
+    oldEnv = JSON.parse(JSON.stringify(process.env))
+    process.env['API_KEY'] = 'fake-api-key'
+    const mockDate = new Date('2022-01-01T11:11:11.111Z')
+    spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
+
+    const adapter = (await import('../../src')).adapter
+    adapter.rateLimiting = undefined
+    testAdapter = await TestAdapter.startWithMockedCache(adapter, {
+      testAdapter: {} as TestAdapter<never>,
+    })
+  })
+
+  afterAll(async () => {
+    setEnvVariables(oldEnv)
+    await testAdapter.api.close()
+    nock.restore()
+    nock.cleanAll()
+    spy.mockRestore()
+  })
+
+  describe('stock quote endpoint - failure', () => {
+    it('should return error for no data returned', async () => {
+      const data = {
+        endpoint: 'stock_quotes',
+        base: 'MISSING_DATA',
+      }
+      mockStockQuoteResponseFailure()
+      const response = await testAdapter.request(data)
+      expect(response.statusCode).toBe(502)
+      expect(response.json()).toMatchSnapshot()
+    })
+  })
+})
