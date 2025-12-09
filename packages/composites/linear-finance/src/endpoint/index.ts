faf8b3ae094fbd1256d1e3f37321a491e544a169
David de Kloet <david.dekloet@smartcontract.com>
2025-12-09 14:31:07 +0100

    git rm -r packages/composites/linear-finance

--- packages/composites/linear-finance/src/endpoint/index.ts
+++
@@ -1,7 +0,0 @@
-import * as allocations from './allocations'
-import * as prices from './prices'
-
-export type TInputParameters = allocations.TInputParameters | prices.TInputParameters
-
-export * as allocations from './allocations'
-export * as prices from './prices'
