2a3c56beda10098cb48587cfc58f50fb7ce97773
Michael Xiao <michael.xiao@smartcontract.com>
2025-12-10 09:45:25 -0500

    Unit test

---
+++ packages/sources/dxfeed/test/unit/ws.test.ts
@@ -0,0 +1,34 @@
+import { BaseEndpointTypes } from '../../src/endpoint/stock-quotes'
+import { buildWsTransport } from '../../src/transport/ws'
+
+describe('DxFeed WebSocket Transport', () => {
+  let mockConnection: { send: jest.Mock }
+  let transport: ReturnType<typeof buildWsTransport<BaseEndpointTypes>>
+
+  beforeEach(() => {
+    mockConnection = { send: jest.fn() }
+
+    transport = buildWsTransport<BaseEndpointTypes>(
+      (params) => [{ Quote: [params.base.toUpperCase()] }],
+      () => [],
+    )
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('heartbeat handler', () => {
+    it('should send heartbeat message when called', () => {
+      ;(transport as any).connectionClientId = 'test-client-id'
+      ;(transport as any).config.handlers.heartbeat(mockConnection)
+
+      expect(mockConnection.send).toHaveBeenCalledTimes(1)
+
+      const sentMessage = mockConnection.send.mock.calls[0][0]
+      expect(sentMessage).toEqual(
+        '[{"id":1,"clientId":"test-client-id","channel":"/meta/connect","connectionType":"websocket"}]',
+      )
+    })
+  })
+})
