4f11f09a419359aba4864ca4764f30f0e42256a8
David de Kloet <david.dekloet@smartcontract.com>
2026-01-07 11:52:14 +0100

    Delete linkpool EA (#4495)
    * git rm -r packages/sources/linkpool
    
    * Manual changes
    
    * yarn
    
    * changeset

--- .pnp.cjs
+++ .pnp.cjs
@@ -535,10 +535,6 @@ const RAW_RUNTIME_STATE =
       "reference": "workspace:packages/sources/lido-por"\
     },\
     {\
-      "name": "@chainlink/linkpool-adapter",\
-      "reference": "workspace:packages/sources/linkpool"\
-    },\
-    {\
       "name": "@chainlink/liveart-adapter",\
       "reference": "workspace:packages/sources/liveart"\
     },\
@@ -927,7 +923,6 @@ const RAW_RUNTIME_STATE =
     ["@chainlink/lcx-adapter", ["workspace:packages/sources/lcx"]],\
     ["@chainlink/lido-adapter", ["workspace:packages/sources/lido"]],\
     ["@chainlink/lido-por-adapter", ["workspace:packages/sources/lido-por"]],\
-    ["@chainlink/linkpool-adapter", ["workspace:packages/sources/linkpool"]],\
     ["@chainlink/liveart-adapter", ["workspace:packages/sources/liveart"]],\
     ["@chainlink/llama-guard-adapter", ["workspace:packages/composites/llama-guard"]],\
     ["@chainlink/lotus-adapter", ["workspace:packages/sources/lotus"]],\
@@ -7165,21 +7160,6 @@ const RAW_RUNTIME_STATE =
           ["ethers", "npm:5.8.0"],\
           ["nock", "npm:13.5.6"],\
           ["tslib", "npm:2.4.1"],\
-          ["typescript", "patch:typescript@npm%3A5.8.3#optional!builtin<compat/typescript>::version=5.8.3&hash=5786d5"]\
-        ],\
-        "linkType": "SOFT"\
-      }]\
-    ]],\
-    ["@chainlink/linkpool-adapter", [\
-      ["workspace:packages/sources/linkpool", {\
-        "packageLocation": "./packages/sources/linkpool/",\
-        "packageDependencies": [\
-          ["@chainlink/linkpool-adapter", "workspace:packages/sources/linkpool"],\
-          ["@chainlink/ea-bootstrap", "workspace:packages/core/bootstrap"],\
-          ["@chainlink/ea-test-helpers", "workspace:packages/core/test-helpers"],\
-          ["@types/jest", "npm:29.5.14"],\
-          ["@types/node", "npm:22.14.1"],\
-          ["tslib", "npm:2.8.1"],\
           ["typescript", "patch:typescript@npm%3A5.8.3#optional!builtin<compat/typescript>::version=5.8.3&hash=5786d5"]\
         ],\
         "linkType": "SOFT"\
