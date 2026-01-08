53bfb4e65dde1572e6a801bdbb78a9d9311f202d
Matthew McAllister <matthew.mcallister@smartcontract.com>
2026-01-08 02:29:25 -0500

    removing customInputValidation on required config

--- packages/sources/dlc-cbtc-por/src/endpoint/attester-supply.ts
+++ packages/sources/dlc-cbtc-por/src/endpoint/attester-supply.ts
@@ -1,5 +1,4 @@
 import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
-import { AdapterInputError } from '@chainlink/external-adapter-framework/validation/error'
 import { EmptyInputParameters } from '@chainlink/external-adapter-framework/validation/input-params'
 import { config } from '../config'
 import { transport } from '../transport/attester-supply'
@@ -14,14 +13,4 @@ export type BaseEndpointTypes = {
 export const attesterSupply = new AdapterEndpoint({
   name: 'attesterSupply',
   transport,
-  customInputValidation: (_req, settings): AdapterInputError | undefined => {
-    if (!settings.ATTESTER_API_URLS) {
-      throw new AdapterInputError({
-        statusCode: 400,
-        message:
-          'ATTESTER_API_URLS environment variable is required for the attesterSupply endpoint',
-      })
-    }
-    return
-  },
 })
