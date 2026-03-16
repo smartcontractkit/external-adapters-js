6849d0b0d6f6a1292c60ca0890467dd0c8279a23
Mohamed Mehany <7327188+mohamed-mehany@users.noreply.github.com>
2026-03-16 14:04:11 +0100

    Data Engine: Adds result selection and scaling (#4736)

--- packages/sources/data-engine/src/transport/rwaV8.ts
+++ packages/sources/data-engine/src/transport/rwaV8.ts
@@ -1,6 +1,7 @@
 import { DecodedV8Report } from '@chainlink/data-streams-sdk'
 import { BaseEndpointTypes } from '../endpoint/rwaV8'
-import { createDataEngineTransport, DECIMALS } from './wsTransportBase'
+import { DECIMALS } from './utils'
+import { createDataEngineTransport } from './wsTransportBase'
 
 export const rwaV8Transport = createDataEngineTransport<BaseEndpointTypes, DecodedV8Report>({
   schemaVersion: 'V8',
