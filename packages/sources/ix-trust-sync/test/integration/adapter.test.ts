bb977ff8d877d82fad25f85a65565354fc94b0e2
David de Kloet <david.dekloet@smartcontract.com>
2026-03-26 14:38:24 +0100

    integration test

--- packages/sources/ix-trust-sync/test/integration/adapter.test.ts
+++ packages/sources/ix-trust-sync/test/integration/adapter.test.ts
@@ -3,7 +3,7 @@ import {
   setEnvVariables,
 } from '@chainlink/external-adapter-framework/util/testing-utils'
 import * as nock from 'nock'
-import { mockResponseSuccess } from './fixtures'
+import { mockTursoPipelineSuccess } from './fixtures'
 
 describe('execute', () => {
   let spy: jest.SpyInstance
@@ -13,6 +13,7 @@ describe('execute', () => {
   beforeAll(async () => {
     oldEnv = JSON.parse(JSON.stringify(process.env))
     process.env.API_KEY = process.env.API_KEY ?? 'fake-api-key'
+    process.env.API_ENDPOINT = 'https://test.url'
 
     const mockDate = new Date('2001-01-01T11:11:11.111Z')
     spy = jest.spyOn(Date, 'now').mockReturnValue(mockDate.getTime())
@@ -32,15 +33,20 @@ describe('execute', () => {
     spy.mockRestore()
   })
 
+  beforeEach(() => {
+    nock.cleanAll()
+  })
+
   describe('cumulativeAmount endpoint', () => {
     it('should return success', async () => {
       const data = {
-        base: 'ETH',
-        quote: 'USD',
         endpoint: 'cumulativeAmount',
         transport: 'rest',
+        auditorAddress: '0x92F78491093bA0dd88A419b1BF07aeb3BA9fD0dc',
+        fractionalContractAddress: '0xd051c326C9Aef673428E6F01eb65d2C52De95D30',
+        chainId: 1,
       }
-      mockResponseSuccess()
+      mockTursoPipelineSuccess()
       const response = await testAdapter.request(data)
       expect(response.statusCode).toBe(200)
       expect(response.json()).toMatchSnapshot()
