f912f9d5b0c460d42b29651068d8d4214495b559
David de Kloet <david.dekloet@smartcontract.com>
2026-03-26 14:15:28 +0100

    Added more unit tests

--- packages/sources/ix-trust-sync/test/unit/cumulativeAmount.test.ts
+++ packages/sources/ix-trust-sync/test/unit/cumulativeAmount.test.ts
@@ -47,10 +47,11 @@ describe('CumulativeAmountHttpTransport', () => {
   const apiEndpoint = 'http://test.api.url'
   const apiKey = 'test-api-key'
 
-  const auditorAddress = '0x92F78491093bA0dd88A419b1BF07aeb3BA9fD0dc'
-  const fractionalContractAddress = '0xd051c326C9Aef673428E6F01eb65d2C52De95D30'
-  const chainId = 1
-  const expectedValue = '5954903980000'
+  const defaultAuditorAddress = '0x92F78491093bA0dd88A419b1BF07aeb3BA9fD0dc'
+  const defaultFractionalContractAddress = '0xd051c326C9Aef673428E6F01eb65d2C52De95D30'
+  const defaultChainId = 1
+  const defaultCumulativeAmount = '5954903980000'
+  const defaultDecimals = 8
 
   const adapterSettings = makeStub('adapterSettings', {
     API_ENDPOINT: apiEndpoint,
@@ -98,93 +99,131 @@ describe('CumulativeAmountHttpTransport', () => {
     return requestKey
   }
 
-  beforeEach(async () => {
-    restoreEnv()
-    jest.resetAllMocks()
-    jest.useFakeTimers()
-
-    transport = new CumulativeAmountHttpTransport()
-
-    await transport.initialize(dependencies, adapterSettings, endpointName, transportName)
-  })
-
-  it('should make the request', async () => {
-    const params = makeStub('params', {
+  const createParams = ({
+    auditorAddress = defaultAuditorAddress,
+    fractionalContractAddress = defaultFractionalContractAddress,
+    chainId = defaultChainId,
+  }: {
+    auditorAddress: string
+    fractionalContractAddress: string
+    chainId: number
+  }): typeof inputParameters.validated => {
+    return makeStub('params', {
       auditorAddress,
       fractionalContractAddress,
       chainId,
     })
-    subscriptionSet.getAll.mockReturnValue([params])
+  }
 
-    const context = makeStub('context', {
-      adapterSettings,
-      endpointName,
-    } as EndpointContext<HttpTransportTypes>)
-
-    const apiResponseData: ResponseSchema = {
-      results: [
-        {
-          type: 'ok',
-          response: {
-            type: 'execute',
-            result: {
-              cols: [
-                {
-                  name: 'attestation_data',
-                  decltype: 'TEXT',
-                },
-                {
-                  name: 'signature',
-                  decltype: 'TEXT',
-                },
-              ],
-              rows: [
-                [
-                  {
-                    type: 'text',
-                    value:
-                      '{"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"NAVAttestation":[{"name":"contractAddress","type":"address"},{"name":"navContractAddress","type":"address"},{"name":"decimals","type":"uint8"},{"name":"amount","type":"uint256"},{"name":"cumulativeAmount","type":"uint256"},{"name":"validFrom","type":"uint256"},{"name":"validTo","type":"uint256"},{"name":"nonce","type":"bytes32"}]},"primaryType":"NAVAttestation","domain":{"name":"RWA Attestation","version":"1","chainId":"1","verifyingContract":"0x0000000000000000000000000000000000000000"},"message":{"contractAddress":"0xd051c326C9Aef673428E6F01eb65d2C52De95D30","navContractAddress":"0x95dc5a797f657391fb5a20bf2846475bb26c8b1a","decimals":8,"amount":"2479938340000","cumulativeAmount":"5954903980000","validFrom":"1765429200","validTo":"0","nonce":"0x03f506db0529a245a5afc10348f5426b13e6b18abe63a4670562422cc8826de1"}}',
-                  },
-                  {
-                    type: 'text',
-                    value:
-                      '0xe4181569bcba9819eb8629cd1ce16d45798fb23904a16143c40781c369dee8e431fe72040ee729157b568e25179c1ce2ad831db5290897d5324a187b3d5e922b1b',
-                  },
-                ],
-              ],
-              affected_row_count: 0,
-              last_insert_rowid: null,
-              replication_index: null,
-              rows_read: 28,
-              rows_written: 0,
-              query_duration_ms: 0.153,
-            },
-          },
-        },
-        {
-          type: 'ok',
-          response: {
-            type: 'close',
-          },
-        },
-      ],
-    }
-
-    const response = makeStub('response', {
+  const createResponse = ({
+    cumulativeAmount = defaultCumulativeAmount,
+    decimals = defaultDecimals,
+  }: {
+    cumulativeAmount: string
+    decimals: number
+  }): ResponseSchema => {
+    return makeStub('response', {
       response: {
         data: {
-          ...apiResponseData,
+          results: [
+            {
+              type: 'ok',
+              response: {
+                type: 'execute',
+                result: {
+                  cols: [
+                    {
+                      name: 'attestation_data',
+                      decltype: 'TEXT',
+                    },
+                    {
+                      name: 'signature',
+                      decltype: 'TEXT',
+                    },
+                  ],
+                  rows: [
+                    [
+                      {
+                        type: 'text',
+                        value: JSON.stringify({
+                          types: {
+                            EIP712Domain: [
+                              { name: 'name', type: 'string' },
+                              { name: 'version', type: 'string' },
+                              { name: 'chainId', type: 'uint256' },
+                              { name: 'verifyingContract', type: 'address' },
+                            ],
+                            NAVAttestation: [
+                              { name: 'contractAddress', type: 'address' },
+                              { name: 'navContractAddress', type: 'address' },
+                              { name: 'decimals', type: 'uint8' },
+                              { name: 'amount', type: 'uint256' },
+                              { name: 'cumulativeAmount', type: 'uint256' },
+                              { name: 'validFrom', type: 'uint256' },
+                              { name: 'validTo', type: 'uint256' },
+                              { name: 'nonce', type: 'bytes32' },
+                            ],
+                          },
+                          primaryType: 'NAVAttestation',
+                          domain: {
+                            name: 'RWA Attestation',
+                            version: '1',
+                            chainId: '1',
+                            verifyingContract: '0x0000000000000000000000000000000000000000',
+                          },
+                          message: {
+                            contractAddress: '0xd051c326C9Aef673428E6F01eb65d2C52De95D30',
+                            navContractAddress: '0x95dc5a797f657391fb5a20bf2846475bb26c8b1a',
+                            decimals,
+                            amount: '2479938340000',
+                            cumulativeAmount,
+                            validFrom: '1765429200',
+                            validTo: '0',
+                            nonce:
+                              '0x03f506db0529a245a5afc10348f5426b13e6b18abe63a4670562422cc8826de1',
+                          },
+                        }),
+                      },
+                      {
+                        type: 'text',
+                        value:
+                          '0xe4181569bcba9819eb8629cd1ce16d45798fb23904a16143c40781c369dee8e431fe72040ee729157b568e25179c1ce2ad831db5290897d5324a187b3d5e922b1b',
+                      },
+                    ],
+                  ],
+                  affected_row_count: 0,
+                  last_insert_rowid: null,
+                  replication_index: null,
+                  rows_read: 28,
+                  rows_written: 0,
+                  query_duration_ms: 0.153,
+                },
+              },
+            },
+            {
+              type: 'ok',
+              response: {
+                type: 'close',
+              },
+            },
+          ],
           cost: undefined,
         },
       },
       timestamps: {},
     })
+  }
 
-    requester.request.mockResolvedValue(response)
-
-    await transport.backgroundExecute(context)
-
-    const expectedRequestConfig = {
+  const createExpectedRequestConfig = ({
+    auditorAddress = defaultAuditorAddress,
+    fractionalContractAddress = defaultFractionalContractAddress,
+    chainId = defaultChainId,
+  }: {
+    auditorAddress: string
+    fractionalContractAddress: string
+    chainId: number
+  }) => {
+    return makeStub('requestConfig', {
       method: 'POST',
       baseURL: apiEndpoint,
       headers: {
@@ -209,15 +248,393 @@ describe('CumulativeAmountHttpTransport', () => {
           { type: 'close' },
         ],
       },
-    }
+    })
+  }
+
+  beforeEach(async () => {
+    restoreEnv()
+    jest.resetAllMocks()
+    jest.useFakeTimers()
+
+    transport = new CumulativeAmountHttpTransport()
+
+    await transport.initialize(dependencies, adapterSettings, endpointName, transportName)
+  })
+
+  it('should make the request', async () => {
+    const params = createParams({})
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({})
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({})
     const expectedRequestKey = requestKeyForParams(params)
 
     const expectedResponse = {
       data: {
-        cumulativeAmount: expectedValue,
+        cumulativeAmount: defaultCumulativeAmount,
         decimals: 8,
       },
-      result: expectedValue,
+      result: defaultCumulativeAmount,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return cumulativeAmount from the response', async () => {
+    const cumulativeAmount = '1234567890000'
+    // Changing the message, also changes the signature verification to expect
+    // this signer:
+    const auditorAddress = '0xF545140753179439B85039bdeFc8e72960Baeb2c'
+
+    const params = createParams({
+      auditorAddress,
+    })
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({
+      cumulativeAmount,
+    })
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({
+      auditorAddress,
+    })
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      data: {
+        cumulativeAmount,
+        decimals: defaultDecimals,
+      },
+      result: cumulativeAmount,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return decimals from the response', async () => {
+    const decimals = 18
+    // Changing the message, also changes the signature verification to expect
+    // this signer:
+    const auditorAddress = '0xd399AAee7366014F971B0093E829B2086a57843d'
+
+    const params = createParams({
+      auditorAddress,
+    })
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({
+      decimals,
+    })
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({
+      auditorAddress,
+    })
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      data: {
+        cumulativeAmount: defaultCumulativeAmount,
+        decimals,
+      },
+      result: defaultCumulativeAmount,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should use the contract address in the request', async () => {
+    const fractionalContractAddress = '0x3333333333333333333333333333333333333333'
+    const params = createParams({
+      fractionalContractAddress,
+    })
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({})
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({
+      fractionalContractAddress,
+    })
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      data: {
+        cumulativeAmount: defaultCumulativeAmount,
+        decimals: defaultDecimals,
+      },
+      result: defaultCumulativeAmount,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should use the chain ID in the request', async () => {
+    const chainId = 137
+    const params = createParams({
+      chainId,
+    })
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({})
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({
+      chainId,
+    })
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      data: {
+        cumulativeAmount: defaultCumulativeAmount,
+        decimals: defaultDecimals,
+      },
+      result: defaultCumulativeAmount,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should give an error if the signer address does not match', async () => {
+    const auditorAddress = '0x1111111111111111111111111111111111111111'
+
+    const params = createParams({
+      auditorAddress,
+    })
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = createResponse({})
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({
+      auditorAddress,
+    })
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      errorMessage: `Signature verification failed. Expected signer address ${auditorAddress}, but got ${defaultAuditorAddress}`,
+      statusCode: 502,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should give an error if the response results are empty', async () => {
+    const params = createParams({})
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = makeStub('response', {
+      response: {
+        data: {
+          results: [],
+          cost: undefined,
+        },
+      },
+      timestamps: {},
+    })
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({})
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      errorMessage: `Unexpected number of results returned by the data provider: 0. Expected 2 results (1 with type execute and 1 with type close).`,
+      statusCode: 502,
+      timestamps: {},
+    }
+
+    expect(requester.request).toHaveBeenCalledWith(
+      expectedRequestKey,
+      expectedRequestConfig,
+      undefined,
+    )
+    expect(requester.request).toHaveBeenCalledTimes(1)
+
+    expect(responseCache.write).toHaveBeenCalledWith(transportName, [
+      {
+        params,
+        response: expectedResponse,
+      },
+    ])
+    expect(responseCache.write).toHaveBeenCalledTimes(1)
+  })
+
+  it('should give an error if no rows are returned', async () => {
+    const params = createParams({})
+    subscriptionSet.getAll.mockReturnValue([params])
+
+    const context = makeStub('context', {
+      adapterSettings,
+      endpointName,
+    } as EndpointContext<HttpTransportTypes>)
+
+    const response = makeStub('response', {
+      response: {
+        data: {
+          results: [
+            { type: 'ok', response: { type: 'execute', result: { rows: [] } } },
+            { type: 'ok', response: { type: 'close' } },
+          ],
+          cost: undefined,
+        },
+      },
+      timestamps: {},
+    })
+
+    requester.request.mockResolvedValue(response)
+
+    await transport.backgroundExecute(context)
+
+    const expectedRequestConfig = createExpectedRequestConfig({})
+    const expectedRequestKey = requestKeyForParams(params)
+
+    const expectedResponse = {
+      errorMessage:
+        'Unexpected number of rows returned by the data provider: 0. Expected exactly 1 row.',
+      statusCode: 502,
       timestamps: {},
     }
 
