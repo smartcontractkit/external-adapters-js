7cbf42da893c7ed0dae521d9f4d53d36a0de07cf
David de Kloet <david.dekloet@smartcontract.com>
2026-04-20 08:02:28 +0200

    OPDATA-6575: proof-of-reserves-2 rewrite (#4835)
    * Generated with yarn new
    
    * KRWQ working
    
    * Working with 2 components of Solv, before converting everything to fixed point
    
    * Working with components in response
    
    * More output
    
    * Merge 'reduce' into 'balances' param. And forward provide error message.
    
    * Separate addressLists and balanceSources sections
    
    * Make conversions explicit
    
    * Remove overrides
    
    * Delete test-payload.json
    
    * request validation and fixed point arithmetic with tests
    
    * Refactor fetch rate
    
    * All tests passing
    
    * Code cleanup
    
    * Order functions
    
    * Validate duplicate and unused items
    
    * changeset
    
    * Add example params
    
    * address comments
    
    * Rename to proof-of-reserves-v2
    
    * Refactor address, balance, conversion
    
    * More files

---
+++ packages/composites/proof-of-reserves-v2/src/config/index.ts
@@ -0,0 +1,18 @@
+import { AdapterConfig } from '@chainlink/external-adapter-framework/config'
+
+export const config = new AdapterConfig({
+  MAX_RESPONSE_TEXT_IN_ERROR_MESSAGE: {
+    description:
+      'How many characters of a response may be included in an error message before trunctating it',
+    type: 'number',
+    default: 200,
+    sensitive: false,
+  },
+  BACKGROUND_EXECUTE_MS: {
+    description:
+      'The amount of time the background execute should sleep before performing the next request',
+    type: 'number',
+    default: 10_000,
+    sensitive: false,
+  },
+})
