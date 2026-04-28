4f77f3ec97db33c9ba303fe39aaf1bbe44aa4756
Michael Xiao <michael.xiao@smartcontract.com>
2026-04-28 19:05:23 -0400

    Typo

--- packages/composites/market-status/src/source/static.ts
+++ packages/composites/market-status/src/source/static.ts
@@ -1,5 +1,5 @@
 import type { MarketStatusResult } from '../transport/base-market-status'
-import { getStatus as staticIceEuropeEnery } from './static-ice-europe-energy'
+import { getStatus as staticIceEuropeEnergy } from './static-ice-europe-energy'
 import { getStatus as staticJpxStatus } from './static-jpx'
 import { getStatus as staticKrxStatus } from './static-krx'
 import { getStatus as staticNymexStatus } from './static-nymex'
@@ -16,7 +16,7 @@ const mapping = {
   STATIC_TPEX: staticTpexStatus,
   STATIC_TWSE: staticTpexStatus,
   STATIC_KRX: staticKrxStatus,
-  STATIC_ICE_EUROPE_ENERGY: staticIceEuropeEnery,
+  STATIC_ICE_EUROPE_ENERGY: staticIceEuropeEnergy,
 }
 
 export type StaticSourceName = keyof typeof mapping
