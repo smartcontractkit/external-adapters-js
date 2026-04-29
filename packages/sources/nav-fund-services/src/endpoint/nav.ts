c7ba215665d41f1b6b7c06f5c7f4f2f0d849d671
Matthew McAllister <matthew.mcallister@smartcontract.com>
2026-04-29 13:22:37 -0400

    comment

--- packages/sources/nav-fund-services/src/endpoint/nav.ts
+++ packages/sources/nav-fund-services/src/endpoint/nav.ts
@@ -5,7 +5,7 @@ import { config } from '../config'
 import { getApiKeys } from '../transport/creds'
 import { navTransport } from '../transport/nav'
 
-/** Default hours after UTC midnight on the accounting date for `navDateTimestampMs` (NA calendar-day alignment). */
+/** Default hours after UTC midnight on the accounting date for navDateTimestampMs. */
 const DEFAULT_NAV_DATE_TIMESTAMP_UTC_OFFSET_HOURS = 6
 const NAV_DATE_TIMESTAMP_UTC_OFFSET_HOURS_OPTIONS = [...Array(24).keys()] as const
 
