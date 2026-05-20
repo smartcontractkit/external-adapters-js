44fc361ee8c0a5590783573c1117328fdd43985f
David de Kloet <david.dekloet@smartcontract.com>
2026-05-20 14:37:03 +0200

    Add unit test for StockQuotesWebSocketTransport of Lo-tech EA (#4978)

--- packages/sources/lo-tech/src/transport/stock_quotes.ts
+++ packages/sources/lo-tech/src/transport/stock_quotes.ts
@@ -19,66 +19,73 @@ export type WsTransportTypes = BaseEndpointTypes & {
     WsMessage: WSResponse
   }
 }
-export const wsTransport = new WebSocketTransport<WsTransportTypes>({
-  url: (context) => context.adapterSettings.WS_API_ENDPOINT,
-  options: (context) => {
-    return {
-      headers: {
-        'X-API-KEY': context.adapterSettings.API_KEY,
+
+export class StockQuotesWebSocketTransport extends WebSocketTransport<WsTransportTypes> {
+  constructor() {
+    super({
+      url: (context) => context.adapterSettings.WS_API_ENDPOINT,
+      options: (context) => {
+        return {
+          headers: {
+            'X-API-KEY': context.adapterSettings.API_KEY,
+          },
+        }
       },
-    }
-  },
-  handlers: {
-    message(message) {
-      if (message.data.type !== 'PRICE') {
-        return
-      }
+      handlers: {
+        message(message) {
+          if (message.data.type !== 'PRICE') {
+            return
+          }
 
-      const mid_price = message.data.price
-      const spread = message.data.spread
-      const bid_price = mid_price - spread / 2
-      const ask_price = mid_price + spread / 2
+          const mid_price = message.data.price
+          const spread = message.data.spread
+          const bid_price = mid_price - spread / 2
+          const ask_price = mid_price + spread / 2
 
-      return [
-        {
-          params: { base: message.data.symbol },
-          response: {
-            result: null,
-            data: {
-              mid_price,
-              bid_price,
-              ask_price,
+          return [
+            {
+              params: { base: message.data.symbol },
+              response: {
+                result: null,
+                data: {
+                  mid_price,
+                  bid_price,
+                  ask_price,
+                },
+                timestamps: {
+                  providerIndicatedTimeUnixMs: Math.floor(message.egress_ts / 1000),
+                },
+              },
             },
-            timestamps: {
-              providerIndicatedTimeUnixMs: Math.floor(message.egress_ts / 1000),
-            },
-          },
+          ]
         },
-      ]
-    },
-  },
-  builders: {
-    subscribeMessage: (params) => {
-      return {
-        op: 'SUBSCRIBE',
-        topics: [
-          {
-            symbol: params.base,
-            type: 'PRICE',
-          },
-        ],
-      }
-    },
-    unsubscribeMessage: (params) => {
-      return {
-        op: 'UNSUBSCRIBE',
-        topics: [
-          {
-            symbol: params.base,
-            type: 'PRICE',
-          },
-        ],
-      }
-    },
-  },
-})
+      },
+      builders: {
+        subscribeMessage: (params) => {
+          return {
+            op: 'SUBSCRIBE',
+            topics: [
+              {
+                symbol: params.base,
+                type: 'PRICE',
+              },
+            ],
+          }
+        },
+        unsubscribeMessage: (params) => {
+          return {
+            op: 'UNSUBSCRIBE',
+            topics: [
+              {
+                symbol: params.base,
+                type: 'PRICE',
+              },
+            ],
+          }
+        },
+      },
+    })
+  }
+}
+
+export const wsTransport = new StockQuotesWebSocketTransport()
