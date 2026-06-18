d1f32a2a60efd0afaf4ff2671dbd17a3dfe2fe27
David de Kloet <david.dekloet@smartcontract.com>
2026-06-18 16:36:48 +0200

    yarn install

--- .pnp.cjs
+++ .pnp.cjs
@@ -4683,7 +4683,6 @@ const RAW_RUNTIME_STATE =
           ["@chainlink/external-adapter-framework", "npm:2.16.1"],\
           ["@types/jest", "npm:29.5.14"],\
           ["@types/node", "npm:22.14.1"],\
-          ["axios", "npm:1.13.4"],\
           ["nock", "npm:13.5.6"],\
           ["tslib", "npm:2.4.1"],\
           ["typescript", "patch:typescript@npm%3A5.8.3#optional!builtin<compat/typescript>::version=5.8.3&hash=5786d5"]\
