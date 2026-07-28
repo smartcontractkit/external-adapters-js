b733cdcfc1e8777c8e73bda561ef86e0445178d9
David de Kloet <david.dekloet@smartcontract.com>
2026-07-28 14:51:34 +0200

    Delete sportsdataio EA (#5244)
    * git rm -r packages/sources/sportsdataio
    
    * Manual changes
    
    * yarn

--- packages/sources/sportsdataio/src/sport/nba/endpoint/index.ts
+++
@@ -1,5 +0,0 @@
-import type { TInputParameters as PlayerStatsInputParameters } from './player-stats'
-
-export type TInputParameters = PlayerStatsInputParameters
-
-export * as playerStats from './player-stats'
