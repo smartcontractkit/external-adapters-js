acfa584883feaf3c0b2a3f4454e5cf3a7c59fd9f
David de Kloet <david.dekloet@smartcontract.com>
2026-08-13 15:21:50 +0200

    Review comments

--- packages/sources/tickerlayer/src/endpoint/stock.ts
+++ packages/sources/tickerlayer/src/endpoint/stock.ts
@@ -1,29 +1,8 @@
 import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
-import { stockEndpointInputParametersDefinition } from '@chainlink/external-adapter-framework/adapter/stock'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
-import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
 import { wsTransport } from '../transport/stock'
-
-export const inputParameters = new InputParameters(
-  {
-    ...stockEndpointInputParametersDefinition,
-    assetType: {
-      description:
-        'The asset type is used to determine what channel to subscribe to. The Tickerlayer API supports "stocks", "fores", "crypto", "indices", "etfs", and "commodities".',
-      type: 'string',
-      default: 'stocks',
-      // We don't restrict to specific options in order to support future
-      // assets types without code changes.
-    },
-  },
-  [
-    {
-      base: 'US:AAPL',
-      assetType: 'stocks',
-    },
-  ],
-)
+import { customInputValidation, inputParameters } from './common'
 
 export type BaseEndpointTypes = {
   Parameters: typeof inputParameters.definition
@@ -36,11 +15,5 @@ export const endpoint = new AdapterEndpoint({
   aliases: ['price'],
   transport: wsTransport,
   inputParameters,
-  customInputValidation: (request): undefined => {
-    const { assetType } = request.requestContext.data
-    if (!/^[a-zA-Z]+$/.test(assetType)) {
-      throw new Error(`Asset type must contain only letters. Found '${assetType}'.`)
-    }
-    return
-  },
+  customInputValidation,
 })
--- packages/sources/tickerlayer/src/endpoint/stock_quotes.ts
+++ packages/sources/tickerlayer/src/endpoint/stock_quotes.ts
@@ -1,28 +1,7 @@
 import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
-import { stockEndpointInputParametersDefinition } from '@chainlink/external-adapter-framework/adapter/stock'
-import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
 import { wsTransport } from '../transport/stock_quotes'
-
-export const inputParameters = new InputParameters(
-  {
-    ...stockEndpointInputParametersDefinition,
-    assetType: {
-      description:
-        'The asset type is used to determine what channel to subscribe to. The Tickerlayer API supports "stocks", "fores", "crypto", "indices", "etfs", and "commodities".',
-      type: 'string',
-      default: 'stocks',
-      // We don't restrict to specific options in order to support future
-      // assets types without code changes.
-    },
-  },
-  [
-    {
-      base: 'US:AAPL',
-      assetType: 'stocks',
-    },
-  ],
-)
+import { customInputValidation, inputParameters } from './common'
 
 export type BaseEndpointTypes = {
   Parameters: typeof inputParameters.definition
@@ -44,11 +23,5 @@ export const endpoint = new AdapterEndpoint({
   aliases: ['quotes'],
   transport: wsTransport,
   inputParameters,
-  customInputValidation: (request): undefined => {
-    const { assetType } = request.requestContext.data
-    if (!/^[a-zA-Z]+$/.test(assetType)) {
-      throw new Error(`Asset type must contain only letters. Found '${assetType}'.`)
-    }
-    return
-  },
+  customInputValidation,
 })
