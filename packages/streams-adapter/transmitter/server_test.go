baeb9d03a5b08470d5bfa772ca87b68744ae2c5e
denis-chernov-smartcontract <148817570+denis-chernov-smartcontract@users.noreply.github.com>
2026-09-03 15:34:44 -0700

    DS-4004/fix for gRPC subscription snapshot (#5359)
    * single bad item does not prevent the rest of the snapshot from being applied
    
    * changeset

--- packages/streams-adapter/transmitter/server_test.go
+++ packages/streams-adapter/transmitter/server_test.go
@@ -1,9 +1,13 @@
 package transmitter
 
 import (
+	"errors"
 	"testing"
 
+	types "streams-adapter/common"
+
 	pb "streams-adapter/gen/streams/v1"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func TestNormalizeSnapshotAcceptsEmptySnapshot(t *testing.T) {
@@ -17,10 +21,56 @@ func TestNormalizeSnapshotAcceptsEmptySnapshot(t *testing.T) {
 	}
 }
 
-func TestNormalizeSnapshotRejectsMissingData(t *testing.T) {
-	server := &StreamTransmitter{}
-	_, err := server.normalizeSnapshot(&pb.SubscribeRequest{Subscriptions: []*pb.Subscription{{}}})
-	if err == nil {
-		t.Fatal("expected missing data error")
+func TestNormalizeSnapshotDiscardsMissingData(t *testing.T) {
+	server := &StreamTransmitter{resolver: func(map[string]interface{}) (*types.ResolvedSubscription, error) {
+		return &types.ResolvedSubscription{}, nil
+	}}
+	normalized, err := server.normalizeSnapshot(&pb.SubscribeRequest{Subscriptions: []*pb.Subscription{{}}})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
 	}
+	if len(normalized) != 0 {
+		t.Fatalf("expected 0 valid subscriptions, got %d", len(normalized))
+	}
+}
+
+func TestNormalizeSnapshotDiscardsInvalidItemsAndKeepsValidOnes(t *testing.T) {
+	server := &StreamTransmitter{resolver: func(data map[string]interface{}) (*types.ResolvedSubscription, error) {
+		if data["valid"] == true {
+			return &types.ResolvedSubscription{}, nil
+		}
+		return nil, errors.New("resolver error")
+	}}
+
+	validSub, err := makeSubscription(map[string]interface{}{"valid": true})
+	if err != nil {
+		t.Fatalf("failed to create valid subscription: %v", err)
+	}
+	invalidSub, err := makeSubscription(map[string]interface{}{"valid": false})
+	if err != nil {
+		t.Fatalf("failed to create invalid subscription: %v", err)
+	}
+
+	req := &pb.SubscribeRequest{Subscriptions: []*pb.Subscription{
+		validSub,
+		invalidSub,
+		{},
+		validSub,
+	}}
+
+	normalized, err := server.normalizeSnapshot(req)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if len(normalized) != 2 {
+		t.Fatalf("expected 2 valid subscriptions, got %d", len(normalized))
+	}
+}
+
+func makeSubscription(data map[string]interface{}) (*pb.Subscription, error) {
+	structData, err := structpb.NewStruct(data)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Subscription{Data: structData}, nil
 }
