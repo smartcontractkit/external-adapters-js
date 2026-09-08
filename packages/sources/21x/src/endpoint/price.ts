b4ffdbd9e764338a3e3b96d288dcae932082f469
David de Kloet <david.dekloet@smartcontract.com>
2026-09-08 15:17:19 +0200

    Remove overrides.json

--- packages/sources/21x/src/endpoint/price.ts
+++ packages/sources/21x/src/endpoint/price.ts
@@ -2,7 +2,6 @@ import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
-import overrides from '../config/overrides.json'
 import { customSubscriptionTransport } from '../transport/price'
 
 export const inputParameters = new InputParameters(
@@ -39,5 +38,4 @@ export const endpoint = new AdapterEndpoint({
   aliases: [],
   transport: customSubscriptionTransport,
   inputParameters,
-  overrides: overrides['21x'],
 })
