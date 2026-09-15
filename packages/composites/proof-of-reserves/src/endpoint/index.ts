500654dc7d655de02574ce792da6ab07437978b0
David de Kloet <david.dekloet@smartcontract.com>
2026-09-15 14:45:30 +0200

    Delete proof-of-reserves EA (#5391)
    * git rm -r packages/composites/proof-of-reserves
    
    * Manual changes
    
    * yarn

--- packages/composites/proof-of-reserves/src/endpoint/index.ts
+++
@@ -1,7 +0,0 @@
-import type { TInputParameters as MultiTInputParameters } from './multiReserves'
-import type { TInputParameters as SingleTInputParameters } from './reserves'
-
-export type TInputParameters = SingleTInputParameters | MultiTInputParameters
-
-export * as multiReserves from './multiReserves'
-export * as reserves from './reserves'
