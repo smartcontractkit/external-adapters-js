ef1400ea04338b0d60d338bd20d5e874ae64634e
David de Kloet <david.dekloet@smartcontract.com>
2026-09-21 17:32:24 +0200

    Delete blockchair EA (#5426)
    * git rm -r packages/sources/blockchair
    
    * Manual changes
    
    * yarn

--- packages/sources/blockchair/src/endpoint/index.ts
+++
@@ -1,7 +0,0 @@
-import type { TInputParameters as BalanceInputParameters } from './balance'
-import type { TInputParameters as StatsInputParameters } from './stats'
-
-export type TInputParameters = BalanceInputParameters | StatsInputParameters
-
-export * as balance from './balance'
-export * as stats from './stats'
