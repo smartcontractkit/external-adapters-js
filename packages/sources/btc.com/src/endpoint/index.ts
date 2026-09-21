5259854476b058e8290f859fe256a157bfe26f09
David de Kloet <david.dekloet@smartcontract.com>
2026-09-21 08:52:29 +0200

    git rm -r packages/sources/btc.com

--- packages/sources/btc.com/src/endpoint/index.ts
+++
@@ -1,7 +0,0 @@
-import type { TInputParameters as BalanceInputParameters } from './balance'
-import type { TInputParameters as BlockInputParameters } from './block'
-
-export type TInputParameters = BalanceInputParameters | BlockInputParameters
-
-export * as balance from './balance'
-export * as block from './block'
