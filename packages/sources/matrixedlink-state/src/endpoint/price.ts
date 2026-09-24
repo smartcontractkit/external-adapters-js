f98804f2818a77c6af6ad0b05c92fe388fafa343
David de Kloet <david.dekloet@smartcontract.com>
2026-09-24 09:05:29 +0200

    Remove overrides

--- packages/sources/matrixedlink-state/src/endpoint/price.ts
+++ packages/sources/matrixedlink-state/src/endpoint/price.ts
@@ -2,7 +2,6 @@ import { AdapterEndpoint } from '@chainlink/external-adapter-framework/adapter'
 import { SingleNumberResultResponse } from '@chainlink/external-adapter-framework/util'
 import { InputParameters } from '@chainlink/external-adapter-framework/validation'
 import { config } from '../config'
-import overrides from '../config/overrides.json'
 import { wsTransport } from '../transport/price'
 
 export const inputParameters = new InputParameters(
@@ -39,5 +38,4 @@ export const endpoint = new AdapterEndpoint({
   aliases: ['state', 'crypto'],
   transport: wsTransport,
   inputParameters,
-  overrides: overrides['matrixedlink-state'],
 })
